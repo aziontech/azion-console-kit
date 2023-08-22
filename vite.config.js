42a0b93c8b459cab06aaa21a70eed0b5796e52b8
Matheus Sunderhus <matheus.sunderhus@azion.com>
2023-08-22 16:16:30 -0300

    feat: add alias path to initial folders under src

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,11 @@ export default defineConfig({
     extensions:['.mjs', '.js', '.mts', '.ts', '.jsx', '.tsx', '.json','.vue'],
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@templates': fileURLToPath(new URL('./src/templates', import.meta.url)),
+      '@views': fileURLToPath(new URL('./src/views', import.meta.url)),
+      '@services': fileURLToPath(new URL('./src/services', import.meta.url)),
+      '@stores': fileURLToPath(new URL('./src/stores', import.meta.url)),
+      '@assets': fileURLToPath(new URL('./src/assets', import.meta.url))
     }
   }
 })
