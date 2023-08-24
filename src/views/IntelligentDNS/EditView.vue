97fb0e1d33a60e25ecbf1cf1035106b54c175c3d
Matheus Sunderhus <matheus.sunderhus@azion.com>
2023-08-24 15:48:51 -0300

    feat: add domain schema validation to edit IntelligentDNS

--- src/views/IntelligentDNS/EditView.vue
+++ src/views/IntelligentDNS/EditView.vue
@@ -51,7 +51,14 @@
     data: () => {
       const validationSchema = yup.object({
           name: yup.string().required(),
-          domain: yup.string().required(),
+          domain: yup.string().required().test(
+            'valid-domain',
+            'Invalid domain',
+            function (value) {
+              const domainRegex = /^(?:[-A-Za-z0-9]+\.)+[A-Za-z]{2,6}$/;
+              return domainRegex.test(value);
+            }
+          ),
           isActive: yup.boolean().required(),
       });
 
