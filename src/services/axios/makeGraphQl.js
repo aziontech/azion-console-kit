32767860b1c2c42ee14a00d7237113e81cda2f80
paulosf0 <paulo.ferreira@azion.com>
2023-08-28 10:00:49 -0300

    feat: remove if(token) from makeGraphQl

--- src/services/axios/makeGraphQl.js
+++ src/services/axios/makeGraphQl.js
@@ -1,12 +1,7 @@
 import axios from 'axios'
 const baseURL = 'graphql'
-const token = import.meta.env.VITE_PERSONAL_TOKEN
 
-if(token){
-  axios.defaults.headers.common['Authorization'] = `token ${token}`
-  axios.defaults.headers.common['Accept'] = 'application/json; version=3'
-  axios.defaults.headers.common['Content-Type'] = 'application/json; version=3'
-}
+axios.defaults.headers.common['Content-Type'] = 'application/json; version=3'
 
 const api = axios.create({
   baseURL
