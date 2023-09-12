45aae532243b6f90908e2e3db25a0103bd37cc07
william-tome <william.tome@azion.com>
2023-09-12 16:03:47 -0300

    feat: handle local login in router

--- src/router/routes/login-routes/index.js
+++ src/router/routes/login-routes/index.js
@@ -1,9 +1,16 @@
 import * as LoginService from '@/services/login-services'
 
+const { DEV } = import.meta.env
+
 /** @type {import('vue-router').RouteRecordRaw} */
 export const loginRoutes = {
-  path: '/login',
-  name: 'login',
+  /* 
+    Workaround to handle the login method in dev mode because when in "production" 
+    the login method is handled by the edge application
+  */ 
+  path: DEV ? '/login' : '/login-dev',
+  name: DEV ? 'login' : 'login-dev',
+
   component: () => import('@views/Login/LoginView.vue'),
   props: {
     authenticationLoginService: LoginService.authenticate,
