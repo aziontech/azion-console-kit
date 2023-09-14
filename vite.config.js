36c3d894de43edbfce59272d56a18f3a9c220075
Cicero, Feijo <cicero.feijo@gmail.com>
2023-09-14 14:35:59 -0300

    fix: add proxies to vite config

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,9 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
+  plugins: [vue(), vueJsx()],
   resolve: {
-    extensions:['.mjs', '.js', '.mts', '.ts', '.jsx', '.tsx', '.json','.vue'],
+    extensions: ['.mjs', '.js', '.mts', '.ts', '.jsx', '.tsx', '.json', '.vue'],
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
       '@templates': fileURLToPath(new URL('./src/templates', import.meta.url)),
@@ -23,11 +20,26 @@ export default defineConfig({
   },
   server: {
     proxy: {
-      '/api': {
-        //target: 'https://stage-api.azion.net',
-        target: 'https://api.azionapi.net/',
+      '/api/edge_services': {
+        target: 'https://stage-manager-origin.azion.com/services/api/v1/services',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        rewrite: (path) => path.replace(/^\/api\/edge_services/, '')
+      },
+      '/api/variables': {
+        //target: 'https://cors-stage-api.azion.net',
+        target: 'https://stage-manager.azion.com/variables/api/',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      },
+      '/network-lists/graphql': {
+        target: 'https://stage-cities.azion.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/network-lists\/graphql/, '')
+      },
+      '/api': {
+        target: 'https://stage-manager-origin.azion.com/api/',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
       }
     }
   }
