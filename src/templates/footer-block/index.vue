6b176ce081414f5b3f4326aa9b54655c18d8b750
peterpaulo-azion <peterson.silva@azion.com>
2023-09-14 16:45:56 -0300

    refactor: svg to azion logo component

--- src/templates/footer-block/index.vue
+++ src/templates/footer-block/index.vue
@@ -5,25 +5,8 @@
       class="z-10 flex px-3 flex-wrap flex-col md:flex-row pr-3 py-5 md:py-3
       justify-center items-center gap-4 surface-ground border-t surface-border "
     >
-      <!-- Logo Azion -->
-      <svg @click="this.$router.push('/base')" xmlns="http://www.w3.org/2000/svg"
-      class="cursor-pointer"
-        width="73" height="14" fill="none" viewBox="0 0 73 14">
-        <path
-          fill="#F3652B"
-          fill-rule="evenodd"
-          d="M15.266 0 1.08 11.944.688
-          14H2.73l10.99-9.247L11.95 14h2.68l2.683-14h-2.047Zm4.728 0-.507
-          2.645h9.26L17.709 11.94 17.313 14h13.943l.507-2.645h-9.26l11.04-9.296L33.938
-          0H19.994Zm16.569 0-2.625 14h2.624l2.626-14h-2.625Zm5.306 0-2.681
-          14H53.13l2.682-14H41.869Zm2.178 2.645h8.57l-1.667 8.71h-8.571l1.668-8.71ZM69.757
-          0l-1.19 6.21 2.166 2.693L72.438 0h-2.681ZM58.494 0l-2.681 14h2.682l1.838-9.598L68.08
-          14h1.676l.332-1.714L60.17 0h-1.677Z"
-          clip-rule="evenodd"
-        />
-      </svg>
+    <AzionLogo />
 
-      <!--  -->
       <div class="text-sm font-normal text-color">© 2023 Azion Technologies.</div>
 
       <!-- Links -->
@@ -85,6 +68,7 @@
 // Imports
 import Dropdown from 'primevue/dropdown';
 import PrimeButton from 'primevue/button';
+import AzionLogo from '../../assets/svg/azion'
 
 // Export my assets and components
 export default {
@@ -92,6 +76,7 @@ export default {
   components: {
     PrimeButton,
     Dropdown,
+    AzionLogo
   },
   data() {
     return {
