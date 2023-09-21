9229835c5e89e58b8e7323176e1e0ec0efeca610
Cicero, Feijo <cicero.feijo@gmail.com>
2023-09-21 15:55:16 -0300

    feat: add dynamic import to reduce bundle

--- src/templates/monaco-editor-block/index.vue
+++ src/templates/monaco-editor-block/index.vue
@@ -7,7 +7,6 @@
 </template>
 
 <script>
-  import * as monaco from 'monaco-editor/esm/vs/editor/editor.api'
   import { ref, onMounted, watch } from 'vue'
 
   export default {
@@ -20,31 +19,36 @@
     setup(props) {
       const editor = ref(null)
 
-      onMounted(() => {
-        const monacoEditor = monaco.editor.create(editor.value, {
-          value: props.code,
-          minimap: { enabled: false },
-          language: props.language || 'python',
-          automaticLayout: true,
-          readOnly: true,
-          overviewRulerLanes: 100,
-          formatOnType: true,
-          theme: 'vs-dark',
-          fixedOverflowWidgets: true,
-          scrollbar: {
-            vertical: 'auto',
-            horizontal: 'auto',
-            handleMouseWheel: false
-          },
-          wordWrap: 'on'
-        })
+      onMounted(async () => {
+        try {
+          const monaco = await import('monaco-editor/esm/vs/editor/editor.api')
+          const monacoEditor = monaco.editor.create(editor.value, {
+            value: props.code,
+            minimap: { enabled: false },
+            language: props.language || 'python',
+            automaticLayout: true,
+            readOnly: true,
+            overviewRulerLanes: 100,
+            formatOnType: true,
+            theme: 'vs-dark',
+            fixedOverflowWidgets: true,
+            scrollbar: {
+              vertical: 'auto',
+              horizontal: 'auto',
+              handleMouseWheel: false
+            },
+            wordWrap: 'on'
+          })
 
-        watch(
-          () => props.code,
-          (newCode) => {
-            monacoEditor.setValue(newCode)
-          }
-        )
+          watch(
+            () => props.code,
+            (newCode) => {
+              monacoEditor.setValue(newCode)
+            }
+          )
+        } catch (error) {
+          console.error('Erro ao carregar o Monaco Editor', error)
+        }
       })
 
       return {
