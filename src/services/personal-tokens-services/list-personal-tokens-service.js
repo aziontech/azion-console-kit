b2bdabcc5cb0bc77165b4dcac85ce5c76e9d7573
william-tome <william.tome@azion.com>
2023-09-25 14:12:56 -0300

    feat: create personal-tokens services

---
+++ src/services/personal-tokens-services/list-personal-tokens-service.js
@@ -0,0 +1,25 @@
+import { AxiosHttpClientAdapter, parseHttpResponse } from '../axios/AxiosHttpClientAdapter'
+import { makePersonalTokensBaseUrl } from './make-personal-tokens-base-url'
+
+
+export const listPersonalTokens = async ({ page = 1, search = '' }) => {
+  let httpResponse = await AxiosHttpClientAdapter.request({
+    url: `${makePersonalTokensBaseUrl()}?&page=${page}&search=${search}`,
+    method: 'GET'
+  })
+
+  httpResponse = await adapt(httpResponse)
+
+  return parseHttpResponse(httpResponse)
+}
+
+const adapt = async (httpResponse) => {
+  const parsedData = httpResponse.body.results.map((item) => {
+   return { ...item, id: item.uuid }
+  })
+
+  return {
+    body: parsedData,
+    statusCode: httpResponse.statusCode
+  }
+}
