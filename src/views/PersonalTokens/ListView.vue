39276757d36d9bd89eb1b8df4a819ecf805306a1
william-tome <william.tome@azion.com>
2023-09-25 14:29:41 -0300

    chore: run linter

--- src/views/PersonalTokens/ListView.vue
+++ src/views/PersonalTokens/ListView.vue
@@ -11,44 +11,44 @@
 </template>
 
 <script>
-import ListTableBlock from '@/templates/list-table-block'
+  import ListTableBlock from '@/templates/list-table-block'
 
-export default {
-  name: 'personal-tokens-view',
-  components: {
-    ListTableBlock
-  },
-  props: {
-    listPersonalTokensService: {
-      required: true,
-      type: Function
+  export default {
+    name: 'personal-tokens-view',
+    components: {
+      ListTableBlock
     },
-    deletePersonalTokenService: {
-      required: true,
-      type: Function
-    }
-  },
-  computed: {
-    getColumns() {
-      return [
-        {
-          field: 'name',
-          header: 'Token Name'
-        },
-        {
-          field: 'scope',
-          header: 'Scope'
-        },
-        {
-          field: 'created',
-          header: 'Last modified'
-        },
-        {
-          field: 'expires_at',
-          header: 'Expires'
-        }
-      ]
+    props: {
+      listPersonalTokensService: {
+        required: true,
+        type: Function
+      },
+      deletePersonalTokenService: {
+        required: true,
+        type: Function
+      }
+    },
+    computed: {
+      getColumns() {
+        return [
+          {
+            field: 'name',
+            header: 'Token Name'
+          },
+          {
+            field: 'scope',
+            header: 'Scope'
+          },
+          {
+            field: 'created',
+            header: 'Last modified'
+          },
+          {
+            field: 'expires_at',
+            header: 'Expires'
+          }
+        ]
+      }
     }
   }
-}
 </script>
