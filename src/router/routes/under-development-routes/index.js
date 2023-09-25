3e0b330dae1b83521f5642de1c2d0be6e5971478
Matheus Sunderhus <matheus.sunderhus@azion.com>
2023-09-25 14:39:45 -0300

    feat: add under development view

--- src/router/routes/under-development-routes/index.js
+++ src/router/routes/under-development-routes/index.js
@@ -1,7 +1,6 @@
-
 /** @type {import('vue-router').RouteRecordRaw} */
 export const underDevelopmentRoutes = {
   path: '/:catchAll(.*)',
   name: 'underDevelopment',
-  component: () => import('@views/UnderDevelopment/UnderDevelopment.vue'),
+  component: () => import('@views/UnderDevelopment/UnderDevelopment.vue')
 }
