cc6fc78ba53b4e4c3acb044e24c3ad87829e2507
Aloisio Bastian <aloisiomiguelb@gmail.com>
2023-09-26 15:36:33 -0300

    fix: removedivider from header

--- src/templates/main-menu-block/index.vue
+++ src/templates/main-menu-block/index.vue
@@ -18,7 +18,17 @@
           style="height: 32px; width: 32px"
         />
 
-        <AzionLogo />
+        <AzionLogo class="max-md:hidden" />
+        <svg xmlns="http://www.w3.org/2000/svg" class="block md:hidden" width="22" height="18" fill="none" viewBox="0 0 22 18">
+          <path
+            fill="#F3652B"
+            fill-rule="evenodd"
+            d="M18.744 0 .504 15.357
+            0 18h2.628l14.13-11.89L14.48
+            18h3.447l3.448-18h-2.631Z"
+            clip-rule="evenodd"
+          />
+        </svg>
         <!-- Azion client -->
         <PrimeButton
           v-tooltip.bottom="'Switch Account'"
@@ -116,11 +126,6 @@
             root: { class: 'overflow-visible' },
             badge: { class: 'absolute right-[-4px] top-[-8px]' }
           }"
-        />
-
-        <Divider
-          layout="vertical"
-          class="px-0 mx-1 surface-border"
         />
 
         <!-- Avatar Desktop -->
