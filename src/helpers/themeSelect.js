b144c5a70dceef1a29b932816ad757b74517a636
william-tome <william.tome@azion.com>
2023-09-29 19:25:32 -0300

    refactor: rename variable

--- src/helpers/themeSelect.js
+++ src/helpers/themeSelect.js
@@ -3,6 +3,6 @@ export const themeSelect = ({ pvInstance, theme }) => {
     theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
   }
 
-  const oldTheme = theme === 'light' ? 'dark' : 'light'
-  pvInstance.changeTheme(`theme-${oldTheme}`, `theme-${theme}`, 'theme-link', () => {})
+  const prevTheme = theme === 'light' ? 'dark' : 'light'
+  pvInstance.changeTheme(`theme-${prevTheme}`, `theme-${theme}`, 'theme-link', () => {})
 }
