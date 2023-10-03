5d7aaa2de2b4cc0db8683adf6728e6ff088e1383
Gabriel Lisboa <95422158+gablisb0a@users.noreply.github.com>
2023-10-03 16:42:29 -0300

    style: change stepper shape and values

--- src/views/Playground/PlaygroundView.vue
+++ src/views/Playground/PlaygroundView.vue
@@ -1,36 +1,19 @@
 <template>
-  <div class="card py-3">
-    <Steps
-      :model="items"
-      aria-label="Form Steps"
-      :readonly="false"
-      :pt="{
-        menuitem: ({ context }) => ({
-          class: isActive(context.item) && 'p-highlight p-steps-current'
-        })
-      }"
-    >
+  <div class="card py-10">
+    <Steps :model="items" aria-label="Form Steps" :readonly="false" :pt="{
+      menuitem: ({ context }) => ({
+        class: isActive(context.item) && 'p-highlight p-steps-current'
+      })
+    }">
       <template #item="{ label, item, index, props }">
-        <router-link
-          v-if="item.route"
-          v-slot="routerProps"
-          :to="item.route"
-          custom
-        >
-          <a
-            :href="routerProps.href"
-            v-bind="props.action"
-            @click="($event) => routerProps.navigate($event)"
-            @keydown.enter="($event) => routerProps.navigate($event)"
-          >
+        <router-link v-if="item.route" v-slot="routerProps" :to="item.route" custom>
+          <a :href="routerProps.href" v-bind="props.action" @click="($event) => routerProps.navigate($event)"
+            @keydown.enter="($event) => routerProps.navigate($event)">
             <span v-bind="props.step">{{ index + 1 }}</span>
             <span v-bind="props.label">{{ label }}</span>
           </a>
         </router-link>
-        <span
-          v-else
-          v-bind="props.action"
-        >
+        <span v-else v-bind="props.action">
           <span v-bind="props.step">{{ index + 1 }}</span>
           <span v-bind="props.label">{{ label }}</span>
         </span>
@@ -40,33 +23,33 @@
 </template>
 
 <script setup>
-  import Steps from 'primevue/steps'
-  import { ref } from 'vue'
-  import { useRouter, useRoute } from 'vue-router'
+import Steps from 'primevue/steps'
+import { ref } from 'vue'
+import { useRouter, useRoute } from 'vue-router'
 
-  const router = useRouter()
-  const route = useRoute()
+const router = useRouter()
+const route = useRoute()
 
-  const items = ref([
-    {
-      label: 'Step 1',
-      route: '/'
-    },
-    {
-      label: 'Step 2',
-      route: '/playground'
-    },
-    {
-      label: 'Step 3',
-      route: '/'
-    },
-    {
-      label: 'Step 4',
-      route: '/'
-    }
-  ])
-
-  const isActive = (item) => {
-    return item.route ? router.resolve(item.route).path === route.path : false
+const items = ref([
+  {
+    label: 'Item',
+    route: '/'
+  },
+  {
+    label: 'Item',
+    route: '/playground'
+  },
+  {
+    label: 'Item',
+    route: '/'
+  },
+  {
+    label: 'Item',
+    route: '/'
   }
+])
+
+const isActive = (item) => {
+  return item.route ? router.resolve(item.route).path === route.path : false
+}
 </script>
