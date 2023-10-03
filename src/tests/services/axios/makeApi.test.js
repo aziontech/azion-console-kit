6917043cd3f8cd5e829d3ee1f20f22fc126143e6
Matheus Sunderhus <matheus.sunderhus@azion.com>
2023-10-03 13:56:39 -0300

    test: ensure makeGraphQl generates an instance with correct base url and headers

--- src/tests/services/axios/makeApi.test.js
+++ src/tests/services/axios/makeApi.test.js
@@ -1,5 +1,5 @@
 import api from '@/services/axios/makeApi'
-import { it, describe, assert, expect } from 'vitest'
+import { it, describe, expect } from 'vitest'
 
 const makeSut = ({ token }) => {
   const sut = api(token)
@@ -12,19 +12,21 @@ const makeSut = ({ token }) => {
 describe.concurrent('MakeApi', () => {
   it('should have default headers set', () => {
     const { sut } = makeSut({})
-    assert.equal(sut.defaults.headers.common['Accept'], 'application/json; version=3')
-    assert.equal(sut.defaults.withCredentials, true)
+
+    expect(sut.defaults.headers.common['Accept']).toBe('application/json; version=3')
+    expect(sut.defaults.withCredentials).toBe(true)
   })
 
   it('should have the correct base URL', () => {
     const { sut } = makeSut({})
 
-    assert.equal(sut.defaults.baseURL, '/api')
+    expect(sut.defaults.baseURL).toBe('/api')
   })
   it('should set Authorization header when a token is provided', () => {
     const personalTokenMock = 'r4nd0m-testing-token'
     const { sut } = makeSut({ token: personalTokenMock })
-    assert.equal(sut.defaults.headers.common['Authorization'], `token ${personalTokenMock}`)
+
+    expect(sut.defaults.headers.common['Authorization']).toBe(`token ${personalTokenMock}`)
   })
 
   it('should not set Authorization header when no token is provided', () => {
