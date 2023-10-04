c09655f1622065cfd51a63425d7deb549611f54c
Matheus Sunderhus <matheus.sunderhus@azion.com>
2023-10-04 18:23:16 -0300

    test: ensure listDomainsService parse each returned domain to the correct schema

--- src/tests/services/domains-services/list-domains-service.test.js
+++ src/tests/services/domains-services/list-domains-service.test.js
@@ -2,6 +2,16 @@ import { AxiosHttpClientAdapter } from '@/services/axios/AxiosHttpClientAdapter'
 import { listDomainsService } from '@/services/domains-services'
 import { describe, expect, it, vi } from 'vitest'
 
+const fixtures = {
+  domainMock: {
+    id: '1234',
+    name: 'Edge App X',
+    domain_name: 'domain A',
+    cnames: ['CName 1', 'CName 2'],
+    digital_certificate_id: '862026'
+  }
+}
+
 const makeSut = () => {
   const sut = listDomainsService
 
@@ -24,5 +34,26 @@ describe('DomainsServices', () => {
       url: `domains?order_by=name&sort=asc&page=1&page_size=200`,
       method: 'GET'
     })
+  })
+
+  it('should parsed correctly all returned domains', async () => {
+    vi.spyOn(AxiosHttpClientAdapter, 'request').mockResolvedValueOnce({
+      statusCode: 200,
+      body: { results: [fixtures.domainMock] }
+    })
+    const { sut } = makeSut()
+
+    const domains = await sut({})
+
+    expect(domains).toEqual([
+      {
+        id: fixtures.domainMock.id,
+        name: fixtures.domainMock.name,
+        domainName: fixtures.domainMock.domain_name,
+        cnames: 'CName 1,CName 2',
+        edgeApplicationName: fixtures.domainMock.name,
+        digitalCertificateId: fixtures.domainMock.digital_certificate_id
+      }
+    ])
   })
 })
