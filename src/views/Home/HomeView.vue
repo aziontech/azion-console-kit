26a033468853632fdb454713d1857a2420331728
Eduardo de Cesaro <cesaroeduardo@icloud.com>
2023-10-04 16:50:35 -0300

    feat: custom selectbutton

--- src/views/Home/HomeView.vue
+++ src/views/Home/HomeView.vue
@@ -1,7 +1,31 @@
 <template>
   <div class="p-4 w-full">
-    <div class="max-w-screen-sm lg:max-w-7xl mx-auto">
+    <div class="max-w-screen-sm lg:max-w-7xl mx-auto gap-4">
       <h1 class="text-4xl self-center font-normal text-gray-600">Home</h1>
+      <div class="flex">
+        <SelectButton
+          v-model="value"
+          :options="options"
+          aria-labelledby="basic"
+        />
+      </div>
     </div>
   </div>
 </template>
+
+<script>
+  import SelectButton from 'primevue/selectbutton'
+
+  // Export my assets and components
+  export default {
+    components: {
+      SelectButton
+    },
+    data() {
+      return {
+        value: 'All phases',
+        options: ['All phases', 'Request', 'Response']
+      }
+    }
+  }
+</script>
