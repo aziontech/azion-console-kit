612b2cadff8dc8a6ea46b39358eae3d66c7ddf52
Matheus Sunderhus <matheus.sunderhus@azion.com>
2023-10-04 17:59:23 -0300

    test: ensure deleteDomainService throws correct erros based on http status code

--- src/tests/services/domains-services/delete-domain-service.test.js
+++ src/tests/services/domains-services/delete-domain-service.test.js
@@ -1,4 +1,5 @@
 import { AxiosHttpClientAdapter } from '@/services/axios/AxiosHttpClientAdapter'
+import * as Errors from '@/services/axios/errors'
 import { deleteDomainService } from '@/services/domains-services'
 import { describe, expect, it, vi } from 'vitest'
 
@@ -39,4 +40,44 @@ describe('DomainsServices', () => {
 
     expect(feedbackMessage).toBe('Resource successfully deleted')
   })
+
+  it.each([
+    {
+      statusCode: 400,
+      expectedError: new Errors.InvalidApiRequestError().message
+    },
+    {
+      statusCode: 401,
+      expectedError: new Errors.InvalidApiTokenError().message
+    },
+    {
+      statusCode: 403,
+      expectedError: new Errors.PermissionError().message
+    },
+    {
+      statusCode: 404,
+      expectedError: new Errors.NotFoundError().message
+    },
+    {
+      statusCode: 500,
+      expectedError: new Errors.InternalServerError().message
+    },
+    {
+      statusCode: 'unmappedStatusCode',
+      expectedError: new Errors.UnexpectedError().message
+    }
+  ])(
+    'should throw when request fails with statusCode $statusCode',
+    async ({ statusCode, expectedError }) => {
+      vi.spyOn(AxiosHttpClientAdapter, 'request').mockResolvedValueOnce({
+        statusCode
+      })
+      const stubId = '1'
+      const { sut } = makeSut()
+
+      const response = sut(stubId)
+
+      expect(response).rejects.toBe(expectedError)
+    }
+  )
 })
