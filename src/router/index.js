78f8429efb94eb93a1d7961574529f134453dd21
william-tome <william.tome@azion.com>
2023-10-04 14:11:43 -0300

    feat: add account routes and oganize code

--- src/router/index.js
+++ src/router/index.js
@@ -1,32 +1,36 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import beforeEachRoute from './hooks/beforeEachRoute'
+
+import { accountRoutes } from '@routes/account-routes'
 import { dataStreamingRoutes } from '@routes/data-streaming-routes'
 import { digitalCertificatesRoutes } from '@routes/digital-certificates-routes'
 import { domainsRoutes } from '@routes/domains-routes'
-import { homeRoutes } from '@routes/home-routes'
 import { edgeApplicationRoutes } from '@routes/edge-application-routes'
 import { edgeFirewallRoutes } from '@routes/edge-firewall-routes'
 import { edgeFunctionsRoutes } from '@routes/edge-functions-routes'
+import { edgeNodeRoutes } from '@routes/edge-node-routes'
 import { edgePulseRoutes } from '@routes/edge-pulse-routes'
 import { edgeServicesRoutes } from '@routes/edge-services-routes'
+import { homeRoutes } from '@routes/home-routes'
 import { intelligentDnsRoutes } from '@routes/intelligent-dns-routes'
 import { loginRoutes } from '@routes/login-routes'
 import { networkListsRoutes } from '@routes/network-lists-routes'
 import { personalTokensRoutes } from '@routes/personal-tokens-routes'
-import { variablesRoutes } from '@routes/variables-routes'
 import { underDevelopmentRoutes } from '@routes/under-development-routes'
-import { edgeNodeRoutes } from '@routes/edge-node-routes'
-import beforeEachRoute from './hooks/beforeEachRoute'
 import { usersListsRoutes } from '@routes/users-routes'
+import { variablesRoutes } from '@routes/variables-routes'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    accountRoutes,
     dataStreamingRoutes,
     digitalCertificatesRoutes,
     domainsRoutes,
     edgeApplicationRoutes,
     edgeFirewallRoutes,
     edgeFunctionsRoutes,
+    edgeNodeRoutes,
     edgePulseRoutes,
     edgeServicesRoutes,
     homeRoutes,
@@ -34,10 +38,9 @@ const router = createRouter({
     loginRoutes,
     networkListsRoutes,
     personalTokensRoutes,
-    variablesRoutes,
-    edgeNodeRoutes,
+    underDevelopmentRoutes,
     usersListsRoutes,
-    underDevelopmentRoutes
+    variablesRoutes
   ]
 })
 
