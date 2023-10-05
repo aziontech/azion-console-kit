a8a84f2e44a885e1e37ac4a52200a9ff0645d05e
Eduardo de Cesaro <cesaroeduardo@icloud.com>
2023-10-05 08:57:26 -0300

    feat: selectbutton and tabview

--- src/views/Home/HomeView.vue
+++ src/views/Home/HomeView.vue
@@ -2,12 +2,33 @@
   <div class="p-4 w-full">
     <div class="max-w-screen-sm lg:max-w-7xl mx-auto gap-4">
       <h1 class="text-4xl self-center font-normal text-gray-600">Home</h1>
-      <div class="flex">
+      <!-- SelectButton Custom -->
+      <div class="flex my-2 mx-auto">
         <SelectButton
           v-model="value"
           :options="options"
           aria-labelledby="basic"
         />
+      </div>
+      <!-- TabView Custom -->
+      <div class="flex my-2 mx-auto">
+        <TabView>
+          <TabPanel header="Main Settings">
+          <div class="p-4">
+            <p>Tab 1</p>
+          </div>
+          </TabPanel>
+          <TabPanel header="Origins">
+          <div class="p-4">
+            <p>Tab 2</p>
+          </div>
+          </TabPanel>
+          <TabPanel header="Rules Engine">
+          <div class="p-4">
+            <p>Tab 3</p>
+          </div>
+          </TabPanel>
+        </TabView>
       </div>
     </div>
   </div>
@@ -15,16 +36,20 @@
 
 <script>
   import SelectButton from 'primevue/selectbutton'
+  import TabView from 'primevue/tabview'
+  import TabPanel from 'primevue/tabpanel'
 
   // Export my assets and components
   export default {
     components: {
-      SelectButton
+      SelectButton,
+      TabView,
+      TabPanel,
     },
     data() {
       return {
         value: 'All phases',
-        options: ['All phases', 'Request', 'Response']
+        options: ['All phases', 'Request', 'Response'],
       }
     }
   }
