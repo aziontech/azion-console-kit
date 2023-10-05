cec9bcf4cfab05870f8c6832318173a04248435d
Gabriel Lisboa <95422158+gablisb0a@users.noreply.github.com>
2023-10-05 07:31:13 -0300

    chore: change gaps and some sizes

--- src/templates/footer-block/index.vue
+++ src/templates/footer-block/index.vue
@@ -2,21 +2,46 @@
 <template>
   <!-- Footer -->
   <footer
-    class="z-10 flex px-3 py-6 flex-wrap flex-col justify-center items-center gap-3 lg:gap-4 surface-ground border-t surface-border lg:flex-row pr-3 lg:py-3 lg:px-8 lg:justify-between">
+    class="z-10 flex px-3 py-6 flex-wrap flex-col justify-center items-center gap-3 lg:gap-4 surface-ground border-t surface-border lg:flex-row pr-3 lg:py-3 lg:px-8 lg:justify-between"
+  >
     <span class="text-sm text-normal text-color">© 2023 Azion Technologies.</span>
 
     <div class="flex flex-col gap-3 lg:gap-8 items-center lg:flex lg:flex-row">
       <div class="flex gap-1">
-        <PrimeButton label="About" link />
-        <PrimeButton label="Blog" link />
-        <PrimeButton label="Legal" link />
-        <PrimeButton label="Docs" link />
+        <PrimeButton
+          label="About"
+          link
+        />
+        <PrimeButton
+          label="Blog"
+          link
+        />
+        <PrimeButton
+          label="Legal"
+          link
+        />
+        <PrimeButton
+          label="Docs"
+          link
+        />
       </div>
       <!-- Social Buttons -->
       <div class="flex gap-2">
-        <PrimeButton icon="pi pi-github" text size="small" />
-        <PrimeButton icon="pi pi-discord" text size="small" />
-        <PrimeButton icon="pi pi-twitter" text size="small" />
+        <PrimeButton
+          icon="pi pi-github"
+          text
+          size="small"
+        />
+        <PrimeButton
+          icon="pi pi-discord"
+          text
+          size="small"
+        />
+        <PrimeButton
+          icon="pi pi-twitter"
+          text
+          size="small"
+        />
         <!--Change Twitter icon to X-->
       </div>
 
@@ -24,16 +49,24 @@
         <!-- System Status -->
         <SystemStatusBarBlock />
 
-        <Dropdown v-model="selectedTheme" :options="themeOptions" optionLabel="name" :pt="{
-          root: {
-            class: 'w-auto items-center h-7',
-            style: 'background: var(--surface-section) !important'
-          },
-          item: { class: 'text-sm' },
-          input: { class: 'text-sm' }
-        }">
+        <Dropdown
+          v-model="selectedTheme"
+          :options="themeOptions"
+          optionLabel="name"
+          :pt="{
+            root: {
+              class: 'w-auto items-center h-7',
+              style: 'background: var(--surface-section) !important'
+            },
+            item: { class: 'text-sm' },
+            input: { class: 'text-sm' }
+          }"
+        >
           <template #value="slotProps">
-            <div v-if="slotProps.value" class="flex gap-2 align-items-center">
+            <div
+              v-if="slotProps.value"
+              class="flex gap-2 align-items-center"
+            >
               <i :class="slotProps.value.icon"></i>
               <div>{{ slotProps.value.name }}</div>
             </div>
@@ -51,26 +84,26 @@
 </template>
 
 <script>
-import Dropdown from 'primevue/dropdown'
-import PrimeButton from 'primevue/button'
-import SystemStatusBarBlock from '@templates/system-status-bar-block'
+  import Dropdown from 'primevue/dropdown'
+  import PrimeButton from 'primevue/button'
+  import SystemStatusBarBlock from '@templates/system-status-bar-block'
 
-export default {
-  name: 'FooterTemplate',
-  components: {
-    PrimeButton,
-    Dropdown,
-    SystemStatusBarBlock
-  },
-  data() {
-    return {
-      selectedTheme: { name: 'Light', icon: 'pi pi-sun' },
-      themeOptions: [
-        { name: 'Light', icon: 'pi pi-sun' },
-        { name: 'Dark', icon: 'pi pi-moon' },
-        { name: 'System', icon: 'pi pi-desktop' }
-      ]
+  export default {
+    name: 'FooterTemplate',
+    components: {
+      PrimeButton,
+      Dropdown,
+      SystemStatusBarBlock
+    },
+    data() {
+      return {
+        selectedTheme: { name: 'Light', icon: 'pi pi-sun' },
+        themeOptions: [
+          { name: 'Light', icon: 'pi pi-sun' },
+          { name: 'Dark', icon: 'pi pi-moon' },
+          { name: 'System', icon: 'pi pi-desktop' }
+        ]
+      }
     }
   }
-}
 </script>
--- src/templates/system-status-bar-block/index.vue
+++ src/templates/system-status-bar-block/index.vue
@@ -1,96 +1,102 @@
 <template>
-  <a class="border flex gap-1 surface-border surface-section text-color rounded-lg text-sm px-2 align-items-center justify-center h-7"
-    :href="link" target="_blank">
-    <i class="pi pi-circle-fill text-xs leading-none" :style="colorStatus" />
+  <a
+    class="border flex gap-1 surface-border surface-section text-color rounded-lg text-sm px-2 align-items-center justify-center h-7"
+    :href="link"
+    target="_blank"
+  >
+    <i
+      class="pi pi-circle-fill text-xs leading-none"
+      :style="colorStatus"
+    />
     <span class="leading-none">{{ label }}</span>
   </a>
 </template>
 
 <script>
-import {
-  loadStatusPageService,
-  loadComponentsStatusService
-} from '@/services/status-page-services'
+  import {
+    loadStatusPageService,
+    loadComponentsStatusService
+  } from '@/services/status-page-services'
 
-const STATUS_PAGE = {
-  none: 'operational',
-  minor: 'minor-outage',
-  major: 'partial-outage',
-  critical: 'major-outage',
-  maintenance: 'maintenance'
-}
+  const STATUS_PAGE = {
+    none: 'operational',
+    minor: 'minor-outage',
+    major: 'partial-outage',
+    critical: 'major-outage',
+    maintenance: 'maintenance'
+  }
 
-const STATUS_PAGE_COLORS = {
-  none: '#8bc249',
-  minor: '#fec111',
-  major: '#f3652b',
-  critical: '#ff4141',
-  maintenance: '#6e7cf7'
-}
+  const STATUS_PAGE_COLORS = {
+    none: '#8bc249',
+    minor: '#fec111',
+    major: '#f3652b',
+    critical: '#ff4141',
+    maintenance: '#6e7cf7'
+  }
 
-const OPERATIONAL_STATUS = {
-  indicator: 'none',
-  description: 'All Systems Operational'
-}
+  const OPERATIONAL_STATUS = {
+    indicator: 'none',
+    description: 'All Systems Operational'
+  }
 
-export default {
-  name: 'SystemStatusBarBlock',
-  data() {
-    return {
-      status: '',
-      label: '',
-      link: 'https://status.azion.com',
-      color: STATUS_PAGE_COLORS.none
-    }
-  },
-  created() {
-    this.checkComponentStatus()
-  },
-  computed: {
-    colorStatus() {
-      return { color: this.color }
-    }
-  },
-  methods: {
-    async checkComponentStatus() {
-      try {
-        const { components } = await loadComponentsStatusService()
-
-        const checkComponents = (component) =>
-          component.status !== 'operational' && component.status !== 'partial_outage'
-        const hasImpactedComponent = components?.some(checkComponents)
-
-        const status = await this.getStatus(hasImpactedComponent)
-        this.updateSystemStatus(status)
-      } catch (error) {
-        this.$toast.add({
-          closable: true,
-          severity: 'error',
-          summary: error,
-          life: 10000
-        })
+  export default {
+    name: 'SystemStatusBarBlock',
+    data() {
+      return {
+        status: '',
+        label: '',
+        link: 'https://status.azion.com',
+        color: STATUS_PAGE_COLORS.none
       }
     },
-    async getStatus(checkStatusPage) {
-      let status
-
-      if (checkStatusPage) {
-        status = await this.getStatusPage()
-      } else {
-        status = OPERATIONAL_STATUS
+    created() {
+      this.checkComponentStatus()
+    },
+    computed: {
+      colorStatus() {
+        return { color: this.color }
       }
+    },
+    methods: {
+      async checkComponentStatus() {
+        try {
+          const { components } = await loadComponentsStatusService()
 
-      return status
-    },
-    async getStatusPage() {
-      const status = await loadStatusPageService()
-      return status
-    },
-    updateSystemStatus({ indicator, description }) {
-      this.status = STATUS_PAGE[indicator]
-      this.color = STATUS_PAGE_COLORS[indicator]
-      this.label = description
+          const checkComponents = (component) =>
+            component.status !== 'operational' && component.status !== 'partial_outage'
+          const hasImpactedComponent = components?.some(checkComponents)
+
+          const status = await this.getStatus(hasImpactedComponent)
+          this.updateSystemStatus(status)
+        } catch (error) {
+          this.$toast.add({
+            closable: true,
+            severity: 'error',
+            summary: error,
+            life: 10000
+          })
+        }
+      },
+      async getStatus(checkStatusPage) {
+        let status
+
+        if (checkStatusPage) {
+          status = await this.getStatusPage()
+        } else {
+          status = OPERATIONAL_STATUS
+        }
+
+        return status
+      },
+      async getStatusPage() {
+        const status = await loadStatusPageService()
+        return status
+      },
+      updateSystemStatus({ indicator, description }) {
+        this.status = STATUS_PAGE[indicator]
+        this.color = STATUS_PAGE_COLORS[indicator]
+        this.label = description
+      }
     }
   }
-}
 </script>
