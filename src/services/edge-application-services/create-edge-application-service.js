0061e79dea56ff94ff66b11a1814a814b0ba75ed
peterpaulo-azion <peterson.silva@azion.com>
2023-10-06 16:41:05 -0300

    feat: added edge applicaiton create form

---
+++ src/services/edge-application-services/create-edge-application-service.js
@@ -0,0 +1,33 @@
+import { AxiosHttpClientAdapter, parseHttpResponse } from '../axios/AxiosHttpClientAdapter'
+import { makeEdgeApplicationBaseUrl } from './make-edge-application-base-url'
+
+export const createEdgeApplicationService = async (payload) => {
+  let httpResponse = await AxiosHttpClientAdapter.request({
+    url: `${makeEdgeApplicationBaseUrl()}`,
+    method: 'POST',
+    body: adapt(payload)
+  })
+
+  return parseHttpResponse(httpResponse)
+}
+
+const adapt = (payload) => {
+  return {
+    name: payload.name,
+    delivery_protocol: payload.deliveryProtocol,
+    http3: false,
+    http_port: payload.httpPort.value.value,
+    https_port: payload.httpsPort.value,
+    minimum_tls_version: payload.minimumTlsVersion.value,
+    origin_type: payload.originType.value,
+    address: payload.address,
+    origin_protocol_policy: payload.originProtocolPolicy,
+    host_header: payload.hostHeader,
+    browser_cache_settings: payload.browserCacheSettings,
+    browser_cache_settings_maximum_ttl: payload.browserCacheSettingsMaximumTtl
+      ? payload.browserCacheSettingsMaximumTtl
+      : 0,
+    cdn_cache_settings: payload.cdnCacheSettings,
+    cdn_cache_settings_maximum_ttl: payload.cdnCacheSettingsMaximumTtl,
+  }
+}
---
+++ src/views/EdgeApplications/Tabs/TabsView.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <TabView :active-index="0">
+      <TabPanel header="Main Settings">
+        <MainSettingsTab />
+      </TabPanel>
+
+      <TabPanel header="Origins">
+        <OriginsTab />
+      </TabPanel>
+
+      <TabPanel header="Device Groups">
+        <DeviceGroupsTab />
+      </TabPanel>
+
+      <TabPanel header="Error Responses">
+        <ErrorResponsesTab />
+      </TabPanel>
+
+      <TabPanel header="Cache Settings">
+        <CacheSettingsTab />
+      </TabPanel>
+
+      <TabPanel
+        v-if="activatedFunctions"
+        header="Functions"
+      >
+        <EdgeFunctionsTab />
+      </TabPanel>
+
+      <TabPanel header="Rules Engine">
+        <RulesEngineTab />
+      </TabPanel>
+    </TabView>
+  </div>
+</template>
+
+<script setup>
+  import TabView from 'primevue/tabview'
+  import TabPanel from 'primevue/tabpanel'
+
+  // Tabs
+  import OriginsTab from 'Tabs/Origins'
+  import MainSettingsTab from 'Tabs/MainSettings'
+  import DeviceGroupsTab from 'Tabs/DeviceGroups'
+  import ErrorResponsesTab from 'Tabs/ErrorResponses'
+  import CacheSettingsTab from 'Tabs/CacheSettings'
+  import EdgeFunctionsTab from 'Tabs/EdgeFunctions'
+  import RulesEngineTab from 'Tabs/RulesEngine'
+
+  const activatedFunctions = false
+</script>
