e9d5f60b65aef6ac6d5540cac4cb12a9162f6793
Matheus Sunderhus <matheus.sunderhus@azion.com>
2023-10-09 10:40:46 -0300

    test: ensure editIntelligentDNSService calls API with correct params Also returns the correct feedback message on successfully edit

--- src/tests/services/intelligent-dns-services/edit-intelligent-dns-service.test.js
+++ src/tests/services/intelligent-dns-services/edit-intelligent-dns-service.test.js
@@ -1,6 +1,6 @@
 // continuar daqui.
 import { AxiosHttpClientAdapter } from '@/services/axios/AxiosHttpClientAdapter'
-import { deleteIntelligentDNSService } from '@/services/intelligent-dns-services'
+import { editIntelligentDNSService } from '@/services/intelligent-dns-services'
 import { describe, expect, it, vi } from 'vitest'
 
 const fixtures = {
@@ -13,7 +13,7 @@ const fixtures = {
 }
 
 const makeSut = () => {
-  const sut = deleteIntelligentDNSService
+  const sut = editIntelligentDNSService
 
   return {
     sut
@@ -23,26 +23,31 @@ const makeSut = () => {
 describe('IntelligentDnsServices', () => {
   it('should call API with correct params', async () => {
     const requestSpy = vi.spyOn(AxiosHttpClientAdapter, 'request').mockResolvedValueOnce({
-      statusCode: 204
+      statusCode: 202
     })
     const { sut } = makeSut()
 
-    await sut(fixtures.dnsMock.id)
+    await sut(fixtures.dnsMock)
 
     expect(requestSpy).toHaveBeenCalledWith({
       url: `intelligent_dns/${fixtures.dnsMock.id}`,
-      method: 'DELETE'
+      method: 'PUT',
+      body: {
+        name: fixtures.dnsMock.name,
+        domain: fixtures.dnsMock.domain,
+        is_active: fixtures.dnsMock.isActive
+      }
     })
   })
 
-  it('should return a feedback message on successfully deleted', async () => {
+  it('should return a feedback message on successfully updated', async () => {
     vi.spyOn(AxiosHttpClientAdapter, 'request').mockResolvedValueOnce({
-      statusCode: 204
+      statusCode: 202
     })
     const { sut } = makeSut()
 
-    const feedbackMessage = await sut(fixtures.dnsMock.id)
+    const feedbackMessage = await sut(fixtures.dnsMock)
 
-    expect(feedbackMessage).toBe('Resource successfully deleted')
+    expect(feedbackMessage).toBe('Resource successfully updated')
   })
 })
