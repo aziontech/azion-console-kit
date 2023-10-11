358b5072e9cb0f0e68721249b4f1d25454ab3709
Cicero, Feijo <cicero.feijo@gmail.com>
2023-10-11 16:04:33 -0300

    fix: update logic to get team names

--- src/services/users-services/list-users-service.js
+++ src/services/users-services/list-users-service.js
@@ -26,7 +26,10 @@ const adapt = (httpResponse) => {
         firstName: user.first_name,
         lastName: user.last_name,
         email: user.email,
-        teams: user.teams,
+        teams:
+          user.teams.length > 1
+            ? user.teams.map((t) => t.name).join(', ')
+            : user.teams.map((t) => t.name),
         mfa: user.two_factor_enabled ? 'Yes' : 'No',
         active: user.is_active ? 'Yes' : 'No',
         owner: user.is_account_owner ? 'Yes' : 'No'
