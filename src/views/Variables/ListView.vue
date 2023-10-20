6fdf7566ef101b3de610fa0cad8d49a9d3f1f663
Matheus Sunderhus <matheus.sunderhus@azion.com>
2023-10-20 15:50:29 -0300

    chore: project lint

--- src/views/Variables/ListView.vue
+++ src/views/Variables/ListView.vue
@@ -1,47 +1,54 @@
 <template>
-  <ListTableBlock :listService="listVariablesService" :deleteService="deleteVariablesService" :columns="getColumns"
-    pageTitle="Variables" addButtonLabel="Add Variable" createPagePath="variables/create" editPagePath="variables/edit" />
+  <ListTableBlock
+    :listService="listVariablesService"
+    :deleteService="deleteVariablesService"
+    :columns="getColumns"
+    pageTitle="Variables"
+    addButtonLabel="Add Variable"
+    createPagePath="variables/create"
+    editPagePath="variables/edit"
+  />
 </template>
 
 <script>
-import ListTableBlock from '@/templates/list-table-block'
+  import ListTableBlock from '@/templates/list-table-block'
 
-export default {
-  name: 'variables-view',
-  components: {
-    ListTableBlock
-  },
-  props: {
-    listVariablesService: {
-      required: true,
-      type: Function
+  export default {
+    name: 'variables-view',
+    components: {
+      ListTableBlock
     },
-    deleteVariablesService: {
-      required: true,
-      type: Function
-    }
-  },
-  computed: {
-    getColumns() {
-      return [
-        {
-          field: 'key',
-          header: 'Key'
-        },
-        {
-          field: 'value',
-          header: 'Value',
-        },
-        {
-          field: 'lastEditor',
-          header: 'Last Editor'
-        },
-        {
-          field: 'updatedAt',
-          header: 'Last Update'
-        }
-      ]
+    props: {
+      listVariablesService: {
+        required: true,
+        type: Function
+      },
+      deleteVariablesService: {
+        required: true,
+        type: Function
+      }
+    },
+    computed: {
+      getColumns() {
+        return [
+          {
+            field: 'key',
+            header: 'Key'
+          },
+          {
+            field: 'value',
+            header: 'Value'
+          },
+          {
+            field: 'lastEditor',
+            header: 'Last Editor'
+          },
+          {
+            field: 'updatedAt',
+            header: 'Last Update'
+          }
+        ]
+      }
     }
   }
-}
 </script>
