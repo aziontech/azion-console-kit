233aa52d76cfcbcadc6d8236e90dbe176d540c66
Matheus Sunderhus <matheus.sunderhus@azion.com>
2023-10-20 15:49:52 -0300

    chore: remove  custom columns from listView of variables

--- src/views/Variables/ListView.vue
+++ src/views/Variables/ListView.vue
@@ -1,107 +1,47 @@
 <template>
-  <ListTableBlock
-    :listService="listVariablesService"
-    :deleteService="deleteVariablesService"
-    :columns="getColumns"
-    pageTitle="Variables"
-    addButtonLabel="Add Variable"
-    createPagePath="variables/create"
-    editPagePath="variables/edit"
-  />
+  <ListTableBlock :listService="listVariablesService" :deleteService="deleteVariablesService" :columns="getColumns"
+    pageTitle="Variables" addButtonLabel="Add Variable" createPagePath="variables/create" editPagePath="variables/edit" />
 </template>
 
 <script>
-  import ListTableBlock from '@/templates/list-table-block'
-  import { columnBuilder } from '@/templates/list-table-block/columns/column-builder'
+import ListTableBlock from '@/templates/list-table-block'
 
-  export default {
-    name: 'variables-view',
-    components: {
-      ListTableBlock
+export default {
+  name: 'variables-view',
+  components: {
+    ListTableBlock
+  },
+  props: {
+    listVariablesService: {
+      required: true,
+      type: Function
     },
-    props: {
-      listVariablesService: {
-        required: true,
-        type: Function
-      },
-      deleteVariablesService: {
-        required: true,
-        type: Function
-      }
-    },
-    computed: {
-      getColumns() {
-        return [
-          {
-            field: 'key',
-            header: 'Text'
-          },
-          {
-            field: 'updatedAt',
-            header: 'clickable text',
-            type: 'component',
-            component: (columnData) =>
-              columnBuilder({
-                data: columnData,
-                columnAppearance: 'clickable-text',
-                dependencies: {
-                  clickAction: async () => this.$router.push('/')
-                }
-              })
-          },
-          {
-            field: 'updatedAt',
-            header: 'text+clipboard',
-            type: 'component',
-            component: (columnData) =>
-              columnBuilder({
-                data: columnData,
-                columnAppearance: 'text-with-clipboard',
-                dependencies: {
-                  copyContentService: async (text) => await navigator.clipboard.writeText(text)
-                }
-              })
-          },
-          {
-            field: 'updatedAt',
-            header: 'Tooltip+avatar+text',
-            type: 'component',
-            component: (data) =>
-              columnBuilder({ data, columnAppearance: 'avatar-with-text-and-tooltip' })
-          },
-          {
-            field: 'updatedAt',
-            header: 'avatar+text',
-            type: 'component',
-            component: (data) => columnBuilder({ data, columnAppearance: 'avatar-with-text' })
-          },
-          {
-            field: 'value',
-            header: 'tag',
-            type: 'component',
-            component: (data) => columnBuilder({ data, columnAppearance: 'tag' })
-          }
-          // {
-          //   field: 'updatedAt',
-          //   header: 'Last Update',
-          //   type: 'component',
-          //   component: (data) => withDirectives(
-          //     h(PrimeBadge, {
-          //       value: data.timeDifference,
-          //       severity: data.indicator,
-          //       onClick: () => window.alert(JSON.stringify(data))
-          //     }), [[
-          //       resolveDirective('tooltip'),
-          //       {
-          //         value: data.editor,
-          //       },
-          //       null,
-          //       { bottom: true }
-          //     ]]
-          //   ),
-          // }
-        ]
-      }
+    deleteVariablesService: {
+      required: true,
+      type: Function
+    }
+  },
+  computed: {
+    getColumns() {
+      return [
+        {
+          field: 'key',
+          header: 'Key'
+        },
+        {
+          field: 'value',
+          header: 'Value',
+        },
+        {
+          field: 'lastEditor',
+          header: 'Last Editor'
+        },
+        {
+          field: 'updatedAt',
+          header: 'Last Update'
+        }
+      ]
     }
   }
+}
 </script>
