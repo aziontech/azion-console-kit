e294915f1933afd64d7dd8e7f937bc0451fc0740
Matheus Sunderhus <matheus.sunderhus@azion.com>
2023-10-20 16:45:22 -0300

    chore: lint  project

--- src/views/Credentials/ListView.vue
+++ src/views/Credentials/ListView.vue
@@ -1,42 +1,48 @@
 <template>
-  <ListTableBlock :listService="listCredentialsService" :deleteService="deleteCredentialService" :columns="getColumns"
-    pageTitle="Credentials" addButtonLabel="Add Credential" editPagePath="credentials/edit"
-    createPagePath="credentials/create" />
+  <ListTableBlock
+    :listService="listCredentialsService"
+    :deleteService="deleteCredentialService"
+    :columns="getColumns"
+    pageTitle="Credentials"
+    addButtonLabel="Add Credential"
+    editPagePath="credentials/edit"
+    createPagePath="credentials/create"
+  />
 </template>
 
 <script>
-import ListTableBlock from '@/templates/list-table-block'
+  import ListTableBlock from '@/templates/list-table-block'
 
-export default {
-  name: 'credentials-view',
-  components: {
-    ListTableBlock
-  },
-  props: {
-    listCredentialsService: Function,
-    deleteCredentialService: Function
-  },
-  computed: {
-    getColumns() {
-      return [
-        {
-          field: 'name',
-          header: 'Name'
-        },
-        {
-          field: 'lastEditor',
-          header: 'Last Editor'
-        },
-        {
-          field: 'lastModified',
-          header: 'Last Modified'
-        },
-        {
-          field: 'status',
-          header: 'Active'
-        }
-      ]
+  export default {
+    name: 'credentials-view',
+    components: {
+      ListTableBlock
+    },
+    props: {
+      listCredentialsService: Function,
+      deleteCredentialService: Function
+    },
+    computed: {
+      getColumns() {
+        return [
+          {
+            field: 'name',
+            header: 'Name'
+          },
+          {
+            field: 'lastEditor',
+            header: 'Last Editor'
+          },
+          {
+            field: 'lastModified',
+            header: 'Last Modified'
+          },
+          {
+            field: 'status',
+            header: 'Active'
+          }
+        ]
+      }
     }
   }
-}
 </script>
