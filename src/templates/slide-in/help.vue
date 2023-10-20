cda534dc5715323521c744c6775d52f2efa20613
Eduardo de Cesaro <cesaroeduardo@icloud.com>
2023-10-20 10:31:42 -0300

    fix: ajustes no slidein de help

--- src/templates/slide-in/help.vue
+++ src/templates/slide-in/help.vue
@@ -3,7 +3,7 @@
     class="flex flex-col surface border-l w-slide h-full absolute right-0 transform translate-x-full transition-transform duration-300 ease-in-out"
   >
     <!-- Title  -->
-    <div class="flex w-full justify-between p-3 h-16 border-b">
+    <div class="flex w-full justify-between pl-6 pr-8 py-3 border-b">
       <div class="text-color text-lg font-bold">Help</div>
       <PrimeButton
         icon="pi pi-times"
@@ -13,6 +13,13 @@
         aria-label="Cancel"
         @click="close"
       />
+    </div>
+    <div
+      class="surface-border border border-dashed rounded-md flex items-center h-96 ml-6 mr-8 my-3"
+    >
+      <p class="text-color text-sm font-medium text-center w-full">
+        This section is under development.
+      </p>
     </div>
   </article>
 </template>
