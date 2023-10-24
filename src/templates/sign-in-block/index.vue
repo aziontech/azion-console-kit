ef3377dbbd058b5825c4e11ca1f72edc2418b971
peterpaulo-azion <peterson.silva@azion.com>
2023-10-24 11:10:07 -0300

    feat: email regex simplificate

--- src/templates/sign-in-block/index.vue
+++ src/templates/sign-in-block/index.vue
@@ -183,8 +183,7 @@
     }
   })
 
-  const emailValidateRegex =
-    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  const emailValidateRegex = /^\S+@\S+\.\S+$/
 
   const validationSchema = yup.object({
     email: yup.string().required().matches(emailValidateRegex, 'Invalid email address'),
