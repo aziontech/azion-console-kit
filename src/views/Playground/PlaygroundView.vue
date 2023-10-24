7ad15051629915020a15edb2b1e99789d0b7f3e5
Gabriel Lisboa <95422158+gablisb0a@users.noreply.github.com>
2023-10-24 14:16:08 -0300

    chore: inclide fluid width

--- src/views/Playground/PlaygroundView.vue
+++ src/views/Playground/PlaygroundView.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="flex flex-col p-10 items-center justify-center gap-2">
-    <InlineMessage severity="success">Message Content</InlineMessage>
-    <InlineMessage severity="info">Message Content</InlineMessage>
-    <InlineMessage severity="warn">Message Content</InlineMessage>
-    <InlineMessage severity="error">Message Content</InlineMessage>
+    <InlineMessage class="w-full" severity="success">Message Content</InlineMessage>
+    <InlineMessage class="w-full" severity="info">Message Content</InlineMessage>
+    <InlineMessage class="w-full" severity="warn">Message Content</InlineMessage>
+    <InlineMessage class="w-full" severity="error">Message Content</InlineMessage>
   </div>
 </template>
 
