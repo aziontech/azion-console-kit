5b23251b0b26ac649ab35067880db1b741375d31
Matheus Sunderhus <matheus.sunderhus@azion.com>
2023-10-24 18:49:30 -0300

    test: ensure AzionDocumentationCatalog open variables documentation with correct link

---
+++ src/tests/helpers/azion-documentation-catalog.test.js
@@ -0,0 +1,30 @@
+import { documentationCatalog } from '@/helpers/azion-documentation-catalog'
+import { afterAll, describe, expect, it, vi } from 'vitest'
+
+vi.stubGlobal('window', {
+  open: (url) => url
+})
+
+const makeSut = () => {
+  const sut = documentationCatalog
+
+  return {
+    sut
+  }
+}
+
+describe('AzionDocumentationCatalog', () => {
+  afterAll(() => {
+    vi.unstubAllGlobals()
+  })
+  it('should open variables documentation with correct link', () => {
+    const openWindowSpy = vi.spyOn(window, 'open')
+    const { sut } = makeSut()
+
+    sut.variables()
+
+    expect(openWindowSpy).toHaveBeenCalledWith(
+      `https://www.azion.com/en/search-result/?q=variables&filter=doc`
+    )
+  })
+})
