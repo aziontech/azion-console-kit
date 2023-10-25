c367e02a01dffd842c597b2809dc1c9c4435172c
Matheus Sunderhus <matheus.sunderhus@azion.com>
2023-10-25 16:54:29 -0300

    feat: add resource documentation link to documentationCatalog

--- src/helpers/azion-documentation-catalog.js
+++ src/helpers/azion-documentation-catalog.js
@@ -1,5 +1,6 @@
 import { azionDocumentationWindowOpener } from './azion-documentation-window-opener'
 
 export const documentationCatalog = {
+  resources: () => azionDocumentationWindowOpener(''),
   variables: () => azionDocumentationWindowOpener('variables')
 }
