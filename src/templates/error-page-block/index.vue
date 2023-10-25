9f953c7581d51627d70f9ef83fe1dd4c5f96ecb3
Matheus Sunderhus <matheus.sunderhus@azion.com>
2023-10-25 16:53:24 -0300

    feat: add error-page-block, ForbiddenView and NotFoundView

---
+++ src/templates/error-page-block/index.vue
@@ -0,0 +1,27 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div class="py-20 px-8 self-center items-center flex flex-col gap-10">
+    <slot name="illustration" />
+    <div class="flex flex-col gap-6 max-w-3xl">
+      <h1 class="text-center dark:text-zinc-900 text-5xl font-medium">
+        {{ title }}
+      </h1>
+      <p class="max-w-xl text-center dark:text-stone-500 text-lg font-normal leading-7">
+        {{ description }}
+      </p>
+
+      <div class="flex gap-3 justify-center items-center">
+        <slot name="actions"> </slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+  import { defineProps } from 'vue'
+
+  defineProps({
+    title: { type: String, required: true },
+    description: { type: String, required: true }
+  })
+</script>
---
+++ src/views/Error/NotFoundView.vue
@@ -0,0 +1,38 @@
+<template>
+  <ErrorPageBlock
+    title="Not Found"
+    description="This page doesn't exist yet, usually occurs due to an incorrect or outdated link. Refresh this page and trying again, or you can navigate to a different section of the website."
+  >
+    <template #illustration>
+      <Illustration404 />
+    </template>
+    <template #actions>
+      <PrimeButton
+        label="Back to Home"
+        @click="handleGoBackHome"
+      />
+      <PrimeButton
+        label="Documentation"
+        outlined
+        @click="documentationService"
+      />
+    </template>
+  </ErrorPageBlock>
+</template>
+
+<script setup>
+  import Illustration404 from '@/assets/svg/error-404'
+  import PrimeButton from 'primevue/button'
+  import ErrorPageBlock from '@/templates/error-page-block'
+  import { defineProps } from 'vue'
+  import { useRouter } from 'vue-router'
+  const router = useRouter()
+
+  defineProps({
+    documentationService: { type: Function, required: true }
+  })
+
+  function handleGoBackHome() {
+    router.push('/')
+  }
+</script>
