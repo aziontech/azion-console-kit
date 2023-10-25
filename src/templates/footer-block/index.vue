3d14f78be2aed6895e765042d95d8630dccfacf0
Gabriel Lisboa <95422158+gablisb0a@users.noreply.github.com>
2023-10-25 11:15:54 -0300

    chore: tooltips for footer

--- src/templates/footer-block/index.vue
+++ src/templates/footer-block/index.vue
@@ -31,24 +31,29 @@
           icon="pi pi-github"
           text
           size="small"
+          v-tooltip.top="'Github'"
         />
         <PrimeButton
           icon="pi pi-discord"
           text
           size="small"
+          v-tooltip.top="'Discord'"
         />
         <PrimeButton
           icon="pi pi-twitter"
           text
           size="small"
+          v-tooltip.top="'X'"
         />
         <!--Change Twitter icon to X-->
       </div>
 
       <div class="flex gap-4">
         <!-- System Status -->
-        <SystemStatusBarBlock />
+        <SystemStatusBarBlock 
+        v-tooltip.top="'System status'"/>
         <Dropdown
+          class="w-"
           :modelValue="selectedTheme"
           @update:modelValue="selectTheme"
           optionValue="value"
@@ -61,12 +66,13 @@
               class: ' w-auto py-0 h-[30px] items-center align-middle',
               style: 'background: var(--surface-section) !important'
             },
-            item: { class: 'text-sm' },
-            input: { class: 'text-sm' }
+            item: { class: 'w-full text-sm' },
+            input: { class: 'w-24 text-sm' }
           }"
         >
           <template #value="slotProps">
             <div
+              v-tooltip.top="'Theme'"
               v-if="slotProps.value"
               class="flex gap-2 align-items-center"
             >
