985fe3d282f597862ca9a2a054bff692b74bf525
william-tome <william.tome@azion.com>
2023-10-26 16:58:55 -0300

    feat: create chart owner tag

---
+++ src/templates/graphs-card-block/components/chart-owner.vue
@@ -0,0 +1,31 @@
+<template>
+  <PrimeTag
+    class="bg-purple-100 text-black"
+    icon="pi pi-user"
+    :pt="{ icon: { class: 'mr-0' } }"
+    v-if="chartOwner === 'user'"
+  />
+  <PrimeTag
+    class="bg-green-100 text-black"
+    icon="pi pi-users"
+    :pt="{ icon: { class: 'mr-0' } }"
+    v-if="chartOwner === 'account'"
+  />
+  <PrimeTag
+    class="bg-orange-100 text-black"
+    :pt="{ icon: { class: 'mr-0' } }"
+    v-if="chartOwner === 'azion'"
+  >
+    <MobileLogo
+      class="w-3 h-3"
+      fill="#111111"
+    />
+  </PrimeTag>
+</template>
+
+<script setup>
+  import PrimeTag from 'primevue/tag'
+  import MobileLogo from '@assets/svg/mobile-logo'
+
+  defineProps({ chartOwner: String })
+</script>
