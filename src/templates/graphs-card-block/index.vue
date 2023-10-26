1cba50be6548c6dae29e058ecd79faddbedbf341
william-tome <william.tome@azion.com>
2023-10-26 17:00:53 -0300

    feat: implement owner tag

--- src/templates/graphs-card-block/index.vue
+++ src/templates/graphs-card-block/index.vue
@@ -1,13 +1,9 @@
 <template>
-  <div :class="cardClasses">
+  <div :class="[cardColumns, 'flex flex-col rounded-md h-[552px] border col-span-12']">
     <header class="flex w-full items-center justify-between gap-2 py-3 px-6">
       <span class="w-full gap-2 flex">
-        <PrimeTag
-          icon="pi pi-user"
-          severity="primary"
-          :pt="{ icon: { class: 'mr-0' } }"
-        />
-        <span class="font-medium overflow-hidden overflow-ellipsis break-all">{{ title }}</span>
+        <ChartOwner :chartOwner="chartOwner" />
+        <span class="font-medium overflow-ellipsis break-all line-clamp-1">{{ title }}</span>
       </span>
       <span class="flex gap-4">
         <PrimeButton
@@ -27,30 +23,60 @@
       </span>
     </header>
     <PrimeDivider />
-    <div class="flex flex-col gap-4 bg-black">OK</div>
+    <div class="flex h-full flex-col gap-6 flex-auto p-6">
+      <div class="flex flex-col gap-2">
+        <span class="break-all">{{ description }}</span>
+        <slot name="aggregation" />
+      </div>
+      <section class="flex-auto bg-slate-400 gap-2">Chart</section>
+    </div>
   </div>
 </template>
 
 <script setup>
   import { computed } from 'vue'
-  import PrimeTag from 'primevue/tag'
   import PrimeButton from 'primevue/button'
   import PrimeDivider from 'primevue/divider'
+  import ChartOwner from './components/chart-owner.vue'
 
   defineOptions({ name: 'GraphsCardBlock' })
 
   const props = defineProps({
-    title: String,
+    chartOwner: {
+      type: String,
+      default: 'azion',
+      validator(value) {
+        return ['azion', 'account', 'user'].includes(value)
+      }
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
     cols: {
       type: Number,
-      default: 8,
+      default: 6,
       validator(value) {
         return [4, 6, 8, 12].includes(value)
       }
     }
   })
 
-  const cardClasses = computed(() => {
-    return `col-span-${props.cols} rounded-md h-[552px] border`
+  const cardColumns = computed(() => {
+    // For some reason, template strings do not work here
+    switch (props.cols) {
+      case 4:
+        return 'lg:col-span-4'
+      case 8:
+        return 'lg:col-span-8'
+      case 12:
+        return 'lg:col-span-12'
+      default:
+        return 'lg:col-span-6'
+    }
   })
 </script>
