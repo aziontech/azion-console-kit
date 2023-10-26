b23e336a56c49d78d90640a8eb28021f3363c3bd
Matheus Sunderhus <matheus.sunderhus@azion.com>
2023-10-26 16:22:58 -0300

    chore: disable globally eslint no-case-declarations rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,22 +3,21 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-prettier/skip-formatting'
   ],
   overrides: [
     {
-      files: [
-        'cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}'
-      ],
-      'extends': [
-        'plugin:cypress/recommended'
-      ]
+      files: ['cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}'],
+      extends: ['plugin:cypress/recommended']
     }
   ],
   parserOptions: {
     ecmaVersion: 'latest'
+  },
+  rules: {
+    'no-case-declarations': 0
   }
 }
