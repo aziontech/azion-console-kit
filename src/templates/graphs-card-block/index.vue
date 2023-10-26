2118ed20c8108a449a68cf43b6e051cf78228f98
william-tome <william.tome@azion.com>
2023-10-26 08:04:28 -0300

    feat: create base

---
+++ src/templates/graphs-card-block/index.vue
@@ -0,0 +1,56 @@
+<template>
+  <div :class="cardClasses">
+    <header class="flex w-full items-center justify-between gap-2 py-3 px-6">
+      <span class="w-full gap-2 flex">
+        <PrimeTag
+          icon="pi pi-user"
+          severity="primary"
+          :pt="{ icon: { class: 'mr-0' } }"
+        />
+        <span class="font-medium overflow-hidden overflow-ellipsis break-all">{{ title }}</span>
+      </span>
+      <span class="flex gap-4">
+        <PrimeButton
+          icon="pi pi-question-circle"
+          text
+          aria-label="Get help"
+          size="small"
+          @click="console.log('help')"
+        />
+        <PrimeButton
+          icon="pi pi-ellipsis-h"
+          text
+          aria-label="More options"
+          size="small"
+          @click="console.log('more options')"
+        />
+      </span>
+    </header>
+    <PrimeDivider />
+    <div class="flex flex-col gap-4 bg-black">OK</div>
+  </div>
+</template>
+
+<script setup>
+  import { computed } from 'vue'
+  import PrimeTag from 'primevue/tag'
+  import PrimeButton from 'primevue/button'
+  import PrimeDivider from 'primevue/divider'
+
+  defineOptions({ name: 'GraphsCardBlock' })
+
+  const props = defineProps({
+    title: String,
+    cols: {
+      type: Number,
+      default: 8,
+      validator(value) {
+        return [4, 6, 8, 12].includes(value)
+      }
+    }
+  })
+
+  const cardClasses = computed(() => {
+    return `col-span-${props.cols} rounded-md h-[552px] border`
+  })
+</script>
--- src/views/Playground/PlaygroundView.vue
+++ src/views/Playground/PlaygroundView.vue
@@ -1,12 +1,16 @@
 <template>
-  <div class="flex flex-col p-10 items-center justify-center gap-2">
-    <InlineMessage class="w-full" severity="success">Message Content</InlineMessage>
-    <InlineMessage class="w-full" severity="info">Message Content</InlineMessage>
-    <InlineMessage class="w-full" severity="warn">Message Content</InlineMessage>
-    <InlineMessage class="w-full" severity="error">Message Content</InlineMessage>
+  <div class="p-4 grid grid-cols-12 gap-4">
+    <GraphsCardBlock
+      title="Name Chart 1"
+      :cols="8"
+    />
+    <GraphsCardBlock
+      title="Name Chart 2"
+      :cols="4"
+    />
   </div>
 </template>
 
 <script setup>
-import InlineMessage from 'primevue/inlinemessage';
+import GraphsCardBlock from '@/templates/graphs-card-block'
 </script>
