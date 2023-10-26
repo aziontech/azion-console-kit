6a2b5a6e64cf2f32f986466493d98c3d9e1dfd33
Matheus Sunderhus <matheus.sunderhus@azion.com>
2023-10-26 16:30:11 -0300

    feat: ensure createVariablesService uses its own parseHttpResponse

--- src/services/variables-services/create-variables-service.js
+++ src/services/variables-services/create-variables-service.js
@@ -1,4 +1,5 @@
-import { AxiosHttpClientAdapter, parseHttpResponse } from '../axios/AxiosHttpClientAdapter'
+import { AxiosHttpClientAdapter } from '../axios/AxiosHttpClientAdapter'
+import * as Errors from '@/services/axios/errors'
 import { makeVariablesBaseUrl } from './make-variables-base-url'
 
 export const createVariablesService = async (payload) => {
@@ -9,4 +10,31 @@ export const createVariablesService = async (payload) => {
   })
 
   return parseHttpResponse(httpResponse)
+}
+
+/**
+ * @param {Object} httpResponse
+ * @param {Object} httpResponse.body
+ * @param {String} httpResponse.statuscode
+ */
+const parseHttpResponse = (httpResponse) => {
+  switch (httpResponse.statusCode) {
+    case 201:
+      return 'Your variable has been created'
+    case 400:
+      const alreadyUsedKeyError = httpResponse.body['non_field_errors']?.at(0)
+      const invalidKeyCharacterError = httpResponse.body['key']?.at(0)
+      const invalidValueCharacterError = httpResponse.body['value']?.at(0)
+      throw new Error(alreadyUsedKeyError || invalidKeyCharacterError || invalidValueCharacterError)
+    case 401:
+      throw new Errors.InvalidApiTokenError().message
+    case 403:
+      throw new Errors.PermissionError().message
+    case 404:
+      throw new Errors.NotFoundError().message
+    case 500:
+      throw new Errors.InternalServerError().message
+    default:
+      throw new Errors.UnexpectedError().message
+  }
 }
