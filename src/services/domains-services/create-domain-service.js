0c995c9760b5623a026dc4fb82a0ff02572f1acf
paulosf0 <paulo.ferreira@azion.com>
2023-10-27 14:51:11 -0300

    feat: add msg error

--- src/services/domains-services/create-domain-service.js
+++ src/services/domains-services/create-domain-service.js
@@ -1,5 +1,7 @@
-import { AxiosHttpClientAdapter, parseHttpResponse } from '../axios/AxiosHttpClientAdapter'
+import { AxiosHttpClientAdapter } from '../axios/AxiosHttpClientAdapter'
+import getMsgError from './error-msg'
 import { makeDomainsBaseUrl } from './make-domains-base-url'
+import * as Errors from '@/services/axios/errors'
 
 export const createDomainService = async (payload) => {
   let httpResponse = await AxiosHttpClientAdapter.request({
@@ -21,5 +23,34 @@ const adapt = (payload) => {
     is_mtls_enabled: payload.mtlsIsEnabled,
     mtls_verification: payload.mtlsVerification,
     mtls_trusted_ca_certificate_id: payload.mtlsTrustedCertificate
+  }
+}
+
+
+/**
+ * @param {Object} httpResponse - The HTTP response object.
+ * @param {Object} httpResponse.body - The response body.
+ * @param {String} httpResponse.statusCode - The HTTP status code.
+ * @returns {string} The result message based on the status code.
+ * @throws {Error} If there is an error with the response.
+ */
+const parseHttpResponse = (httpResponse) => {
+  switch (httpResponse.statusCode) {
+    case 201:
+      return 'Your domain has been created'
+    case 400:
+      throw new Error(getMsgError(httpResponse))
+    case 409:
+      throw new Error(getMsgError(httpResponse))
+    case 401:
+      throw new Errors.InvalidApiTokenError().message
+    case 403:
+      throw new Errors.PermissionError().message
+    case 404:
+      throw new Errors.NotFoundError().message
+    case 500:
+      throw new Errors.InternalServerError().message
+    default:
+      throw new Errors.UnexpectedError().message
   }
 }
