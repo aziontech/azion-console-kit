8569792a24ee3704b9a7709d82e9d27a68bfdbd0
william-tome <william.tome@azion.com>
2023-10-30 20:07:54 -0300

    refactor: code review fixes

--- src/views/Playground/PlaygroundView.vue
+++ src/views/Playground/PlaygroundView.vue
@@ -1,23 +1,61 @@
 <template>
   <div class="flex flex-wrap p-10 items-center justify-center gap-2">
     <PrimeButton label="Primary"></PrimeButton>
-    <PrimeButton severity="secondary" label="Secondary"></PrimeButton>
-    <PrimeButton plain text label="Tertiary"></PrimeButton>
-    <PrimeButton link label="Link"></PrimeButton>
-    <PrimeButton outlined label="Outlined"></PrimeButton>
+    <PrimeButton
+      severity="secondary"
+      label="Secondary"
+    ></PrimeButton>
+    <PrimeButton
+      plain
+      text
+      label="Tertiary"
+    ></PrimeButton>
+    <PrimeButton
+      link
+      label="Link"
+    ></PrimeButton>
+    <PrimeButton
+      outlined
+      label="Outlined"
+    ></PrimeButton>
     <InputText placeholder="Placeholder"></InputText>
-    <InputSwitch class="flex-none" v-model="checked" />
+    <InputSwitch
+      class="flex-none"
+      v-model="checked"
+    />
     <Tag value="Primary"></Tag>
-    <Tag severity="success" value="Success"></Tag>
-    <Tag severity="info" value="Info"></Tag>
-    <Tag severity="warning" value="Warning"></Tag>
-    <Tag severity="danger" value="Danger"></Tag>
-    <RadioButton v-model="active" value="Radio Button" />
-    <Checkbox inputId="checkbox" v-model="selected" :binary="true" />
-    <Checkbox inputId="checkbox1" v-model="marked" :binary="true" />
+    <Tag
+      severity="success"
+      value="Success"
+    ></Tag>
+    <Tag
+      severity="info"
+      value="Info"
+    ></Tag>
+    <Tag
+      severity="warning"
+      value="Warning"
+    ></Tag>
+    <Tag
+    severity="danger"
+    value="Danger"
+  ></Tag>
+  <RadioButton
+    v-model="active"
+    value="Radio Button"
+  />
+  <Checkbox
+    inputId="checkbox"
+    v-model="selected"
+    :binary="true"
+  />
+  <Checkbox
+    inputId="checkbox1"
+    v-model="marked"
+    :binary="true"
+  />
 
-  </div>
-</template>
+</div></template>
 
 <script setup>
 import PrimeButton from 'primevue/button';
