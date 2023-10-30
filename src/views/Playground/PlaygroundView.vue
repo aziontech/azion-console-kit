8e9343ab39bca6bbed2746ba12fe98a995c47377
william-tome <william.tome@azion.com>
2023-10-30 15:46:27 -0300

    feat: revert playground

--- src/views/Playground/PlaygroundView.vue
+++ src/views/Playground/PlaygroundView.vue
@@ -1,22 +1,11 @@
 <template>
   <div class="flex flex-col p-10 items-center justify-center gap-2">
-    <InlineMessage
-      class="w-full"
-      severity="success"
-    >Message Content</InlineMessage>
-    <InlineMessage
-      class="w-full"
-      severity="info"
-    >Message Content</InlineMessage>
-    <InlineMessage
-      class="w-full"
-      severity="warn"
-  >Message Content</InlineMessage>
-  <InlineMessage
-    class="w-full"
-    severity="error"
-  >Message Content</InlineMessage>
-</div></template>
+    <InlineMessage class="w-full" severity="success">Message Content</InlineMessage>
+    <InlineMessage class="w-full" severity="info">Message Content</InlineMessage>
+    <InlineMessage class="w-full" severity="warn">Message Content</InlineMessage>
+    <InlineMessage class="w-full" severity="error">Message Content</InlineMessage>
+  </div>
+</template>
 
 <script setup>
 import InlineMessage from 'primevue/inlinemessage';
