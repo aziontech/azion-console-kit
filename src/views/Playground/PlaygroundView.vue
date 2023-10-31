fe200bb4e2bebdc5de9ce8cbcbaeaf56a2c5ba35
Gabriel Lisboa <95422158+gablisb0a@users.noreply.github.com>
2023-10-31 10:59:16 -0300

    feat: colors for switch, radio, checkbox and tags

--- src/views/Playground/PlaygroundView.vue
+++ src/views/Playground/PlaygroundView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col p-10 items-center justify-center gap-2">
+  <div class="flex flex-wrap p-10 items-center justify-center gap-2">
     <PrimeButton label="Primary"></PrimeButton>
     <PrimeButton severity="secondary" label="Secondary"></PrimeButton>
     <PrimeButton plain text label="Tertiary"></PrimeButton>
@@ -7,7 +7,15 @@
     <PrimeButton outlined label="Outlined"></PrimeButton>
     <InputText placeholder="Placeholder"></InputText>
     <InputSwitch class="flex-none" v-model="checked" />
-    <RadioButton></RadioButton>
+    <Tag value="Primary"></Tag>
+    <Tag severity="success" value="Success"></Tag>
+    <Tag severity="info" value="Info"></Tag>
+    <Tag severity="warning" value="Warning"></Tag>
+    <Tag severity="danger" value="Danger"></Tag>
+    <RadioButton v-model="active" value="Radio Button" />
+    <Checkbox inputId="checkbox" v-model="selected" :binary="true" />
+    <Checkbox inputId="checkbox1" v-model="marked" :binary="true" />
+
   </div>
 </template>
 
@@ -15,8 +23,15 @@
 import PrimeButton from 'primevue/button';
 import InputText from 'primevue/inputtext'
 import InputSwitch from 'primevue/inputswitch';
+import Checkbox from 'primevue/checkbox';
+import Tag from 'primevue/tag';
+import RadioButton from 'primevue/radiobutton';
 
-import { ref } from 'vue';
+
+import { ref } from "vue";
 const checked = ref(false);
+const selected = ref(false);
+const marked = ref(false);
+const active = ref('');
 
 </script>
