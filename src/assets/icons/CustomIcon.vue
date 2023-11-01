0116892d60fb2e4892e95177edfee5d4e5d175c0
Eduardo de Cesaro <cesaroeduardo@icloud.com>
2023-11-01 14:08:02 -0300

    fix: ajustes de escrita

--- src/assets/icons/CustomIcon.vue
+++ src/assets/icons/CustomIcon.vue
@@ -1,38 +1,41 @@
 <template>
-    <i :class="iconClass" :style="customStyles"></i>
+  <i
+    :class="iconClass"
+    :style="customStyles"
+  ></i>
 </template>
-  
+
 <script>
   export default {
     props: {
-      name: String,
+      name: String
     },
     computed: {
       iconClass() {
-        return `ci ci-${this.name}`;
+        return `ci ci-${this.name}`
       },
       customStyles() {
         return {
           fontSize: this.fontSize,
-          color: this.color,
-        };
-      },
+          color: this.color
+        }
+      }
     },
     data() {
       return {
-        fontSize: "inherit",
-        color: "inherit",
-      };
-    },
-  };
+        fontSize: 'inherit',
+        color: 'inherit'
+      }
+    }
+  }
 </script>
-    
+
 <style>
-@font-face {
-  font-family: 'azionicons';
-  src: url('azionicons.woff2') format('woff2');
-}
-.ai {
+  @font-face {
+    font-family: 'azionicons';
+    src: url('azionicons.woff2') format('woff2');
+  }
+  .ai {
     font-family: 'azionicons';
     speak: none;
     font-style: normal;
@@ -43,13 +46,13 @@
     display: inline-block;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-}
+  }
 
-/* Icons */
-.ai.ai-twitter::before {
+  /* Icons */
+  .ai.ai-twitter::before {
     content: '\0001';
-}
-.ai.ai-column::before {
+  }
+  .ai.ai-column::before {
     content: '\0002';
-}
+  }
 </style>
