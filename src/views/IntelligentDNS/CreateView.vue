a65cd2f16946499bf6cf68e22908e9f80363e70d
Matheus Sunderhus <matheus.sunderhus@azion.com>
2023-11-04 14:41:02 -0300

    feat: migrate CreateView of IDNS to use form-horizontal block

--- src/views/IntelligentDNS/CreateView.vue
+++ src/views/IntelligentDNS/CreateView.vue
@@ -7,34 +7,68 @@
     :cleanFormCallback="resetForm"
   >
     <template #form>
-      <InputText
-        placeholder="Zone Name"
-        v-bind="name"
-        type="text"
-        :class="{ 'p-invalid': errors.name }"
-        v-tooltip.top="{ value: errors.name, showDelay: 200 }"
-      />
-      <InputText
-        placeholder="Domain"
-        v-bind="domain"
-        type="text"
-        :class="{ 'p-invalid': errors.domain }"
-        v-tooltip.top="{ value: errors.domain, showDelay: 200 }"
-      />
-      <div class="flex gap-3 items-center">
-        <label for="">Active</label>
-        <InputSwitch
-          v-bind="isActive"
-          v-model="isActive.value"
-          :class="{ 'p-invalid': errors.isActive }"
-        />
-      </div>
+      <FormHorizontal
+        title="Variables"
+        description="Espaço livre para descrição e instruções de preenchimento. Esse conteúdo deve ser criado pensando tanto em funcionalidade quanto em em alinhamento e estética. Devemos sempre criar os blocos conforme o contexto, cuidando sempre para não ter blocos muito longos."
+      >
+        <template #inputs>
+          <div class="flex flex-col sm:max-w-lg w-full gap-2">
+            <label
+              for="name"
+              class="text-color text-base font-medium"
+              >Name *</label
+            >
+            <InputText
+              placeholder="Zone Name"
+              v-bind="name"
+              id="name"
+              type="text"
+              :class="{ 'p-invalid': errors.name }"
+              v-tooltip.top="{ value: errors.name, showDelay: 200 }"
+            />
+            <small
+              v-if="errors.name"
+              class="p-error text-xs font-normal leading-tight"
+              >{{ errors.name }}</small
+            >
+          </div>
+          <div class="flex flex-col sm:max-w-lg w-full gap-2">
+            <label
+              for="domain"
+              class="text-color text-base font-medium"
+              >Domain *</label
+            >
+            <InputText
+              placeholder="Domain"
+              id="domain"
+              v-bind="domain"
+              type="text"
+              :class="{ 'p-invalid': errors.domain }"
+              v-tooltip.top="{ value: errors.domain, showDelay: 200 }"
+            />
+            <small
+              v-if="errors.domain"
+              class="p-error text-xs font-normal leading-tight"
+              >{{ errors.domain }}</small
+            >
+          </div>
+          <div class="flex gap-3 items-center">
+            <label for="">Active</label>
+            <InputSwitch
+              v-bind="isActive"
+              v-model="isActive.value"
+              :class="{ 'p-invalid': errors.isActive }"
+            />
+          </div>
+        </template>
+      </FormHorizontal>
     </template>
   </CreateFormBlock>
 </template>
 
 <script setup>
-  import CreateFormBlock from '@/templates/create-form-block'
+  import CreateFormBlock from '@templates/create-form-block-new'
+  import FormHorizontal from '@templates/create-form-block-new/form-horizontal'
   import InputText from 'primevue/inputtext'
   import InputSwitch from 'primevue/inputswitch'
   import { useForm } from 'vee-validate'
