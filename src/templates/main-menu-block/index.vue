959515c2f911aa3739c37618eeef0029db15ddb4
paulosf0 <paulo.ferreira@azion.com>
2023-11-07 09:12:42 -0300

    feat: husky format

--- src/templates/main-menu-block/index.vue
+++ src/templates/main-menu-block/index.vue
@@ -48,7 +48,9 @@
       </div>
 
       <!-- Search -->
-      <span class="top-0 p-input-icon-left p-input-icon-right hidden lg:flex md:absolute md:my-3 md:ml-[calc(50%-10rem)]">
+      <span
+        class="top-0 p-input-icon-left p-input-icon-right hidden lg:flex md:absolute md:my-3 md:ml-[calc(50%-10rem)]"
+      >
         <i class="pi pi-search text-white" />
         <i class="!top-[32%]">
           <span
