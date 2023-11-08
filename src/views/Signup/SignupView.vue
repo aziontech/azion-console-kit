319d664d37dbe89822d2d14adbf729a2eef2500b
william-tome <william.tome@azion.com>
2023-11-08 10:36:28 -0300

    feat: create signup view

---
+++ src/views/Signup/SignupView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <SignUpOnlineSalesBlock :signupService="signupService" />
+  </div>
+</template>
+
+<script setup>
+  import SignUpOnlineSalesBlock from '@/templates/signup-block/online-sales.vue'
+
+  defineProps({
+    signupService: {
+      required: true,
+      type: Function
+    }
+  })
+</script>
