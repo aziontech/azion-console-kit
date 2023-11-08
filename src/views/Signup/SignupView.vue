5e7a89d58641043e02f9bf2cd10b0b741bcd0881
william-tome <william.tome@azion.com>
2023-11-08 15:35:33 -0300

    refactor: separate activation view responsibility

--- src/views/Signup/SignupView.vue
+++ src/views/Signup/SignupView.vue
@@ -1,32 +1,16 @@
 <template>
   <div>
-    <SignUpOnlineSalesBlock
-      :signupService="signupService"
-      @signup-success="showSuccess = true"
-      v-if="!showSuccess"
-    />
-    <SignupSuccess
-      :resendEmailService="resendEmailService"
-      v-else
-    />
+    <SignUpOnlineSalesBlock :signupService="signupService" />
   </div>
 </template>
 
 <script setup>
   import SignUpOnlineSalesBlock from '@/templates/signup-block/online-sales.vue'
-  import SignupSuccess from '@/templates/signup-block/signup-success.vue'
-  import { ref } from 'vue'
 
   defineProps({
     signupService: {
       required: true,
       type: Function
-    },
-    resendEmailService: {
-      required: true,
-      type: Function
     }
   })
-
-  const showSuccess = ref(true)
 </script>
