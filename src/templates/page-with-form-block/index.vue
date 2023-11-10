6c9689cffc6dc1818f4de5ffc7c88b6322e75219
william-tome <william.tome@azion.com>
2023-11-10 16:38:05 -0300

    feat: adjust classes in additional data form

--- src/templates/page-with-form-block/index.vue
+++ src/templates/page-with-form-block/index.vue
@@ -1,8 +1,6 @@
 <template>
-  <section
-    class="flex gap-6 xl:gap-0 px-3 py-6 lg:py-20 lg:px-6 surface-section min-h-[calc(100vh-60px-56px)]"
-  >
-    <div class="w-full hidden lg:flex flex-col items-center xl:justify-center">
+  <section class="flex gap-6 xl:gap-0 px-3 surface-section h-[calc(100vh-60px-56px)]">
+    <div class="w-full hidden lg:flex flex-col items-center justify-center">
       <div class="w-full max-w-md lg:justify-end">
         <h2 class="text-4xl xl:text-6xl font-normal leading-10">{{ pageTitle }}</h2>
         <div class="flex flex-col gap-3 mt-10">
@@ -18,40 +16,40 @@
         </div>
       </div>
     </div>
-    <div class="w-full flex flex-col justify-center items-center">
-      <div class="w-full max-w-md">
-        <div
-          class="card surface-border border rounded-md surface-section p-6 xl:p-10 flex flex-col gap-6 max-w-md"
-        >
-          <h2 class="text-center lg:text-start text-xl lg:text-2xl font-medium">
-            {{ formTitle }}
-          </h2>
-          <slot name="form" />
-          <p class="text-sm font-normal text-center text-color-secondary">
-            By signing up you agree to the
-            <PrimeButton
-              label="Terms of Service"
-              link
-              class="p-0 text-sm"
-              @click="azionTermsAndServicesWindowOpener"
-            />
-            and
-            <PrimeButton
-              label="Privacy Policy."
-              link
-              class="p-0 text-sm"
-              @click="azionPrivacyPolicyWindowOpener"
-            />
-          </p>
-        </div>
-        <div class="flex flex-wrap justify-center items-center gap-3 mt-6 max-w-md">
-          <p class="text-sm font-normal">Already have an account?</p>
+    <div
+      class="w-full flex flex-col justify-start items-center mt-10 overflow-y-auto max-h-[calc(100vh-60px-56px-2rem)] pb-8"
+    >
+      <div
+        class="card surface-border border rounded-md surface-section p-6 xl:p-10 flex flex-col gap-6 max-w-md"
+      >
+        <h2 class="text-center lg:text-start text-xl lg:text-2xl font-medium">
+          {{ formTitle }}
+        </h2>
+        <slot name="form" />
+        <p class="text-sm font-normal text-center text-color-secondary">
+          By signing up you agree to the
           <PrimeButton
-            label="Sign in here"
-            severity="secondary"
-            @click="$router.push({ name: 'login' })"
+            label="Terms of Service"
+            link
+            class="p-0 text-sm"
+            @click="azionTermsAndServicesWindowOpener"
           />
-        </div>
+          and
+          <PrimeButton
+            label="Privacy Policy."
+            link
+            class="p-0 text-sm"
+            @click="azionPrivacyPolicyWindowOpener"
+          />
+        </p>
+      </div>
+      <div class="flex flex-wrap justify-center items-center gap-3 mt-6 max-w-md">
+        <p class="text-sm font-normal">Already have an account?</p>
+        <PrimeButton
+          label="Sign in here"
+          severity="secondary"
+          @click="$router.push({ name: 'login' })"
+        />
       </div>
     </div>
   </section>
