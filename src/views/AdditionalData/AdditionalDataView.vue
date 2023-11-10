4d9a992cc4a3f942614511050b2626ec2737e7ac
william-tome <william.tome@azion.com>
2023-11-10 08:58:22 -0300

    feat: implement page block in additional data view

--- src/views/AdditionalData/AdditionalDataView.vue
+++ src/views/AdditionalData/AdditionalDataView.vue
@@ -1,5 +1,23 @@
 <template>
-  <div>ok</div>
+  <PageWithFormBlock
+    :pageTitle="pageTitle"
+    :benefitsList="benefitsList"
+    :formTitle="formTitle"
+  >
+    <template #form> Form </template>
+  </PageWithFormBlock>
 </template>
 
-<script setup></script>
+<script setup>
+  import PageWithFormBlock from '@/templates/page-with-form-block'
+
+  const pageTitle = 'Almost there!'
+  const benefitsList = [
+    'Improve content delivery experience',
+    'Write and deploy serverless functions',
+    'Create zero-trust security architectures',
+    '$300 credit to use over 12 months',
+    'Free onboarding session with the Customer Success team'
+  ]
+  const formTitle = 'Additional Data'
+</script>
