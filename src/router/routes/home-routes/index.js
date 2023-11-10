921f9259f6d5fde911d77e2cf73c75f51d64e9a7
David Basilio <dbasilioesp@gmail.com>
2023-11-10 18:00:38 -0300

    feat: layout of Home with invite

--- src/router/routes/home-routes/index.js
+++ src/router/routes/home-routes/index.js
@@ -1,6 +1,14 @@
+import * as UsersService from '@/services/users-services'
+import InviteSession from '@/tests/helpers/invite-session'
+
 /** @type {import('vue-router').RouteRecordRaw} */
 export const homeRoutes = {
   path: '/',
   name: 'home',
-  component: () => import('@views/Home/HomeView.vue')
+  component: () => import('@views/Home/HomeView.vue'),
+  props: {
+    listTeamsService: UsersService.listTeamsService,
+    inviteYourTeamService: UsersService.inviteYourTeamService,
+    inviteSession: InviteSession
+  }
 }
