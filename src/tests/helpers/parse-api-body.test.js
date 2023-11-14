18ca70818e7be674853256dcf86e981b378d5a92
william-tome <william.tome@azion.com>
2023-11-14 16:56:24 -0300

    test: create body parser and tests

---
+++ src/tests/helpers/parse-api-body.test.js
@@ -0,0 +1,17 @@
+import { describe, expect, it } from 'vitest'
+import { parseCamelToSnake, parseSnakeToCamel } from '@/helpers/parse-api-body'
+
+const camelCaseBodyMock = { testKey: '' }
+const snakeCaseBodyMock = { test_key: '' }
+
+describe('ParseApiBody', () => {
+  it('should correctly parse camelCase to snake_case', () => {
+    const parsedBody = parseCamelToSnake(camelCaseBodyMock)
+    expect(parsedBody).toEqual(snakeCaseBodyMock)
+  })
+
+  it('should correctly parse snake_case to camelCase', () => {
+    const parsedBody = parseSnakeToCamel(snakeCaseBodyMock)
+    expect(parsedBody).toEqual(camelCaseBodyMock)
+  })
+})
