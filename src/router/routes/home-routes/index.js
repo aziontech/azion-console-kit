21d9294b6b02325968622d246f2a589919db61a9
David Basilio Espitalher <dbasilioesp@gmail.com>
2023-11-14 15:47:44 -0300

    [ENG-28003] feat: layout of Home with invite your team (#273)
    * feat: layout of Home with invite
    
    * fix: general spacings
    
    * fix: add sugestions and improvements to code
    
    - Invite Session on right place
    - Window manager to open links
    
    ---------
    
    Co-authored-by: Gabriel Lisboa <95422158+gablisb0a@users.noreply.github.com>

--- src/router/routes/home-routes/index.js
+++ src/router/routes/home-routes/index.js
@@ -1,6 +1,26 @@
+import * as UsersService from '@/services/users-services'
+import {
+  InviteSession,
+  documentationGuideProducts,
+  openDocumentation,
+  openAPIDocumentation,
+  openContactSupport
+} from '@/helpers'
+
 /** @type {import('vue-router').RouteRecordRaw} */
 export const homeRoutes = {
   path: '/',
   name: 'home',
-  component: () => import('@views/Home/HomeView.vue')
+  component: () => import('@views/Home/HomeView.vue'),
+  props: {
+    listTeamsService: UsersService.listTeamsService,
+    inviteYourTeamService: UsersService.inviteYourTeamService,
+    inviteSession: InviteSession,
+    windowManager: {
+      documentationGuideProducts,
+      openDocumentation,
+      openAPIDocumentation,
+      openContactSupport
+    }
+  }
 }
