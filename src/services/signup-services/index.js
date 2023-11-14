9c8f8bd54a9a8ec2be47a900a360a32add56ce06
william-tome <william.tome@azion.com>
2023-11-14 14:21:04 -0300

    feat: export signup services

---
+++ src/services/signup-services/index.js
@@ -0,0 +1,6 @@
+import { signupService } from './signup-service'
+import { resendEmailService } from './resend-email-service'
+import { listAdditionalDataInfoService } from './list-additional-data-info-service'
+import { listCountriesService } from './list-countries-service'
+
+export { signupService, resendEmailService, listAdditionalDataInfoService, listCountriesService }
