b0a50fda27c94ac915a8b7c376ac52effd91e594
paulosf0 <paulo.ferreira@azion.com>
2023-11-17 08:50:37 -0300

    feat: remove empty block in origin

--- src/views/EdgeApplicationsOrigins/ListView.vue
+++ src/views/EdgeApplicationsOrigins/ListView.vue
@@ -1,14 +1,10 @@
 <script>
   import ListTableBlock from '@/templates/list-table-block/no-header'
-  import EmptyResultsBlock from '@/templates/empty-results-block'
-  import Illustration from '@/assets/svg/illustration-layers.vue'
 
   export default {
     name: 'origins-view',
     components: {
-      ListTableBlock,
-      EmptyResultsBlock,
-      Illustration
+      ListTableBlock
     },
     props: {
       listOriginsService: {
@@ -19,18 +15,8 @@
         required: true,
         type: Function
       },
-      documentationService: {
-        required: true,
-        type: Function
-      }
     },
-    data: () => ({
-      hasContentToList: true
-    }),
     methods: {
-      handleLoadData(event) {
-        this.hasContentToList = event
-      },
       async listOrigins() {
         return await this.listOriginsService({ id: this.edgeApplicationId })
       },
@@ -72,7 +58,6 @@
 
 <template>
   <ListTableBlock
-    v-if="hasContentToList"
     pageTitle="Origins"
     addButtonLabel="Add origins"
     :createPagePath="`${edgeApplicationId}/origins/create`"
@@ -81,19 +66,5 @@
     :listService="listOrigins"
     :deleteService="deleteOrigin"
     :columns="getColumns"
-    @on-load-data="handleLoadData"
   />
-  <EmptyResultsBlock
-    v-else
-    pageTitle="Origins"
-    title="No origins added"
-    description="Create your first origins."
-    createButtonLabel="Origins"
-    :createPagePath="`${edgeApplicationId}/origins/create`"
-    :documentationService="documentationService"
-  >
-    <template #illustration>
-      <Illustration />
-    </template>
-  </EmptyResultsBlock>
 </template>
