05e27393f89724bd79347aa55cbe1af61094f971
David Basilio <dbasilioesp@gmail.com>
2023-11-22 16:26:33 -0300

    feat: use PrimeButton instead of PrimeCard

--- src/views/Marketplace/ListTemplates.vue
+++ src/views/Marketplace/ListTemplates.vue
@@ -1,43 +1,38 @@
 <template>
   <div class="ml-0 w-full mt-0 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-    <PrimeCard
+    <PrimeButton
       v-for="template in props.templates"
       :key="template.id"
-      :pt="{
-        root: 'p-4 flex-shrink basis-6/12',
-        body: 'w-full h-full',
-        content: 'h-full'
-      }"
+      class="p-4 text-left border-solid border surface-border hover:border-primary transition-all"
+      link
     >
-      <template #content>
-        <div class="flex flex-col h-full justify-between gap-3.5 items-start">
-          <div class="flex gap-3.5 flex-col">
-            <div
-              class="w-10 h-10 rounded surface-border border flex justify-center items-center bg-white"
-            >
-              <img
-                class="rounded"
-                :src="template.vendor.icon"
-                alt=""
-              />
-            </div>
-            <div class="flex p-0.5 flex-col">
-              <span class="text-color text-sm font-medium">
-                {{ template.name }}
-              </span>
-              <span class="h-10 pb-4 text-sm text-color-secondary mt-1.5 line-clamp-2">
-                {{ template.headline }}
-              </span>
-            </div>
+      <div class="flex flex-col h-full justify-between gap-3.5 items-start">
+        <div class="flex gap-3.5 flex-col">
+          <div
+            class="w-10 h-10 rounded surface-border border flex justify-center items-center bg-white"
+          >
+            <img
+              class="rounded"
+              :src="template.vendor.icon"
+              alt=""
+            />
+          </div>
+          <div class="flex p-0.5 flex-col">
+            <span class="text-color text-sm font-medium">
+              {{ template.name }}
+            </span>
+            <span class="h-10 pb-4 text-sm text-color-secondary mt-1.5 line-clamp-2">
+              {{ template.headline }}
+            </span>
           </div>
         </div>
-      </template>
-    </PrimeCard>
+      </div>
+    </PrimeButton>
   </div>
 </template>
 
 <script setup>
-  import PrimeCard from 'primevue/card'
+  import PrimeButton from 'primevue/button'
 
   const props = defineProps({
     templates: Array
--- src/views/Marketplace/LoadingListTemplate.vue
+++ src/views/Marketplace/LoadingListTemplate.vue
@@ -2,57 +2,47 @@
   <!-- <div class="w-52 h-8 rounded bg-gray-200"></div> -->
   <Skeleton class="w-52 h-8 rounded" />
   <div class="ml-0 w-full mt-0 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-    <PrimeCard
+    <PrimeButton
       v-for="item of 12"
       :key="item"
-      :pt="{
-        root: 'p-4 flex-shrink basis-6/12',
-        body: 'w-full h-full',
-        content: 'h-full'
-      }"
+      class="p-4 text-left border-solid border surface-border hover:border-primary transition-all"
+      link
     >
-      <template #content>
-        <div class="flex flex-col h-full justify-between gap-3.5 items-start">
-          <div class="flex gap-3.5 flex-col">
-            <Skeleton class="w-10 h-10 rounded" />
-            <div class="flex p-0.5 flex-col gap-1.5">
-              <Skeleton class="w-48 h-5 rounded" />
-              <Skeleton class="w-60 h-5 rounded" />
-              <Skeleton class="w-32 h-5 rounded" />
-            </div>
+      <div class="flex flex-col h-full justify-between gap-3.5 items-start">
+        <div class="flex gap-3.5 flex-col">
+          <Skeleton class="w-10 h-10 rounded" />
+          <div class="flex p-0.5 flex-col gap-1.5">
+            <Skeleton class="w-48 h-5 rounded" />
+            <Skeleton class="w-60 h-5 rounded" />
+            <Skeleton class="w-32 h-5 rounded" />
           </div>
         </div>
-      </template>
-    </PrimeCard>
+      </div>
+    </PrimeButton>
   </div>
   <Skeleton class="w-52 h-6 rounded" />
   <div class="ml-0 w-full mt-0 grid grid-cols-3 gap-4">
-    <PrimeCard
+    <PrimeButton
       v-for="item of 6"
       :key="item"
-      :pt="{
-        root: 'p-4 flex-shrink basis-6/12',
-        body: 'w-full h-full',
-        content: 'h-full'
-      }"
+      class="p-4 text-left border-solid border surface-border hover:border-primary transition-all"
+      link
     >
-      <template #content>
-        <div class="flex flex-col h-full justify-between gap-3.5 items-start">
-          <div class="flex gap-3.5 flex-col">
-            <Skeleton class="w-10 h-10 rounded" />
-            <div class="flex p-0.5 flex-col gap-1.5">
-              <Skeleton class="w-48 h-5 rounded" />
-              <Skeleton class="w-60 h-5 rounded" />
-              <Skeleton class="w-32 h-5 rounded" />
-            </div>
+      <div class="flex flex-col h-full justify-between gap-3.5 items-start">
+        <div class="flex gap-3.5 flex-col">
+          <Skeleton class="w-10 h-10 rounded" />
+          <div class="flex p-0.5 flex-col gap-1.5">
+            <Skeleton class="w-48 h-5 rounded" />
+            <Skeleton class="w-60 h-5 rounded" />
+            <Skeleton class="w-32 h-5 rounded" />
           </div>
         </div>
-      </template>
-    </PrimeCard>
+      </div>
+    </PrimeButton>
   </div>
 </template>
 
 <script setup>
-  import PrimeCard from 'primevue/card'
+  import PrimeButton from 'primevue/button'
   import Skeleton from 'primevue/skeleton'
 </script>
