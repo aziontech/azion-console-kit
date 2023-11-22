a680f6ee7c0c5491d1597e3a4f63ce64d1b75c85
Mariana Bellorín Aguilera <89138244+MarianaAguilera@users.noreply.github.com>
2023-11-22 10:06:31 -0300

    Update src/views/Home/HomeView.vue
    Co-authored-by: hannah <109289700+hannah-sk@users.noreply.github.com>

--- src/views/Home/HomeView.vue
+++ src/views/Home/HomeView.vue
@@ -218,7 +218,7 @@
         >
           <div class="text-lg font-medium">Product Documentation</div>
           <div class="text-sm text-color-secondary">
-            Get to know and learn how to configure all Azion products and their features.
+            Understand and find out how to configure all Azion products and their features.
           </div>
         </button>
         <!-- API -->
