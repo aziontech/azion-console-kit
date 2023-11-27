6f5d4f4003a1e58f5324e249646409965a779146
Eduardo de Cesaro <cesaroeduardo@icloud.com>
2023-11-27 17:41:06 -0300

    fix: selectbutton usando token de togglebutton

--- src/views/Playground/PlaygroundView.vue
+++ src/views/Playground/PlaygroundView.vue
@@ -1,16 +1,15 @@
 
 <template>
-  <div class="items-center">
-    <Badge class="w-fit" value="2"></Badge>
-    <Badge class="w-fit" value="8" severity="success"></Badge>
-    <Badge class="w-fit" value="4" severity="info"></Badge>
-    <Badge class="w-fit" value="12" severity="warning"></Badge>
-    <Badge class="w-fit" value="3" severity="danger"></Badge>
+  <div class="flex p-20 items-center">
+    <ToggleButton v-model="checked" onIcon="pi pi-moon" offIcon="pi pi-dark" onLabel="" offLabel="" />
   </div>
 
 </template>
-  
-  <script setup>
-    import Badge from 'primevue/badge';
-  </script>
+
+<script setup>
+  import { ref } from 'vue';
+  import ToggleButton from 'primevue/togglebutton';
+
+  const checked = ref(false);
+</script>
   
