bbc1e6256fac7bfd73cfad10450e496f2f5db2af
william-tome <william.tome@azion.com>
2023-11-27 14:40:23 -0300

    feat: create form column with language icon

---
+++ src/templates/list-table-block/columns/language-icon-with-text-column.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="gap-2 flex items-center">
+    <component :is="languageIcon" />
+    <p>{{ value }}</p>
+  </div>
+</template>
+
+<script setup>
+  import { computed, defineAsyncComponent } from 'vue'
+
+  const props = defineProps({
+    language: {
+      type: String,
+      required: true
+    },
+    value: {
+      type: String,
+      required: true
+    }
+  })
+
+  const languageIcon = computed(() => {
+    switch (props.language) {
+      case 'javascript':
+        return defineAsyncComponent(() => import('@/assets/svg/js-logo.vue'))
+      case 'lua':
+        return defineAsyncComponent(() => import('@/assets/svg/lua-logo.vue'))
+      default:
+        throw new Error('Invalid column appearance')
+    }
+  })
+</script>
