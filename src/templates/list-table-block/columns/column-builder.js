0f80f197cbaab643ca20c91bd3d0cd522b127492
william-tome <william.tome@azion.com>
2023-11-27 14:42:26 -0300

    refactor: reorder columns in edge functions list

--- src/templates/list-table-block/columns/column-builder.js
+++ src/templates/list-table-block/columns/column-builder.js
@@ -8,6 +8,7 @@ import ClickableTag from './clickable-tag-column.vue'
 import ExpandColumn from './expand-column.vue'
 import ExpandTextColumn from './expand-text-column.vue'
 import Tag from 'primevue/tag'
+import LanguageIconWithTextColumn from './language-icon-with-text-column.vue'
 
 /**
  * Build and return a specific column based on the given appearance.
@@ -24,6 +25,7 @@ import Tag from 'primevue/tag'
  *   - 'clickable-tag'
  *   - 'clickable-link'
  *   - 'tag'
+ *   - 'language-icon-with-text'
  * @param {Object} params.data - The data with specific properties of the column appearance.
  * @param {Object} [params.dependencies]- (Optional) The dependencies needed for rendering this column appearance.
  *
@@ -80,6 +82,11 @@ export const columnBuilder = ({ data, columnAppearance, dependencies }) => {
         value: data.content,
         icon: data.icon,
         severity: data.severity
+      })
+    case 'language-icon-with-text':
+      return h(LanguageIconWithTextColumn, {
+        language: data.icon,
+        value: data.content
       })
     default:
       throw new Error('Invalid column appearance')
