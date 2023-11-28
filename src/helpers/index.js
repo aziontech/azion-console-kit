c051fa6173a490af33e5d8513de9a7995963df62
william-tome <william.tome@azion.com>
2023-11-28 20:54:59 -0300

    feat: create window resize composable

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -13,6 +13,7 @@ import { azionPrivacyPolicyWindowOpener } from './azion-privacy-policy-opener'
 import { azionTermsAndServicesWindowOpener } from './azion-terms-and-services-opener'
 import { parseCamelToSnake, parseSnakeToCamel } from './parse-api-body'
 import { getEnvironmentFromUrl } from './get-environment-from-url'
+import { useWindowSize } from './composables/use-window-resize'
 
 export {
   themeSelect,
@@ -29,5 +30,6 @@ export {
   parseCamelToSnake,
   parseSnakeToCamel,
   openSearchResult,
-  getEnvironmentFromUrl
+  getEnvironmentFromUrl,
+  useWindowSize
 }
