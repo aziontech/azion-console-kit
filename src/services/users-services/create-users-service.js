3f564ffd2467945c634da321b0e7d6411de41f2e
peterpaulo-azion <peterson.silva@azion.com>
2023-12-07 10:20:54 -0300

    fix: linter

--- src/services/users-services/create-users-service.js
+++ src/services/users-services/create-users-service.js
@@ -61,8 +61,8 @@ const parseHttpResponse = (httpResponse) => {
 const getFirstApiError = (body) => {
   for (let key in body) {
     if (Array.isArray(body[key]) && body[key].length > 0) {
-      return body[key][0];
+      return body[key][0]
     }
   }
-  return null;
+  return null
 }
