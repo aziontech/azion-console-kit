0bd17ea8971fd2cefa9c89f485edf13bba739575
Aloisio Bastian <aloisiomiguelb@gmail.com>
2023-12-07 12:06:26 -0300

    fix: add error status code tests

--- src/tests/services/script-runner-services/get-script-runner-results-serice.test.js
+++ src/tests/services/script-runner-services/get-script-runner-results-serice.test.js
@@ -1,6 +1,7 @@
 import { AxiosHttpClientAdapter } from '@/services/axios/AxiosHttpClientAdapter'
 import { getScriptRunnerResultsService } from '@/services/script-runner-service'
 import { describe, expect, it, vi } from 'vitest'
+import * as Errors from '@/services/axios/errors'
 
 const fixtures = {
     executionId: 'xx',
@@ -46,4 +47,40 @@ describe('ScriptRunnerServices', () => {
 
     expect(result).rejects.toBe(fixtures.deploymentFailed.result.message)
   })
+
+  it.each([
+    {
+      statusCode: 401,
+      expectedError: new Errors.InvalidApiTokenError().message
+    },
+    {
+      statusCode: 403,
+      expectedError: new Errors.PermissionError().message
+    },
+    {
+      statusCode: 404,
+      expectedError: new Errors.NotFoundError().message
+    },
+    {
+      statusCode: 500,
+      expectedError: new Errors.InternalServerError().message
+    },
+    {
+      statusCode: 'unmappedStatusCode',
+      expectedError: new Errors.UnexpectedError().message
+    }
+  ])(
+    'should throw when request fails with status code $statusCode',
+    async ({ statusCode, expectedError }) => {
+      vi.spyOn(AxiosHttpClientAdapter, 'request').mockResolvedValueOnce({
+        statusCode
+      })
+      const { sut } = makeSut()
+
+      const response = sut(fixtures.networkMock)
+
+      expect(response).rejects.toBe(expectedError)
+    }
+  )
+  
 })
