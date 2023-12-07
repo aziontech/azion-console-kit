a7ee12f51baac2087d6fef258d982b3446c74065
paulosf0 <paulo.ferreira@azion.com>
2023-12-07 14:42:18 -0300

    feat: remove comment

--- src/views/Playground/PlaygroundView.vue
+++ src/views/Playground/PlaygroundView.vue
@@ -1,12 +1,10 @@
 <template>
   <div class="relative bg-black/50 z-10 flex h-screen justify-content-start">
-    <!-- <ProgressSpinner />  -->
     <ProgressBar class="top-[56px] sticky" mode="indeterminate" style="height: 6px"></ProgressBar>
   </div>
 </template>
 
 <script setup>
   import ProgressBar from 'primevue/progressbar';
-  // import ProgressSpinner from 'primevue/progressspinner';
 </script>
 
