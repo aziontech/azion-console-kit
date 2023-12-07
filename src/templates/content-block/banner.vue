1ed30ef70536458980f6b3dc073fc01b8bd4af70
paulosf0 <paulo.ferreira@azion.com>
2023-12-07 16:08:55 -0300

    feat: add new layout in marketplace

--- src/templates/content-block/banner.vue
+++ src/templates/content-block/banner.vue
@@ -1,27 +1,30 @@
 <template>
   <section class="w-full min-h-[calc(100vh-120px)] relative">
-    <div class="w-full max-w-screen-2xl mx-auto">
-      <Message
-        severity="warn"
-        :closable="false"
-        :pt="{
-          root: { class: 'mx-3 sm:mx-8 mt-4' },
-          wrapper: { class: 'py-3 px-8 ' }
-        }"
-      >
-        <template #messageicon>
-          <Avatar
-            icon="pi pi-exclamation-triangle"
-            class="bg-yellow-600 bg-opacity-20 text-yellow-600 mr-2"
-          />
-        </template>
-        <p class="text-color-secondary">
-          <b class="text-color">Experiment in progress.</b>
-          You're currently using Azion's Platform Kit. Avoid complex setups as this platform is
-          currently undergoing testing and fine-tuning.
-        </p>
-      </Message>
+    <div class="surface-ground w-full">
+      <div class="max-w-screen-2xl mx-auto">
+        <Message
+          severity="warn"
+          :closable="false"
+          :pt="{
+            root: { class: 'mx-3 sm:mx-8 mt-4' },
+            wrapper: { class: 'py-3 px-8' }
+          }"
+        >
+          <template #messageicon>
+            <Avatar
+              icon="pi pi-exclamation-triangle"
+              class="bg-yellow-600 bg-opacity-20 text-yellow-600 mr-2"
+            />
+          </template>
+          <p class="text-color-secondary">
+            <b class="text-color">Experiment in progress.</b>
+            You're currently using Azion's Platform Kit. Avoid complex setups as this platform is
+            currently undergoing testing and fine-tuning.
+          </p>
+        </Message>
+      </div>
     </div>
+    <div class="w-full max-w-screen-2xl mx-auto"></div>
     <slot name="heading"></slot>
     <section class="w-full h-full flex flex-col max-w-screen-2xl mx-auto mb-8">
       <slot name="content"></slot>
