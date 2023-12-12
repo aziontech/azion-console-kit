7f7335150edea60c67cb54fccf977785be90f2cc
william-tome <william.tome@azion.com>
2023-12-12 12:03:46 -0300

    feat: set validationSchema and other props in account settings

--- src/views/AccountSettings/AccountSettingsView.vue
+++ src/views/AccountSettings/AccountSettingsView.vue
@@ -1,9 +1,10 @@
-<template>
-  <AccountSettingsBlock :accountSettingsService="accountSettingsService" />
-</template>
-
 <script setup>
-  import AccountSettingsBlock from '@/templates/account-settings-block'
+  import * as yup from 'yup'
+  import EditFormBlock from '@/templates/edit-form-block'
+  import ContentBlock from '@/templates/content-block'
+  import PageHeadingBlock from '@/templates/page-heading-block'
+  import ActionBarBlockWithTeleport from '@/templates/action-bar-block/action-bar-with-teleport'
+  import FormFieldsAccountSettings from './FormFields/FormFieldsAccountSettings'
 
   defineProps({
     accountSettingsService: {
@@ -11,4 +12,49 @@
       required: false
     }
   })
+
+  const validationSchema = yup.object({
+    accountName: yup.string().required('Account name is a required field.'),
+    clientId: yup.string(),
+    companyName: yup.string(),
+    uniqueIdentifier: yup.string(),
+    billingEmails: yup.string(),
+    postalCode: yup.string().required('Postal code is a required field.'),
+    country: yup.string().required('Country is a required field.'),
+    region: yup.string().required('State/Region is a required field.'),
+    city: yup.string().required('City is a required field.'),
+    address: yup.string().required('Address is a required field.'),
+    number: yup.string().required('Number is a required field.'),
+    complement: yup.string(),
+    isSocialLoginEnabled: yup.boolean(),
+    isEnabledMfaToAllUsers: yup.boolean()
+  })
 </script>
+
+<template>
+  <ContentBlock>
+    <template #heading>
+      <PageHeadingBlock pageTitle="Account Settings" />
+    </template>
+    <template #content>
+      <EditFormBlock
+        :editService="() => {}"
+        :loadService="() => {}"
+        :updatedRedirect="''"
+        :schema="validationSchema"
+      >
+        <template #form>
+          <FormFieldsAccountSettings />
+        </template>
+        <template #action-bar="{ onSubmit, formValid, onCancel, loading }">
+          <ActionBarBlockWithTeleport
+            @onSubmit="onSubmit"
+            @onCancel="onCancel"
+            :loading="loading"
+            :submitDisabled="!formValid"
+          />
+        </template>
+      </EditFormBlock>
+    </template>
+  </ContentBlock>
+</template>
