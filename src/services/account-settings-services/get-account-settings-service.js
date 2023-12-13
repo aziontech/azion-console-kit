46080d7a709648c20d746489c9d7952f7dd2e71f
william-tome <william.tome@azion.com>
2023-12-13 12:12:50 -0300

    feat: configure accout settings service

--- src/services/account-settings-services/get-account-settings-service.js
+++ src/services/account-settings-services/get-account-settings-service.js
@@ -2,13 +2,31 @@ import { AxiosHttpClientAdapter } from '@/services/axios/AxiosHttpClientAdapter'
 import * as Errors from '@/services/axios/errors'
 import { makeAccountSettingsBaseUrl } from './make-account-settings-base-url'
 
-export const accountSettingsService = async (payload) => {
+export const getAccountSettingsService = async (payload) => {
   let httpResponse = await AxiosHttpClientAdapter.request({
     url: makeAccountSettingsBaseUrl(),
     method: 'GET',
     body: payload
   })
   return parseHttpResponse(httpResponse)
+}
+
+const adapt = (response) => {
+  return {
+    accountName: response.name,
+    clientId: response.id,
+    companyName: response.company_name,
+    uniqueIdentifier: response.unique_identifier,
+    billingEmails: response.billing_emails,
+    postalCode: response.postal_code,
+    country: response.country,
+    region: response.region,
+    city: response.city,
+    address: response.address,
+    complement: response.complement,
+    isSocialLoginEnabled: response.is_social_login_enabled,
+    isEnabledMfaToAllUsers: response.is_enabled_mfa_to_all_users,
+  }
 }
 
 /**
@@ -22,7 +40,7 @@ export const accountSettingsService = async (payload) => {
 const parseHttpResponse = (httpResponse) => {
   switch (httpResponse.statusCode) {
     case 200:
-      return httpResponse.body
+      return adapt(httpResponse.body.data)
     case 400:
       throw new Errors.InvalidApiRequestError().message
     case 403:
