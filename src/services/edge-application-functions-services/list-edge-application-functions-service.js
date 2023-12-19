462a26e652aa9d42a360020172ddd658feaa0c3f
peterpaulo-azion <peterson.silva@azion.com>
2023-12-19 16:58:11 -0300

    fix: conflicts resolve

--- src/services/edge-application-functions-services/list-edge-application-functions-service.js
+++ src/services/edge-application-functions-services/list-edge-application-functions-service.js
@@ -2,27 +2,29 @@ import * as FunctionsInstanceService from '@/services/edge-application-functions
 import * as EdgeFunctionsService from '@/services/edge-functions-services'
 
 export const listEdgeApplicationFunctionsService = async (edgeApplicationId) => {
-    const functionsInstances = await FunctionsInstanceService.listFunctionsService({ id: edgeApplicationId })
+  const functionsInstances = await FunctionsInstanceService.listFunctionsService({
+    id: edgeApplicationId
+  })
 
-    const functionsList = await Promise.all(
-      functionsInstances.map(async (edgeApplicationFunction) => {
-        let functionData = await EdgeFunctionsService.loadEdgeFunctionsService({
-          id: edgeApplicationFunction.edgeFunctionId
-        })
-
-        return {
-          id: edgeApplicationFunction.id,
-          name: edgeApplicationFunction.name,
-          languageIcon: functionData.languageIcon,
-          referenceCount: functionData.referenceCount,
-          initiatorType: functionData.initiatorType,
-          lastEditor: functionData.lastEditor,
-          modified: functionData.modified,
-          statusTag: functionData.statusTag,
-          version: functionData.version
-        }
+  const functionsList = await Promise.all(
+    functionsInstances.map(async (edgeApplicationFunction) => {
+      let functionData = await EdgeFunctionsService.loadEdgeFunctionsService({
+        id: edgeApplicationFunction.edgeFunctionId
       })
-    )
 
-    return await functionsList
+      return {
+        id: edgeApplicationFunction.id,
+        name: edgeApplicationFunction.name,
+        languageIcon: functionData.languageIcon,
+        referenceCount: functionData.referenceCount,
+        initiatorType: functionData.initiatorType,
+        lastEditor: functionData.lastEditor,
+        modified: functionData.modified,
+        statusTag: functionData.statusTag,
+        version: functionData.version
+      }
+    })
+  )
+
+  return await functionsList
 }
