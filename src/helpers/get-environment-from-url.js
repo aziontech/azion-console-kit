719ee50195619129dca901b7f899e2ff0e74ce34
Matheus Sunderhus <matheus.sunderhus@azion.com>
2023-12-20 18:54:28 -0300

    test: ensure getEnvironmentFromUrl helper return correct environment

--- src/helpers/get-environment-from-url.js
+++ src/helpers/get-environment-from-url.js
@@ -1,3 +1,7 @@
+/**
+ * @param {string} url - url used to calculate the environment.
+ * @returns {'stage'|'prod'}
+ */
 export const getEnvironmentFromUrl = (url) => {
   const stageUrls = ['stage-console', 'localhost', '127.0.0.1']
   const isStage = stageUrls.some((str) => url.includes(str))
