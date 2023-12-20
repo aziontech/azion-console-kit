76b9a6ec7a2f111cd0e99a993b9b287293bb767d
Matheus Sunderhus <matheus.sunderhus@azion.com>
2023-12-20 18:56:42 -0300

    feat: add index file to Cache settings services

---
+++ src/services/edge-application-cache-settings-services/index.js
@@ -0,0 +1,3 @@
+import { listCacheSettingsService } from './list-cache-settings-service'
+import { deleteCacheSettingsService } from './delete-cache-settings-service'
+export { listCacheSettingsService, deleteCacheSettingsService }
