aa38cb8fa7e03d78f05df3b685ce13fd6f5c686f
Cicero, Feijo <cicero.feijo@gmail.com>
2023-12-21 10:05:58 -0300

    feat: add page to CLI callback

---
+++ src/views/CliCallback/FailView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="min-h-[calc(100vh-60px-56px)] sm:py-20 pt-4 pb-8 px-3">FAIL</div>
+</template>
+
+<script>
+  export default {
+    name: 'cli-callback-fail-view'
+  }
+</script>
---
+++ src/views/CliCallback/SuccessView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="min-h-[calc(100vh-60px-56px)] sm:py-20 pt-4 pb-8 px-3">SUCCESS</div>
+</template>
+
+<script>
+  export default {
+    name: 'cli-callback-success-view'
+  }
+</script>
