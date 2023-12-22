baecbf8f1a5dcdd42a2687cda79399bc3b6aaf0e
Cicero, Feijo <cicero.feijo@gmail.com>
2023-12-21 22:23:52 -0300

    chore: revoved ususal information

--- src/views/CliCallback/FailView.vue
+++ src/views/CliCallback/FailView.vue
@@ -1,26 +1,28 @@
 <template>
   <div class="mx-auto max-w-[720px] items-center text-center gap-6">
-    <div style="background: linear-gradient(to bottom, var(--surface-200) 0%, transparent 100%);" class=" p-4 rounded-md">
-      <i class="pi pi-times" style="color:var(--red-400); font-size: 2rem;"></i>
+    <div
+      style="background: linear-gradient(to bottom, var(--surface-200) 0%, transparent 100%)"
+      class="p-4 rounded-md"
+    >
+      <i
+        class="pi pi-times"
+        style="color: var(--red-400); font-size: 2rem"
+      ></i>
     </div>
     <h1 class="text-3xl lg:text-4xl text-medium">Azion CLI Authentication Failed</h1>
-    <p class="mx-8 text-lg text-color-secondary">Unable to authenticate the Azion CLI with the provided credentials. Please double-check your account details and try again. If the issue persists, ensure that you have the correct permissions or consider <PrimeButton class="p-0 font-normal text-lg"  label="resetting your password." link></PrimeButton>
+    <p class="mx-8 text-lg text-color-secondary">
+      Unable to authenticate the Azion CLI with the provided credentials. Please double-check your
+      account details and try again.
     </p>
-    
-    <p class="mx-8 text-lg text-color-secondary">For assistance, you can refer to the documentation or contact Azion support.
+
+    <p class="mx-8 text-lg text-color-secondary">
+      For assistance, you can refer to the documentation or contact Azion support.
     </p>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'cli-callback-fail-view',
-    components: {
-      PrimeButton,
-  },
+    name: 'cli-callback-fail-view'
   }
-
-
-import PrimeButton from 'primevue/button';
-
 </script>
--- src/views/CliCallback/SuccessView.vue
+++ src/views/CliCallback/SuccessView.vue
@@ -1,25 +1,27 @@
 <template>
   <div class="mx-auto max-w-[720px] items-center text-center gap-6">
-    <div style="background: linear-gradient(to bottom, var(--surface-200) 0%, transparent 100%);" class=" p-4 rounded-md">
-      <i class="pi pi-check" style="color:var(--green-500); font-size: 2rem;"></i>
+    <div
+      style="background: linear-gradient(to bottom, var(--surface-200) 0%, transparent 100%)"
+      class="p-4 rounded-md"
+    >
+      <i
+        class="pi pi-check"
+        style="color: var(--green-500); font-size: 2rem"
+      ></i>
     </div>
     <h1 class="mx-8 text-3xl lg:text-4xl text-medium">Azion CLI Authentication Successfull</h1>
-    <p class="mx-8 text-lg text-color-secondary">You've successfully authenticated the Azion CLI with the following account:
+    <p class="mx-8 text-lg text-color-secondary">
+      You've successfully authenticated the Azion CLI.
     </p>
-    <span class="p-4 text-lg font-robotomono text-color-link surface-200">alessandro.cauduro@gmail.com</span>
-    <p class="mx-8 text-lg text-color-secondary">you can now close this tab and return to the CLI.
-    </p>
+    <p class="mx-8 text-lg text-color-secondary">You can now close this tab.</p>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'cli-callback-success-view',
-  PrimeButton,
-}
+  export default {
+    name: 'cli-callback-success-view',
+    PrimeButton
+  }
 
-
-import PrimeButton from 'primevue/button';
-
-
+  import PrimeButton from 'primevue/button'
 </script>
