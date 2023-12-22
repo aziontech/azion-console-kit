686b3030d13e8143ca440f186ecd0ff9f1b6c8d3
Erick374 <erick.myrn@gmail.com>
2023-12-22 14:55:28 -0300

    feat: tabs consumming from service

--- src/views/Metrics/Blocks/TabsPage.vue
+++ src/views/Metrics/Blocks/TabsPage.vue
@@ -1,41 +1,78 @@
 <script setup>
-  import TabMenu from 'primevue/tabmenu'
-  import Dropdown from 'primevue/dropdown'
-  import { onMounted, ref } from 'vue'
+import TabMenu from 'primevue/tabmenu'
+import Dropdown from 'primevue/dropdown'
+import { onMounted, ref, watch } from 'vue'
 
-  const props = defineProps({
-    tabsReportsMetricsService: {
-      type: Function,
-      required: true
-    },
-    dropdownReportsMetricsService: {
-      type: Function,
-      required: true
+const emit = defineEmits(['teste', 'change:tabs'])
+
+const props = defineProps({
+  tabsReportsMetricsService: {
+    type: Function,
+    required: true
+  },
+  dropdownReportsMetricsService: {
+    type: Function,
+    required: true
+  },
+  tabs: {
+    type: Array,
+    required: true
+  },
+})
+
+onMounted(async () => {
+  tabItems.value = props.tabsReportsMetricsService()
+  dropdownItems.value = props.dropdownReportsMetricsService()
+
+
+  mapDropdownItems(tabItems.value)
+  selectedDropdown.value = dropdownItems.value[0].name
+  setTimeout(() => {
+    named(dropdownItems.value[0].code)
+    active.value = 0
+  }, 100);
+})
+
+const tabItems = ref([])
+
+let dropdownItems = ref()
+let active = ref()
+const selectedDropdown = ref()
+
+
+function mapDropdownItems(params) {
+
+  const retornoMap = params.map((drop) => {
+    return {
+      name: drop.label, code: drop.id
     }
   })
+  dropdownItems.value = retornoMap
+}
 
-  onMounted(() => {
-    tabItems.value = props.tabsReportsMetricsService()
-    dropdownItems.value = props.dropdownReportsMetricsService()
+function named(params) {
+  emit('teste', params)
 
-    selectedDropdown.value = dropdownItems.value[0]
-  })
+}
 
-  const tabItems = ref([])
+function name(params) {
+  emit('change:tabs', params)
+}
 
-  const dropdownItems = ref()
-  const selectedDropdown = ref()
+watch(
+  active,
+  async (newX) => {
+    name(newX)
+    console.log('watch', newX)
+  }
+)
+
 </script>
 
 <template>
   <div class="flex w-full items-end gap-3 mb-4">
-    <Dropdown
-      v-model="selectedDropdown"
-      :options="dropdownItems"
-      optionLabel="name"
-      :placeholder="selectedDropdown"
-      class="flex self-start"
-    />
-    <TabMenu :model="tabItems" />
+    <Dropdown v-model="selectedDropdown" :options="dropdownItems" optionLabel="name" class="flex self-start"
+      :placeholder="selectedDropdown" @change="named" />
+    <TabMenu v-model:activeIndex="active" :model="props.tabs" @tab-change="name" :key="active" />
   </div>
 </template>
--- src/views/Metrics/MetricsView.vue
+++ src/views/Metrics/MetricsView.vue
@@ -6,53 +6,114 @@
     <template #content>
       <!-- <MetricsBlock
       :fetchDataFromBeholderService="fetchDataFromBeholderService" /> -->
-      <TabsPage
-        :listAllDashboards="listAllDashboards"
-        :tabsReportsMetricsService="tabsReportsMetricsService"
-        :dropdownReportsMetricsService="dropdownReportsMetricsService"
-      />
+      <TabsPage @teste="loadDomains" @change:tabs="dashboadsItems" :tabsReportsMetricsService="tabsReportsMetricsService"
+        :dropdownReportsMetricsService="dropdownReportsMetricsService" :tabs="tabsVarible" />
       <FilterPanel />
-      <DashboardPanel />
+      <DashboardPanel :subDash="subDashVarible" />
     </template>
   </ContentBlock>
 </template>
 
 <script setup>
-  import ContentBlock from '@/templates/content-block'
-  import PageHeadingBlock from '@/templates/page-heading-block'
-  import TabsPage from './Blocks/TabsPage.vue'
-  import DashboardPanel from './Blocks/Dashboard/DashboardPanel'
-  import FilterPanel from './Blocks/Filter/FilterPanel.vue'
+import ContentBlock from '@/templates/content-block'
+import PageHeadingBlock from '@/templates/page-heading-block'
+import TabsPage from './Blocks/TabsPage.vue'
+import DashboardPanel from './Blocks/Dashboard/DashboardPanel'
+import FilterPanel from './Blocks/Filter/FilterPanel.vue'
+import { onMounted, ref } from 'vue'
+import { object } from 'yup'
 
-  const props = defineProps({
-    fetchDataFromBeholderService: {
-      type: Function,
-      required: true
-    },
-    searchDomainsMetricsService: {
-      type: Function,
-      required: true
-    },
-    listAllDashboards: {
-      type: Array,
-      required: true
-    },
+const props = defineProps({
+  fetchDataFromBeholderService: {
+    type: Function,
+    required: true
+  },
 
-    tabsReportsMetricsService: {
-      type: Function,
-      required: true
-    },
-    dropdownReportsMetricsService: {
-      type: Function,
-      required: true
+  searchDomainsMetricsService: {
+    type: Function,
+    required: true
+  },
+
+  tabsReportsMetricsService: {
+    type: Function,
+    required: true
+  },
+
+  dropdownReportsMetricsService: {
+    type: Function,
+    required: true
+  },
+})
+
+
+onMounted(() => {
+  tabItems.value = props.tabsReportsMetricsService()
+
+  mapDropdownItems(tabItems.value)
+
+})
+
+const tabItems = ref([])
+let tabsList = []
+let tabsVarible = ref([])
+let subDashVarible = ref([])
+
+function mapDropdownItems(params) {
+
+  const matTabs = params.map((element) => {
+    return {
+      parentId: element.id,
+      pages: element.pagesDashboards
     }
   })
 
-  async function loadDomains() {
-    const domainLike = 'Joao'
-    const data = await props.searchDomainsMetricsService({ domainLike })
-    return data
-  }
+  tabsList.value = matTabs
+}
 
-  loadDomains()
+function formatDash(params) {
+  return {
+    name: params?.label,
+    url: params?.url,
+    dataset: params?.dataset
+  }
+}
+
+function dropdown(params) {
+  const retornoMap = params.map((drop) => {
+    return {
+      label: drop.label, code: drop.id
+    }
+  })
+}
+
+function dashboadsItems(tabsIndex) {
+  let index = tabsIndex
+
+  const value = tabsVarible.value
+
+  const retornoMap = value[index].dashboards.map((dash) => {
+    return formatDash(dash)
+  })
+
+  subDashVarible.value = retornoMap
+}
+
+async function loadDomains(value) {
+  let code = value
+  if (value.value) code = value.value.code
+  const result = tabsList.value.filter((element) => element.parentId === code);
+
+  tabsVarible.value = result[0].pages
+
+  //dashboadsItems(result[0].pages)
+}
+
+/*
+async function loadDomains() {
+  const domainLike = 'Joao'
+  const data = await props.searchDomainsMetricsService({ domainLike })
+  return data
+}
+loadDomains()
+*/
 </script>
