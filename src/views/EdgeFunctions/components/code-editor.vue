92b256270407ca2c3480e22834cd04fce2688282
paulosf0 <paulo.ferreira@azion.com>
2024-01-08 17:20:03 -0300

    feat: add cursor not allowed

--- src/views/EdgeFunctions/components/code-editor.vue
+++ src/views/EdgeFunctions/components/code-editor.vue
@@ -4,7 +4,7 @@
     :language="language"
     :theme="theme"
     class="!w-[99%] h-full surface-border border-r"
-    :class="{ 'border-red-500 border rounded-md h-[calc(100%-1.5rem)]': errors }"
+    :class="{ 'border-red-500 border rounded-md h-[calc(100%-1.5rem)]': errors, 'cursor-not-allowed': readOnly}"
     :options="EDITOR_OPTIONS"
     @change="emit('update:modelValue', $event)"
   />
