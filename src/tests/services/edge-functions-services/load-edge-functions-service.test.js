c1538e71418ffc081d789fa3fa3151b4be48b4ad
paulosf0 <paulo.ferreira@azion.com>
2024-01-10 10:15:47 -0300

    feat: add javascript language in test

--- src/tests/services/edge-functions-services/load-edge-functions-service.test.js
+++ src/tests/services/edge-functions-services/load-edge-functions-service.test.js
@@ -16,6 +16,20 @@ const fixtures = {
     modified: '2023-10-17T18:26:23.047240Z',
     name: 'baggio',
     reference_count: 0
+  },
+  edgeFunctionsMockJsAndInactive: {
+    active: false,
+    code: 'local cjson = require("cjson")\r\nlocal function main(api, args)\r\n    local request = cjson.decode(user_req)\r\n    local response = cjson.decode(user_res)\r\n    local json_args = cjson.decode(user_param)\r\n    if  ngx.var.geo_city == "São Paulo" then \r\n     ngx.header["aassss"] = ngx.var.geo_city\r\n    end\r\n    return cjson.encode(request), cjson.encode(response)\r\nend\r\n',
+    function_to_run: 'main',
+    id: 28518,
+    initiator_type: 'edge_application',
+    is_proprietary_code: false,
+    json_args: {},
+    language: 'javascript',
+    last_editor: 'Azion',
+    modified: '2023-10-17T18:26:23.047240Z',
+    name: 'baggio',
+    reference_count: 0
   }
 }
 
@@ -73,6 +87,39 @@ describe('EdgeFunctionsServices', () => {
         icon: 'lua'
       },
       isProprietaryCode: fixtures.edgeFunctionsMock.is_proprietary_code
+    })
+  })
+
+  it('should parsed correctly the returned with javascript language and inactive', async () => {
+    vi.spyOn(AxiosHttpClientAdapter, 'request').mockResolvedValueOnce({
+      statusCode: 200,
+      body: { results: fixtures.edgeFunctionsMockJsAndInactive }
+    })
+    const { sut } = makeSut()
+
+    const result = await sut({ id: fixtures.edgeFunctionsMockJsAndInactive.id })
+
+    expect(result).toEqual({
+      id: fixtures.edgeFunctionsMockJsAndInactive.id,
+      active: fixtures.edgeFunctionsMockJsAndInactive.active,
+      language: fixtures.edgeFunctionsMockJsAndInactive.language,
+      initiatorType: fixtures.edgeFunctionsMockJsAndInactive.initiator_type,
+      lastEditor: fixtures.edgeFunctionsMockJsAndInactive.last_editor,
+      referenceCount: fixtures.edgeFunctionsMockJsAndInactive.reference_count,
+      jsonArgs: '{}',
+      name: fixtures.edgeFunctionsMockJsAndInactive.name,
+      code: fixtures.edgeFunctionsMockJsAndInactive.code,
+      version: '-',
+      modified: 'Tuesday, October 17, 2023',
+      statusTag: {
+        content: 'Inactive',
+        severity: 'danger'
+      },
+      languageIcon: {
+        content: 'JavaScript',
+        icon: 'javascript'
+      },
+      isProprietaryCode: fixtures.edgeFunctionsMockJsAndInactive.is_proprietary_code
     })
   })
 })
