20ad50f2ab929da606a6c155b109526f5a0bc638
paulosf0 <paulo.ferreira@azion.com>
2024-01-10 17:40:29 -0300

    feat: add create waf rules page

--- src/services/waf-rules-services/index.js
+++ src/services/waf-rules-services/index.js
@@ -1,4 +1,6 @@
 import { listWafRulesService } from './list-waf-rules-service'
 import { deleteWafRulesService } from './delete-waf-rules-service'
+import { createWafRulesService } from './create-waf-rules-service'
 
-export { listWafRulesService, deleteWafRulesService }
+
+export { listWafRulesService, deleteWafRulesService, createWafRulesService }
