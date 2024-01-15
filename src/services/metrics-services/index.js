83b09fe9f8c4c5225d22c3650404a47aeca4b56e
william-tome <william.tome@azion.com>
2024-01-15 10:18:06 -0300

    feat: [metrics] export metrics groups and metrics products services

--- src/services/metrics-services/index.js
+++ src/services/metrics-services/index.js
@@ -1,4 +1,11 @@
 import { fetchDataFromBeholderService } from './fetch-data-from-beholder-service'
+import { fetchMetricsGroupsService } from './fetch-metrics-groups-service'
+import { fetchMetricsProductsService } from './fetch-metrics-products-service'
 import { searchDomainsMetricsService } from './search-domains-metrics-service'
 
-export { fetchDataFromBeholderService, searchDomainsMetricsService }
+export {
+  fetchDataFromBeholderService,
+  fetchMetricsGroupsService,
+  fetchMetricsProductsService,
+  searchDomainsMetricsService
+}
