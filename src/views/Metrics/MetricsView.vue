165e69092de22d8f029a5a9fda5b3101a470e023
william-tome <william.tome@azion.com>
2024-01-15 10:22:51 -0300

    refactor: [metrics] adjust tabs page to handle new structure of services

--- src/views/Metrics/MetricsView.vue
+++ src/views/Metrics/MetricsView.vue
@@ -1,23 +1,41 @@
 <template>
-  <div class="px-3 sm:px-8 pt-4 sm:pt-8 pb-14 flex flex-col gap-8 sm:gap-6 w-full">
-    <MetricsBlock :fetchDataFromBeholderService="fetchDataFromBeholderService" />
-  </div>
+  <ContentBlock>
+    <template #heading>
+      <PageHeadingBlock pageTitle="Real-Time Metrics" />
+    </template>
+    <template #content>
+      <TabsPageBlock
+        :metricsProductsService="fetchMetricsProductsService"
+        :metricsGroupsService="fetchMetricsGroupsService"
+        :params="routeParams"
+      />
+    </template>
+  </ContentBlock>
 </template>
 
 <script setup>
-  import MetricsBlock from '@/templates/metrics-block'
+  import ContentBlock from '@/templates/content-block'
+  import PageHeadingBlock from '@/templates/page-heading-block'
+  import { onMounted, ref } from 'vue'
+  import { useRoute } from 'vue-router'
+  import TabsPageBlock from './blocks/tabs-page-block'
+
   defineProps({
-    fetchDataFromBeholderService: {
+    fetchMetricsGroupsService: {
       type: Function,
       required: true
     },
-    searchDomainsMetricsService: {
+    fetchMetricsProductsService: {
       type: Function,
       required: true
     }
   })
-  async function loadDomains() {
-    return
-  }
-  loadDomains()
+
+  const route = useRoute()
+  const routeParams = ref({})
+
+  onMounted(() => {
+    const { pageId, dashboardId } = route.params
+    routeParams.value = { pageId: pageId || '1', dashboardId: dashboardId || '1' }
+  })
 </script>
