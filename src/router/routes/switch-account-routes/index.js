c3be72424007a6d4e2e234a0953d1df2fe560e1f
william-tome <william.tome@azion.com>
2024-01-16 10:24:05 -0300

    refactor: [switch-account] replace helper with class methods inside route hook

--- src/router/routes/switch-account-routes/index.js
+++ src/router/routes/switch-account-routes/index.js
@@ -1,4 +1,6 @@
-import { switchAccountFromSocialIdp } from '@/helpers'
+import { AccountHandler } from '@/helpers/account-handler'
+import * as AuthServices from '@/services/auth-services'
+import { listTypeAccountService } from '@/services/switch-account-services/list-type-account-service'
 
 /** @type {import('vue-router').RouteRecordRaw} */
 export const switchAccountRoutes = {
@@ -8,8 +10,15 @@ export const switchAccountRoutes = {
     isPublic: true
   },
   beforeEnter: async (_, __, next) => {
+    const accountHandler = new AccountHandler(
+      AuthServices.switchAccountService,
+      listTypeAccountService
+    )
+    const verify = AuthServices.verifyAuthenticationService
+    const refresh = AuthServices.refreshAuthenticationService
+
     try {
-      const redirect = await switchAccountFromSocialIdp()
+      const redirect = await accountHandler.switchAccountFromSocialIdp(verify, refresh)
       next(redirect)
     } catch {
       next({ name: 'login' })
