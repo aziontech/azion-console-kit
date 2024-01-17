4e17de581551953c2f9541295747797cade95024
william-tome <william.tome@azion.com>
2024-01-17 13:14:27 -0300

    refactor: [metrics] rename page and dashboard to group and product

--- src/router/routes/metrics-routes/index.js
+++ src/router/routes/metrics-routes/index.js
@@ -3,7 +3,7 @@ import * as MetricsServices from '@/services/metrics-services'
 
 /** @type {import('vue-router').RouteRecordRaw} */
 export const metricsRoutes = {
-  path: '/real-time-metrics/:pageId?/:dashboardId?',
+  path: '/real-time-metrics/:group?/:product?',
   name: 'real-time-metrics',
   component: () => import('@views/Metrics/MetricsView.vue'),
   props: {
