0674d4ffa0b2d814739a63b16f49c114a27de0e5
william-tome <william.tome@azion.com>
2024-01-19 13:40:22 -0300

    test: [metrics] create convert beholder to chart method test

---
+++ src/tests/store/metrics-store/helpers/convert-beholder-to-chart.test.js
@@ -0,0 +1,42 @@
+import '@/helpers/convert-date'
+import convertBeholderToChart from '@stores/metrics-store/helpers/convert-beholder-to-chart'
+import { describe, expect, it } from 'vitest'
+import FIXTURES from './fixtures/convert-beholder-to-chart-fixture'
+
+const makeSut = () => {
+  const sut = convertBeholderToChart
+
+  return { sut }
+}
+
+const mockScenarios = [
+  {
+    type: 'with all filters',
+    params: FIXTURES.allFilters.params,
+    result: FIXTURES.allFilters.result
+  },
+  {
+    type: 'with isTopX',
+    params: FIXTURES.isTopX.params,
+    result: FIXTURES.isTopX.result
+  },
+  {
+    type: 'with variables',
+    params: FIXTURES.withVariable.params,
+    result: FIXTURES.withVariable.result
+  },
+  {
+    type: 'without groupBy',
+    params: FIXTURES.withoutGroupBy.params,
+    result: FIXTURES.withoutGroupBy.result
+  }
+]
+
+describe('ConvertBeholderToChart', () => {
+  it.each(mockScenarios)('should convert $type', ({ params, result }) => {
+    const { sut } = makeSut()
+    const transformedData = sut(params)
+
+    expect(transformedData).toEqual(result)
+  })
+})
