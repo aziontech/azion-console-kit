856447de1b1ca8539560fff9b8f93b5f30d98bdc
william-tome <william.tome@azion.com>
2024-01-23 13:22:50 -0300

    test: [metrics] create whitelist validation method tests

---
+++ src/tests/store/metrics-store/helpers/verify-whitelist-fields.test.js
@@ -0,0 +1,39 @@
+import VerifyWhiteListField from '@/stores/metrics-store/helpers/verify-whitelist-fields.js'
+import { describe, expect, it } from 'vitest'
+
+const makeSut = () => {
+  const sut = VerifyWhiteListField
+  return { sut }
+}
+
+const mockWhitelistedFields = [
+  'String',
+  'Int',
+  'Float',
+  'IntRange',
+  'FloatRange',
+  'configurationIdIn',
+  'zoneIdIn',
+  'edgeFunctionIdIn'
+]
+
+const mockNotWhitelistedFields = ['clientId', 'clientIdNe', 'clientIdLike', 'clientIdEq']
+
+describe('VerifyWhiteListField', () => {
+  it.each(mockWhitelistedFields)('should validate whitelisted key: %s', (key) => {
+    const { sut } = makeSut()
+
+    const isWhitelisted = sut({ name: key, type: { name: key } })
+
+    expect(isWhitelisted).toBeTruthy()
+  })
+
+  it.each(mockNotWhitelistedFields)('should validate when key is not whitelisted: %s', (key) => {
+    const { sut } = makeSut()
+
+    const isNotWhitelisted = sut({ name: key, type: { name: key } })
+
+    // the inversion is used to improve readability as whitelisted keys should return true
+    expect(!isNotWhitelisted).toBeTruthy()
+  })
+})
