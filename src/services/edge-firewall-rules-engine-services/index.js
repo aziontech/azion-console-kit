9a786a51abb51ad05704d4f8431b02768c13696e
Matheus Sunderhus <matheus.sunderhus@azion.com>
2024-01-23 11:05:31 -0300

    feat: add listEdgeFirewallRulesEngineService

---
+++ src/services/edge-firewall-rules-engine-services/index.js
@@ -0,0 +1,4 @@
+import { listEdgeFirewallRulesEngineService } from './list-rules-engine-service'
+import { deleteEdgeFirewallRulesEngineService } from './delete-edge-firewall-rules-engine-service'
+
+export { listEdgeFirewallRulesEngineService, deleteEdgeFirewallRulesEngineService }
