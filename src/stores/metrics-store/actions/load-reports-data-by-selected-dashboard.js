4d4a7309d7b4ea2af241204565e0c22d0375432b
william-tome <william.tome@azion.com>
2024-01-24 17:13:39 -0300

    feat: [metrics] create load-reports-data-by-seleted-dashboard action

---
+++ src/stores/metrics-store/actions/load-reports-data-by-selected-dashboard.js
@@ -0,0 +1,49 @@
+import { useMetricsStore } from '@/stores/metrics'
+import Axios from 'axios'
+import { LoadReportWithMeta } from '.'
+
+let ReportsRequestTokenSource = null
+
+function reportsBySelectedDashboard(reports, currentDashboard) {
+  const reportsList = reports
+    .filter((report) => {
+      const dashboardId = currentDashboard?.id?.toString()
+      return report.dashboardId.toString() === dashboardId
+    })
+    .map((report) => ({ ...report, resultQuery: [] }))
+  return reportsList
+}
+
+async function resolveReport(report, filters) {
+  const reportWithCancelation = {
+    ...report,
+    ReportsRequestTokenSource
+  }
+  const reportData = await LoadReportWithMeta(filters, reportWithCancelation)
+  const reportInfo = {
+    reportId: report.id,
+    reportData: reportData.resultChart,
+    reportQuery: reportData.gqlQuery,
+    error: reportData.error
+  }
+
+  const metricsStore = useMetricsStore()
+  metricsStore.setCurrentReportValue({ ...reportInfo })
+}
+
+export default async (filters, reports) => {
+  if (ReportsRequestTokenSource) ReportsRequestTokenSource.cancel()
+
+  const ReportsRequestToken = Axios.CancelToken
+  ReportsRequestTokenSource = ReportsRequestToken.source()
+
+  const availableReports = reportsBySelectedDashboard(reports)
+
+  const metricsStore = useMetricsStore()
+
+  metricsStore.setCurrentReports(availableReports)
+
+  availableReports.forEach((report) => {
+    resolveReport(report, filters)
+  })
+}
