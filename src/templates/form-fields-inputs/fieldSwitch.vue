0131761409fd2f86966d1559f7d3c67b73613618
Matheus Sunderhus <matheus.sunderhus@azion.com>
2024-01-24 14:31:12 -0300

    feat: create fieldSwitch component to be used inside form fields

---
+++ src/templates/form-fields-inputs/fieldSwitch.vue
@@ -0,0 +1,41 @@
+<script setup>
+  import { useField } from 'vee-validate'
+  import { toRef } from 'vue'
+  import InputSwitch from 'primevue/inputswitch'
+
+  const props = defineProps({
+    name: {
+      type: String,
+      required: true
+    },
+    label: {
+      type: String,
+      default: ''
+    },
+    description: {
+      type: String,
+      default: ''
+    }
+  })
+
+  const inputName = toRef(props.name)
+  const { value: inputValue } = useField(inputName)
+</script>
+<template>
+  <InputSwitch
+    v-model="inputValue"
+    :inputId="inputName"
+  />
+  <label
+    :for="inputName"
+    class="flex flex-col items-start gap-1"
+  >
+    <span class="text-color text-sm font-normal leading-5">{{ props.label }} </span>
+    <span
+      v-if="props.description"
+      class="text-sm text-color-secondary font-normal leading-5"
+    >
+      {{ props.description }}
+    </span>
+  </label>
+</template>
