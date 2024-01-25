52294aa41c87e6875dbc21742a35507ef98a8d74
william-tome <william.tome@azion.com>
2024-01-25 11:45:08 -0300

    refactor: [metrics] adjust route file

--- src/router/routes/metrics-routes/index.js
+++ src/router/routes/metrics-routes/index.js
@@ -1,18 +1,12 @@
 import { metricsPlaygroundOpener } from '@/helpers'
-import * as MetricsServices from '@/services/metrics-services'
 
 /** @type {import('vue-router').RouteRecordRaw} */
 export const metricsRoutes = {
-  path: '/real-time-metrics/:group?/:product?',
+  path: '/real-time-metrics/:pageId?/:dashboardId?',
   name: 'real-time-metrics',
   component: () => import('@views/Metrics/MetricsView.vue'),
   props: {
-    playgroundOpener: metricsPlaygroundOpener,
-    fetchDataFromBeholderService: MetricsServices.fetchDataFromBeholderService,
-    searchDomainsMetricsService: MetricsServices.searchDomainsMetricsService,
-    fetchMetricsGroupsService: MetricsServices.fetchMetricsGroupsService,
-    fetchMetricsProductsService: MetricsServices.fetchMetricsProductsService,
-    fetchMetricsDashboardsService: MetricsServices.fetchMetricsDashboardsService
+    playgroundOpener: metricsPlaygroundOpener
   },
   meta: {
     breadCrumbs: [
