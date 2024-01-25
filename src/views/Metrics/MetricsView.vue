ac4301635a36613fe06f69346a589460809609df
william-tome <william.tome@azion.com>
2024-01-25 17:17:32 -0300

    feat: [metrics] connect tabs to store

--- src/views/Metrics/MetricsView.vue
+++ src/views/Metrics/MetricsView.vue
@@ -18,7 +18,9 @@
   import { useMetricsStore } from '@/stores/metrics'
   import ContentBlock from '@/templates/content-block'
   import PageHeadingBlock from '@/templates/page-heading-block'
-  import { onMounted } from 'vue'
+  import { storeToRefs } from 'pinia'
+  import { computed, onMounted } from 'vue'
+  import { useRoute, useRouter } from 'vue-router'
   import ContentFilterBlock from './blocks/content-filter-block.vue'
   import DashboardPanelBlock from './blocks/dashboard-panel-block.vue'
   import IntervalFilterBlock from './blocks/interval-filter-block.vue'
@@ -36,7 +38,44 @@
   })
 
   const metricsStore = useMetricsStore()
+  const {
+    setInfoAvailableFilters,
+    setGroupPage,
+    setInitialPageAndDashboardCurrent,
+    setInitialCurrentsByIds
+  } = metricsStore
+
+  const { currentIdPageAndDashboard } = storeToRefs(metricsStore)
+
+  const getCurrentIds = computed(() => {
+    return currentIdPageAndDashboard.value
+  })
+
   const loadPageInfo = async () => {
-    await metricsStore.setInfoAvailableFilters()
+    await setInfoAvailableFilters()
+    await setGroupPage()
+    setCurrentPageAndDashboard()
+    updateRouter()
+  }
+
+  const route = useRoute()
+
+  const setCurrentPageAndDashboard = () => {
+    const { pageId, dashboardId } = route.params
+
+    if (!pageId || !dashboardId) {
+      return setInitialPageAndDashboardCurrent()
+    }
+
+    return setInitialCurrentsByIds({ pageId, dashboardId })
+  }
+
+  const router = useRouter()
+
+  const updateRouter = () => {
+    router.push({
+      name: 'real-time-metrics',
+      params: getCurrentIds.value
+    })
   }
 </script>
