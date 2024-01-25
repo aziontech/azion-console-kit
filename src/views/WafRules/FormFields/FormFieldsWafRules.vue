836388b607e75711d673687eb58451d52262a6e7
paulosf0 <paulo.ferreira@azion.com>
2024-01-25 17:23:05 -0300

    feat: add fix layout

--- src/views/WafRules/FormFields/FormFieldsWafRules.vue
+++ src/views/WafRules/FormFields/FormFieldsWafRules.vue
@@ -64,258 +64,263 @@
     description='Create a new WAF Rule Set to use on your Edge Firewall rules. To apply a WAF Rule Set, you need to add it into a Rule using the behavior "Set WAF Rule Set" in your Edge Firewall > Rules Engine.'
   >
     <template #inputs>
-      <Card
-        :pt="{
-          body: { class: 'p-4' },
-          title: { class: 'flex justify-between  text-base m-0 font-medium' },
-          subtitle: {
-            class: 'text-sm font-normal text-color-secondary m-0 pr-0 md:pr-[2.5rem]'
-          }
-        }"
-      >
-        <template #title>
-          <span class="text-base">SQL Injection</span>
-          <InputSwitch
-            id="secret"
-            v-model="sqlInjection"
-          />
-        </template>
-        <template #subtitle>
-          Detect attempts to insert a SQL query via the input data from the client to the
-          application.
-        </template>
-        <template #content>
-          <div class="flex flex-col w-full sm:max-w-xs gap-2 mt-3">
-            <Dropdown
-              v-model="sqlInjectionSensitivity"
-              :options="sensitivity"
-              optionLabel="name"
-              optionValue="value"
-              class="w-full"
-            />
-          </div>
-        </template>
-      </Card>
+      <div class="flex flex-col gap-2">
+        <label class="text-color text-base font-medium">Threat Type Configuration</label>
+        <div class="flex flex-col gap-3">
+          <Card
+            :pt="{
+              body: { class: `p-4 border rounded-md ${sqlInjection ? 'border-orange-500' : 'border-transparent'}` },
+              title: { class: 'flex justify-between  text-base m-0 font-medium' },
+              subtitle: {
+                class: 'text-sm font-normal text-color-secondary m-0 pr-0 md:pr-[2.5rem]'
+              }
+            }"
+          >
+            <template #title>
+              <span class="text-base">SQL Injection</span>
+              <InputSwitch
+                id="secret"
+                v-model="sqlInjection"
+              />
+            </template>
+            <template #subtitle>
+              Detect attempts to insert a SQL query via the input data from the client to the
+              application.
+            </template>
+            <template #content>
+              <div class="flex flex-col w-full sm:max-w-xs gap-2 mt-3">
+                <Dropdown
+                  v-model="sqlInjectionSensitivity"
+                  :options="sensitivity"
+                  optionLabel="name"
+                  optionValue="value"
+                  class="w-full"
+                />
+              </div>
+            </template>
+          </Card>
 
-      <Card
-        :pt="{
-          body: { class: 'p-4' },
-          title: { class: 'flex justify-between  text-base m-0 font-medium' },
-          subtitle: {
-            class: 'text-sm font-normal text-color-secondary m-0 pr-0 md:pr-[2.5rem]'
-          }
-        }"
-      >
-        <template #title>
-          <span class="text-base">Remote File Inclusions (RFI)</span>
-          <InputSwitch
-            id="secret"
-            v-model="remoteFileInclusion"
-          />
-        </template>
-        <template #subtitle>
-          Detect attempts to include a remote file, usually through a script on the web server.
-        </template>
-        <template #content>
-          <div class="flex flex-col w-full sm:max-w-xs gap-2 mt-3">
-            <Dropdown
-              v-model="remoteFileInclusionSensitivity"
-              :options="sensitivity"
-              optionLabel="name"
-              optionValue="value"
-              class="w-full"
-            />
-          </div>
-        </template>
-      </Card>
+          <Card
+            :pt="{
+              body: { class: `p-4 border rounded-md ${remoteFileInclusion ? 'border-orange-500' : 'border-transparent'} ` },
+              title: { class: 'flex justify-between  text-base m-0 font-medium' },
+              subtitle: {
+                class: 'text-sm font-normal text-color-secondary m-0 pr-0 md:pr-[2.5rem]'
+              }
+            }"
+          >
+            <template #title>
+              <span class="text-base">Remote File Inclusions (RFI)</span>
+              <InputSwitch
+                id="secret"
+                v-model="remoteFileInclusion"
+              />
+            </template>
+            <template #subtitle>
+              Detect attempts to include a remote file, usually through a script on the web server.
+            </template>
+            <template #content>
+              <div class="flex flex-col w-full sm:max-w-xs gap-2 mt-3">
+                <Dropdown
+                  v-model="remoteFileInclusionSensitivity"
+                  :options="sensitivity"
+                  optionLabel="name"
+                  optionValue="value"
+                  class="w-full"
+                />
+              </div>
+            </template>
+          </Card>
 
-      <Card
-        :pt="{
-          body: { class: 'p-4' },
-          title: { class: 'flex justify-between  text-base m-0 font-medium' },
-          subtitle: {
-            class: 'text-sm font-normal text-color-secondary m-0 pr-0 md:pr-[2.5rem]'
-          }
-        }"
-      >
-        <template #title>
-          <span class="text-base">Directory Traversal</span>
-          <InputSwitch
-            id="secret"
-            v-model="directoryTraversal"
-          />
-        </template>
-        <template #subtitle>
-          Prevent exploiting insufficient security sanitizing of user-supplied input file names, so
-          that characters representing "traverse to parent directory" are passed through to the file
-          APIs.
-        </template>
-        <template #content>
-          <div class="flex flex-col w-full sm:max-w-xs gap-2 mt-3">
-            <Dropdown
-              v-model="directoryTraversalSensitivity"
-              :options="sensitivity"
-              optionLabel="name"
-              optionValue="value"
-              class="w-full"
-            />
-          </div>
-        </template>
-      </Card>
+          <Card
+            :pt="{
+              body: { class: `p-4 border rounded-md ${directoryTraversal ? 'border-orange-500' : 'border-transparent'}` },
+              title: { class: 'flex justify-between  text-base m-0 font-medium' },
+              subtitle: {
+                class: 'text-sm font-normal text-color-secondary m-0 pr-0 md:pr-[2.5rem]'
+              }
+            }"
+          >
+            <template #title>
+              <span class="text-base">Directory Traversal</span>
+              <InputSwitch
+                id="secret"
+                v-model="directoryTraversal"
+              />
+            </template>
+            <template #subtitle>
+              Prevent exploiting insufficient security sanitizing of user-supplied input file names,
+              so that characters representing "traverse to parent directory" are passed through to
+              the file APIs.
+            </template>
+            <template #content>
+              <div class="flex flex-col w-full sm:max-w-xs gap-2 mt-3">
+                <Dropdown
+                  v-model="directoryTraversalSensitivity"
+                  :options="sensitivity"
+                  optionLabel="name"
+                  optionValue="value"
+                  class="w-full"
+                />
+              </div>
+            </template>
+          </Card>
 
-      <Card
-        :pt="{
-          body: { class: 'p-4' },
-          title: { class: 'flex justify-between  text-base m-0 font-medium' },
-          subtitle: {
-            class: 'text-sm font-normal text-color-secondary m-0 pr-0 md:pr-[2.5rem]'
-          }
-        }"
-      >
-        <template #title>
-          <span class="text-base">Cross-Site Scripting (XSS)</span>
-          <InputSwitch
-            id="secret"
-            v-model="crossSiteScripting"
-          />
-        </template>
-        <template #subtitle>
-          Detect attempts to inject client-side scripts into web pages viewed by your visitors.
-        </template>
-        <template #content>
-          <div class="flex flex-col w-full sm:max-w-xs gap-2 mt-3">
-            <Dropdown
-              v-model="crossSiteScriptingSensitivity"
-              :options="sensitivity"
-              optionLabel="name"
-              optionValue="value"
-              class="w-full"
-            />
-          </div>
-        </template>
-      </Card>
-      <Card
-        :pt="{
-          body: { class: 'p-4' },
-          title: { class: 'flex justify-between  text-base m-0 font-medium' },
-          subtitle: {
-            class: 'text-sm font-normal text-color-secondary m-0 pr-0 md:pr-[2.5rem]'
-          }
-        }"
-      >
-        <template #title>
-          <span class="text-base">File Upload</span>
-          <InputSwitch
-            id="secret"
-            v-model="fileUpload"
-          />
-        </template>
-        <template #subtitle> Detect attempts to upload files. </template>
-        <template #content>
-          <div class="flex flex-col w-full sm:max-w-xs gap-2 mt-3">
-            <Dropdown
-              v-model="fileUploadSensitivity"
-              :options="sensitivity"
-              optionLabel="name"
-              optionValue="value"
-              class="w-full"
-            />
-          </div>
-        </template>
-      </Card>
+          <Card
+            :pt="{
+              body: { class: `p-4 border rounded-md ${crossSiteScripting ? 'border-orange-500' : 'border-transparent'}` },
+              title: { class: 'flex justify-between  text-base m-0 font-medium' },
+              subtitle: {
+                class: 'text-sm font-normal text-color-secondary m-0 pr-0 md:pr-[2.5rem]'
+              }
+            }"
+          >
+            <template #title>
+              <span class="text-base">Cross-Site Scripting (XSS)</span>
+              <InputSwitch
+                id="secret"
+                v-model="crossSiteScripting"
+              />
+            </template>
+            <template #subtitle>
+              Detect attempts to inject client-side scripts into web pages viewed by your visitors.
+            </template>
+            <template #content>
+              <div class="flex flex-col w-full sm:max-w-xs gap-2 mt-3">
+                <Dropdown
+                  v-model="crossSiteScriptingSensitivity"
+                  :options="sensitivity"
+                  optionLabel="name"
+                  optionValue="value"
+                  class="w-full"
+                />
+              </div>
+            </template>
+          </Card>
+          <Card
+            :pt="{
+              body: { class: `p-4 border rounded-md ${fileUpload ? 'border-orange-500' : 'border-transparent'}` },
+              title: { class: 'flex justify-between  text-base m-0 font-medium' },
+              subtitle: {
+                class: 'text-sm font-normal text-color-secondary m-0 pr-0 md:pr-[2.5rem]'
+              }
+            }"
+          >
+            <template #title>
+              <span class="text-base">File Upload</span>
+              <InputSwitch
+                id="secret"
+                v-model="fileUpload"
+              />
+            </template>
+            <template #subtitle> Detect attempts to upload files. </template>
+            <template #content>
+              <div class="flex flex-col w-full sm:max-w-xs gap-2 mt-3">
+                <Dropdown
+                  v-model="fileUploadSensitivity"
+                  :options="sensitivity"
+                  optionLabel="name"
+                  optionValue="value"
+                  class="w-full"
+                />
+              </div>
+            </template>
+          </Card>
 
-      <Card
-        :pt="{
-          body: { class: 'p-4' },
-          title: { class: 'flex justify-between  text-base m-0 font-medium' },
-          subtitle: {
-            class: 'text-sm font-normal text-color-secondary m-0 pr-0 md:pr-[2.5rem]'
-          }
-        }"
-      >
-        <template #title>
-          <span class="text-base">Evading Tricks</span>
-          <InputSwitch
-            id="secret"
-            v-model="evadingTricks"
-          />
-        </template>
-        <template #subtitle> Prevent the use of encoding tricks to evade protection. </template>
-        <template #content>
-          <div class="flex flex-col w-full sm:max-w-xs gap-2 mt-3">
-            <Dropdown
-              v-model="evadingTricksSensitivity"
-              :options="sensitivity"
-              optionLabel="name"
-              optionValue="value"
-              class="w-full"
-            />
-          </div>
-        </template>
-      </Card>
-      <Card
-        :pt="{
-          body: { class: 'p-4' },
-          title: { class: 'flex justify-between  text-base m-0 font-medium' },
-          subtitle: {
-            class: 'text-sm font-normal text-color-secondary m-0 pr-0 md:pr-[2.5rem]'
-          }
-        }"
-      >
-        <template #title>
-          <span class="text-base">Unwanted Access</span>
-          <InputSwitch
-            id="secret"
-            v-model="unwantedAccess"
-          />
-        </template>
-        <template #subtitle>
-          Detect attempts to access vulnerable or administrative pages and the use of security
-          scanning bots and tools.
-        </template>
-        <template #content>
-          <div class="flex flex-col w-full sm:max-w-xs gap-2 mt-3">
-            <Dropdown
-              v-model="unwantedAccessSensitivity"
-              :options="sensitivity"
-              optionLabel="name"
-              optionValue="value"
-              class="w-full"
-            />
-          </div>
-        </template>
-      </Card>
-      <Card
-        :pt="{
-          body: { class: 'p-4' },
-          title: { class: 'flex justify-between  text-base m-0 font-medium' },
-          subtitle: {
-            class: 'text-sm font-normal text-color-secondary m-0 pr-0 md:pr-[2.5rem]'
-          }
-        }"
-      >
-        <template #title>
-          <span class="text-base">Identified Attack</span>
-          <InputSwitch
-            id="secret"
-            v-model="identifiedAttack"
-          />
-        </template>
-        <template #subtitle>
-          Prevent common vulnerabilities by blocking known attacks in applications and servers.
-        </template>
-        <template #content>
-          <div class="flex flex-col w-full sm:max-w-xs gap-2 mt-3">
-            <Dropdown
-              v-model="identifiedAttackSensitivity"
-              :options="sensitivity"
-              optionLabel="name"
-              optionValue="value"
-              class="w-full"
-            />
-          </div>
-        </template>
-      </Card>
+          <Card
+            :pt="{
+              body: { class: `p-4 border rounded-md ${evadingTricks ? 'border-orange-500' : 'border-transparent'}` },
+              title: { class: 'flex justify-between  text-base m-0 font-medium' },
+              subtitle: {
+                class: 'text-sm font-normal text-color-secondary m-0 pr-0 md:pr-[2.5rem]'
+              }
+            }"
+          >
+            <template #title>
+              <span class="text-base">Evading Tricks</span>
+              <InputSwitch
+                id="secret"
+                v-model="evadingTricks"
+              />
+            </template>
+            <template #subtitle> Prevent the use of encoding tricks to evade protection. </template>
+            <template #content>
+              <div class="flex flex-col w-full sm:max-w-xs gap-2 mt-3">
+                <Dropdown
+                  v-model="evadingTricksSensitivity"
+                  :options="sensitivity"
+                  optionLabel="name"
+                  optionValue="value"
+                  class="w-full"
+                />
+              </div>
+            </template>
+          </Card>
+          <Card
+            :pt="{
+              body: { class: `p-4 border rounded-md ${unwantedAccess ? 'border-orange-500' : 'border-transparent'}` },
+              title: { class: 'flex justify-between  text-base m-0 font-medium' },
+              subtitle: {
+                class: 'text-sm font-normal text-color-secondary m-0 pr-0 md:pr-[2.5rem]'
+              }
+            }"
+          >
+            <template #title>
+              <span class="text-base">Unwanted Access</span>
+              <InputSwitch
+                id="secret"
+                v-model="unwantedAccess"
+              />
+            </template>
+            <template #subtitle>
+              Detect attempts to access vulnerable or administrative pages and the use of security
+              scanning bots and tools.
+            </template>
+            <template #content>
+              <div class="flex flex-col w-full sm:max-w-xs gap-2 mt-3">
+                <Dropdown
+                  v-model="unwantedAccessSensitivity"
+                  :options="sensitivity"
+                  optionLabel="name"
+                  optionValue="value"
+                  class="w-full"
+                />
+              </div>
+            </template>
+          </Card>
+          <Card
+            :pt="{
+              body: { class: `p-4 border rounded-md ${identifiedAttack ? 'border-orange-500' : 'border-transparent'}` },
+              title: { class: 'flex justify-between  text-base m-0 font-medium' },
+              subtitle: {
+                class: 'text-sm font-normal text-color-secondary m-0 pr-0 md:pr-[2.5rem]'
+              }
+            }"
+          >
+            <template #title>
+              <span class="text-base">Identified Attack</span>
+              <InputSwitch
+                id="secret"
+                v-model="identifiedAttack"
+              />
+            </template>
+            <template #subtitle>
+              Prevent common vulnerabilities by blocking known attacks in applications and servers.
+            </template>
+            <template #content>
+              <div class="flex flex-col w-full sm:max-w-xs gap-2 mt-3">
+                <Dropdown
+                  v-model="identifiedAttackSensitivity"
+                  :options="sensitivity"
+                  optionLabel="name"
+                  optionValue="value"
+                  class="w-full"
+                />
+              </div>
+            </template>
+          </Card>
+        </div>
+      </div>
     </template>
   </FormHorizontal>
   <form-horizontal title="Status">
