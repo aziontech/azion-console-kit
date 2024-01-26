d68c55a820f1df55415b24d51ac74e072b21e042
william-tome <william.tome@azion.com>
2024-01-26 12:18:30 -0300

    feat: [metrics] connect dashboardpanel to store

--- src/views/Metrics/blocks/dashboard-panel-block.vue
+++ src/views/Metrics/blocks/dashboard-panel-block.vue
@@ -1,21 +1,41 @@
 <script setup>
+  import { useMetricsStore } from '@/stores/metrics'
   import GraphsCardBlock from '@/templates/graphs-card-block'
+  import { storeToRefs } from 'pinia'
   import SelectButton from 'primevue/selectbutton'
-  import { ref } from 'vue'
+  import { computed } from 'vue'
 
-  const dashboards = ref([])
-  const selectedDashboard = ref(null)
+  const metricsStore = useMetricsStore()
+  const { dashboardBySelectedPage, dashboardCurrent } = storeToRefs(metricsStore)
+  const { setCurrentDashboard } = metricsStore
+
+  const dashboards = computed(() => {
+    return dashboardBySelectedPage.value
+  })
+
+  const selectedDashboard = computed(() => {
+    return dashboardCurrent.value
+  })
+
+  const changeDashboard = (evt) => {
+    setCurrentDashboard(evt.value)
+  }
+
+  const showTabs = computed(() => {
+    return dashboards.value?.length > 1
+  })
 </script>
 
 <template>
   <div class="flex flex-column mt-8 gap-4">
     <SelectButton
       class="w-fit"
-      v-model="selectedDashboard"
+      :modelValue="selectedDashboard"
       :options="dashboards"
       optionLabel="label"
-      dataKey="id"
       aria-labelledby="basic"
+      @change="changeDashboard"
+      v-if="showTabs"
     />
     <div class="grid grid-cols-12 gap-4 m-0">
       <template
