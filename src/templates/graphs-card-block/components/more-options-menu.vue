534a3189f4d903d8005e25c856bbdd63aca9e06c
william-tome <william.tome@azion.com>
2024-02-01 16:55:14 -0300

    feat: [metrics] add menu options validations and commands

--- src/templates/graphs-card-block/components/more-options-menu.vue
+++ src/templates/graphs-card-block/components/more-options-menu.vue
@@ -7,7 +7,7 @@
       size="small"
       aria-controls="overlay_menu"
       aria-haspopup="true"
-      @click="toggle"
+      @click="toggleMenu"
     />
     <PrimeMenu
       ref="optionsMenu"
@@ -15,6 +15,7 @@
       :model="items"
       :popup="true"
       :pt="{ submenuHeader: { class: 'hidden' } }"
+      class="w-fit"
     />
   </div>
 </template>
@@ -22,25 +23,78 @@
 <script setup>
   import PrimeButton from 'primevue/button'
   import PrimeMenu from 'primevue/menu'
-  import { ref } from 'vue'
+  import { computed, ref } from 'vue'
+
+  const props = defineProps({
+    hasMeanLine: Boolean,
+    hasMeanLinePerSeries: Boolean
+  })
+
+  const showMeanLine = ref(true)
+  const showMeanLinePerSeries = ref(true)
 
   const optionsMenu = ref()
-  const items = ref([
-    {
-      items: [
-        {
-          label: 'Copy query',
-          icon: 'pi pi-copy'
-        },
-        {
-          label: 'Export CSV',
-          icon: 'pi pi-download'
-        }
-      ]
-    }
-  ])
 
-  const toggle = (event) => {
-    optionsMenu.value.toggle(event)
+  const items = computed(() => {
+    const options = {
+      playground: {
+        label: 'GraphQL Playground',
+        icon: 'pi pi-external-link',
+        command: () => openPlayground(),
+        show: true
+      },
+      copyQuery: {
+        label: 'Copy query',
+        icon: 'pi pi-copy',
+        command: () => copyQuery(),
+        show: true
+      },
+      exportCsv: {
+        label: 'Export CSV',
+        icon: 'pi pi-download',
+        command: () => exportCsv(),
+        show: true
+      },
+      showMeanLine: {
+        label: `${showMeanLine.value ? 'Show' : 'Hide'} Mean Line`,
+        icon: showMeanLine.value ? 'pi pi-eye-slash' : 'pi pi-eye',
+        command: () => toggleMeanLine(),
+        show: props.hasMeanLine
+      },
+      showMeanLinePerSeries: {
+        label: `${showMeanLinePerSeries.value ? 'Show' : 'Hide'} Mean Line per series`,
+        icon: showMeanLinePerSeries.value ? 'pi pi-eye-slash' : 'pi pi-eye',
+        command: () => toggleMeanLinePerSeries(),
+        show: props.hasMeanLinePerSeries
+      }
+    }
+
+    return Object.values(options).filter((option) => option.show)
+  })
+
+  const toggleMenu = (evt) => {
+    optionsMenu.value.toggle(evt)
+  }
+
+  const openPlayground = () => {
+    return null
+  }
+
+  const copyQuery = () => {
+    return null
+  }
+
+  const exportCsv = () => {
+    return null
+  }
+
+  const toggleMeanLine = () => {
+    showMeanLine.value = !showMeanLine.value
+    return null
+  }
+
+  const toggleMeanLinePerSeries = () => {
+    showMeanLinePerSeries.value = !showMeanLinePerSeries.value
+    return null
   }
 </script>
