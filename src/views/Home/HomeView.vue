68b15bb4cbf773e681e6b2be4fbf4f009cc05ce5
paulosf0 <paulo.ferreira@azion.com>
2024-02-12 15:30:47 -0300

    feat: add new way after code review

--- src/views/Home/HomeView.vue
+++ src/views/Home/HomeView.vue
@@ -266,12 +266,11 @@
         this.$router.push({ name: 'list-edge-applications' })
       },
       navigateToPayment() {
+        let billingLink = 'https://manager.azion.com/billing-subscriptions/payment-methods'
         if (import.meta.env.DEV) {
-          window.open('https://stage-manager.azion.com/billing-subscriptions/bills', '_blank')
+          billingLink = 'https://stage-manager.azion.com/billing-subscriptions/bills'
         }
-        if (import.meta.env.PROD) {
-          window.open('https://manager.azion.com/billing-subscriptions/payment-methods', '_blank')
-        }
+        window.open(billingLink, '_blank')
       },
       navigateToRealTimeMetrics() {
         this.$router.push({ name: 'real-time-metrics' })
