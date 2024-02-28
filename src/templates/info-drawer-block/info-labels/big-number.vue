329dd1fb1cbb0747a76ffe5a61440a4a95c432a1
peterpaulo-azion <peterson.silva@azion.com>
2024-02-28 11:36:56 -0300

    fix: changed big number weight

--- src/templates/info-drawer-block/info-labels/big-number.vue
+++ src/templates/info-drawer-block/info-labels/big-number.vue
@@ -18,13 +18,13 @@
   <div class="flex flex-col sm:w-60 w-44 whitespace-nowrap">
     <label class="flex text-sm gap-1 items-center font-medium">
       {{ props.label }}
-      <span class="pi pi-question-circle text-color-secondary" />
+      <span class="pi pi-question-circle text-color-primary" />
     </label>
     <div class="flex gap-1 items-center">
-      <span class="text-2xl font-medium">
+      <span class="text-2xl font-bold">
         <slot></slot>
       </span>
-      <span class="text-color-secondary">{{ props.sufix }}</span>
+      <span class="text-color">{{ props.sufix }}</span>
     </div>
   </div>
 </template>
