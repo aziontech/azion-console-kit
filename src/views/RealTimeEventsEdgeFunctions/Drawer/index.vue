09af2d3873b35403a58c00d0f56cbcb0e83b7811
Aloisio Bastian <aloisiomiguelb@gmail.com>
2024-03-01 11:37:10 -0300

    fix: remove debug

--- src/views/RealTimeEventsEdgeFunctions/Drawer/index.vue
+++ src/views/RealTimeEventsEdgeFunctions/Drawer/index.vue
@@ -76,7 +76,6 @@
                   </li>
                 </ul>
               </TextInfo>
-              teste
               <BigNumber
                 class="flex-1"
                 label="Edge Function Time"
