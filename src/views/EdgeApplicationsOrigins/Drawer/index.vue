ddbbed9db03c61d4c8ecd8d220340132a666cb27
paulosf0 <paulo.ferreira@azion.com>
2024-03-06 14:23:27 -0300

    feat: add edited origin  event tracker

--- src/views/EdgeApplicationsOrigins/Drawer/index.vue
+++ src/views/EdgeApplicationsOrigins/Drawer/index.vue
@@ -158,6 +158,14 @@
     }
   }
 
+  const handleTrackEdit = () => {
+    tracker.productEdited({
+      productName: 'Origin'
+    }).track()
+
+    emit('onSuccess')
+  }
+
   const closeDrawerEdit = () => {
     showEditOriginDrawer.value = false
   }
@@ -235,7 +243,7 @@
     :loadService="loadService"
     :editService="editService"
     :schema="validationSchema"
-    @onSuccess="emit('onSuccess')"
+    @onSuccess="handleTrackEdit"
     :showBarGoBack="true"
     @onError="closeDrawerEdit"
     title="Edit Origin"
