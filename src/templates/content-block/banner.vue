497795c048a7aa8a9327549c25f4fea810100b59
bruna.andrade <bruna.andrade@azion.com>
2024-03-11 08:41:28 -0300

    chore: remove button

--- src/templates/content-block/banner.vue
+++ src/templates/content-block/banner.vue
@@ -20,14 +20,6 @@
             <b class="text-color">Preview stage.</b>
             This platform is under development. Expect occasional instability or bugs during this
             time.
-            <ButtonPrime
-              class="p-0"
-              label="Report an issue"
-              icon="pi pi-external-link"
-              iconPos="right"
-              link
-              @click="goToCreateIssue"
-            />
           </p>
         </Message>
       </div>
@@ -45,9 +37,4 @@
 
   import Message from 'primevue/message'
   import Avatar from 'primevue/avatar'
-  import ButtonPrime from 'primevue/button'
-
-  const goToCreateIssue = () => {
-    windowOpen('https://github.com/aziontech/azion-platform-kit/issues/new', '_blank')
-  }
 </script>
--- src/templates/content-block/index.vue
+++ src/templates/content-block/index.vue
@@ -19,14 +19,6 @@
           <b class="text-color">Preview stage.</b>
           This platform is under development. Expect occasional instability or bugs during this
           time.
-          <ButtonPrime
-            class="p-0"
-            label="Report an issue"
-            icon="pi pi-external-link"
-            iconPos="right"
-            link
-            @click="goToCreateIssue"
-          />
         </p>
       </Message>
       <div
@@ -54,13 +46,8 @@
 
   import Message from 'primevue/message'
   import Avatar from 'primevue/avatar'
-  import ButtonPrime from 'primevue/button'
 
   import { computed, useSlots } from 'vue'
   const slots = useSlots()
   const hasHeadingSlot = computed(() => !!slots.heading)
-
-  const goToCreateIssue = () => {
-    windowOpen('https://github.com/aziontech/azion-platform-kit/issues/new', '_blank')
-  }
 </script>
