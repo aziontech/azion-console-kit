57c980eae62785600d1b4b1117a4de7b032ba767
lucasmendes21 <lucas.mendes@azion.com>
2024-03-19 17:06:30 -0300

    fix: adjust eslint

--- src/helpers/store-handler.js
+++ src/helpers/store-handler.js
@@ -1,12 +1,12 @@
 window.addEventListener('storage', handleStorageChange)
 
 function handleStorageChange(event) {
-    if (event.key !== '__user_traits') return
+  if (event.key !== '__user_traits') return
 
-    const { newValue, oldValue } = event
-    const isNewValueDifferent = JSON.parse(newValue).client_id !== JSON.parse(oldValue).client_id
+  const { newValue, oldValue } = event
+  const isNewValueDifferent = JSON.parse(newValue).client_id !== JSON.parse(oldValue).client_id
 
-    if (isNewValueDifferent) {
-        window.location.reload()
-    }
+  if (isNewValueDifferent) {
+    window.location.reload()
+  }
 }
