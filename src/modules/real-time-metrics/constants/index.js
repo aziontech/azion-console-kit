4ab52ed3f75fa203e71246ee8444057eacbabe58
william-tome <william.tome@azion.com>
2024-03-20 11:13:12 -0300

    refactor: centralize constants import/export

--- src/modules/real-time-metrics/constants/index.js
+++ src/modules/real-time-metrics/constants/index.js
@@ -2,8 +2,4 @@ import DATE_TIME_INTERVALS from './date-time-interval'
 import GROUP_DASHBOARDS from './dashboards'
 import REPORTS from './reports'
 
-export default {
-  DATE_TIME_INTERVALS,
-  GROUP_DASHBOARDS,
-  REPORTS
-}
+export { DATE_TIME_INTERVALS, GROUP_DASHBOARDS, REPORTS }
