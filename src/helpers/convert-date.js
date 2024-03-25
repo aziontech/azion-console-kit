8e48fb86250046944716b098489b917f53f3eba2
william-tome <william.tome@azion.com>
2024-03-25 11:52:26 -0300

    refactor: move personal tokens date methosd to convert-date in helpers

--- src/helpers/convert-date.js
+++ src/helpers/convert-date.js
@@ -1,3 +1,6 @@
+const MINUTE_IN_MILLISECONDS = 60_000
+const HOUR_IN_MILLISECONDS = 3_600_000
+
 /**
  * Converts a given value to a date string in a specific format.
  *
@@ -17,4 +20,53 @@ const convertValueToDate = (value) => {
   return date.toLocaleString('en-US', options)
 }
 
-export { convertValueToDate }
+/**
+ * Formats a given date to ISO format without milliseconds.
+ *
+ * @param {Date} value - The date to be formatted.
+ * @returns {string} The date in ISO format.
+ */
+const formatToEndOfDayIsoDate = (value) => {
+  value.setUTCHours(23, 59, 59, 999)
+  return value.toISOString().slice(0, -5)
+}
+
+/**
+ * Converts a given UTC offset to decimal format.
+ *
+ * @param {string} offset - The UTC offset to be converted.
+ * @returns {string} The UTC offset in decimal format.
+ */
+const convertOffsetToDecimal = (offset) => {
+  const [offsetSign, hourTens, hourUnits, minuteTens, minuteUnits] = offset
+  const hours = `${hourTens}${hourUnits}`
+  const minutes = `${minuteTens}${minuteUnits}`
+
+  const minuteToDecimalConversion = {
+    15: 25,
+    30: 50,
+    45: 75
+  }
+
+  const decimalMinutes = minutes > 0 ? minuteToDecimalConversion[minutes] : minutes
+
+  return `${offsetSign}${hours}.${decimalMinutes}`
+}
+
+/**
+ * Converts the instance of Date to the local timezone based on the provided UTC offset.
+ *
+ * @param {string} utcOffset - The UTC offset for the desired timezone.
+ * @returns {string} The date converted to the local timezone in ISO format.
+ */
+const convertDateToLocalTimezone = (date, utcOffset) => {
+  const userOffset = convertOffsetToDecimal(utcOffset)
+  const timeZoneOffsetMinutesToMilli = date.getTimezoneOffset() * MINUTE_IN_MILLISECONDS
+  const toUTC = date.getTime() + timeZoneOffsetMinutesToMilli
+  const offsetHoursToMilli = userOffset * HOUR_IN_MILLISECONDS
+
+  const userRealDate = new Date(toUTC + offsetHoursToMilli)
+  return formatToEndOfDayIsoDate(userRealDate)
+}
+
+export { convertValueToDate, convertDateToLocalTimezone }
