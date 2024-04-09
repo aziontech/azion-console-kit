b41251f72c0233fd30befce49fcaae67c206503c
peterpaulo-azion <peterson.silva@azion.com>
2024-04-09 14:50:56 -0300

    test: adjust it message in edit domain service test

--- src/tests/services/domains-services/edit-domain-service.test.js
+++ src/tests/services/domains-services/edit-domain-service.test.js
@@ -61,7 +61,7 @@ describe('DomainsServices', () => {
     expect(feedbackMessage).toBe('Your domain has been edited')
   })
 
-  it('should return a success message on null digital certificate id', async () => {
+  it('should return a success message even when not provided a digital certificate', async () => {
     vi.spyOn(AxiosHttpClientAdapter, 'request').mockResolvedValueOnce({
       statusCode: 200
     })
