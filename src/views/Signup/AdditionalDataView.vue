c29adb6a334e564f756e4b633ae839f38bc4e2fb
william-tome <william.tome@azion.com>
2024-04-10 17:16:54 -0300

    feat: create new additional data form block

--- src/views/Signup/AdditionalDataView.vue
+++ src/views/Signup/AdditionalDataView.vue
@@ -1,23 +1,33 @@
 <template>
-  <PageWithFormBlock
-    :pageTitle="pageTitle"
-    :benefitsList="benefitsList"
-    :formTitle="formTitle"
-    :showSignupBtn="false"
-  >
-    <template #form>
-      <AdditionalDataFormBlock
-        :listAdditionalDataInfoService="listAdditionalDataInfoService"
-        :listCountriesService="listCountriesService"
-        :putAdditionalDataService="putAdditionalDataService"
-      />
-    </template>
-  </PageWithFormBlock>
+  <div>
+    <section class="flex gap-8 px-3 surface-section h-visible-area py-20 overflow-y-auto">
+      <div class="w-4/5 flex flex-col items-center justify-center gap-16 sticky top-20 px-20">
+        <div class="w-full h-64 bg-slate-500/25" />
+        <div class="w-full flex flex-col items-center text-center gap-4">
+          <h1 class="text-3xl font-medium">Personalize Your Experience</h1>
+          <p class="text-xl font-normal text-color-secondary">
+            Find opportunities to explore Azion and improve your projects following a unique
+            journey, aligned with your needs.
+          </p>
+        </div>
+      </div>
+      <div class="w-full flex justify-start items-center min-h-visible-area my-auto">
+        <div
+          class="card w-full surface-border border rounded-md surface-section p-6 xl:p-8 flex flex-col gap-8"
+        >
+          <AdditionalDataFormBlock
+            :listAdditionalDataInfoService="listAdditionalDataInfoService"
+            :listCountriesService="listCountriesService"
+            :putAdditionalDataService="putAdditionalDataService"
+          />
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script setup>
   import AdditionalDataFormBlock from '@/templates/signup-block/additional-data-form-block'
-  import PageWithFormBlock from '@/templates/signup-block/page-with-form-block'
 
   defineProps({
     listAdditionalDataInfoService: {
@@ -33,14 +43,4 @@
       required: true
     }
   })
-
-  const pageTitle = 'Almost there!'
-  const benefitsList = [
-    'Improve content delivery experience',
-    'Write and deploy serverless functions',
-    'Create zero-trust security architectures',
-    '$300 credit to use over 12 months',
-    'Free onboarding session with the Customer Success team'
-  ]
-  const formTitle = 'Additional Data'
 </script>
