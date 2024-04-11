6402583c2dc5bd483b25a50eb12fc72df2b5aac6
william-tome <william.tome@azion.com>
2024-04-11 11:54:10 -0300

    refactor: treat api data

--- src/services/signup-services/list-additional-data-info-service.js
+++ src/services/signup-services/list-additional-data-info-service.js
@@ -1,23 +1,60 @@
-// import { AxiosHttpClientAdapter } from '../axios/AxiosHttpClientAdapter'
+import { AxiosHttpClientAdapter } from '../axios/AxiosHttpClientAdapter'
 import * as Errors from '@/services/axios/errors'
-// import { makeAdditionalDataBaseUrl } from './make-additional-data-base-url'
-import { RESPONSE } from './mock-response'
+import { makeAdditionalDataBaseUrl } from './make-additional-data-base-url'
 
 export const listAdditionalDataInfoService = async () => {
-  // let httpResponse = await AxiosHttpClientAdapter.request({
-  //   url: `${makeAdditionalDataBaseUrl()}`,
-  //   method: 'GET'
-  // })
+  let httpResponse = await AxiosHttpClientAdapter.request({
+    url: `${makeAdditionalDataBaseUrl()}`,
+    method: 'GET'
+  })
 
-  return parseHttpResponse({ body: RESPONSE, statusCode: 200 })
+  return parseHttpResponse(httpResponse)
+}
+
+const addNameFieldAtIndexThree = (response) => {
+  const nameField = {
+    id: 4,
+    key: 'Full Name',
+    required: true,
+    values: []
+  }
+
+  return [...response.slice(0, 3), nameField, ...response.slice(3)]
 }
 
 const addResponseFields = (response) => {
-  const fieldTypes = ['plan', 'role', 'companySize', 'name']
+  response = addNameFieldAtIndexThree(response)
+
+  const fields = [
+    { type: 'plan', required: true, fieldType: 'radio' },
+    { type: 'role', required: true, fieldType: 'radio', show: false },
+    { type: 'companySize', required: true, fieldType: 'radio', show: false },
+    {
+      type: 'fullName',
+      required: true,
+      fieldType: 'text',
+      show: false,
+      values: [{ id: 1, value: '' }]
+    },
+    {
+      type: 'companyWebsite',
+      required: true,
+      fieldType: 'text',
+      show: false,
+      values: [{ id: 1, value: '' }]
+    },
+    {
+      type: 'onboardingSession',
+      required: false,
+      fieldType: 'switch',
+      show: false,
+      values: [{ id: 1, value: true }]
+    }
+  ]
   return response.map((item) => {
     return {
       ...item,
-      type: fieldTypes.shift()
+      ...fields.shift()
     }
   })
 }
@@ -32,7 +69,7 @@ const addResponseFields = (response) => {
 export const parseHttpResponse = (httpResponse) => {
   switch (httpResponse.statusCode) {
     case 200:
-      return addResponseFields(httpResponse.body)
+      return addResponseFields(httpResponse.body.results)
     case 400:
       throw new Errors.InvalidApiRequestError().message
     case 401:
