536fd059711ad6d6e230a9e98347dc7a658b0e0d
william-tome <william.tome@azion.com>
2024-04-12 11:57:18 -0300

    refactor: adjust additional data view

--- src/views/Signup/AdditionalDataView.vue
+++ src/views/Signup/AdditionalDataView.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
-    <section class="flex gap-8 px-3 surface-section h-visible-area py-20 overflow-y-auto">
-      <div class="w-4/5 flex flex-col items-center justify-center gap-16 sticky top-20 px-20">
+    <section
+      class="flex flex-col gap-8 px-3 surface-section py-20 overflow-y-auto md:flex-row md:h-visible-area"
+    >
+      <div
+        class="flex flex-col items-center justify-center gap-16 px-20 md:w-4/5 md:sticky md:top-20"
+      >
         <div class="w-full h-64 bg-slate-500/25" />
         <div class="w-full flex flex-col items-center text-center gap-4">
           <h1 class="text-3xl font-medium">Personalize Your Experience</h1>
@@ -13,28 +17,54 @@
       </div>
       <div class="w-full flex justify-start items-center min-h-visible-area my-auto">
         <div
-          class="card w-full surface-border border rounded-md surface-section p-6 xl:p-8 flex flex-col gap-8"
+          class="card w-full surface-border border rounded-md surface-section p-6 flex flex-col gap-8 xl:p-8"
         >
           <AdditionalDataFormBlock
             :listAdditionalDataInfoService="listAdditionalDataInfoService"
-            :listCountriesService="listCountriesService"
             :putAdditionalDataService="putAdditionalDataService"
+            ref="additionalDataRef"
           />
         </div>
       </div>
     </section>
+    <ActionBar>
+      <template #default>
+        <PrimeButton
+          severity="primary"
+          label="Submit"
+          icon-pos="right"
+          class="max-md:w-full"
+          :icon="showLoading"
+          :disabled="isDisabledSubmit"
+          @click="onSubmit"
+        />
+      </template>
+    </ActionBar>
   </div>
 </template>
 
 <script setup>
   import AdditionalDataFormBlock from '@/templates/signup-block/additional-data-form-block'
+  import ActionBar from '@/templates/action-bar-block'
+  import PrimeButton from 'primevue/button'
+  import { computed, ref } from 'vue'
+
+  const additionalDataRef = ref(null)
+
+  const isDisabledSubmit = computed(() => {
+    return !additionalDataRef.value?.meta?.valid || additionalDataRef.value?.loading
+  })
+
+  const showLoading = computed(() => {
+    return additionalDataRef.value?.loading ? 'pi pi-spin pi-spinner' : ''
+  })
+
+  const onSubmit = () => {
+    additionalDataRef.value?.submitForm()
+  }
 
   defineProps({
     listAdditionalDataInfoService: {
-      type: Function,
-      required: true
-    },
-    listCountriesService: {
       type: Function,
       required: true
     },
