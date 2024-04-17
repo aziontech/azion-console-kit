00d2cb0610c6534f1d6b252474139631b134fd4c
william-tome <william.tome@azion.com>
2024-04-17 14:19:23 -0300

    test: create base patch-fullname-service test

---
+++ src/tests/services/signup-services/patch-fullname-service.test.js
@@ -0,0 +1,70 @@
+import { describe, expect, it, vi } from 'vitest'
+import { AxiosHttpClientAdapter } from '@/services/axios/AxiosHttpClientAdapter'
+import * as Errors from '@/services/axios/errors'
+import { patchFullnameService } from '@/services/signup-services/patch-fullname-service'
+
+const basePayloadMock = {
+  key: 'value'
+}
+
+const makeSut = () => {
+  const sut = patchFullnameService
+
+  return {
+    sut
+  }
+}
+
+describe('SignupServices', () => {
+  it('should call API with correct params', async () => {
+    const requestSpy = vi.spyOn(AxiosHttpClientAdapter, 'request').mockResolvedValueOnce({
+      statusCode: 200,
+      body: basePayloadMock
+    })
+
+    const { sut } = makeSut()
+
+    await sut()
+
+    expect(requestSpy).toHaveBeenCalledWith({
+      url: '',
+      method: 'GET',
+      body: basePayloadMock
+    })
+  })
+
+  it.each([
+    {
+      statusCode: 400,
+      expectedError: new Errors.InvalidApiRequestError().message
+    },
+    {
+      statusCode: 403,
+      expectedError: new Errors.PermissionError().message
+    },
+    {
+      statusCode: 404,
+      expectedError: new Errors.NotFoundError().message
+    },
+    {
+      statusCode: 500,
+      expectedError: new Errors.InternalServerError().message
+    },
+    {
+      statusCode: 'unmappedStatusCode',
+      expectedError: new Errors.UnexpectedError().message
+    }
+  ])(
+    'should throw when request fails with status code $statusCode',
+    async ({ statusCode, expectedError }) => {
+      vi.spyOn(AxiosHttpClientAdapter, 'request').mockResolvedValueOnce({
+        statusCode
+      })
+      const { sut } = makeSut()
+
+      const request = sut()
+
+      expect(request).rejects.toBe(expectedError)
+    }
+  )
+})
