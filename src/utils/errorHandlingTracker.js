18f42a6d51b1075b745b5bed52393540e34ddc6d
paulosf0 <paulo.ferreira@azion.com>
2024-04-19 14:57:21 -0300

    refactor: remove console.log

---
+++ src/utils/errorHandlingTracker.js
@@ -0,0 +1,13 @@
+export const handleTrackerError = (error) => {
+  if(!error.includes(':')) {
+    return {
+      fieldName: '',
+      message: error
+    }
+  }
+
+  const [fieldName, ...restOfStringArr] = error.split(':')
+  const message = restOfStringArr.join(':').trim()
+
+  return { fieldName, message }
+}
