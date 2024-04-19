6002bf5149d80e673b8dc06c6d72bde2b7ed9bf1
peterpaulo-azion <peterson.silva@azion.com>
2024-04-19 17:16:42 -0300

    feat: load solution import-github to get the templateid

--- src/router/routes/import-github-routes/index.js
+++ src/router/routes/import-github-routes/index.js
@@ -1,17 +1,17 @@
 import * as VersionControlSystemService from '@/services/version-control-system-integration-services'
 import * as VulcanService from '@/services/vulcan-services'
-import * as ScriptRunnerService from '@/services/script-runner-service'
 import * as GitHubServices from '@/services/github-services'
 import * as VariablesService from '@/services/variables-services'
 import * as TemplateEngineService from '@/services/template-engine-services'
+import * as MarketplaceService from '@/services/marketplace-services'
 
 /** @type {import('vue-router').RouteRecordRaw} */
 export const importGithubRoutes = {
-  path: '/import-github',
+  path: '/static',
   name: 'import-github',
   children: [
     {
-      path: 'static',
+      path: ':vendor/:solution',
       name: 'github-static',
       component: () => import('@views/ImportGitHub/ImportStaticView.vue'),
       props: {
@@ -21,10 +21,10 @@ export const importGithubRoutes = {
         listRepositoriesService: VersionControlSystemService.listRepositoriesService,
         listVulcanPresetsService: VulcanService.listVulcanPresetsService,
         getModesByPresetService: VulcanService.getModesByPresetService,
-        createScriptRunnerExecutionService: ScriptRunnerService.createScriptRunnerExecutionService,
         frameworkDetectorService: GitHubServices.frameworkDetectorService,
         createVariablesService: VariablesService.createVariablesService,
         instantiateTemplateService: TemplateEngineService.instantiateTemplate,
+        loadSolutionService: MarketplaceService.loadSolutionService,
       },
       meta: {
         breadCrumbs: [
