b2a4c1ab6eed098007a15d2ed2f977495f0951ea
william-tome <william.tome@azion.com>
2024-04-22 19:22:56 -0300

    feat: add job role map to get service

--- src/services/signup-services/list-additional-data-info-service.js
+++ src/services/signup-services/list-additional-data-info-service.js
@@ -11,6 +11,34 @@ export const listAdditionalDataInfoService = async () => {
   return parseHttpResponse(httpResponse)
 }
 
+/**
+ * Updates the `key` property of each field in the `values` array of the second element in the `response` array.
+ * The `key` is obtained from a mapping of the `value` property of each field.
+ *
+ * @param {Array} response - The array containing the response data.
+ * @return {void} This function does not return anything.
+ */
+const addJobFunctionUsedInAccountAPI = ([...response]) => {
+  const jobRolesMap = {
+    'Software Developer': 'software-developer',
+    'DevOps Engineer': 'devops-engineer',
+    'Infrastructure Analyst': 'infrastructure-analyst',
+    'Network Engineer': 'network-engineer',
+    'Security Specialist': 'security-specialist',
+    'Data Engineer': 'data-engineer',
+    'AI/ML Engineer': 'ai-ml-engineer',
+    'IoT Engineer': 'iot-engineer',
+    'Team Lead': 'team-lead',
+    Other: 'other'
+  }
+
+  const jobRolesIdx = 1
+
+  response[jobRolesIdx].values.forEach((field) => {
+    field.key = jobRolesMap[field.value]
+  })
+}
+
 const adapt = ([...response]) => {
   /* 
     this adaptation is required to handle the missing fullname field in the v4 api
@@ -23,6 +51,8 @@ const adapt = ([...response]) => {
   const fullNameField = { id: 999, key: 'Full Name', required: true, show: true }
 
   response.splice(indexToInsertFullName, 0, fullNameField)
+
+  addJobFunctionUsedInAccountAPI(response)
 
   return response
 }
