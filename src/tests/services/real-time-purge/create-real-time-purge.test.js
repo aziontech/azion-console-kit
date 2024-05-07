e86a7bef8722b3540136de64024be0dc22d3ee17
lucasmendes21 <lucas.mendes@azion.com>
2024-05-07 11:33:29 -0300

    feat: add new description to create real time purge

--- src/tests/services/real-time-purge/create-real-time-purge.test.js
+++ src/tests/services/real-time-purge/create-real-time-purge.test.js
@@ -46,7 +46,7 @@ describe('RealTimePurgeServices', () => {
 
     const data = await sut(fixtures.realTimePurgeMock)
 
-    expect(data.feedback).toBe('Successfully created')
+    expect(data.feedback).toBe('The purge is queued for execution. It’ll appear in the history once completed.')
   })
 
   it('Should return an API error for an 400 error status', async () => {
