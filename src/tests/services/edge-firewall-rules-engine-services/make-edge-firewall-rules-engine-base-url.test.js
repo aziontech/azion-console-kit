7ec08ef656032259c654c3357b10cad47407314e
Lucas Mendes <92529166+lucasmendes21@users.noreply.github.com>
2024-05-17 14:41:23 -0300

    [UXE-1946] feat: adding ordering to the edge firewall rules engine table (#1157)
    * feat: add reorder table rules engine
    
    * fix: adjust eslint
    
    * fix: adjust required in pr
    
    * fix: adjust eslint
    
    * fix: add unit test to factory reorder

---
+++ src/tests/services/edge-firewall-rules-engine-services/make-edge-firewall-rules-engine-base-url.test.js
@@ -0,0 +1,22 @@
+import { makeEdgeFirewallRulesEngineReorderBaseUrl } from '@/services/edge-firewall-rules-engine-services/make-edge-firewall-rules-engine-reorder-base-url'
+import { assert, describe, it } from 'vitest'
+
+const makeSut = () => {
+  const sut = makeEdgeFirewallRulesEngineReorderBaseUrl
+
+  return {
+    sut
+  }
+}
+
+describe('EdgeFirewallServices', () => {
+  it('should return the API base url to edge firewall rules engine reorder service', () => {
+    const edgeFirewallId = '123'
+    const { sut } = makeSut()
+    const correctApiUrl = `v4/edge/firewall/${edgeFirewallId}/rules/order`
+
+    const baseUrl = sut(edgeFirewallId)
+
+    assert.strictEqual(baseUrl, correctApiUrl)
+  })
+})
