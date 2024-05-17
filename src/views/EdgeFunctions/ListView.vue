7502ef14839e47e87b1850209e2e73faf8fef24c
aloisio-m-bastian <107002324+aloisio-m-bastian@users.noreply.github.com>
2024-05-17 13:49:21 -0300

    [UXE-2130] feat: add created function tracker (#1156)
    * feat: add created function tracker
    
    * fix: change event name

--- src/views/EdgeFunctions/ListView.vue
+++ src/views/EdgeFunctions/ListView.vue
@@ -11,7 +11,7 @@
         :columns="getColumns"
         pageTitleDelete="edge function"
         addButtonLabel="Edge Function"
-        createPagePath="edge-functions/create"
+        createPagePath="edge-functions/create?origin=list"
         editPagePath="edge-functions/edit"
         @on-load-data="handleLoadData"
         @on-before-go-to-add-page="handleCreateTrackEvent"
