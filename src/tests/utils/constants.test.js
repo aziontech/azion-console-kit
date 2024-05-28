6d28e620097c5afa2ba849af835cff7ac8b23825
paulosf0 <paulo.ferreira@azion.com>
2024-05-28 15:45:48 -0300

    refactor: add refactor after code review

--- src/tests/utils/constants.test.js
+++ src/tests/utils/constants.test.js
@@ -4,19 +4,19 @@ import { describe, it, expect } from 'vitest'
 import * as constants from '@/utils/constants'
 
 describe('Constants Test', () => {
-    it('CDN_MAXIMUM_TTL_MAX_VALUE should be 60', () => {
-        expect(constants.CDN_MAXIMUM_TTL_MAX_VALUE).toBe(60);
-    });
+  it('CDN_MAXIMUM_TTL_MAX_VALUE should be 60', () => {
+    expect(constants.CDN_MAXIMUM_TTL_MAX_VALUE).toBe(60)
+  })
 
-    it('CDN_MAXIMUM_TTL_MIN_VALUE should be 3', () => {
-        expect(constants.CDN_MAXIMUM_TTL_MIN_VALUE).toBe(3);
-    });
+  it('CDN_MAXIMUM_TTL_MIN_VALUE should be 3', () => {
+    expect(constants.CDN_MAXIMUM_TTL_MIN_VALUE).toBe(3)
+  })
 
-    it('TOAST_LIFE should be 30000', () => {
-        expect(constants.TOAST_LIFE).toBe(30000);
-    });
+  it('TOAST_LIFE should be 30000', () => {
+    expect(constants.TOAST_LIFE).toBe(30000)
+  })
 
-    it('TTL_MAX_VALEU_RECORDS should be 604800', () => {
-        expect(constants.TTL_MAX_VALUE_RECORDS).toBe(604800);
-    });
-});
+  it('TTL_MAX_VALEU_RECORDS should be 604800', () => {
+    expect(constants.TTL_MAX_VALUE_RECORDS).toBe(604800)
+  })
+})
