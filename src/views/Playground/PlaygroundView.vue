12de2872b8cf09fe60a7c7637b1dcea9ed4746a4
paulosf0 <paulo.ferreira@azion.com>
2024-06-20 11:53:27 -0300

    refactor: remove inlineMessage of playground page

--- src/views/Playground/PlaygroundView.vue
+++ src/views/Playground/PlaygroundView.vue
@@ -1,29 +1,6 @@
 <template>
   <div class="flex flex-col gap-10 max-w-screen-xl mx-auto">
-    <div class="mt-10 flex flex-col gap-10">
-      <InlineMessage severity="info"
-        >Please save this secret key in a secure and easily accessible location. For security
-        reasons, you won't be able to view it again through your Azion account. If you lose this
-        secret key, you'll need to generate a new one.</InlineMessage
-      >
-      <InlineMessage severity="success"
-        >Please save this secret key in a secure and easily accessible location. For security
-        reasons, you won't be able to view it again through your Azion account. If you lose this
-        secret key, you'll need to generate a new one.</InlineMessage
-      >
-      <InlineMessage severity="error"
-        >Please save this secret key in a secure and easily accessible location. For security
-        reasons, you won't be able to view it again through your Azion account. If you lose this
-        secret key, you'll need to generate a new one.</InlineMessage
-      >
-      <InlineMessage severity="warn"
-        >Please save this secret key in a secure and easily accessible location. For security
-        reasons, you won't be able to view it again through your Azion account. If you lose this
-        secret key, you'll need to generate a new one.</InlineMessage
-      >
-    </div>
-
-    <div class="flex gap-4 h-10">
+    <div class="flex gap-4 h-10 mt-10">
       <PrimeButton
         label="Show toast 5s"
         severity="secondary"
@@ -45,7 +22,6 @@
 
 <script setup>
   import PrimeButton from 'primevue/button'
-  import InlineMessage from 'primevue/inlinemessage'
   import { useToast } from 'primevue/usetoast'
 
   const toast = useToast()
