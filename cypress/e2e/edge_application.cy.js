92717bb1ef61c7d56ddc35ed724b599295981520
Rafael Prado <80526390+andradepradorafael@users.noreply.github.com>
2024-06-25 16:28:55 -0300

    Update cypress/e2e/edge_application.cy.js
    Co-authored-by: William <william.tome@azion.com>

--- cypress/e2e/edge_application.cy.js
+++ cypress/e2e/edge_application.cy.js
@@ -35,57 +35,57 @@ const selectors = {
 }
 
 const generateUniqueName = (prefix) => {
-  const timestamp = Date.now();
-  return `${prefix}-${timestamp}`;
+  const timestamp = Date.now()
+  return `${prefix}-${timestamp}`
 };
 
-const email = Cypress.env('CYPRESS_EMAIL_STAGE');
-const password = Cypress.env('CYPRESS_PASSWORD_STAGE');
-const edgeApplicationName = generateUniqueName('EdgeApp');
-const rulesEngineName = generateUniqueName('RulesEng');
+const email = Cypress.env('CYPRESS_EMAIL_STAGE')
+const password = Cypress.env('CYPRESS_PASSWORD_STAGE')
+const edgeApplicationName = generateUniqueName('EdgeApp')
+const rulesEngineName = generateUniqueName('RulesEng')
 
 describe('Edge Application', () => {
   beforeEach(() => {
     // Login
-    cy.visit('/login');
-    cy.get(selectors.login.emailInput).type(email);
-    cy.get(selectors.login.nextButton).click();
-    cy.get(selectors.login.passwordInput).type(password);
-    cy.get(selectors.login.signInButton).click();
-  });
+    cy.visit('/login')
+    cy.get(selectors.login.emailInput).type(email)
+    cy.get(selectors.login.nextButton).click()
+    cy.get(selectors.login.passwordInput).type(password)
+    cy.get(selectors.login.signInButton).click()
+  })
 
   it('Create and delete an edge application, and create a rule', () => {
     // Navigate to Edge Applications
-    cy.get(selectors.sidebar.menuToggleButton).click();
-    cy.get(selectors.sidebar.edgeApplicationMenuItem).click();
+    cy.get(selectors.sidebar.menuToggleButton).click()
+    cy.get(selectors.sidebar.edgeApplicationMenuItem).click()
 
     // Create an edge application
-    cy.get(selectors.edgeApplication.createButton).click();
-    cy.get(selectors.edgeApplication.nameInput).type(edgeApplicationName);
-    cy.get(selectors.edgeApplication.addressInput).clear();
-    cy.get(selectors.edgeApplication.addressInput).type('httpbingo.org');
-    cy.get(selectors.edgeApplication.saveButton).click();
-    cy.get(selectors.edgeApplication.cancelButton).click();
+    cy.get(selectors.edgeApplication.createButton).click()
+    cy.get(selectors.edgeApplication.nameInput).type(edgeApplicationName)
+    cy.get(selectors.edgeApplication.addressInput).clear()
+    cy.get(selectors.edgeApplication.addressInput).type('httpbingo.org')
+    cy.get(selectors.edgeApplication.saveButton).click()
+    cy.get(selectors.edgeApplication.cancelButton).click()
 
     // Verify the edge application was created
     cy.get(selectors.edgeApplication.searchInput).type(edgeApplicationName)
     cy.get(selectors.edgeApplication.tableRowName).should('be.visible').should('have.text', edgeApplicationName)
 
     // Navigate to Rules Engine Tab
-    cy.get(selectors.edgeApplication.tableRowLastEditor).click();
-    cy.get(selectors.edgeApplication.rulesEngineTab).click();
+    cy.get(selectors.edgeApplication.tableRowLastEditor).click()
+    cy.get(selectors.edgeApplication.rulesEngineTab).click()
 
     // Create a rule
-    cy.get(selectors.edgeApplication.addRuleButton).click();
-    cy.get(selectors.edgeApplication.ruleNameInput).type(rulesEngineName);
-    cy.get(selectors.edgeApplication.criteriaOperatorDropdown).click();
-    cy.get(selectors.edgeApplication.criteriaOperator).click();
-    cy.get(selectors.edgeApplication.criteriaInputValue).clear('/');
-    cy.get(selectors.edgeApplication.criteriaInputValue).type('/');
-    cy.get(selectors.edgeApplication.behaviorsDropdown).click();
-    cy.get(selectors.edgeApplication.behaviors).click();
-    cy.get(selectors.edgeApplication.saveButton).click();
-    cy.get('.flex-column > .text-sm').should('be.visible');
+    cy.get(selectors.edgeApplication.addRuleButton).click()
+    cy.get(selectors.edgeApplication.ruleNameInput).type(rulesEngineName)
+    cy.get(selectors.edgeApplication.criteriaOperatorDropdown).click()
+    cy.get(selectors.edgeApplication.criteriaOperator).click()
+    cy.get(selectors.edgeApplication.criteriaInputValue).clear('/')
+    cy.get(selectors.edgeApplication.criteriaInputValue).type('/')
+    cy.get(selectors.edgeApplication.behaviorsDropdown).click()
+    cy.get(selectors.edgeApplication.behaviors).click()
+    cy.get(selectors.edgeApplication.saveButton).click()
+    cy.get('.flex-column > .text-sm').should('be.visible')
 
     // Verify the rule was created
     cy.get(selectors.edgeApplication.searchInput).type(rulesEngineName)
