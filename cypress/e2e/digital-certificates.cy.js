da21fab6462cc8e4502c7e74434ee0ca6fe2b95d
peterpaulo-azion <peterson.silva@azion.com>
2024-06-26 09:35:43 -0300

    test: e2e digital certificate crud adjust by toggle menu fix

--- cypress/e2e/digital-certificates.cy.js
+++ cypress/e2e/digital-certificates.cy.js
@@ -9,7 +9,7 @@ describe('Digital Certificates spec', () => {
     // Act
     cy.getByTestId('digital_certificate__name_field').clear()
     cy.getByTestId('digital_certificate__name_field').type('EntityName')
-    cy.getByTestId('Save_button').click()
+    cy.getByTestId('form-actions-submit-button').click()
 
     // Assert
     cy.get(':nth-child(2) > .p-toast-message-content > .flex-column > .text-sm').should(
@@ -18,8 +18,8 @@ describe('Digital Certificates spec', () => {
     )
     cy.getByTestId('page_title_Edit Digital Certificate').should('have.text', 'Edit Digital Certificate')
     cy.get(':nth-child(3) > .p-menuitem-link').click()
-    cy.getByTestId('search_input').clear()
-    cy.getByTestId('search_input').type('EntityName')
+    cy.getByTestId('data-table-search-input').clear()
+    cy.getByTestId('data-table-search-input').type('EntityName')
     cy.getByTestId('list-table-block__column__name__row').should('have.text', 'EntityName')
     cy.get('[data-testid="list-table-block__column__status__row"] > .p-tag-value').should(
       'have.text',
@@ -27,11 +27,8 @@ describe('Digital Certificates spec', () => {
     )
 
     // Cleanup
-    cy.getByTestId('search_input').clear()
-    cy.get('.p-inputwrapper-filled.p-paginator-rpp-options > .p-dropdown-trigger').click()
-    cy.get('.p-dropdown-item [aria-posinset="4"]').click()
-    cy.get(':nth-child(18) > .p-frozen-column > .flex > .p-button').click()
-    cy.get('.p-menuitem-content > .p-menuitem-link').click()
+    cy.get('[data-testid="data-table-actions-column-body-actions-menu-button"]').click();
+    cy.get('.p-menuitem-content > .p-menuitem-link').click();
     cy.get('#confirm-input').clear()
     cy.get('#confirm-input').type('delete{enter}')
     cy.get(':nth-child(3) > .p-toast-message-content > .flex-column > .flex > .text-color').should(
