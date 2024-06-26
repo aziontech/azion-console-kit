78fd9135dc5f34f2ea3795808a2cf164631471de
Matheus Sunderhus <matheus.sunderhus@azion.com>
2024-06-26 19:57:55 -0300

    refactor: mode chat header to its own file

---
+++ src/modules/ask-azion/ask-azion-header.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex w-full justify-between pl-6 md:pr-8 pr-3 py-3 border-b surface-border">
+    <h3 class="text-color text-lg font-medium">
+      Ask Azion
+      <PrimeTag
+        class="ml-2"
+        value="experimental"
+      />
+    </h3>
+
+    <slot name="header-actions"> </slot>
+  </div>
+</template>
+
+<script setup>
+  import PrimeTag from 'primevue/tag'
+
+  defineOptions({
+    name: 'ask-azion-header'
+  })
+</script>
