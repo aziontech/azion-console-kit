ac095687dff12e1019f18a0cd765e2d9cc7ff6cb
Alisson Marcelo <alisson.marcelo@azion.com>
2024-06-29 13:10:47 -0300

    [QA-2034] test: add WAF Rules E2E test

--- cypress/support/selectors.js
+++ cypress/support/selectors.js
@@ -116,6 +116,19 @@ const selectors = {
       confirmPasswordError: '[data-testid="security__confirm-password__error-text"]',
       twoFactorToggle: '.p-inputswitch-slider',
     },
+    wafs: {
+      createButton: '[data-testid="create_WAF Rule_button"] > .p-button-label',
+      nameInput: '[data-testid="waf-rules-form__name-field__input"]',
+      saveButton: '[data-testid="form-actions-submit-button"]',
+      cancelButton: '[data-testid="form-actions-cancel-button"]',
+      searchInput: '[data-testid="data-table-search-input"]',
+      nameRow: '[data-testid="list-table-block__column__name__row"]',
+      threatTypesRow: '[data-testid="list-table-block__column__threatTypes__row"] > :nth-child(1)',
+      actionButton: '[data-testid="data-table-actions-column-body-actions-menu-button"] > .p-button-icon',
+      deleteButton: '.p-menuitem-content > .p-menuitem-link > .p-menuitem-text',
+      deleteInput: '[data-testid="delete-dialog-confirmation-input-field"]',
+      confirmDeleteButton: '[data-testid="delete-dialog-footer-delete-button"] > .p-button-label',
+    }
   };
   
   export default selectors;
