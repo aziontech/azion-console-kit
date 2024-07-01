4554a928a79810dea1bf6d923d113048b1227add
Alisson Marcelo <81633584+alisson-marcelo-azion@users.noreply.github.com>
2024-07-01 17:21:11 -0300

    [QA-2039] test: add functions E2E test (#1340)

--- cypress/support/selectors.js
+++ cypress/support/selectors.js
@@ -158,6 +158,16 @@ const selectors = {
       deleteInput: '[data-testid="delete-dialog-confirmation-input-field"]',
       confirmDeleteButton: '[data-testid="delete-dialog-footer-delete-button"] > .p-button-label',
     },
+    functions: {
+      createButton: '[data-testid="create_Edge Function_button"] > .p-button-label',
+      nameInput: '[data-testid="field-text__input"]',
+      saveButton: '[data-testid="form-actions-submit-button"] > .p-button-label',
+      cancelButton: '[data-testid="form-actions-cancel-button"] > .p-button-label',
+      searchInput: '[data-testid="data-table-search-input"]',
+      nameRow: '[data-testid="list-table-block__column__name__row"] > p',
+      languageRow: '[data-testid="list-table-block__column__language__row"] > p',
+      initiatorTypeRow: '[data-testid="list-table-block__column__initiatorType__row"]',
+    },
     edgeFirewall: {
       createButton: '[data-testid="create_Edge Firewall_button"] > .p-button-label',
       nameInput: '[data-testid="edge-firewall-form__name-field__input"]',
