42d55452c5089dd750da289a814e4b433976090f
Rafael Prado <80526390+andradepradorafael@users.noreply.github.com>
2024-07-01 11:02:19 -0300

    fix: update variables.cy.js

--- cypress/e2e/variables.cy.js
+++ cypress/e2e/variables.cy.js
@@ -2,6 +2,7 @@ import generateUniqueName from '../support/utils'
 import selectors from '../support/selectors'
 
 const variableKey = generateUniqueName('VARIABLE')
+const variableValue = generateUniqueName('myvalue')
 
 describe('Variables spec', () => {
   beforeEach(() => {
@@ -12,19 +13,16 @@ describe('Variables spec', () => {
   it('Create a variable', function() {
     // Act
     cy.get(selectors.variables.createButton).click()
-    cy.get(selectors.variables.keyInput).clear('')
     cy.get(selectors.variables.keyInput).type(variableKey)
-    cy.get(selectors.variables.valueInput).clear()
-    cy.get(selectors.variables.valueInput).type('myvalue')
+    cy.get(selectors.variables.valueInput).type(variableValue)
     cy.get(selectors.variables.saveButton).click()
     cy.verifyToast('success', 'Your variable has been created')
     cy.get(selectors.variables.cancelButton).click()
 
     // Assert
-    cy.get(selectors.variables.searchInput).clear()
-    cy.get(selectors.variables.searchInput).type(`${variableKey}{enter}`)
+    cy.get(selectors.variables.searchInput).type(`${variableKey}`)
     cy.get(selectors.variables.keyRow).should('have.text', variableKey)
-    cy.get(selectors.variables.valueRow).should('have.text', 'myvalue')
+    cy.get(selectors.variables.valueRow).should('have.text', variableValue)
   })
   afterEach(() => {
     // Delete the variable
