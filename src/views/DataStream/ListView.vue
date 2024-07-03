3bd8beff57bde177db9a89b8c16d36649d2559de
lucasmendes21 <lucas.mendes@azion.com>
2024-07-03 14:19:10 -0300

    refactor: add action button to data stream

--- src/views/DataStream/ListView.vue
+++ src/views/DataStream/ListView.vue
@@ -1,77 +1,3 @@
-<script>
-  import Illustration from '@/assets/svg/illustration-layers.vue'
-  import ContentBlock from '@/templates/content-block'
-  import EmptyResultsBlock from '@/templates/empty-results-block'
-  import ListTableBlock from '@/templates/list-table-block'
-  import { columnBuilder } from '@/templates/list-table-block/columns/column-builder'
-  import PageHeadingBlock from '@/templates/page-heading-block'
-
-  export default {
-    name: 'data-stream-view',
-    components: {
-      ListTableBlock,
-      EmptyResultsBlock,
-      Illustration,
-      ContentBlock,
-      PageHeadingBlock
-    },
-    props: {
-      listDataStreamService: {
-        required: true,
-        type: Function
-      },
-      deleteDataStreamService: {
-        required: true,
-        type: Function
-      },
-      documentationService: {
-        required: true,
-        type: Function
-      }
-    },
-    data: () => ({
-      hasContentToList: true
-    }),
-    computed: {
-      getColumns() {
-        return [
-          {
-            field: 'name',
-            header: 'Name'
-          },
-          {
-            field: 'dataSource',
-            header: 'Source'
-          },
-          {
-            field: 'templateName',
-            header: 'Template'
-          },
-          {
-            field: 'endpointType',
-            header: 'Connector'
-          },
-          {
-            field: 'active',
-            header: 'Status',
-            type: 'component',
-            component: (columnData) =>
-              columnBuilder({
-                data: columnData,
-                columnAppearance: 'tag'
-              })
-          }
-        ]
-      }
-    },
-    methods: {
-      handleLoadData(event) {
-        this.hasContentToList = event
-      }
-    }
-  }
-</script>
-
 <template>
   <ContentBlock>
     <template #heading>
@@ -83,12 +9,11 @@
         addButtonLabel="Stream"
         createPagePath="/data-stream/create"
         editPagePath="/data-stream/edit"
-        pageTitleDelete="stream"
         :listService="listDataStreamService"
-        :deleteService="deleteDataStreamService"
         :columns="getColumns"
         @on-load-data="handleLoadData"
         emptyListMessage="No streams found."
+        :actions="actions"
       ></ListTableBlock>
       <EmptyResultsBlock
         v-else
@@ -105,3 +30,75 @@
     </template>
   </ContentBlock>
 </template>
+
+<script setup>
+  import { computed, ref } from 'vue'
+  import Illustration from '@/assets/svg/illustration-layers.vue'
+  import ContentBlock from '@/templates/content-block'
+  import EmptyResultsBlock from '@/templates/empty-results-block'
+  import ListTableBlock from '@/templates/list-table-block/action-column.vue'
+  import { columnBuilder } from '@/templates/list-table-block/columns/column-builder'
+  import PageHeadingBlock from '@/templates/page-heading-block'
+
+  defineOptions({ name: 'data-stream-view' })
+
+  const props = defineProps({
+    listDataStreamService: {
+      required: true,
+      type: Function
+    },
+    deleteDataStreamService: {
+      required: true,
+      type: Function
+    },
+    documentationService: {
+      required: true,
+      type: Function
+    }
+  })
+
+  const hasContentToList = ref(true)
+  const actions = [
+    {
+      type: 'delete',
+      title: 'stream',
+      icon: 'pi pi-trash',
+      service: props.deleteDataStreamService
+    }
+  ]
+
+  const handleLoadData = (event) => {
+    hasContentToList.value = event
+  }
+
+  const getColumns = computed(() => {
+    return [
+      {
+        field: 'name',
+        header: 'Name'
+      },
+      {
+        field: 'dataSource',
+        header: 'Source'
+      },
+      {
+        field: 'templateName',
+        header: 'Template'
+      },
+      {
+        field: 'endpointType',
+        header: 'Connector'
+      },
+      {
+        field: 'active',
+        header: 'Status',
+        type: 'component',
+        component: (columnData) =>
+          columnBuilder({
+            data: columnData,
+            columnAppearance: 'tag'
+          })
+      }
+    ]
+  })
+</script>
