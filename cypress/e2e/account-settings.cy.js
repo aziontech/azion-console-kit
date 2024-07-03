adfb106806d09df0e4052691f90a2d2d800213ac
Rafael Prado <80526390+andradepradorafael@users.noreply.github.com>
2024-07-03 14:14:06 -0300

    fix: account-settings test

--- cypress/e2e/account-settings.cy.js
+++ cypress/e2e/account-settings.cy.js
@@ -1,5 +1,9 @@
 import selectors from '../support/selectors'
 
+const companyName = 'Company Teste'
+const companyId = '00.000.000/0001-00'
+const postalCode = '14055-010'
+
 describe('Account Settings spec', () => {
   beforeEach(() => {
     cy.login()
@@ -8,14 +12,13 @@ describe('Account Settings spec', () => {
 
   it('should update account settings successfully', () => {
     // Arrange
-    cy.get(selectors.accountSettings.accountName).should('have.value', Cypress.env('CYPRESS_USERNAME_STAGE'))
     cy.get(selectors.accountSettings.companyName).clear()
-    cy.get(selectors.accountSettings.companyName).type('Company Teste')
+    cy.get(selectors.accountSettings.companyName).type(companyName)
     cy.get(selectors.accountSettings.companyId).clear()
-    cy.get(selectors.accountSettings.companyId).type('00.000.000/0001-00')
+    cy.get(selectors.accountSettings.companyId).type(companyId)
     cy.get(selectors.accountSettings.postalCode).clear()
     cy.get(selectors.accountSettings.postalCodeError).should('have.text', 'Postal Code is a required field')
-    cy.get(selectors.accountSettings.postalCode).type('14055-010')
+    cy.get(selectors.accountSettings.postalCode).type(postalCode)
 
     // Act
     cy.get(selectors.accountSettings.submitButton).click()
@@ -23,8 +26,8 @@ describe('Account Settings spec', () => {
     // Assert
     cy.verifyToast('success', 'Your account settings have been updated')
     cy.openItemThroughMenuAccount('Account Settings')
-    cy.get(selectors.accountSettings.companyName).should('have.value', 'Company Teste')
-    cy.get(selectors.accountSettings.companyId).should('have.value', '00.000.000/0001-00')
-    cy.get(selectors.accountSettings.postalCode).should('have.value', '14055-010')
+    cy.get(selectors.accountSettings.companyName).should('have.value', companyName)
+    cy.get(selectors.accountSettings.companyId).should('have.value', companyId)
+    cy.get(selectors.accountSettings.postalCode).should('have.value', postalCode)
   })
 })
