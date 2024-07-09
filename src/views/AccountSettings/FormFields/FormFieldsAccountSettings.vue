dff1324a0c7234fecbc3dff12f852abff06b4618
William <william.tome@azion.com>
2024-07-09 14:11:53 -0300

    [UXE-3947] test: create e2e/e2mock for missing account settings flow (#1439)
    * chore: add selectors for account settings context
    
    * test: create e2e for updating address in account settings
    
    * chore: add selectors for account settings context
    
    * test: create e2e for updating billing email in account settings
    
    * test: add cities api fixtures
    
    * test: adjust your settings e2mock to handle mockedcities api
    
    * chore: add selectors for account settings context
    
    * chore: create fixture file for account setting api call
    
    * test: create e2mock for enforce mfa in account settings
    
    * test: create e2mock for enable social login in account settings

--- src/views/AccountSettings/FormFields/FormFieldsAccountSettings.vue
+++ src/views/AccountSettings/FormFields/FormFieldsAccountSettings.vue
@@ -220,6 +220,7 @@
           placeholder="example@email.com;holder@email.com"
           name="billingEmails"
           :value="billingEmails"
+          data-testid="account-settings__billing-emails"
         >
           <template #description>
             Billing information will be forwarded to all emails listed in this field. Separate each
@@ -260,6 +261,7 @@
             filter
             appendTo="self"
             description="Account owner's country."
+            data-testid="account-settings__country"
           />
         </div>
       </div>
@@ -278,6 +280,7 @@
             appendTo="self"
             :disabled="hasNoCountryListOrNotSelected"
             description="Account owner's state or region."
+            data-testid="account-settings__region"
           />
         </div>
         <div class="flex flex-col w-full gap-2 sm:max-w-lg">
@@ -294,6 +297,7 @@
             appendTo="self"
             :disabled="hasNoRegionListOrNotSelected"
             description="Account owner's city."
+            data-testid="account-settings__city"
           />
         </div>
       </div>
@@ -306,6 +310,7 @@
             placeholder="123 Example Ave."
             :value="address"
             description="Account owner's street address."
+            data-testid="account-settings__address"
           />
         </div>
         <div class="flex flex-col w-full gap-2 sm:max-w-lg">
@@ -315,6 +320,7 @@
             placeholder="1st floor"
             :value="complement"
             description="Additional information for the address."
+            data-testid="account-settings__complement"
           />
         </div>
       </div>
@@ -330,6 +336,7 @@
           :isCard="false"
           input-class="w-full"
           :options="switchOptions"
+          data-testid="account-settings__login-settings"
         />
       </div>
     </template>
