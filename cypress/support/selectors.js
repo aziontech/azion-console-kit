15dd1a315952e07e7c53aed3ea10497f4fa5a3a8
Aloisio Bertolo Bastian <aloisiomiguelb@gmail.com>
2024-07-10 13:00:44 -0300

    test: add ip/cidr creation test

--- cypress/support/selectors.js
+++ cypress/support/selectors.js
@@ -117,6 +117,7 @@ const selectors = {
     nameInput: '[data-testid="network-list-form__name__input"]',
     typeDropdown: '[data-testid="network-list-form__type__dropdown"]',
     asnTextarea: '[data-testid="network-list-form__asn-list__textarea"]',
+    ipcidrTextarea: '[data-testid="network-list-form__ipcidr-list__textarea"]',
     saveButton: '[data-testid="form-actions-submit-button"]',
     cancelButton: '[data-testid="form-actions-cancel-button"]',
     searchInput: '[data-testid="data-table-search-input"]',
