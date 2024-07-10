7a09c8621189b0b9e94f09f6309b24f9fafb8b82
Rafael <80526390+rafael-qazion@users.noreply.github.com>
2024-07-10 08:28:25 -0300

    [QA-2045] feat: enhance test execution with tag filtering and workflow configurations (#1438)

--- cypress.config.stage.js
+++ cypress.config.stage.js
@@ -2,27 +2,20 @@
 import { defineConfig } from 'cypress';
 import baseConfig from './cypress.config';
 
-const e2eOverride = {
-  hosts: {
-    "console.azion.com": "179.191.172.120"
-  },
-};
-
 const envOverride = {
   // TODO: remove this WORKAROUND for https://github.com/cypress-io/cypress/issues/20647,
-  baseUrl: 'http://console.azion.com',
-  CYPRESS_EMAIL: process.env.PREVIEW_PROD_CYPRESS_EMAIL,
-  CYPRESS_PASSWORD: process.env.PREVIEW_PROD_CYPRESS_PASSWORD,
+  baseUrl: 'http://stage-console.azion.com',
+  CYPRESS_EMAIL: process.env.STAGE_CYPRESS_EMAIL,
+  CYPRESS_PASSWORD: process.env.STAGE_CYPRESS_PASSWORD,
 };
 
 export default defineConfig({
   ...baseConfig,
   e2e: {
     ...baseConfig.e2e,
-    ...e2eOverride,
   },
   env: {
-      ...baseConfig.env,
+    ...baseConfig.env,
     ...envOverride,
   },
 });
