6892b0c85046d4ade1a78b2a9b13a5fa6ad77653
HerbertJulio <herbert.julio@azion.com>
2024-07-10 14:40:48 -0300

    chore: update environment variables for production and stage deployments

--- src/helpers/get-environment.js
+++ src/helpers/get-environment.js
@@ -5,5 +5,5 @@ export const getEnvironment = () => {
   const environment = import.meta.env
   // eslint-disable-next-line no-console
   console.log('🚀 ~ getEnvironment ~ environment:', environment);
-  return environment.MODE
+  return environment.VITE_ENVIRONMENT
 }
