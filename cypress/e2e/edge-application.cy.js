48c9e886a8ad547a7a7871f3a4c8be94e37a37ef
Alisson Marcelo <alisson.marcelo@azion.com>
2024-07-12 08:58:48 -0300

    [NO-ISSUE] test: remove cypress delay when typing

--- cypress/e2e/edge-application.cy.js
+++ cypress/e2e/edge-application.cy.js
@@ -14,14 +14,14 @@ describe('Edge Application', { tags: ['run',] }, () => {
   it('Create and delete an edge application, and create a rule', () => {
     // Create an edge application
     cy.get(selectors.edgeApplication.createButton).click()
-    cy.get(selectors.edgeApplication.nameInput).type(edgeApplicationName)
+    cy.get(selectors.edgeApplication.nameInput).type(edgeApplicationName, {delay: 0})
     cy.get(selectors.edgeApplication.addressInput).clear()
-    cy.get(selectors.edgeApplication.addressInput).type('httpbingo.org')
+    cy.get(selectors.edgeApplication.addressInput).type('httpbingo.org', {delay: 0})
     cy.get(selectors.edgeApplication.saveButton).click()
     cy.get(selectors.edgeApplication.cancelButton).click()
 
     // Verify the edge application was created
-    cy.get(selectors.edgeApplication.searchInput).type(edgeApplicationName)
+    cy.get(selectors.edgeApplication.searchInput).type(edgeApplicationName, {delay: 0})
     cy.get(selectors.edgeApplication.tableRowName)
       .should('be.visible')
       .should('have.text', edgeApplicationName)
@@ -32,18 +32,18 @@ describe('Edge Application', { tags: ['run',] }, () => {
 
     // Create a rule
     cy.get(selectors.edgeApplication.addRuleButton).click()
-    cy.get(selectors.edgeApplication.ruleNameInput).type(rulesEngineName)
+    cy.get(selectors.edgeApplication.ruleNameInput).type(rulesEngineName, {delay: 0})
     cy.get(selectors.edgeApplication.criteriaOperatorDropdown).click()
     cy.get(selectors.edgeApplication.criteriaOperator).click()
     cy.get(selectors.edgeApplication.criteriaInputValue).clear('/')
-    cy.get(selectors.edgeApplication.criteriaInputValue).type('/')
+    cy.get(selectors.edgeApplication.criteriaInputValue).type('/', {delay: 0})
     cy.get(selectors.edgeApplication.behaviorsDropdown).click()
     cy.get(selectors.edgeApplication.behaviors).click()
     cy.get(selectors.edgeApplication.saveButton).click()
     cy.get('.flex-column > .text-sm').should('be.visible')
 
     // Verify the rule was created
-    cy.get(selectors.edgeApplication.searchInput).type(rulesEngineName)
+    cy.get(selectors.edgeApplication.searchInput).type(rulesEngineName, {delay: 0})
     cy.get(selectors.edgeApplication.ruleTable)
       .should('be.visible')
       .should('have.text', rulesEngineName)
--- cypress/e2e/functions.cy.js
+++ cypress/e2e/functions.cy.js
@@ -13,14 +13,14 @@ describe('Functions spec', { tags: ['run',] }, () => {
     // Act
     cy.get(selectors.functions.createButton).click()
     cy.get(selectors.functions.nameInput).clear()
-    cy.get(selectors.functions.nameInput).type(functionName)
+    cy.get(selectors.functions.nameInput).type(functionName, {delay: 0})
     cy.get(selectors.functions.saveButton).click()
     cy.verifyToast('success', 'Your edge function has been created')
     cy.get(selectors.functions.cancelButton).click()
 
     // Assert
     cy.get(selectors.functions.searchInput).clear()
-    cy.get(selectors.functions.searchInput).type(functionName)
+    cy.get(selectors.functions.searchInput).type(functionName, {delay: 0})
     cy.get(selectors.functions.nameRow).should('have.text', functionName)
     cy.get(selectors.functions.languageRow).should('have.text', 'JavaScript')
     cy.get(selectors.functions.initiatorTypeRow).should('have.text', 'edge_application')
