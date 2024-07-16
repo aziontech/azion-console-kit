83d2f5cce7f3d84e62afd9cee98456ae6ff6c896
HerbertJulio <herbert.julio@azion.com>
2024-07-16 15:57:23 -0300

    feat: add loadPaymentMethodDefaultService to billing routes

---
+++ src/services/billing-services/load-payment-method-default-service.js
@@ -0,0 +1,37 @@
+import { AxiosHttpClientAdapter, parseHttpResponse } from '../axios/AxiosHttpClientAdapter'
+import { makePaymentBaseUrl } from './make-payment-base-url'
+
+export const loadPaymentMethodDefaultService = async () => {
+  let httpResponse = await AxiosHttpClientAdapter.request({
+    url: `${makePaymentBaseUrl()}/credit_cards?is_default=True`,
+    method: 'GET'
+  })
+
+  httpResponse = adapt(httpResponse)
+
+  return parseHttpResponse(httpResponse)
+}
+
+const adapt = (httpResponse) => {
+  const { body, statusCode } = httpResponse
+  const results = body?.results || []
+
+  const responseCard = results.find((card) => card.is_default) || {}
+
+  const parseBilling = responseCard.id
+    ? {
+        id: responseCard.id,
+        cardData: {
+          cardNumber: `Ending in ${responseCard.card_last_4_digits}`,
+          cardNumberValue: responseCard.card_last_4_digits,
+          cardBrand: responseCard.card_brand?.toLowerCase(),
+          isDefault: responseCard.is_default
+        }
+      }
+    : {}
+
+  return {
+    body: parseBilling,
+    statusCode
+  }
+}
