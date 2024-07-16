547841e4fc490a5f4242cfd7b5bc18aad94072e7
aloisio-m-bastian <107002324+aloisio-m-bastian@users.noreply.github.com>
2024-07-16 11:14:55 -0300

    [UXE-3790] test: add e2mock test for activity history list (#1481)
    * test: add e2mock test for activity history list
    
    * test: move fixture to file
    
    * test: assert static text
    
    * test: assert static text

---
+++ cypress/e2mock/activity-history.cy.js
@@ -0,0 +1,22 @@
+import selectors from '../support/selectors'
+
+
+describe('Activity History Spec', () => {
+  beforeEach(() => {
+    cy.login()
+    cy.intercept('POST', '/api/v3/events/graphql', { fixture: 'activity-history.json' }).as('activityHistoryApi')
+    cy.openProduct('Activity History')
+  })
+
+  it('should registry when a network list is created', function () {
+    //act
+    cy.get(selectors.activityHistory.searchInput).clear()
+    cy.get(selectors.activityHistory.searchInput).type('Network List NetworkListTest was created')
+
+    //assert
+    cy.get(selectors.activityHistory.timeLineEvent).should(
+      'have.text',
+      'Network List NetworkListTest was created'
+    )
+  })
+})
