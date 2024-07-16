eae16276511698ab978445bef66c63912aab4293
william-tome <william.tome@azion.com>
2024-07-16 08:30:42 -0300

    test: create e2e for wafs/allowed rule with conditional query string zone

--- cypress/e2e/waf.cy.js
+++ cypress/e2e/waf.cy.js
@@ -149,6 +149,56 @@ describe('WAF spec', () => {
     cy.get(selectors.list.searchInput).type(wafName)
   })
 
+  it('should create an allowed rule with "conditional query string" zone', () => {
+    // Arrange
+    const fixtures = {
+      ruleId: '1 - Validation of protocol compliance: weird request, unable to parse',
+      description: 'Description',
+      path: '/',
+      matchZone: 'Conditional Query String (Value)',
+      status: 'Active'
+    }
+
+    // Create WAF
+    cy.get(selectors.wafs.createButton).click()
+    cy.get(selectors.wafs.nameInput).clear()
+    cy.get(selectors.wafs.nameInput).type(wafName)
+    cy.get(selectors.form.actionsSubmitButton).click()
+    cy.verifyToast('success', 'Your waf rule has been created')
+    cy.wait('@saveRuleset')
+
+    // Create Allowed Rule
+    cy.get(selectors.wafs.allowedRulesTab).click()
+    cy.get(selectors.wafs.allowedRules.createButton).click()
+
+    // Act
+    cy.get(selectors.wafs.allowedRules.descriptionField).type(fixtures.description)
+    cy.get(selectors.wafs.allowedRules.pathField).type(fixtures.path)
+    cy.get(selectors.wafs.allowedRules.ruleIdDropdown).click()
+    cy.get(selectors.wafs.allowedRules.ruleIdOption(1)).click()
+
+    cy.get(selectors.wafs.allowedRules.matchZoneDropdown(0)).click()
+    cy.get(selectors.wafs.allowedRules.matchZoneOption(0)).click()
+
+    cy.get(selectors.form.actionsSubmitButton).click()
+
+    // Assert
+    cy.verifyToast('success', 'Your waf rule allowed has been created')
+
+    cy.get(selectors.list.searchInput).type(fixtures.ruleId)
+    cy.get(selectors.wafs.seeMore('ruleId')).click()
+
+    cy.get(selectors.wafs.listRow('ruleId')).should('contain.text', fixtures.ruleId)
+    cy.get(selectors.wafs.listRow('description')).should('have.text', fixtures.description)
+    cy.get(selectors.wafs.listRow('path')).should('have.text', fixtures.path)
+    cy.get(selectors.wafs.listRow('matchZones')).should('have.text', fixtures.matchZone)
+    cy.get(selectors.wafs.listRow('status')).should('have.text', fixtures.status)
+    cy.get(selectors.wafs.listRow('lastModified')).should('not.be.empty')
+
+    cy.get(selectors.wafs.breadcumbToList).click()
+    cy.get(selectors.list.searchInput).type(wafName)
+  })
+
   afterEach(() => {
     // Delete the waf
     cy.deleteEntityFromLoadedList().then(() => {
