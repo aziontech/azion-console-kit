eeebd4af1994916213e8c267799d3c208b7d4880
William <william.tome@azion.com>
2024-07-16 10:32:16 -0300

    [UXE-3949] test: create waf e2e with different match zones (#1482)
    * test: add selectors related to wafs/allowed rules
    
    * test: add selectors related to wafs/allowed rules
    
    * test: create e2e for wafs/allowed rule with path zone
    
    * test: create e2e for wafs/allowed rule with conditional query string zone
    
    * test: create e2e for wafs/allowed rule with conditional request body zone
    
    * test: create e2e for wafs/allowed rule with conditional request header zone
    
    * test: create e2e for wafs/allowed rule with file name zone
    
    * test: create e2e for wafs/allowed rule with raw body zone

--- cypress/support/selectors.js
+++ cypress/support/selectors.js
@@ -164,7 +164,33 @@ const selectors = {
     breadcumbToList: '.p-breadcrumb-list li.p-menuitem:nth-child(3) .p-menuitem-link',
     listRow: (columnName) => `[data-testid="list-table-block__column__${columnName}__row"]`,
     seeMore: (columnName) =>
-      `[data-testid="list-table-block__column__${columnName}__row"] .underline`
+      `[data-testid="list-table-block__column__${columnName}__row"] .underline`,
+    mainSettingsTab: '[data-testid="waf-rules-tabs__tab__main-settings"] > a',
+    tuningTab: '[data-testid="waf-rules-tabs__tab__tuning"] > a',
+    allowedRulesTab: '[data-testid="waf-rules-tabs__tab__allowed-rules"] > a',
+    allowedRules: {
+      createButton: '[data-testid="create_Allowed Rule_button"]',
+      ruleIdDropdown: '[data-testid="allowed-rules-form__rule-id-field"] > .p-dropdown-trigger',
+      ruleIdOption: (ruleId) =>
+        `[data-testid="allowed-rules-form__rule-id-field"] #ruleid_${ruleId}`,
+      descriptionField: '[data-testid="allowed-rules-form__description-field__input"]',
+      pathField: '[data-testid="allowed-rules-form__path-field__input"]',
+      matchZoneDropdown: (zoneId) =>
+        `[data-testid="allowed-rules-form__match-zone[${zoneId}]-field"] > .p-dropdown-trigger`,
+      matchZoneOption: (zoneId, position) =>
+        `[data-testid="allowed-rules-form__match-zone[${zoneId}]-field"] #ruleid_${position}`,
+      headerField: (zoneId) =>
+        `[data-testid="allowed-rules-form__zone[${zoneId}]__header-field__input"]`,
+      matchesOnRadio: (zoneId, position) =>
+        `[data-testid="allowed-rules-form__zone[${zoneId}]__matches-on-field__radio"][name="matchZones[${zoneId}].matches_on-radio-${position}"]`,
+      deleteMatchZoneButton: (zoneId) =>
+        `[data-testid="allowed-rules-form__delete-match-zone[${zoneId}]__button"]`,
+      addZoneButton: '[data-testid="allowed-rules-form__add-match-zone__button"]',
+      regexSwitch:
+        '[data-testid="allowed-rules-form__use-regex-field__switch"] > .p-inputswitch-slider',
+      statusSwitch:
+        '[data-testid="allowed-rules-form__status-field__switch"] > .p-inputswitch-slider'
+    }
   },
   functions: {
     createButton: '[data-testid="create_Edge Function_button"] > .p-button-label',
