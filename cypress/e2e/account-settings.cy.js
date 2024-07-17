0d14b39b3f2e5306025f0bef2bf57050c7d73c87
William <william.tome@azion.com>
2024-07-17 10:34:07 -0300

    [UXE-3958] test: create e2e for cache settings edition in edge application (#1489)
    * refactor: split selectors to file
    
    * chore: add selectors for edge application context
    
    * test: create e2e for edge-application/cache-settings edition
    
    * test: wait for 1 sec for cities api response
    
    * test: refactor selectors for edge-applications
    
    * test: refactor selectors for edge-applications
    
    * test: update selectors file

--- cypress/e2e/account-settings.cy.js
+++ cypress/e2e/account-settings.cy.js
@@ -1,3 +1,4 @@
+/* eslint-disable cypress/no-unnecessary-waiting */
 import selectors from '../support/selectors'
 import generateUniqueName from '../support/utils'
 
@@ -34,9 +35,9 @@ describe('Account Settings spec', { tags: ['@dev', '@xfail'] }, () => {
     cy.get(selectors.accountSettings.postalCode).clear()
 
     // Act
-    cy.get(selectors.accountSettings.companyName).type(fixtures.companyName, {delay: 0})
-    cy.get(selectors.accountSettings.companyId).type(fixtures.companyId, {delay: 0})
-    cy.get(selectors.accountSettings.postalCode).type(fixtures.postalCode, {delay: 0})
+    cy.get(selectors.accountSettings.companyName).type(fixtures.companyName, { delay: 0 })
+    cy.get(selectors.accountSettings.companyId).type(fixtures.companyId, { delay: 0 })
+    cy.get(selectors.accountSettings.postalCode).type(fixtures.postalCode, { delay: 0 })
 
     cy.get(selectors.accountSettings.submitButton).click()
 
@@ -58,7 +59,7 @@ describe('Account Settings spec', { tags: ['@dev', '@xfail'] }, () => {
     cy.get(selectors.accountSettings.complement).clear()
 
     // Act
-    cy.get(selectors.accountSettings.postalCode).type(fixtures.postalCode, {delay: 0})
+    cy.get(selectors.accountSettings.postalCode).type(fixtures.postalCode, { delay: 0 })
 
     cy.get(selectors.accountSettings.countryDropdown).click()
     cy.get(selectors.accountSettings.countryOption(randomCountryOption)).click()
@@ -67,7 +68,7 @@ describe('Account Settings spec', { tags: ['@dev', '@xfail'] }, () => {
     cy.get(selectors.accountSettings.country).invoke('text').as('countryValue')
 
     // wait for cities api to provide region options
-    cy.wait('@citiesApi')
+    cy.wait(1000)
     cy.get(selectors.accountSettings.regionDropdown).click()
     cy.get(selectors.accountSettings.regionOption(firstOption)).click()
 
@@ -75,15 +76,15 @@ describe('Account Settings spec', { tags: ['@dev', '@xfail'] }, () => {
     cy.get(selectors.accountSettings.region).invoke('text').as('regionValue')
 
     // wait for cities api to provide city options
-    cy.wait('@citiesApi')
+    cy.wait(1000)
     cy.get(selectors.accountSettings.cityDropdown).click()
     cy.get(selectors.accountSettings.cityOption(firstOption)).click()
 
     // store city alias value to be used for validation
     cy.get(selectors.accountSettings.city).invoke('text').as('cityValue')
 
-    cy.get(selectors.accountSettings.address).type(fixtures.address, {delay: 0})
-    cy.get(selectors.accountSettings.complement).type(fixtures.complement, {delay: 0})
+    cy.get(selectors.accountSettings.address).type(fixtures.address, { delay: 0 })
+    cy.get(selectors.accountSettings.complement).type(fixtures.complement, { delay: 0 })
 
     cy.get(selectors.accountSettings.submitButton).click()
 
@@ -113,7 +114,7 @@ describe('Account Settings spec', { tags: ['@dev', '@xfail'] }, () => {
     cy.get(selectors.accountSettings.billingEmails).clear()
 
     // Act
-    cy.get(selectors.accountSettings.billingEmails).type(emails, {delay: 0})
+    cy.get(selectors.accountSettings.billingEmails).type(emails, { delay: 0 })
 
     cy.get(selectors.accountSettings.submitButton).click()
 
