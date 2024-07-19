dcb0617b66f96a72785b874350a72d8bfada4f0c
HerbertJulio <herbert.julio@azion.com>
2024-07-19 17:46:51 -0300

    chore: add account blocked check in billing

--- src/views/Billing/Drawer/DrawerPaymentMethod.vue
+++ src/views/Billing/Drawer/DrawerPaymentMethod.vue
@@ -2,7 +2,7 @@
   import CreatePaymentMethodBlock from '@templates/add-payment-method-block'
   import { refDebounced } from '@vueuse/core'
   import { onMounted, ref } from 'vue'
-  import { useRoute, useRouter } from 'vue-router'
+  import { useRoute } from 'vue-router'
 
   defineOptions({
     name: 'payment-method-drawer'
@@ -19,7 +19,6 @@
 
   const showCreatePaymentMethodDrawer = ref(false)
   const route = useRoute()
-  const router = useRouter()
   const debouncedDrawerAnimate = 300
 
   const showCreateDrawer = refDebounced(showCreatePaymentMethodDrawer, debouncedDrawerAnimate)
@@ -40,7 +39,6 @@
   const showPaymentMethod = () => {
     if (route.query.paymentSession) {
       openDrawer()
-      router.push({ query: {} })
     }
   }
 
