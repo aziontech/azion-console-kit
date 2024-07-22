93c204896bde59c049d8aa3277e4685805e11a19
paulosf0 <paulo.ferreira@azion.com>
2024-07-22 13:55:34 -0300

    feat: load the default credit card when bill tab it is active

--- src/views/Billing/TabsView.vue
+++ src/views/Billing/TabsView.vue
@@ -9,7 +9,7 @@
   import DrawerAddCredit from '@/views/Billing/Drawer/DrawerAddCredit'
   import DrawerPaymentMethod from '@/views/Billing/Drawer/DrawerPaymentMethod'
 
-  import { ref, computed, provide, onMounted } from 'vue'
+  import { ref, computed, provide, onMounted, watch } from 'vue'
 
   import { useRoute, useRouter } from 'vue-router'
   import { useAccountStore } from '@/stores/account'
@@ -71,7 +71,7 @@
       query
     })
   }
-
+  TABS_MAP.bills
   const isPaymentTabActive = computed(() => activeTab.value === TABS_MAP.payment)
   const isBillsTabActive = computed(() => activeTab.value === TABS_MAP.bills)
 
@@ -99,6 +99,12 @@
   const successAddCredit = async () => {
     await viewBillsRef.value?.reloadList()
   }
+
+  watch(isBillsTabActive, (newValue) => {
+    if (newValue) {
+      loadCardDefault()
+    }
+  })
 
   onMounted(() => {
     renderTabCurrentRouter()
