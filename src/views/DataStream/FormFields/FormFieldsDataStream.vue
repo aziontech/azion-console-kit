4d72694e31e1b00d32f8aed277f6706a8020c8c6
Aloisio Bertolo Bastian <aloisiomiguelb@gmail.com>
2024-07-23 14:15:53 -0300

    fix: add computed to verify if is lass than five

--- src/views/DataStream/FormFields/FormFieldsDataStream.vue
+++ src/views/DataStream/FormFields/FormFieldsDataStream.vue
@@ -260,7 +260,7 @@
           <ButtonPrimer
             outlined
             icon="pi pi-plus-circle"
-            v-if="headers.length < 5"
+            v-if="hasLessThanFive"
             iconPos="left"
             label="Header"
             size="small"
@@ -1128,6 +1128,11 @@
 
   // Using the store
   const store = useAccountStore()
+
+  const MAX_HEADER_COUNT = 5
+  const hasLessThanFive = computed(() => {
+    return headers.value.length < MAX_HEADER_COUNT
+  })
 
   const placeholderLineSeparator = computed(() => {
     const text = '"\\n"'
