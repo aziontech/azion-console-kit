8b1518894ff054af1fcd24f8d9fdfe01a09f40f1
Rafael <80526390+rafael-qazion@users.noreply.github.com>
2024-07-30 09:29:54 -0300

    [NO-ISSUE] tests: add xfail on failing tests (#1548)

--- cypress/e2e/edge-functions.cy.js
+++ cypress/e2e/edge-functions.cy.js
@@ -3,7 +3,7 @@ import selectors from '../support/selectors'
 
 let functionName = ''
 
-describe('Edge Functions spec', { tags: ['@dev'] }, () => {
+describe('Edge Functions spec', { tags: ['@dev', '@xfail'] }, () => {
   beforeEach(() => {
     cy.login()
     functionName = generateUniqueName('Function')
