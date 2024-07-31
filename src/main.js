d98615ce56257d36526f2e8e8ffbeaa1ddcf5558
Luis Igreja <lfsigreja@gmail.com>
2024-07-31 10:05:09 -0300

    feat: remove azion theme from console

--- src/main.js
+++ src/main.js
@@ -4,8 +4,8 @@
 import 'primeicons/primeicons.css'
 import 'primeflex/primeflex.css'
 import './assets/main.css'
-import '@aziontech/azion-theme/src/azion-dark/theme.scss';
-import '@aziontech/azion-theme/src/azion-light/theme.scss';
+import '@aziontech/azion-theme/src/azion-dark/theme.scss'
+import '@aziontech/azion-theme/src/azion-light/theme.scss'
 import '@assets/icons/azionicons.scss'
 import '@assets/c3.scss'
 /**
