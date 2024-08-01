033d0b97921c019cd8e9b1a0544ab33bb1804f41
Matheus Sunderhus <matheus.sunderhus@azion.com>
2024-08-01 15:41:09 -0300

    chore: remove not used noRecordsFound slot from list-table-block

--- src/templates/list-table-block/index.vue
+++ src/templates/list-table-block/index.vue
@@ -185,19 +185,17 @@
         </template>
       </Column>
       <template #empty>
-        <slot
-          name="noRecordsFound"
+        <div
           data-testid="data-table-empty-content"
+          class="my-4 flex flex-col gap-3 justify-center items-start"
         >
-          <div class="my-4 flex flex-col gap-3 justify-center items-start">
-            <p
-              class="text-md font-normal text-secondary"
-              data-testid="list-table-block__empty-message__text"
-            >
-              {{ emptyListMessage }}
-            </p>
-          </div>
-        </slot>
+          <p
+            class="text-md font-normal text-secondary"
+            data-testid="list-table-block__empty-message__text"
+          >
+            {{ emptyListMessage }}
+          </p>
+        </div>
       </template>
     </DataTable>
 
