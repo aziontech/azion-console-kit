03bc8a8b86fba803b8bc1e418f9d29badc07d8bb
william-tome <william.tome@azion.com>
2024-08-07 14:57:08 -0300

    feat: create rotated bars chart

---
+++ src/views/Playground/charts/RotatedBarChart.vue
@@ -0,0 +1,23 @@
+<script setup>
+  import { onMounted } from 'vue'
+  import { ROTATED_BAR_CHART_DATA } from './constants/data'
+  import c3 from 'c3'
+
+  onMounted(() => {
+    generate()
+  })
+
+  const generate = () => {
+    c3.generate({
+      bindto: `#playground-rotated_bar-chart`,
+      ...ROTATED_BAR_CHART_DATA
+    })
+  }
+</script>
+
+<template>
+  <div
+    class="[&>svg]:w-auto [&>svg]:flex"
+    :id="`playground-rotated_bar-chart`"
+  />
+</template>
