59c8387513f7b54672ce2fd0bcb13d753a3ad0d1
william-tome <william.tome@azion.com>
2024-08-07 11:07:13 -0300

    feat: create gauge chart

---
+++ src/views/Playground/charts/GaugeChart.vue
@@ -0,0 +1,23 @@
+<script setup>
+  import { onMounted } from 'vue'
+  import { GAUGE_CHART_DATA } from './constants/data'
+  import c3 from 'c3'
+
+  onMounted(() => {
+    generate()
+  })
+
+  const generate = () => {
+    c3.generate({
+      bindto: `#playground-gauge-chart`,
+      ...GAUGE_CHART_DATA
+    })
+  }
+</script>
+
+<template>
+  <div
+    class="[&>svg]:w-auto [&>svg]:flex"
+    :id="`playground-gauge-chart`"
+  />
+</template>
