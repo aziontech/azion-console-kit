f6a767602bce1860474d7e13bbc0adfc4557ac0f
william-tome <william.tome@azion.com>
2024-08-07 10:50:40 -0300

    feat: create stacked bar chart

---
+++ src/views/Playground/charts/StackedBarChart.vue
@@ -0,0 +1,23 @@
+<script setup>
+  import { onMounted } from 'vue'
+  import { STACKED_BAR_CHART_DATA } from './constants/data'
+  import c3 from 'c3'
+
+  onMounted(() => {
+    generate()
+  })
+
+  const generate = () => {
+    c3.generate({
+      bindto: `#playground-stacked-bar-chart`,
+      ...STACKED_BAR_CHART_DATA
+    })
+  }
+</script>
+
+<template>
+  <div
+    class="[&>svg]:w-auto [&>svg]:flex"
+    :id="`playground-stacked-bar-chart`"
+  />
+</template>
