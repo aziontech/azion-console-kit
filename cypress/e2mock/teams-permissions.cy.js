04e130fdb20e3f0be89eaaecf58b87f7ac4e084d
aloisio-m-bastian <107002324+aloisio-m-bastian@users.noreply.github.com>
2024-08-12 19:04:51 -0300

    [UXE-4473] fix: block users without permission of acessing team permissions edit page (#1603)
    * fix: block users whitout permission of acessing team permissions edit page
    
    * test: add e2e test to verify if users withou acess are being blocked
    
    * test: change describe text

---
+++ cypress/e2mock/teams-permissions.cy.js
@@ -0,0 +1,26 @@
+import selectors from '../support/selectors'
+
+const fixtures = {
+  errorMessage: {
+    detail: 'You do not have permission to perform this action (teams management).'
+  }
+}
+describe('Tams Permissions Spec', () => {
+  beforeEach(() => {
+    cy.login()
+    cy.openProduct('Teams Permissions')
+  })
+
+  it('should block users without access of entering in the edit page', function () {
+    //act
+    cy.get(selectors.list.searchField).type('Default Team')
+    cy.intercept('GET', '/api/v4/iam/teams/*', { statusCode: 403, body: fixtures.errorMessage }).as(
+      'teamsPermissions'
+    )
+    cy.get(selectors.teams.listRow('name')).click()
+
+    //assert
+    cy.verifyToast('error', `detail: ${fixtures.errorMessage.detail}`)
+    cy.location('pathname').should('eq', '/teams-permission')
+  })
+})
