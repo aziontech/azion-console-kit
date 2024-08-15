b5a2a9c0c00c0f05488104619cb33df790f4aa5f
Aloisio Bertolo Bastian <aloisiomiguelb@gmail.com>
2024-08-15 14:46:58 -0300

    fix: remove duplicated logic

--- src/router/hooks/afterEachRoute.js
+++ src/router/hooks/afterEachRoute.js
@@ -9,8 +9,7 @@ export default function afterEachRoute(to, from, failure) {
   const { hasActiveUserId } = useAccountStore()
   loadingStore.finishLoading()
 
-  if (failure) return
-  if (!hasActiveUserId) return
+  if (failure || !hasActiveUserId) return
   /** @type {import('@/plugins/analytics/AnalyticsTrackerAdapter').AnalyticsTrackerAdapter} */
   const tracker = inject('tracker')
   tracker.product
