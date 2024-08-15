de026583c59eed012d391d639658ede5ec160e68
william-tome <william.tome@azion.com>
2024-08-15 12:32:27 -0300

    feat: position tooltip dinamically

--- src/views/Playground/maps/map-chart-blocks/map-tooltip-block.vue
+++ src/views/Playground/maps/map-chart-blocks/map-tooltip-block.vue
@@ -2,6 +2,7 @@
   <div
     v-if="data?.kind"
     class="absolute surface-card rounded border surface-border px-4 py-3 pointer-events-none"
+    :style="`top: ${data?.yAxis}px; left: ${data?.xAxis}px; transform: translate(-50%, -100%)`"
   >
     <component
       :is="COMPONENTS[data.kind]"
