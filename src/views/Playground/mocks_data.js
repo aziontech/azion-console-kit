acc69b2c49f03d2aaf4d11510bce1591f3952516
william-tome <william.tome@azion.com>
2024-08-21 17:13:05 -0300

    refactor: remove mock data

--- src/views/Playground/mocks_data.js
+++ src/views/Playground/mocks_data.js
@@ -144,20 +144,6 @@ export const STACKED_LINE_CHART_DATA = {
   ...COLOR_PATTERNS
 }
 
-export const PIE_CHART_DATA = {
-  id: crypto.randomUUID().toString(),
-  data: {
-    columns: [
-      ['Netherlands', 30],
-      ['Nigeria', 120],
-      ['France', 40],
-      ['Brazil', 80]
-    ],
-    type: 'pie'
-  },
-  ...COLOR_PATTERNS
-}
-
 export const DONUT_CHART_DATA = {
   id: crypto.randomUUID().toString(),
   data: {
