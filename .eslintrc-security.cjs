8891b98a44ac2d4cf679129ee00ec3b0c3ead1e3
Matheus Chandelier <matheus.chandelier@azion.com>
2024-08-22 14:55:17 -0300

    [QA-2122] ci: return error instead of warning with ESLint plugin security

--- .eslintrc-security.cjs
+++ .eslintrc-security.cjs
@@ -13,5 +13,19 @@ module.exports = {
   rules: {
     'no-unsanitized/method': 'error',
     'no-unsanitized/property': 'error',
+    'security/detect-bidi-characters': 'error',
+    'security/detect-buffer-noassert': 'error',
+    'security/detect-child-process': 'error',
+    'security/detect-disable-mustache-escape': 'error',
+    'security/detect-eval-with-expression': 'error',
+    'security/detect-new-buffer': 'error',
+    'security/detect-no-csrf-before-method-override': 'error',
+    'security/detect-non-literal-fs-filename': 'error',
+    'security/detect-non-literal-regexp': 'error',
+    'security/detect-non-literal-require': 'error',
+    'security/detect-object-injection': 'error',
+    'security/detect-possible-timing-attacks': 'error',
+    'security/detect-pseudoRandomBytes': 'error',
+    'security/detect-unsafe-regex': 'error'
   }
 }
