be4640f6cfe93e53288e3fc4b339f40954c4dc51
william-tome <william.tome@azion.com>
2024-09-25 08:58:18 -0300

    refactor: rename bot captcha series

--- src/modules/real-time-metrics/reports/convert-beholder-to-chart.js
+++ src/modules/real-time-metrics/reports/convert-beholder-to-chart.js
@@ -330,6 +330,18 @@ const formatTsChartData = ({
   })
 
   const seriesArray = fillSeriesWithZeroes(series, countValues)
+
+  if (report.id === '071851224118431167') {
+    seriesArray.forEach((serie) => {
+      // linter complains about ternary
+      if (serie[0] === 'false') {
+        serie[0] = 'Not Solved'
+      }
+      if (serie[0] === 'true') {
+        serie[0] = 'Solved'
+      }
+    })
+  }
   // ensures that the X-axis is the first set of data.
   return [xAxisData, ...seriesArray]
 }
