3bbbdc593383fd24d9effe08ecd0b6ec0f91d1c2
Herbert Vicente Cotta Julio <hvcotta@gmail.com>
2024-09-25 10:14:11 -0300

    [UXE-5123] fix: add access control with allow_console and restrict metrics access with allow_only_metrics_on_console (#1751)
    Access control has been added using the allow_console client flag, allowing specific users to view the console. Additionally, the allow_only_metrics_on_console flag has been introduced to ensure that the user can only access the console metrics screen and nothing else.

--- src/services/account-services/index.js
+++ src/services/account-services/index.js
@@ -1,5 +1,4 @@
 import { getUserInfoService } from './get-user-info-service'
 import { getAccountInfoService } from './get-account-info-service'
-import { listClientIdsReleasedForConsoleService } from './list-client-ids-released-for-console-service'
 
-export { getAccountInfoService, getUserInfoService, listClientIdsReleasedForConsoleService }
+export { getAccountInfoService, getUserInfoService }
