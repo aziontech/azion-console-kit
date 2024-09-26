3347dc7ee130c4058e8dcb413aede225050d1dca
HerbertJulio <herbert.julio@azion.com>
2024-09-26 19:01:27 -0300

    feat: update route guards to accept accountStore as a parameter

--- src/router/hooks/beforeEachRoute.js
+++ src/router/hooks/beforeEachRoute.js
@@ -2,16 +2,18 @@ import * as guards from '@/router/hooks/guards'
 import { useHelpCenterStore } from '@/stores/help-center'
 import { useRouter } from 'vue-router'
 
-/** @type {import('vue-router').NavigationGuardWithThis} */
-export default async function beforeEachRoute(to, from, next) {
+export default async function beforeEachRoute(routeHandler, guardDependency) {
+  const { to, next } = routeHandler
+  const { accountStore } = guardDependency
+
   const helpCenterStore = useHelpCenterStore()
   helpCenterStore.close()
   const router = useRouter()
-  await guards.logoutGuard(to, next)
+  await guards.logoutGuard(to, next, accountStore)
   guards.loadingGuard(to)
-  await guards.accountGuard(to, next)
-  guards.themeGuard()
-  guards.billingGuard(to, next)
+  await guards.accountGuard(to, next, accountStore)
+  guards.themeGuard(accountStore)
+  guards.billingGuard(to, next, accountStore)
   guards.redirectGuard(to, next, router)
 
   return next()
