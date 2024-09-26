868df2f07729dfe959d1dd48534c7304afc798e5
HerbertJulio <herbert.julio@azion.com>
2024-09-26 18:59:25 -0300

    refactor: remove unused import and update logoutGuard to accept accountStore as a parameter

--- src/router/hooks/guards/logoutGuard.js
+++ src/router/hooks/guards/logoutGuard.js
@@ -1,11 +1,9 @@
 import { logoutService } from '@/services/auth-services'
-import { useAccountStore } from '@/stores/account'
 
 /** @type {import('vue-router').NavigationGuardWithThis} */
-export async function logoutGuard(to, next) {
+export async function logoutGuard(to, next, accountStore) {
   if (to.path === '/logout') {
     await logoutService()
-    const accountStore = useAccountStore()
     accountStore.setAccountData({})
     return next()
   }
