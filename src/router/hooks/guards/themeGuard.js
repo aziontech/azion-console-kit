ea7f91b9e5a67051c1112168b9167399d549ee76
HerbertJulio <herbert.julio@azion.com>
2024-09-26 18:53:39 -0300

    feat: refactor themeGuard to accept accountStore as a parameter

--- src/router/hooks/guards/themeGuard.js
+++ src/router/hooks/guards/themeGuard.js
@@ -1,8 +1,4 @@
-import { useAccountStore } from '@/stores/account'
-
-export async function themeGuard() {
-  const accountStore = useAccountStore()
-
+export async function themeGuard(accountStore) {
   // TODO: remove the usage of localStorage when API returns the theme
   const theme = localStorage.getItem('theme')
   const fallbackTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
