901b8208f683986560462ded75753f69ee92d76a
Herbert Vicente Cotta Julio <hvcotta@gmail.com>
2024-10-02 13:59:02 -0300

    [NO-ISSUE] fix: adjustment to the route guards to standardize execution and dependency injection as parameters. (#1775)
    This pull request includes significant changes to the routing guards in the application to improve code clarity and maintainability. The most important changes involve refactoring the guards to use a consistent dependency injection pattern and simplifying the guard logic

--- src/router/hooks/guards/logoutGuard.js
+++ src/router/hooks/guards/logoutGuard.js
@@ -1,10 +1,10 @@
 import { logoutService } from '@/services/auth-services'
 
 /** @type {import('vue-router').NavigationGuardWithThis} */
-export async function logoutGuard(to, next, accountStore) {
+export async function logoutGuard({ to, accountStore }) {
   if (to.path === '/logout') {
     await logoutService()
     accountStore.setAccountData({})
-    return next()
+    return true
   }
 }
