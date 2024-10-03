a75d1263876d0851c00355b4f83b499c01fc7067
Herbert Vicente Cotta Julio <hvcotta@gmail.com>
2024-10-03 15:24:03 -0300

    Revert "[UXE-4942] feat: add Azion Copilot with deepchat to AI Chat component…"
    This reverts commit 9c0c520c94f2d491284dde8ba5881ef513498d04.

--- src/modules/azion-ai-chat/azion-ai-get-help.vue
+++ src/modules/azion-ai-chat/azion-ai-get-help.vue
@@ -1,31 +1,11 @@
 <script setup>
   import PrimeButton from 'primevue/button'
-  import { computed } from 'vue'
 
   defineOptions({ name: 'get-help' })
 
   const props = defineProps({
     prompt: {
       type: String
-    },
-    context: {
-      type: Object,
-      default: null
-    }
-  })
-
-  const formatPrompt = computed(() => {
-    if (!props.context) return props.prompt
-
-    const replaceKeys = (text) => {
-      return Object.keys(props.context).reduce((str, key) => {
-        return str.replaceAll(`\${${key}}`, props.context[key])
-      }, text)
-    }
-
-    return {
-      user: replaceKeys(props.prompt.user),
-      system: replaceKeys(props.prompt.system)
     }
   })
 </script>
@@ -37,6 +17,6 @@
     label="Get Help"
     icon="ai ai-ask-azion"
     icon-pos="right"
-    v-prompt="formatPrompt"
+    v-prompt="props.prompt"
   />
 </template>
