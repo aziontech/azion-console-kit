b13bc956d3f796f2361987b854585689e15191dc
HerbertJulio <herbert.julio@azion.com>
2024-10-03 18:41:54 -0300

    tests: update tests to AnalyticsTrackerAdapter for reset method

--- src/tests/plugins/analytics/AnalyticsTrackerAdapter.test.js
+++ src/tests/plugins/analytics/AnalyticsTrackerAdapter.test.js
@@ -8,7 +8,8 @@ const fixtures = {
 const makeSut = () => {
   const analyticsClientSpy = {
     track: vi.fn(),
-    identify: vi.fn()
+    identify: vi.fn(),
+    reset: vi.fn()
   }
   const sut = new AnalyticsTrackerAdapter(analyticsClientSpy)
 
@@ -43,6 +44,18 @@ describe('AnalyticsTrackerAdapter', () => {
     sut.track()
 
     expect(analyticsClientSpy.track).toHaveBeenCalledTimes(3)
+  })
+
+  it('should reset the analytics client and clear stored events', () => {
+    const { sut, analyticsClientSpy } = makeSut()
+    sut.identify('test-user-id')
+    sut.product.pageLoad({ url: 'test-url-1' })
+    sut.product.pageLoad({ url: 'test-url-2' })
+    sut.reset()
+    sut.track()
+
+    expect(analyticsClientSpy.reset).toHaveBeenCalled()
+    expect(sut.track).toHaveLength(0)
   })
 
   it('should be able to track page load event with correct params', () => {
