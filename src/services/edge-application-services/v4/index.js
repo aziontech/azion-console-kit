e0dc484401d67a2494ebf8c7e0915bcee70ac303
Peterson Paulo <109550332+peterpaulo-azion@users.noreply.github.com>
2024-10-16 14:51:01 -0300

    Merge pull request #1797 from aziontech/poc-uxe-api-v4-review
    [UXE-5176] feat: migrate edge application list to V4 in a new lazy component

---
+++ src/services/edge-application-services/v4/index.js
@@ -0,0 +1,11 @@
+import { listEdgeApplicationsService } from './list-edge-applications-service'
+
+/**
+ * @typedef {Object} ExportedServicesType - The type of the exported services
+ * @property {typeof listEdgeApplicationsService} listEdgeApplicationsService - The listEdgeApplicationsService reference
+ */
+
+/**
+ * @type {ExportedServicesType}
+ */
+export { listEdgeApplicationsService }
