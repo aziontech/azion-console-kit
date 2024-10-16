00c8bb5a82b9cc7eb1b44a08772e2fa9ccec4193
Aloisio Bertolo Bastian <aloisiomiguelb@gmail.com>
2024-10-16 19:08:58 -0300

    fix: send header criterias operator as matches instead of is in list

---
+++ cypress/e2e/edge-firewall/create-edge-firewall-rule-engine-header-criteria.cy.js
@@ -0,0 +1,82 @@
+/* eslint-disable cypress/no-unnecessary-waiting */
+import generateUniqueName from '../../support/utils'
+import selectors from '../../support/selectors'
+
+let firewallName, ruleName
+
+describe('Edge Firewall spec', { tags: ['@dev5', '@xfail'] }, () => {
+  beforeEach(() => {
+    cy.login()
+    firewallName = generateUniqueName('EdgeFirewall')
+    ruleName = generateUniqueName('EdgeFirewallRule')
+  })
+
+  it('should create an Edge Firewall and run a function', () => {
+    cy.openProduct('Edge Firewall')
+
+    // Act - create Edge Firewall
+    cy.get(selectors.edgeFirewall.createButton).click()
+    cy.get(selectors.edgeFirewall.nameInput).clear()
+    cy.get(selectors.edgeFirewall.nameInput).type(firewallName)
+    cy.get(selectors.edgeFirewall.edgeFunctionSwitch).click()
+    cy.get(selectors.edgeFirewall.wafEnabledSwitch).click()
+    cy.get(selectors.edgeFirewall.saveButton).click()
+    cy.verifyToast('success', 'Your Edge Firewall has been created')
+
+    cy.get(selectors.edgeFirewall.rulesEngineTab).click()
+    cy.get(selectors.edgeFirewall.createRuleButton).click()
+    cy.get(selectors.edgeFirewall.ruleNameInput).click()
+    cy.get(selectors.edgeFirewall.ruleNameInput).type(ruleName)
+    cy.get(selectors.edgeFirewall.ruleDescriptionInput).clear()
+    cy.get(selectors.edgeFirewall.ruleDescriptionInput).type('My Rule Description')
+    cy.get(selectors.edgeFirewall.ruleCriteriaVariableDropdown).click()
+    cy.get(selectors.edgeFirewall.ruleCriteriaVariableDropdownHeaderAccept).click()
+    cy.get(selectors.edgeFirewall.ruleCriteriaOperatorDropdown).click()
+    cy.get(selectors.edgeFirewall.ruleCriteriaOperatorMatches).click()
+    cy.get(selectors.edgeFirewall.ruleCriteriaInput).clear()
+    cy.get(selectors.edgeFirewall.ruleCriteriaInput).type('test')
+    cy.get(selectors.edgeFirewall.ruleBehaviorDropdown).click()
+    cy.get(selectors.edgeFirewall.ruleBehaviorFirstOption).click()
+
+    cy.intercept('POST', '/api/v3/edge_firewall/*/rules_engine*').as('addEdgeFirewallRule')
+
+    cy.get(selectors.edgeFirewall.ruleSubmit).click()
+
+    cy.wait('@addEdgeFirewallRule').then((interception) => {
+      const requestBody = interception.request.body
+      expect(requestBody.criteria[0][0].operator).to.equal('matches')
+    })
+
+    cy.verifyToast('success', 'Rule Engine successfully created')
+
+    // Assert - Find the created rule
+    cy.get(selectors.edgeFirewall.rulesTableSearchInput).clear()
+    cy.get(selectors.edgeFirewall.rulesTableSearchInput).type(ruleName)
+    cy.get(selectors.edgeFirewall.rulesTableColumnName).should('have.text', ruleName)
+    cy.get(selectors.edgeFirewall.rulesTableColumnDescriptionShowMore).click()
+    cy.get(selectors.edgeFirewall.rulesTableColumnDescription).should(
+      'have.text',
+      'My Rule Description'
+    )
+
+    // Cleanup - Remove the created rule
+    cy.deleteEntityFromLoadedList().then(() => {
+      cy.verifyToast('Rules Engine successfully deleted')
+    })
+
+    // Assert - Find the created firewall
+    cy.get(selectors.edgeFirewall.mainSettingsTab).click()
+    cy.get(selectors.edgeFirewall.cancelButton).click()
+    cy.get(selectors.edgeFirewall.searchInput).clear()
+    cy.get(selectors.edgeFirewall.searchInput).type(firewallName)
+    cy.get(selectors.edgeFirewall.nameRow).should('have.text', firewallName)
+    cy.get(selectors.edgeFirewall.activeRow).should('have.text', 'Active')
+  })
+
+  afterEach(() => {
+    // Delete the firewall
+    cy.deleteEntityFromList({ entityName: firewallName, productName: 'Edge Firewall' }).then(() => {
+      cy.verifyToast('Edge Firewall successfully deleted')
+    })
+  })
+})
