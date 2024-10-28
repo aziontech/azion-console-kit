1d962c8504eee2863166d7d1ab41dccb9ec35cb0
peterpaulo-azion <peterson.silva@azion.com>
2024-10-28 16:10:19 -0300

    fix: all identity-providers route guard

--- src/router/hooks/guards/SSOManagementGuard.js
+++ src/router/hooks/guards/SSOManagementGuard.js
@@ -1,7 +1,7 @@
 /** @type {import('vue-router').NavigationGuardWithThis} */
 
 export function ssoManagementGuard({ to, accountStore }) {
-  if (to.name === 'identity-providers' && !accountStore.hasAccessToSSOManagement) {
+  if (to.fullPath.includes('identity-providers') && !accountStore.hasAccessToSSOManagement) {
     return '/'
   }
 }
