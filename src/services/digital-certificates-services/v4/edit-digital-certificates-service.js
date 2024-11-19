a30743eab98512f4411ef4b83da9a7f7e8ec4cbb
paulosf0 <paulo.ferreira@azion.com>
2024-11-19 09:56:46 -0300

    feat: migrate edit to v4

---
+++ src/services/digital-certificates-services/v4/edit-digital-certificates-service.js
@@ -0,0 +1,68 @@
+import { AxiosHttpClientAdapter } from '../axios/AxiosHttpClientAdapter'
+import { makeDigitalCertificatesBaseUrl } from './make-digital-certificates-base-url'
+import * as Errors from '@/services/axios/errors'
+
+export const editDigitalCertificateService = async (payload) => {
+  const parsedPayload = adapt(payload)
+  let httpResponse = await AxiosHttpClientAdapter.request({
+    url: `${makeDigitalCertificatesBaseUrl()}/${payload.id}`,
+    method: 'PATCH',
+    body: parsedPayload
+  })
+
+  return parseHttpResponse(httpResponse)
+}
+
+/**
+ * @param {Object} body - The response body.
+ * @returns {string} The result message based on the status code.
+ */
+const extractApiError = (body) => {
+  let apiError = ''
+  const keys = Object.keys(body)
+
+  for (const keyError of keys) {
+    if (Array.isArray(body[keyError])) {
+      const errorValue = body[keyError][0]
+      if (typeof errorValue === 'string') {
+        apiError = errorValue
+        break
+      }
+      if (typeof errorValue === 'object') {
+        apiError = errorValue.message[0]
+        break
+      }
+    } else {
+      apiError = body[keyError]
+      break
+    }
+  }
+
+  return apiError
+}
+const parseHttpResponse = (httpResponse) => {
+  switch (httpResponse.statusCode) {
+    case 200:
+      return 'Your digital certificate has been updated!'
+    case 400:
+      throw new Error(extractApiError(httpResponse.body)).message
+    case 401:
+      throw new Errors.InvalidApiTokenError().message
+    case 403:
+      throw new Errors.PermissionError().message
+    case 404:
+      throw new Errors.NotFoundError().message
+    case 500:
+      throw new Errors.InternalServerError().message
+    default:
+      throw new Errors.UnexpectedError().message
+  }
+}
+
+const adapt = (payload) => {
+  return {
+    name: payload.name,
+    certificate: payload.certificate,
+    private_key: payload.privateKey === '' ? undefined : payload.privateKey
+  }
+}
