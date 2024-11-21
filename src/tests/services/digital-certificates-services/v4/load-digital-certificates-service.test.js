def0dccb7b42995aad0f79cbd8669abf482a605e
paulosf0 <paulo.ferreira@azion.com>
2024-11-21 08:48:50 -0300

    refactor: remove certificate value on the edit page

--- src/tests/services/digital-certificates-services/v4/load-digital-certificates-service.test.js
+++ src/tests/services/digital-certificates-services/v4/load-digital-certificates-service.test.js
@@ -7,8 +7,6 @@ const fixtures = {
   certificateMock: {
     id: 72395,
     name: 'Cert_A.pem',
-    certificate:
-      '-----BEGIN CERTIFICATE-----\nMIIFazCCA1OgAwIBAgIUJawdmUAWN1sII1Jpe3Em5b9a7GowDQYJKoZIhvcNAQEL\nBQAwRTELMAkGA1UEBhMCQVUxEzARBgNVBAgMClNvbWUtU3RhdGUxITAfBgNVBAoM\nGEludGVybmV0IFdpZGdpdHMgUHR5IEx0ZDAeFw0yNDA0MTcxMjU5MzlaFw0yNTA0\nMTcxMjU5MzlaMEUxCzAJBgNVBAYTAkFVMRMwEQYDVQQIDApTb21lLVN0YXRlMSEw\nHwYDVQQKDBhJbnRlcm5ldCBXaWRnaXRzIFB0eSBMdGQwggIiMA0GCSqGSIb3DQEB\nAQUAA4ICDwAwggIKAoICAQCvfATcbfsz8WwH6MCri/DoLpfcGZ8E7rEsQvLtKTT7\n38qwkJbu1PI8jaMJCIG1gi45tKLW4FNI/9ipcCfb1bLUryd+tIUyvjNVHJXW/9MI\np9SYATAma3fVmvdLj1LpVeduLUp3EN8wGC/DpCGalw06d0P0UT3MHvKvHQO2udg3\nyv3+x7QaTsoDF96ynffM1/Mjfqmx/EyHfumzXFYXPP3kc5M+ONM4hxLzHaw5zvJf\niE+z5w9JJkL4YY8MWJcilMMY6RHYwbHn6EAFgKvjblV7meLygWRFJchZQl2WmoRu\nNfds4oxha9gYnoNRmfsHhbvN/6iO5opuLs1rrchcjqbaM/UpXBVVxZUmc9/Hki0q\niVTvazU1hjAt2mddrTOCGFwXh3kS6LMaDDpDN2IIQZyC7SnfeqkmtjAKr3+2dcu8\nqVOx3/bbXxeuRmpn0thazIU5a60EbG1lKmhSkVXrNDoMQhZpmIsks0gXhRVJh8PL\njNiFRFHbSQlu0mcEeYEK+jAnZOt2+edKmIE85WG7Qj51Tg3COiFNz3KIb2JhAC2l\nAAkuGMqRd1pv6OlkPp6rRG6s2qkS4fa0rqiOsnKovjRH8IZT6eYd74GEugMeqKzl\nGEx9GcN9YuPY/oiLdqqTkr0zkiaMHSAaHQzzhf9FE8sNMyK26erC/jX3vlg7vnYZ\nvwIDAQABo1MwUTAdBgNVHQ4EFgQU1zgmI0MhPLNw7DLoB5Njh5f3RAgwHwYDVR0j\nBBgwFoAU1zgmI0MhPLNw7DLoB5Njh5f3RAgwDwYDVR0TAQH/BAUwAwEB/zANBgkq\nhkiG9w0BAQsFAAOCAgEAOogZtUHAX7J0czEGEfdbV+LqOZoZGbhnd5PkTjwFqjy5\n84Gn32acwiu6g0hTGAhdjf0G0rUXVRexleZ/XvDauBMD9zzUs+zcP8zy6JziZdgk\np+ghvsOud4me0g7+f6hmIG79fs76NvUZN3E0jJqjvb7OTZ6B5jg7W2Jr/OAEYexk\nlX6AxQ9J9anHMqT3G1Sy9e8StNbkI0GDA37yvoOH+T0+9FAEP2jn4jFZh/omWH+D\n9Y6t4oQEile4YtNvJSP61Ek21GSr+4Nkw3FN82/HsIIo7VrFKljZgMFS6IRW7eqx\n+kMFYyE5q4ja+KEg8LD60ZdRUaIM7gkEA5aZ9pdOwel6o18MvjaZEWJ8KE0bW+mw\ntYpfeu/11b2+LlCbm4fviH9cDcN+6Rc//nVJjfxH7hwRj7k02jb8FtowVMQTZmYS\nMX5aYMFMTXbtZ/a+6jjRsm0OlsliR/OlgZhhv5CvPUBenwk1I6WMi4bO85jXMcwB\n1v/+AjiGwWwuNylg2WDR4YywmuRSzT8CV8hWIvv6u4OA9jUYV1Es4jDPe1sB1WUC\n3FZpH5ZKR8QYPemK7lQ8Pei+Rf9ABFLFUom2r/OtG7V/4Lo3gh0DjMUIHl72eD37\nr6+FGF5jfz7KwQ5fB13qt0LjwJBYCgfdDRexT+n2ZtikIQzxtnHkzV2iR1rHDm8=\n-----END CERTIFICATE-----',
     type: 'trusted_ca_certificate',
     managed: false,
     csr: null
@@ -35,7 +33,7 @@ describe('DigitalCertificatesServices', () => {
     await sut({ id: fixtures.certificateMock.id })
 
     expect(requestSpy).toHaveBeenCalledWith({
-      url: `${version}/digital_certificates/certificates/${fixtures.certificateMock.id}?fields=id,name,type,csr,managed,certificate`,
+      url: `${version}/digital_certificates/certificates/${fixtures.certificateMock.id}?fields=id,name,type,csr,managed`,
       method: 'GET'
     })
   })
@@ -54,8 +52,7 @@ describe('DigitalCertificatesServices', () => {
       name: fixtures.certificateMock.name,
       type: fixtures.certificateMock.type,
       managed: fixtures.certificateMock.managed,
-      csr: undefined,
-      certificate: fixtures.certificateMock.certificate
+      csr: undefined
     })
   })
 
