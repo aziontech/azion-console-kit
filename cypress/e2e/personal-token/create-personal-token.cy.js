a4ef6703bd7e7fcbc952abb146054c6d812136e4
paulosf0 <paulo.ferreira@azion.com>
2024-12-03 14:37:56 -0300

    fix: remove xfail

--- cypress/e2e/personal-token/create-personal-token.cy.js
+++ cypress/e2e/personal-token/create-personal-token.cy.js
@@ -3,7 +3,7 @@ import selectors from '../../support/selectors'
 
 const personalTokenName = generateUniqueName('Personal Token')
 
-describe('Personal Token spec', { tags: ['@dev6', '@xfail'] }, () => {
+describe('Personal Token spec', { tags: ['@dev6'] }, () => {
   beforeEach(() => {
     cy.login()
     cy.openProduct('Personal Token')
