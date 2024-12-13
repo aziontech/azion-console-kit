9467b365424a24ce5faeda5517fc568d22f6e1f2
Vinicius Gubiani Ferreira <vini.g.fer@gmail.com>
2024-12-13 17:02:24 -0300

    Update cypress/e2e/edge-application/add-error-response.cy.js

--- cypress/e2e/edge-application/add-error-response.cy.js
+++ cypress/e2e/edge-application/add-error-response.cy.js
@@ -27,7 +27,6 @@ const createEdgeApplicationCase = () => {
   cy.get(selectors.list.filteredRow.column('name')).click()
 }
 
-// TODO: remove xfail tag when the API v4 is fixed
 describe('Edge Application', { tags: ['@dev4'] }, () => {
   beforeEach(() => {
     fixtures.edgeApplicationName = generateUniqueName('EdgeApp')
