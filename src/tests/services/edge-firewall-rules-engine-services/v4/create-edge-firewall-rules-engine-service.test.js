c6ba4ec90bef70d6108c872efac61ef9e0a6d555
peterpaulo-azion <peterson.silva@azion.com>
2024-12-19 16:25:11 -0300

    fix: update average_rate_limit and maximum_burst_size types in edge firewall rules engine service tests

--- src/tests/services/edge-firewall-rules-engine-services/v4/create-edge-firewall-rules-engine-service.test.js
+++ src/tests/services/edge-firewall-rules-engine-services/v4/create-edge-firewall-rules-engine-service.test.js
@@ -67,8 +67,8 @@ describe('EdgeFirewallRulesEngineService', () => {
             argument: {
               type: 'second',
               limit_by: 'ip',
-              average_rate_limit: '10',
-              maximum_burst_size: '20'
+              average_rate_limit: 10,
+              maximum_burst_size: 20
             }
           },
           {
