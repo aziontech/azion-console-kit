0fe704111c71938846654e83438e4aba0a0e9d23
lucasmendes21 <lucas.mendes@azion.com>
2024-12-20 10:41:55 -0300

    refactor: adjust adapt criteria function

--- src/services/edge-application-rules-engine-services/v4/helper-criteria.js
+++ src/services/edge-application-rules-engine-services/v4/helper-criteria.js
@@ -1,13 +1,17 @@
+const isExistenceOperator = (operator) => ['exists', 'does_not_exist'].includes(operator)
+
+const processCriteria = (criteria) => {
+  if (!isExistenceOperator(criteria.operator)) {
+    return criteria
+  }
+
+  // eslint-disable-next-line no-unused-vars
+  const { argument, ...processedCriteria } = criteria
+  return processedCriteria
+}
+
 export const adaptCriteria = (criterias) => {
-    return criterias.map(criteriaArray => {
-        return criteriaArray.map(criteria => {
-            if (criteria.operator === 'exists' || criteria.operator === 'does_not_exist') {
-                // eslint-disable-next-line no-unused-vars
-                const { argument, ...rest } = criteria
-                return rest
-            } else {
-                return criteria
-            }
-        })
-    })
+  return criterias.map(criteriaArray => {
+    return criteriaArray.map(processCriteria)
+  })
 }
