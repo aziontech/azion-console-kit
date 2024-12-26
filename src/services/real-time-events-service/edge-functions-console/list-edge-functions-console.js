19430e2a2b20231debf94bdd167cc16cd9da6d47
Luis Igreja <lfsigreja@gmail.com>
2024-12-26 18:00:42 -0300

    fix: list-edge-functions-console api response handling

--- src/services/real-time-events-service/edge-functions-console/list-edge-functions-console.js
+++ src/services/real-time-events-service/edge-functions-console/list-edge-functions-console.js
@@ -4,6 +4,7 @@ import { makeRealTimeEventsBaseUrl } from '../make-real-time-events-service'
 import { generateCurrentTimestamp } from '@/helpers/generate-timestamp'
 import { convertValueToDate } from '@/helpers'
 import { useGraphQLStore } from '@/stores/graphql-query'
+import * as Errors from '@/services/axios/errors'
 
 export const listEdgeFunctionsConsole = async (filter) => {
   const payload = adapt(filter)
@@ -66,21 +67,36 @@ const levelMap = {
 }
 
 const adaptResponse = (response) => {
-  const { body } = response
+  const { body, statusCode } = response
 
-  return body.data.cellsConsoleEvents?.map((cellsConsoleEvents) => ({
-    configurationId: cellsConsoleEvents.configurationId,
-    functionId: cellsConsoleEvents.functionId,
-    id: generateCurrentTimestamp(),
-    originalId: cellsConsoleEvents.id,
-    level: levelMap[cellsConsoleEvents.level],
-    line: cellsConsoleEvents.line,
-    lineSource: {
-      content: cellsConsoleEvents.lineSource,
-      severity: 'info'
-    },
-    source: cellsConsoleEvents.source,
-    tsFormat: convertValueToDate(cellsConsoleEvents.ts),
-    ts: cellsConsoleEvents.ts
-  }))
+  switch (statusCode) {
+    case 200:
+      return body.data.cellsConsoleEvents?.map((cellsConsoleEvents) => ({
+        configurationId: cellsConsoleEvents.configurationId,
+        functionId: cellsConsoleEvents.functionId,
+        id: generateCurrentTimestamp(),
+        originalId: cellsConsoleEvents.id,
+        level: levelMap[cellsConsoleEvents.level],
+        line: cellsConsoleEvents.line,
+        lineSource: {
+          content: cellsConsoleEvents.lineSource,
+          severity: 'info'
+        },
+        source: cellsConsoleEvents.source,
+        tsFormat: convertValueToDate(cellsConsoleEvents.ts),
+        ts: cellsConsoleEvents.ts
+      }))
+    case 400:
+      const apiError = body.detail
+      throw new Error(apiError).message
+    case 403:
+      const forbiddenError = body.detail
+      throw new Error(forbiddenError).message
+    case 404:
+      throw new Errors.NotFoundError().message
+    case 500:
+      throw new Errors.InternalServerError().message
+    default:
+      throw new Errors.UnexpectedError().message
+  }
 }
