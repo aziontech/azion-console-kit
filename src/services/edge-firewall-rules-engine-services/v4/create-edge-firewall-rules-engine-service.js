9eda4a482fe8b440a6d9903abcf734ec2c1c18fa
HerbertJulio <herbert.julio@azion.com>
2025-01-07 16:15:11 -0300

    refactor: parse criteria and adapt payload in edge firewall rules engine services

--- src/services/edge-firewall-rules-engine-services/v4/create-edge-firewall-rules-engine-service.js
+++ src/services/edge-firewall-rules-engine-services/v4/create-edge-firewall-rules-engine-service.js
@@ -2,6 +2,7 @@ import { AxiosHttpClientAdapter } from '@/services/axios/AxiosHttpClientAdapter'
 import { makeEdgeFirewallBaseUrl } from '../../edge-firewall-services/v4/make-edge-firewall-base-url'
 import * as Errors from '@/services/axios/errors'
 import { extractApiError } from '@/helpers/extract-api-error'
+import { parseCriteria, parseBehaviors } from './parser-utils'
 
 export const createEdgeFirewallRulesEngineService = async ({ edgeFirewallId, payload }) => {
   let httpResponse = await AxiosHttpClientAdapter.request({
@@ -18,73 +19,6 @@ export const createEdgeFirewallRulesEngineService = async ({ edgeFirewallId, pay
  * @param {Array} behaviors
  * @returns {Array}
  */
-const parseBehaviors = (behaviors) => {
-  const parsedBehaviors = behaviors.map((behavior) => {
-    switch (behavior.name) {
-      case 'run_function':
-        return {
-          name: behavior.name,
-          argument: behavior.functionId
-        }
-      case 'set_waf_ruleset':
-        return {
-          name: behavior.name,
-          argument: {
-            mode: behavior.mode,
-            id: behavior.waf_id
-          }
-        }
-      case 'set_rate_limit':
-        const typeToEnableBurstSize = 'second'
-        const argument = {
-          type: behavior.type,
-          limit_by: behavior.limit_by,
-          average_rate_limit: Number(behavior.average_rate_limit)
-        }
-
-        if (behavior.type === typeToEnableBurstSize) {
-          argument.maximum_burst_size = Number(behavior.maximum_burst_size)
-        }
-
-        return {
-          name: behavior.name,
-          argument
-        }
-      case 'set_custom_response':
-        return {
-          name: behavior.name,
-          argument: {
-            status_code: behavior.status_code,
-            content_type: behavior.content_type,
-            content_body: behavior.content_body
-          }
-        }
-      default:
-        return {
-          name: behavior.name
-        }
-    }
-  })
-
-  return parsedBehaviors
-}
-
-const parseCriteria = (criteria) => {
-  const argumentTransformers = {
-    '${network}': (arg) => Number(arg)
-  }
-
-  return criteria.map((criterionGroup) =>
-    criterionGroup.map((criterion) => {
-      return {
-        ...criterion,
-        argument: argumentTransformers[criterion.variable]
-          ? argumentTransformers[criterion.variable](criterion.argument)
-          : criterion.argument
-      }
-    })
-  )
-}
 
 const adapt = (payload) => {
   const parsedPayload = {
