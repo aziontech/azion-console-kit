470c476691e5ed9a7edcc5ff3648b58362361637
paulosf0 <paulo.ferreira@azion.com>
2025-01-10 09:49:01 -0300

    feat: migrate network-list create to v4

--- src/services/network-lists-services/v4/index.js
+++ src/services/network-lists-services/v4/index.js
@@ -1,11 +1,12 @@
 import { listNetworkListService } from './list-network-list-service'
-
+import { createNetworkListService } from './create-network-list-service'
 /**
  * @typedef {Object} ExportedServicesType - The type of the exported services
  * @property {typeof listNetworkListService} listNetworkListService - The listNetworkListService reference
+ * @property {typeof createNetworkListService} createNetworkListService
  */
 
 /**
  * @type {ExportedServicesType}
  */
-export { listNetworkListService }
+export { listNetworkListService, createNetworkListService }
