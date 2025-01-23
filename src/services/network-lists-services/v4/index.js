ea1529d25204b78a489e5a63b12d65de15242dd2
Paulo Sobrinho Ferreira <110847590+pauloSF0@users.noreply.github.com>
2025-01-23 11:42:27 -0300

    [UXE-6003] feat: migrate network list delete to v4 (#2061)

--- src/services/network-lists-services/v4/index.js
+++ src/services/network-lists-services/v4/index.js
@@ -1,12 +1,14 @@
 import { listNetworkListService } from './list-network-list-service'
+import { deleteNetworkListService } from './delete-network-list-service'
 import { loadNetworkListService } from './load-network-list-service'
 
 /**
  * @typedef {Object} ExportedServicesType - The type of the exported services
  * @property {typeof listNetworkListService} listNetworkListService - The listNetworkListService reference
+ * @property {typeof deleteNetworkListService} deleteNetworkListService - The deleteNetworkListService reference
  */
 
 /**
  * @type {ExportedServicesType}
  */
-export { listNetworkListService, loadNetworkListService }
+export { listNetworkListService, deleteNetworkListService, loadNetworkListService }
