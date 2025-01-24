2831706da131fc8d99faada4c209a7ad5c14877d
Aloisio Bertolo Bastian <aloisiomiguelb@gmail.com>
2025-01-24 17:24:18 -0300

    feat: migrate edge dns zone creation to api v4

--- src/services/edge-dns-services/v4/index.js
+++ src/services/edge-dns-services/v4/index.js
@@ -1,3 +1,4 @@
 import { listEdgeDNSService } from './list-edge-dns-service'
+import { createEdgeDNSZonesService } from './create-edge-dns-zones-service'
 
-export { listEdgeDNSService }
+export { listEdgeDNSService, createEdgeDNSZonesService }
