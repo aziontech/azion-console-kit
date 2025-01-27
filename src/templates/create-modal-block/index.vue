4b2095f3a99d4045884aa66fcbbee73d86cd2352
peterpaulo-azion <peterson.silva@azion.com>
2025-01-27 17:19:21 -0300

    feat: add hide create options flag to create modal block

--- src/templates/create-modal-block/index.vue
+++ src/templates/create-modal-block/index.vue
@@ -1,263 +1,295 @@
 <script setup>
-  import PrimeButton from 'primevue/button'
-  import PrimeInputText from 'primevue/inputtext'
-  import LoadingState from './create-modal-block-loading-state.vue'
-  import { computed, onMounted, ref, inject } from 'vue'
-  import { useRouter } from 'vue-router'
-  import { useToast } from 'primevue/usetoast'
-  import { useAccountStore } from '@/stores/account'
+import PrimeButton from "primevue/button";
+import PrimeInputText from "primevue/inputtext";
+import LoadingState from "./create-modal-block-loading-state.vue";
+import { computed, onMounted, ref, inject } from "vue";
+import { useRouter } from "vue-router";
+import { useToast } from "primevue/usetoast";
+import { useAccountStore } from "@/stores/account";
 
-  /**@type {import('@/plugins/adapters/analytics/AnalyticsTrackerAdapter').AnalyticsTrackerAdapter} */
-  const tracker = inject('tracker')
-  defineOptions({
-    name: 'create-modal-block'
-  })
-  const props = defineProps({
-    listSolutionsService: {
-      type: Function,
-      required: true
-    }
-  })
+/**@type {import('@/plugins/adapters/analytics/AnalyticsTrackerAdapter').AnalyticsTrackerAdapter} */
+const tracker = inject("tracker");
+defineOptions({
+  name: "create-modal-block",
+});
+const props = defineProps({
+  listSolutionsService: {
+    type: Function,
+    required: true,
+  },
+});
 
-  const RESOURCES = [
-    {
-      label: 'Domains',
-      to: '/domains/create?origin=create',
-      description: 'Launch an edge application and set up security with digital certificates.'
-    },
-    {
-      label: 'Edge Application',
-      to: '/edge-applications/create?origin=create',
-      description: 'Deploy an edge application to deliver content from the edge.'
-    },
-    {
-      label: 'Variables',
-      to: '/variables/create',
-      description: 'Create environment variables or secrets to use with configured edge functions.'
-    },
-    {
-      label: 'Edge DNS',
-      to: '/edge-dns/create',
-      description: 'Use an authoritative DNS server to host a domain.'
-    },
-    {
-      label: 'Edge Firewall',
-      to: '/edge-firewall/create',
-      description: 'Create security settings to protect applications against threats and attacks.'
-    },
-    {
-      label: 'Edge Nodes',
-      to: '/edge-node/create',
-      description: 'Create edge infrastructure, installing services and resources in real time.'
-    },
-    {
-      label: 'Data Stream',
-      to: '/data-stream/create',
-      description: 'Feed streamimg, SIEM, and big data platforms with the event logs from Azion.'
-    },
-    {
-      label: 'Edge Functions',
-      to: '/edge-functions/create?origin=create',
-      description: 'Create edge functions to use with Edge Application or Edge Firewall.'
-    },
-    {
-      label: 'Edge Services',
-      to: '/edge-services/create',
-      description: 'Create new services to define dependencies between edge resources.'
-    },
-    {
-      label: 'Digital Certificates',
-      to: '/digital-certificates/create',
-      description: 'Add a digital certificate entry to secure HTTPS edge applications.'
-    },
-    {
-      label: 'Network Lists',
-      to: '/network-lists/create',
-      description:
-        'Add allowlists, blocklists, and greylists to use with Rules Engine for Edge Firewall.'
-    }
-  ]
+const accountStore = useAccountStore();
 
-  const TABS = [
-    {
-      label: 'Recommended',
-      value: 'recommended'
+const hideCreateOptions = computed(() => accountStore.hasHideCreateOptionsFlag);
+
+const RESOURCES = [
+  {
+    label: "Domains",
+    to: "/domains/create?origin=create",
+    description:
+      "Launch an edge application and set up security with digital certificates.",
+  },
+  {
+    label: "Edge Application",
+    to: "/edge-applications/create?origin=create",
+    description: "Deploy an edge application to deliver content from the edge.",
+  },
+  {
+    label: "Variables",
+    to: "/variables/create",
+    description:
+      "Create environment variables or secrets to use with configured edge functions.",
+  },
+  {
+    label: "Edge DNS",
+    to: "/edge-dns/create",
+    description: "Use an authoritative DNS server to host a domain.",
+  },
+  {
+    label: "Edge Firewall",
+    to: "/edge-firewall/create",
+    description:
+      "Create security settings to protect applications against threats and attacks.",
+  },
+  {
+    label: "Edge Nodes",
+    to: "/edge-node/create",
+    description:
+      "Create edge infrastructure, installing services and resources in real time.",
+  },
+  {
+    label: "Data Stream",
+    to: "/data-stream/create",
+    description:
+      "Feed streamimg, SIEM, and big data platforms with the event logs from Azion.",
+  },
+  {
+    label: "Edge Functions",
+    to: "/edge-functions/create?origin=create",
+    description: "Create edge functions to use with Edge Application or Edge Firewall.",
+  },
+  {
+    label: "Edge Services",
+    to: "/edge-services/create",
+    description: "Create new services to define dependencies between edge resources.",
+  },
+  {
+    label: "Digital Certificates",
+    to: "/digital-certificates/create",
+    description: "Add a digital certificate entry to secure HTTPS edge applications.",
+  },
+  {
+    label: "Network Lists",
+    to: "/network-lists/create",
+    description:
+      "Add allowlists, blocklists, and greylists to use with Rules Engine for Edge Firewall.",
+  },
+];
+
+const TABS = [
+  {
+    label: "Recommended",
+    value: "recommended",
+    show: !hideCreateOptions.value,
+  },
+  {
+    label: "Templates",
+    value: "templates",
+    show: !hideCreateOptions.value,
+  },
+  {
+    label: "Resources",
+    value: "newResource",
+    show: true,
+  },
+  {
+    label: "Import from GitHub",
+    value: "githubImport",
+    show: !hideCreateOptions.value,
+  },
+];
+
+const filteredTabs = computed(() => {
+  return TABS.filter((menuitem) => menuitem.show);
+});
+
+const emit = defineEmits("closeModal");
+
+onMounted(async () => {
+  await loadRecommendedSolutions();
+});
+
+const router = useRouter();
+const toast = useToast();
+
+const isLoading = ref(false);
+const selectedTab = ref("recommended");
+const search = ref("");
+const isSearching = computed(() => !!search.value.trim().length);
+
+if (hideCreateOptions.value) {
+  selectedTab.value = "newResource";
+}
+
+const templatesData = ref({
+  recommended: [],
+  templates: [],
+  newResource: RESOURCES,
+  githubImport: [],
+});
+
+const tabInfo = computed(() => {
+  return {
+    recommended: {
+      show: selectedTab.value === "recommended",
+      title: "Select a Template",
     },
-    {
-      label: 'Templates',
-      value: 'templates'
+    templates: { show: selectedTab.value === "templates", title: "Select a Template" },
+    newResource: {
+      show: selectedTab.value === "newResource",
+      title: "Select a Resource",
     },
-    {
-      label: 'Resources',
-      value: 'newResource'
+    githubImport: {
+      show: selectedTab.value === "githubImport",
+      title: "Import from GitHub",
     },
-    {
-      label: 'Import from GitHub',
-      value: 'githubImport'
-    }
-  ]
+  };
+});
 
-  const emit = defineEmits('closeModal')
+const toastBuilder = (severity, detail) => {
+  if (!detail) return;
+  const options = {
+    closable: true,
+    severity,
+    summary: severity,
+    detail,
+  };
 
-  onMounted(async () => {
-    await loadRecommendedSolutions()
-  })
+  toast.add(options);
+};
 
-  const router = useRouter()
-  const toast = useToast()
+const loadSolutions = async ({ group, type }) => {
+  try {
+    isLoading.value = true;
+    templatesData.value[group] = await props.listSolutionsService({ group, type });
+  } catch (error) {
+    toastBuilder("error", error);
+  } finally {
+    isLoading.value = false;
+  }
+};
 
-  const isLoading = ref(false)
-  const selectedTab = ref('recommended')
-  const search = ref('')
-  const isSearching = computed(() => !!search.value.trim().length)
+const loadRecommendedSolutions = async () => {
+  await loadSolutions({ group: "recommended", type: accountStore.accountData.jobRole });
+};
 
-  const templatesData = ref({
-    recommended: [],
-    templates: [],
-    newResource: RESOURCES,
-    githubImport: []
-  })
+const loadTemplates = async () => {
+  await loadSolutions({ group: "templates", type: "onboarding" });
+};
 
-  const tabInfo = computed(() => {
-    return {
-      recommended: { show: selectedTab.value === 'recommended', title: 'Select a Template' },
-      templates: { show: selectedTab.value === 'templates', title: 'Select a Template' },
-      newResource: { show: selectedTab.value === 'newResource', title: 'Select a Resource' },
-      githubImport: { show: selectedTab.value === 'githubImport', title: 'Import from GitHub' }
-    }
-  })
+const loadGithubImportSolution = async () => {
+  await loadSolutions({ group: "githubImport", type: "import-from-github" });
+};
 
-  const toastBuilder = (severity, detail) => {
-    if (!detail) return
-    const options = {
-      closable: true,
-      severity,
-      summary: severity,
-      detail
-    }
+const redirect = (toLink, selection) => {
+  tracker.create.selectedOnCreate({
+    section: "resources",
+    selection,
+  });
+  router.push(toLink);
+  emit("closeModal");
+};
 
-    toast.add(options)
+const redirectToSolution = (template, section) => {
+  tracker.create.selectedOnCreate({
+    section,
+    selection: template.name,
+  });
+  const params = {
+    vendor: template.vendor.slug,
+    solution: template.slug,
+  };
+  router.push({ name: "create-something-new", params });
+  emit("closeModal");
+};
+
+const redirectGithubImport = (template, section) => {
+  tracker.create.selectedOnCreate({
+    section,
+    selection: template.name,
+  });
+  const params = {
+    vendor: template.vendor.slug,
+    solution: template.slug,
+  };
+
+  router.push({ name: "github-repository-import", params });
+  emit("closeModal");
+};
+
+const onTabChange = async (target) => {
+  resetFilters();
+  if (isLoading.value) {
+    return;
   }
 
-  const loadSolutions = async ({ group, type }) => {
-    try {
-      isLoading.value = true
-      templatesData.value[group] = await props.listSolutionsService({ group, type })
-    } catch (error) {
-      toastBuilder('error', error)
-    } finally {
-      isLoading.value = false
-    }
+  selectedTab.value = target.value || selectedTab.value;
+  if (target.value === "templates" && templatesData.value.templates.length === 0) {
+    await loadTemplates();
+  } else if (
+    target.value === "githubImport" &&
+    templatesData.value.githubImport.length === 0
+  ) {
+    await loadGithubImportSolution();
+  }
+};
+
+const filterBySearchField = (filter) => {
+  if (!filter?.trim()) return templatesData.value[selectedTab.value];
+
+  return templatesData.value[selectedTab.value].filter((template) => {
+    return Object.keys(template).some((key) => {
+      const props = { template, key, filter };
+
+      if (key === "vendor" || key === "instanceType" || key === "category") {
+        return findTemplatesByFilter({ ...props, nestedKey: "name" });
+      }
+
+      if (typeof template[key] !== "string") return;
+
+      return findTemplatesByFilter(props);
+    });
+  });
+};
+
+const findTemplatesByFilter = ({ template, key, filter, nestedKey = null }) => {
+  if (nestedKey) {
+    return template[key][nestedKey]?.toLowerCase().includes(filter.toLowerCase());
   }
 
-  const loadRecommendedSolutions = async () => {
-    const accountStore = useAccountStore().accountData
-    await loadSolutions({ group: 'recommended', type: accountStore.jobRole })
-  }
+  return template[key].toLowerCase().includes(filter.toLowerCase());
+};
 
-  const loadTemplates = async () => {
-    await loadSolutions({ group: 'templates', type: 'onboarding' })
-  }
+const filteredTemplates = computed(() => {
+  return filterBySearchField(search.value);
+});
 
-  const loadGithubImportSolution = async () => {
-    await loadSolutions({ group: 'githubImport', type: 'import-from-github' })
-  }
+const resetFilters = () => {
+  search.value = "";
+};
 
-  const redirect = (toLink, selection) => {
-    tracker.create.selectedOnCreate({
-      section: 'resources',
-      selection
-    })
-    router.push(toLink)
-    emit('closeModal')
-  }
+const resultsText = computed(() => {
+  const options = {
+    zero: "No results found.",
+    one: "1 search result for",
+    multiple: `${filteredTemplates.value.length} search results for`,
+  };
 
-  const redirectToSolution = (template, section) => {
-    tracker.create.selectedOnCreate({
-      section,
-      selection: template.name
-    })
-    const params = {
-      vendor: template.vendor.slug,
-      solution: template.slug
-    }
-    router.push({ name: 'create-something-new', params })
-    emit('closeModal')
-  }
-
-  const redirectGithubImport = (template, section) => {
-    tracker.create.selectedOnCreate({
-      section,
-      selection: template.name
-    })
-    const params = {
-      vendor: template.vendor.slug,
-      solution: template.slug
-    }
-
-    router.push({ name: 'github-repository-import', params })
-    emit('closeModal')
-  }
-
-  const onTabChange = async (target) => {
-    resetFilters()
-    if (isLoading.value) {
-      return
-    }
-
-    selectedTab.value = target.value || selectedTab.value
-    if (target.value === 'templates' && templatesData.value.templates.length === 0) {
-      await loadTemplates()
-    } else if (target.value === 'githubImport' && templatesData.value.githubImport.length === 0) {
-      await loadGithubImportSolution()
-    }
-  }
-
-  const filterBySearchField = (filter) => {
-    if (!filter?.trim()) return templatesData.value[selectedTab.value]
-
-    return templatesData.value[selectedTab.value].filter((template) => {
-      return Object.keys(template).some((key) => {
-        const props = { template, key, filter }
-
-        if (key === 'vendor' || key === 'instanceType' || key === 'category') {
-          return findTemplatesByFilter({ ...props, nestedKey: 'name' })
-        }
-
-        if (typeof template[key] !== 'string') return
-
-        return findTemplatesByFilter(props)
-      })
-    })
-  }
-
-  const findTemplatesByFilter = ({ template, key, filter, nestedKey = null }) => {
-    if (nestedKey) {
-      return template[key][nestedKey]?.toLowerCase().includes(filter.toLowerCase())
-    }
-
-    return template[key].toLowerCase().includes(filter.toLowerCase())
-  }
-
-  const filteredTemplates = computed(() => {
-    return filterBySearchField(search.value)
-  })
-
-  const resetFilters = () => {
-    search.value = ''
-  }
-
-  const resultsText = computed(() => {
-    const options = {
-      zero: 'No results found.',
-      one: '1 search result for',
-      multiple: `${filteredTemplates.value.length} search results for`
-    }
-
-    if (!filteredTemplates.value.length) return options.zero
-    if (filteredTemplates.value.length === 1) return options.one
-    return options.multiple
-  })
+  if (!filteredTemplates.value.length) return options.zero;
+  if (filteredTemplates.value.length === 1) return options.one;
+  return options.multiple;
+});
 </script>
 
 <template>
@@ -265,25 +297,22 @@
     class="overflow-auto w-full h-full flex flex-col sm:flex-row gap-4"
     data-testid="integrations-list-container"
   >
-    <div
-      class="-ml-2 sm:min-w-[240px]"
-      data-testid="integrations-list-menu"
-    >
+    <div class="-ml-2 sm:min-w-[240px]" data-testid="integrations-list-menu">
       <ul
         class="flex flex-col gap-1 md:fixed md:w-60"
         data-testid="integrations-list-menu-items"
       >
-        <li
-          v-for="(menuitem, index) in TABS"
-          :key="index"
-        >
+        <li v-for="(menuitem, index) in filteredTabs" :key="index">
           <PrimeButton
-            :class="{ 'surface-200': menuitem.value === selectedTab, 'p-disabled': isLoading }"
+            :class="{
+              'surface-200': menuitem.value === selectedTab,
+              'p-disabled': isLoading,
+            }"
             class="w-full whitespace-nowrap h-[38px] flex"
             text
             size="small"
             :pt="{
-              label: { class: 'w-full text-left' }
+              label: { class: 'w-full text-left' },
             }"
             @click="onTabChange(menuitem)"
             :label="menuitem.label"
@@ -303,10 +332,7 @@
         v-else
         data-testid="integrations-list-content-body"
       >
-        <div
-          class="flex flex-col gap-3"
-          data-testid="integrations-list-content-header"
-        >
+        <div class="flex flex-col gap-3" data-testid="integrations-list-content-header">
           <div class="text-base font-medium">
             {{ tabInfo[selectedTab].title }}
           </div>
@@ -346,7 +372,6 @@
           </template>
         </template>
       </div>
-
       <div
         class="mx-0 w-full mt-0 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4"
         v-if="tabInfo.recommended.show"
