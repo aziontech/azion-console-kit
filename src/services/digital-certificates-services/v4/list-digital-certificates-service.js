38e85df8b83589fd51bbaf709e29b999274abfd5
lucasmendes21 <lucas.mendes@azion.com>
2025-01-30 23:00:10 -0300

    fix: update eslint workflow

--- src/services/digital-certificates-services/v4/list-digital-certificates-service.js
+++ src/services/digital-certificates-services/v4/list-digital-certificates-service.js
@@ -4,89 +4,88 @@ import { capitalizeFirstLetter } from '@/helpers'
 import { makeListServiceQueryParams } from '@/helpers/make-list-service-query-params'
 
 export const listDigitalCertificatesService = async ({
-    search = '',
-    fields = '',
-    ordering = 'name',
-    page = 1,
-    pageSize = 10
+  search = '',
+  fields = '',
+  ordering = 'name',
+  page = 1,
+  pageSize = 10
 }) => {
-    const searchParams = makeListServiceQueryParams({ fields, ordering, page, pageSize, search })
-    let httpResponse = await AxiosHttpClientAdapter.request({
-        url: `${makeDigitalCertificatesBaseUrl()}?${searchParams.toString()}`,
-        method: 'GET'
-    })
+  const searchParams = makeListServiceQueryParams({ fields, ordering, page, pageSize, search })
+  let httpResponse = await AxiosHttpClientAdapter.request({
+    url: `${makeDigitalCertificatesBaseUrl()}?${searchParams.toString()}`,
+    method: 'GET'
+  })
 
-    httpResponse = adapt(httpResponse)
+  httpResponse = adapt(httpResponse)
 
-    return parseHttpResponse(httpResponse)
+  return parseHttpResponse(httpResponse)
 }
 
 export const EDGE_CERTIFICATE = 'TLS Certificate'
 export const TRUSTED_CA_CERTIFICATE = 'Trusted CA Certificate'
 
 const parseStatusData = (status) => {
-    const isActive = status.toUpperCase() === 'ACTIVE'
-    const parsedStatus = isActive
-        ? {
-            content: capitalizeFirstLetter(status),
-            severity:
-                'success'
-        }
-        : {
-            content: capitalizeFirstLetter(status),
-            severity: 'danger'
-        }
+  const isActive = status.toUpperCase() === 'ACTIVE'
+  const parsedStatus = isActive
+    ? {
+        content: capitalizeFirstLetter(status),
+        severity: 'success'
+      }
+    : {
+        content: capitalizeFirstLetter(status),
+        severity: 'danger'
+      }
 
-    return parsedStatus
+  return parsedStatus
 }
 
 const parseValidityDate = (validity) => {
-    const formatOptions = {
-        month: 'short',
-        day: 'numeric',
-        year: 'numeric',
-        hour: 'numeric',
-        minute: 'numeric',
-        hour12: true
-    }
+  const formatOptions = {
+    month: 'short',
+    day: 'numeric',
+    year: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    hour12: true
+  }
 
-    const dateFormatted = new Intl.DateTimeFormat('en-US', formatOptions).format(new Date(validity))
+  const dateFormatted = new Intl.DateTimeFormat('en-US', formatOptions).format(new Date(validity))
 
-    return dateFormatted
+  return dateFormatted
 }
 
 const checkIfFieldExist = (field, defaultValue = '-') => field ?? defaultValue
 
 const adapt = (httpResponse) => {
-    const isArray = Array.isArray(httpResponse.body?.results)
+  const isArray = Array.isArray(httpResponse.body?.results)
 
-    const parsedDigitalCertificates = isArray
-        ? httpResponse.body.results?.map((item) => {
-            const subjectNames = checkIfFieldExist(
-                item?.subject_name?.map((subject) => subject)?.join(',')
-            )
-            const typeMap = {
-                edge_certificate: EDGE_CERTIFICATE,
-                trusted_ca_certificate: TRUSTED_CA_CERTIFICATE
-            }
+  const parsedDigitalCertificates = isArray
+    ? httpResponse.body.results?.map((item) => {
+        const subjectNames = checkIfFieldExist(
+          item?.subject_name?.map((subject) => subject)?.join(',')
+        )
+        const typeMap = {
+          edge_certificate: EDGE_CERTIFICATE,
+          trusted_ca_certificate: TRUSTED_CA_CERTIFICATE
+        }
 
-            return {
-                id: checkIfFieldExist(item?.id, null),
-                name: checkIfFieldExist(item?.name),
-                issuer: checkIfFieldExist(item?.issuer),
-                subjectName: subjectNames,
-                type: checkIfFieldExist(typeMap[item?.type]),
-                validity: item?.validity ? parseValidityDate(item.validity) : '-',
-                status: item?.status ? parseStatusData(item.status) : '-'
-            }
-        })
-        : []
+        return {
+          id: checkIfFieldExist(item?.id, null),
+          name: checkIfFieldExist(item?.name),
+          issuer: checkIfFieldExist(item?.issuer),
+          subjectName: subjectNames,
+          type: checkIfFieldExist(typeMap[item?.type]),
+          validity: item?.validity ? parseValidityDate(item.validity) : '-',
+          status: item?.status ? parseStatusData(item.status) : '-'
+        }
+      })
+    : []
 
-    const count = httpResponse.body?.count ?? 0
+  const count = httpResponse.body?.count ?? 0
 
-    return {
-        count,
-        body: parsedDigitalCertificates,
-        statusCode: httpResponse.statusCode
-    }
+  return {
+    count,
+    body: parsedDigitalCertificates,
+    statusCode: httpResponse.statusCode
+  }
 }
