782073a9696d8535aa08a9733ba497fb5e2b783f
peterpaulo-azion <peterson.silva@azion.com>
2025-02-03 15:34:50 -0300

    feat: enhance edge node list view with fetch table block

--- src/views/EdgeNode/ListView.vue
+++ src/views/EdgeNode/ListView.vue
@@ -1,7 +1,7 @@
 <script setup>
   import ContentBlock from '@/templates/content-block'
   import EmptyEdgeNode from '@/templates/empty-results-block/empty-edge-node'
-  import ListTableBlock from '@/templates/list-table-block'
+  import FetchListTableBlock from '@/templates/list-table-block/with-fetch-ordering-and-pagination.vue'
   import { columnBuilder } from '@/templates/list-table-block/columns/column-builder'
   import PageHeadingBlock from '@/templates/page-heading-block'
   import Authorize from '@/views/EdgeNode/Dialog/Authorize'
@@ -75,6 +75,8 @@
       }
     }
   ]
+
+  const EDGE_NODE_API_FIELDS = ['id', 'name', 'hash_id', 'status']
 </script>
 <template>
   <ContentBlock>
@@ -82,8 +84,9 @@
       <PageHeadingBlock pageTitle="Edge Nodes"></PageHeadingBlock>
     </template>
     <template #content>
-      <ListTableBlock
+      <FetchListTableBlock
         v-if="hasContentToList"
+        :apiFields="EDGE_NODE_API_FIELDS"
         :listService="listEdgeNodeService"
         :columns="getColumns"
         editPagePath="edge-node/edit"
