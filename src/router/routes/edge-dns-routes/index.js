4b5224404ecf4352d9f3febbc837de84c9becc94
Aloisio Bastian <aloisiomiguelb@gmail.com>
2025-02-11 09:42:01 -0300

    [UXE-6155] feat: migrate edge dns records list to api v4 (#2137)

--- src/router/routes/edge-dns-routes/index.js
+++ src/router/routes/edge-dns-routes/index.js
@@ -1,7 +1,7 @@
 import * as EdgeDNSService from '@/services/edge-dns-services'
 import * as EdgeDNSServiceV4 from '@/services/edge-dns-services/v4'
-
 import * as EdgeDNSRecordsService from '@/services/edge-dns-records-services'
+import * as EdgeDNSRecordsServiceV4 from '@/services/edge-dns-records-services/v4'
 import * as Helpers from '@/helpers'
 
 /** @type {import('vue-router').RouteRecordRaw} */
@@ -56,7 +56,7 @@ export const edgeDnsRoutes = {
       props: {
         editEdgeDNSService: EdgeDNSServiceV4.editEdgeDNSService,
         loadEdgeDNSService: EdgeDNSServiceV4.loadEdgeDNSService,
-        listRecordsService: EdgeDNSRecordsService.listRecordsService,
+        listRecordsService: EdgeDNSRecordsServiceV4.listRecordsService,
         deleteRecordsService: EdgeDNSRecordsService.deleteRecordsService,
         clipboardWrite: Helpers.clipboardWrite,
         updatedRedirect: 'list-edge-dns'
@@ -80,7 +80,7 @@ export const edgeDnsRoutes = {
       props: {
         editEdgeDNSService: EdgeDNSService.editEdgeDNSService,
         loadEdgeDNSService: EdgeDNSService.loadEdgeDNSService,
-        listRecordsService: EdgeDNSRecordsService.listRecordsService,
+        listRecordsService: EdgeDNSRecordsServiceV4.listRecordsService,
         deleteRecordsService: EdgeDNSRecordsService.deleteRecordsService,
         createRecordsService: EdgeDNSRecordsService.createRecordsService,
         editRecordsService: EdgeDNSRecordsService.editRecordsService,
