3a07ea77276116d180bd6dd953a36e6cc4516843
paulosf0 <paulo.ferreira@azion.com>
2025-02-25 17:15:42 -0300

    chore: upgrade from version-1.32.0 to version-1.32.0

--- src/tests/services/real-time-events-service/get-total-records.test.js
+++ src/tests/services/real-time-events-service/get-total-records.test.js
@@ -53,7 +53,7 @@ describe('getTotalRecords', () => {
     ].join('\n')
 
     expect(requestSpy).toHaveBeenCalledWith({
-      url: 'v3/events/graphql',
+      url: 'v4/events/graphql',
       method: 'POST',
       signal: undefined,
       body: {
