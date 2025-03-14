327d13a0e2c8f8e0e865802d56243c78a6ee5fab
lucasmendes21 <lucas.mendes@azion.com>
2025-03-14 09:54:50 -0300

    chore: adjust lint

--- src/helpers/invoice.js
+++ src/helpers/invoice.js
@@ -1,9 +1,10 @@
 const { VITE_ENVIRONMENT } = import.meta.env
 
 const getLinkDownloadInvoice = (period) => {
-  const URL = VITE_ENVIRONMENT === 'production'
-    ? `https://console.azion.com/v4/billing/invoices/${period}`
-    : `https://stage-console.azion.com/v4/billing/invoices/${period}`
+  const URL =
+    VITE_ENVIRONMENT === 'production'
+      ? `https://console.azion.com/v4/billing/invoices/${period}`
+      : `https://stage-console.azion.com/v4/billing/invoices/${period}`
   return URL
 }
 
