ff06414c0ea7e1b6eec36c1eb81bf1d154cb0a45
lucasmendes21 <lucas.mendes@azion.com>
2025-03-17 10:55:28 -0300

    chore: update download invoice link

--- src/helpers/invoice.js
+++ src/helpers/invoice.js
@@ -3,8 +3,8 @@ const { VITE_ENVIRONMENT } = import.meta.env
 const getLinkDownloadInvoice = (period) => {
   const URL =
     VITE_ENVIRONMENT === 'production'
-      ? `https://console.azion.com/v4/billing/invoices/${period}`
-      : `https://stage-console.azion.com/v4/billing/invoices/${period}`
+      ? `https://console.azion.com/billing/invoices/${period}`
+      : `https://stage-console.azion.com/billing/invoices/${period}`
   return URL
 }
 
