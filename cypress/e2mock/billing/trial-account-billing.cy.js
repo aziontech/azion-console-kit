c958dc32b435f6fd8c6500e6bb9a7e5d12c8504c
Aloisio Bertolo Bastian <aloisiomiguelb@gmail.com>
2025-04-15 10:42:47 -0300

    fix: add a helper to do the date parsing

--- cypress/e2mock/billing/trial-account-billing.cy.js
+++ cypress/e2mock/billing/trial-account-billing.cy.js
@@ -1,16 +1,4 @@
-function getCurrentMonthStartEnd() {
-  const today = new Date()
-  const year = today.getFullYear()
-  const month = today.getMonth()
-
-  const dateInitial = new Date(year, month, 1)
-  const dateFinal = new Date(year, month + 1, 0)
-
-  return {
-    dateInitial: dateInitial.toISOString().split('T')[0],
-    dateFinal: dateFinal.toISOString().split('T')[0]
-  }
-}
+import { getCurrentMonthStartEnd } from '@/helpers/get-current-month-start-end'
 describe('Billing spec', { tags: ['@dev2'] }, () => {
   beforeEach(() => {
     cy.intercept('GET', '/api/account/info', {
---
+++ src/helpers/get-current-month-start-end.js
@@ -0,0 +1,13 @@
+export const getCurrentMonthStartEnd = () => {
+  const today = new Date()
+  const year = today.getFullYear()
+  const month = today.getMonth()
+
+  const dateInitial = new Date(year, month, 1)
+  const dateFinal = new Date(year, month + 1, 0)
+
+  return {
+    dateInitial: dateInitial.toISOString().split('T')[0],
+    dateFinal: dateFinal.toISOString().split('T')[0]
+  }
+}
