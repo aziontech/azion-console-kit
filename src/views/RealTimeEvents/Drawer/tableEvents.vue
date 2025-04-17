51fdfbcf7a2c5ee335717b3c7ff7d5fc7b8499b6
Lucas Mendes <92529166+lucasmendes21@users.noreply.github.com>
2025-04-17 18:28:46 -0300

    [UXE-6854] feat: add wafEvheaders field to Real Time Events drawer (#2372)

--- src/views/RealTimeEvents/Drawer/tableEvents.vue
+++ src/views/RealTimeEvents/Drawer/tableEvents.vue
@@ -27,15 +27,47 @@
         field="value"
         header="Value"
         style="width: 60%"
-      ></Column>
+      >
+        <template #body="{ data }">
+          <div
+            v-if="data.value?.type === 'clipboard'"
+            class="flex gap-4 items-center w-full"
+          >
+            <div
+              class="w-full max-w-[200px] sm:max-w-sm overflow-y-scroll"
+              data-testid="data-table-value"
+              :class="hasContent(data.value.content) ?? 'p-2'"
+            >
+              {{ data.value.content }}
+            </div>
+            <PrimeButton
+              outlined
+              icon="pi pi-copy"
+              class="max-md:w-full"
+              @click="handleCopy(data.value.content)"
+              data-testid="data-table-copy-button"
+              v-if="hasContent(data.value.content)"
+            />
+          </div>
+          <div
+            v-else
+            class="w-full"
+          >
+            {{ data.value }}
+          </div>
+        </template>
+      </Column>
     </DataTable>
   </div>
 </template>
 <script setup>
+  import PrimeButton from 'primevue/button'
   import DataTable from 'primevue/datatable'
   import Column from 'primevue/column'
   import { FilterMatchMode } from 'primevue/api'
   import InputText from 'primevue/inputtext'
+  import { useToast } from 'primevue/usetoast'
+  import { clipboardWrite } from '@/helpers/clipboard'
 
   import { ref } from 'vue'
 
@@ -46,7 +78,21 @@
     }
   })
 
+  const toast = useToast()
+
   const filters = ref({
     global: { value: '', matchMode: FilterMatchMode.CONTAINS }
   })
+
+  const handleCopy = (value) => {
+    clipboardWrite(value)
+    toast.add({
+      closable: true,
+      severity: 'success',
+      summary: 'Success',
+      detail: 'Successfully copied!'
+    })
+  }
+
+  const hasContent = (content) => content !== '-'
 </script>
