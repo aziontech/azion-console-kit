00badbb8c8aa51b59794200b51ee7d0599582bb7
Peterson Paulo <109550332+peterpaulo-azion@users.noreply.github.com>
2025-04-25 20:28:34 -0300

    [UXE-7130] feat: new custom pages get and delete features (#2380)
    * feat: new custom pages get and delete features
    
    * fix: documentation service in custom pages routes

---
+++ src/router/routes/custom-pages-routes/index.js
@@ -0,0 +1,28 @@
+import * as Helpers from '@/helpers'
+import * as CustomPagesServiceV4 from '@/services/custom-pages-services/v4'
+
+/** @type {import('vue-router').RouteRecordRaw} */
+export const customPagesRoutes = {
+  path: '/custom-pages',
+  name: 'custom-pages',
+  children: [
+    {
+      path: '',
+      name: 'list-custom-pages',
+      component: () => import('@views/CustomPages/ListView.vue'),
+      props: {
+        listCustomPagesService: CustomPagesServiceV4.listCustomPagesService,
+        deleteCustomPagesService: CustomPagesServiceV4.deleteCustomPagesService,
+        documentationService: Helpers.documentationCatalog.customPages
+      },
+      meta: {
+        breadCrumbs: [
+          {
+            label: 'Custom Pages',
+            to: '/custom-pages'
+          }
+        ]
+      }
+    }
+  ]
+}
