527a9626679e11111c0f7b1625bd1188a327ba25
HerbertJulio <herbert.julio@azion.com>
2025-04-29 14:36:36 -0300

    feat: enhance feature flag management and integration in user and edge application services

--- src/services/edge-application-services/handleServicesByFlag.js
+++ src/services/edge-application-services/handleServicesByFlag.js
@@ -1,9 +1,10 @@
 import * as EdgeApplicationsServiceV3 from '@/services/edge-application-services'
 import * as EdgeApplicationsServiceV4 from '@/services/edge-application-services/v4'
+import { hasFlagBlockApiV4 } from '@/composables/user-flag'
 
 const useEdgeApplicationServices = () => {
   // eslint-disable-next-line no-constant-condition
-  if (true) {
+  if (hasFlagBlockApiV4()) {
     return {
       listEdgeApplicationsService: EdgeApplicationsServiceV4.listEdgeApplicationsService,
       deleteEdgeApplicationService: EdgeApplicationsServiceV4.deleteEdgeApplicationService,
@@ -13,16 +14,14 @@ const useEdgeApplicationServices = () => {
       contactSalesEdgeApplicationService:
         EdgeApplicationsServiceV3.contactSalesEdgeApplicationService
     }
-  } else {
-    return {
-      listEdgeApplicationsService: EdgeApplicationsServiceV4.listEdgeApplicationsService,
-      deleteEdgeApplicationService: EdgeApplicationsServiceV4.deleteEdgeApplicationService,
-      createEdgeApplicationService: EdgeApplicationsServiceV3.createEdgeApplicationService,
-      editEdgeApplicationService: EdgeApplicationsServiceV3.editEdgeApplicationService,
-      loadEdgeApplicationService: EdgeApplicationsServiceV3.loadEdgeApplicationService,
-      contactSalesEdgeApplicationService:
-        EdgeApplicationsServiceV3.contactSalesEdgeApplicationService
-    }
+  }
+  return {
+    listEdgeApplicationsService: EdgeApplicationsServiceV4.listEdgeApplicationsService,
+    deleteEdgeApplicationService: EdgeApplicationsServiceV4.deleteEdgeApplicationService,
+    createEdgeApplicationService: EdgeApplicationsServiceV3.createEdgeApplicationService,
+    editEdgeApplicationService: EdgeApplicationsServiceV3.editEdgeApplicationService,
+    loadEdgeApplicationService: EdgeApplicationsServiceV3.loadEdgeApplicationService,
+    contactSalesEdgeApplicationService: EdgeApplicationsServiceV3.contactSalesEdgeApplicationService
   }
 }
 
