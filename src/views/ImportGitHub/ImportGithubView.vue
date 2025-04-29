4ab06079a7e71bad8d3e0ff8aef310d52790de18
Herbert Vicente Cotta Julio <hvcotta@gmail.com>
2025-04-29 10:19:56 -0300

    refactor: remove 'mode' field from ImportGithubView form validation and data structure (#2390)

--- src/views/ImportGitHub/ImportGithubView.vue
+++ src/views/ImportGitHub/ImportGithubView.vue
@@ -66,7 +66,6 @@
       .matches(/^\S*$/, 'Root Directory cannot contain spaces')
       .label('Root Directory'),
     preset: yup.string().required().label('Preset'),
-    mode: yup.string().required().label('Mode'),
     repository: yup.string().required().label('Repository'),
     installCommand: yup.string().required().label('Install Command'),
     gitScope: yup.string().required().label('Git Scope'),
@@ -87,7 +86,6 @@
     rootDirectory: '/',
     preset: '',
     newVariables: [],
-    mode: '',
     repository: '',
     installCommand: '',
     gitScope: ''
@@ -121,11 +119,6 @@
         field: 'vulcan_preset',
         instantiation_data_path: 'envs.[2].value',
         value: formValues.preset
-      },
-      {
-        field: 'vulcan_mode',
-        instantiation_data_path: 'envs.[3].value',
-        value: formValues.mode
       },
       {
         field: 'az_command',
