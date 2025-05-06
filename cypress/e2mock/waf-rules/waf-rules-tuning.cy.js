4581c2b5b839f965b852ca653eb45074c0cc11ed
HerbertJulio <herbert.julio@azion.com>
2025-05-06 14:33:24 -0300

    test: update Waf Rules Tuning tests to include detailed rule creation without path

--- cypress/e2mock/waf-rules/waf-rules-tuning.cy.js
+++ cypress/e2mock/waf-rules/waf-rules-tuning.cy.js
@@ -17,7 +17,7 @@ describe('Waf Rules Tuning', { tags: ['@dev4'] }, () => {
     cy.wait('@wafMainSettings', { timeout: 30000 })
   })
 
-  it('should allow correctly rule with ruleId 11', () => {
+  it.skip('should allow to create correct rule with ruleId 11 in more details', () => {
     cy.intercept('GET', '/api/v3/waf/*/domains?page_size=200', {
       fixture: '/waf-rules/tuning/available-domains.json'
     }).as('wafDomains')
@@ -45,6 +45,42 @@ describe('Waf Rules Tuning', { tags: ['@dev4'] }, () => {
         zone: 'request_body',
         matches_on: 'value',
         zone_input: null
+      })
+
+      req.reply({
+        statusCode: 202,
+        body: {}
+      })
+    }).as('allowRules')
+    cy.get(selectors.wafTuning.allowRulesButton).click()
+    cy.wait('@allowRules', { timeout: 30000 })
+  })
+
+  it('should allow to create correct rule with ruleId 11 without path', () => {
+    cy.intercept('GET', '/api/v3/waf/*/domains?page_size=200', {
+      fixture: '/waf-rules/tuning/available-domains.json'
+    }).as('wafDomains')
+    cy.get(selectors.wafTuning.tabs('tuning')).click()
+    cy.wait('@wafDomains', { timeout: 30000 })
+    cy.intercept('GET', '/api/v3/waf/*/waf_events*', {
+      fixture: '/waf-rules/tuning/tuning-list.json'
+    }).as('wafTuning')
+    cy.get(selectors.wafTuning.domainsField).click()
+    cy.get(selectors.wafTuning.domainItem).first().click()
+    cy.wait('@wafTuning', { timeout: 30000 })
+    cy.get(selectors.wafTuning.closeDomainsSelector).click()
+    cy.intercept('GET', '/api/v3/waf/*/waf_events/11*', {
+      fixture: '/waf-rules/tuning/details.json'
+    }).as('wafTuningDetails')
+    cy.get(selectors.wafTuning.dataTableRowCheckbox).click()
+    cy.get(selectors.wafTuning.submitButton).click()
+    cy.get('textarea').type('teste')
+    cy.intercept('POST', '/api/v4/edge_firewall/wafs/6648/exceptions', (req) => {
+      expect(req.body).to.have.property('match_zones')
+      expect(req.body.match_zones).to.be.an('array')
+      expect(req.body.match_zones).to.deep.include({
+        zone: 'request_body',
+        matches_on: 'value'
       })
 
       req.reply({
