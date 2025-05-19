a7ab341484c145111defcc1a7fa4a8edd8eb2bf0
Aloisio Bertolo Bastian <aloisiomiguelb@gmail.com>
2025-05-19 17:01:21 -0300

    feat: add edge pulse events tab to real time events

---
+++ src/services/real-time-events-service/edge-pulse/list-edge-pulse-events.js
@@ -0,0 +1,99 @@
+import { convertGQL } from '@/helpers/convert-gql'
+import { AxiosHttpClientSignalDecorator } from '@/services/axios/AxiosHttpClientSignalDecorator'
+import { makeRealTimeEventsBaseUrl } from '../make-real-time-events-service'
+import { generateCurrentTimestamp } from '@/helpers/generate-timestamp'
+import { useGraphQLStore } from '@/stores/graphql-query'
+import { buildSummary } from '@/helpers'
+import * as Errors from '@/services/axios/errors'
+import { getCurrentTimezone } from '@/helpers'
+
+export const listEdgePulse = async (filter) => {
+  const payload = adapt(filter)
+
+  const graphqlStore = useGraphQLStore()
+  graphqlStore.setQuery(payload)
+
+  const decorator = new AxiosHttpClientSignalDecorator()
+
+  const response = await decorator.request({
+    baseURL: '/',
+    url: makeRealTimeEventsBaseUrl(),
+    method: 'POST',
+    body: payload
+  })
+
+  return parseHttpResponse(response)
+}
+
+const adapt = (filter) => {
+  const table = {
+    dataset: 'edgePulseEvents',
+    limit: 10000,
+    fields: [
+      'ts',
+      'clientId',
+      'scriptid',
+      'platform',
+      'referrer',
+      'locationhref',
+      'useragent',
+      'type',
+      'browser',
+      'rtt',
+      'downlink',
+      'effectivetype',
+      'typebackforward',
+      'typereserved',
+      'typenavigate',
+      'contentdownload',
+      'ssl',
+      'networkduration',
+      'tcp',
+      'rendertime',
+      'dns',
+      'redirectcount',
+      'navtype',
+      'typereload',
+      'ttfb',
+      'pageloadtime'
+    ],
+    orderBy: 'ts_DESC'
+  }
+  return convertGQL(filter, table)
+}
+
+const adaptResponse = (response) => {
+  const data = response.data.edgeFunctionsEvents?.map((edgeFunctionsEvents) => ({
+    id: generateCurrentTimestamp(),
+    summary: buildSummary(edgeFunctionsEvents),
+    ts: edgeFunctionsEvents.ts,
+    tsFormat: getCurrentTimezone(edgeFunctionsEvents.ts)
+  }))
+
+  return {
+    data
+  }
+}
+
+const parseHttpResponse = (response) => {
+  const { body, statusCode } = response
+
+  switch (statusCode) {
+    case 200:
+      return adaptResponse(body)
+    case 400:
+      const apiError = body.detail
+      throw new Error(apiError).message
+    case 401:
+      throw new Errors.InvalidApiTokenError().message
+    case 403:
+      const forbiddenError = body.detail
+      throw new Error(forbiddenError).message
+    case 404:
+      throw new Errors.NotFoundError().message
+    case 500:
+      throw new Errors.InternalServerError().message
+    default:
+      throw new Errors.UnexpectedError().message
+  }
+}
