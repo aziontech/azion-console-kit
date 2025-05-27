10025f5a7cb21bc8ac30de1d19b5f74ace1f7fe5
paulosf0 <paulo.ferreira@azion.com>
2025-05-27 08:34:12 -0300

    feat: add new servicedevice group

--- src/services/v2/index.js
+++ src/services/v2/index.js
@@ -1,10 +1,16 @@
 import { createHttpService } from './base/httpServiceFactory'
 
 // Services
+// Vcs Service
 import { VcsService } from './vcs-service'
 import { VcsAdapter } from './adapters/vcs-adapter'
+// Edge Application Device Group
+import { DeviceGroupAdapter } from './adapters/edge-app-device-group-adapter'
+import { DeviceGroupService } from './dege-app-device-group-service'
 
 const httpService = createHttpService()
 const vcsService = new VcsService(httpService, VcsAdapter)
+const deviceGroupService = new DeviceGroupService(httpService, DeviceGroupAdapter)
 
-export { vcsService }
+
+export { vcsService, deviceGroupService }
