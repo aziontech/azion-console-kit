0b295581897cf570b21b82e0a30d0dc373e04061
Peterson Paulo <109550332+peterpaulo-azion@users.noreply.github.com>
2025-05-29 14:19:27 -0300

    [UXE-7340] feat: network list service integration and error handling (#2450)
    * Revert "Revert "[UXE-5459] feat: enhance edge node list view with fetch table block (#2124)""
    
    This reverts commit e3b961e662137a70697a8941cb4f6e54fee9c5a7.
    
    * feat: network list service integration and error handling
    
    * chore: format files
    
    * chore: merge conflict branch
    
    * chore: remove v3 network lists services
    
    * refactor: remove url search params
    
    * test: fix network list url
    
    * test: fix network list message
    
    * chore: format files
    
    * test: fixed network lists api url

--- src/services/v2/index.js
+++ src/services/v2/index.js
@@ -4,14 +4,20 @@ import { createHttpService } from './base/httpServiceFactory'
 
 // Vcs Service
 import { VcsService } from './vcs-service'
-import { PurgeService } from './purge-service'
-
-// Adapters
 import { VcsAdapter } from './adapters/vcs-adapter'
+
+// Purge
+import { PurgeService } from './purge-service'
 import { PurgeAdapter } from './adapters/purge-adapter'
+
+// Network Lists
+import { NetworkListsService } from './network-lists-service'
+import { NetworkListsAdapter } from './adapters/network-lists-adapter'
+
 // Edge Application Device Group
 import { DeviceGroupAdapter } from './adapters/edge-app-device-group-adapter'
 import { DeviceGroupService } from './edge-app-device-group-service'
+
 // Edge Application Cache Settings
 import { CacheSettingsAdapter } from './adapters/edge-app-cache-settings-adapter'
 import { CacheSettingsService } from './edge-app-cache-settings-service'
@@ -19,8 +25,9 @@ import { CacheSettingsService } from './edge-app-cache-settings-service'
 const httpService = createHttpService()
 
 const vcsService = new VcsService(httpService, VcsAdapter)
+const networkListsService = new NetworkListsService(httpService, NetworkListsAdapter)
 const deviceGroupService = new DeviceGroupService(httpService, DeviceGroupAdapter)
 const cacheSettingsService = new CacheSettingsService(httpService, CacheSettingsAdapter)
 const purgeService = new PurgeService(httpService, PurgeAdapter)
 
-export { vcsService, cacheSettingsService, deviceGroupService, purgeService }
+export { vcsService, cacheSettingsService, deviceGroupService, purgeService, networkListsService }
