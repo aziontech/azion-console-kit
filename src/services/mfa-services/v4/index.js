f06872656e76515decd5c3246da4d9034321a88a
paulosf0 <paulo.ferreira@azion.com>
2025-06-06 11:14:43 -0300

    feat: remove files from mfa

--- src/services/mfa-services/v4/index.js
+++
@@ -1,4 +0,0 @@
-import { listMfaUsersService } from './list-mfa-users-service'
-import { deleteMfaService } from './delete-mfa-service'
-
-export { listMfaUsersService, deleteMfaService }
--- src/services/mfa-services/v4/make-mfa-management-base-url.js
+++
@@ -1,3 +0,0 @@
-export const makeMfaBaseUrl = () => {
-  return 'v4/auth/mfa/totp'
-}
