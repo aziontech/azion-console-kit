5837dbf093c11f4c81e9d0e11cdebcef63ed1e4c
HerbertJulio <herbert.julio@azion.com>
2025-06-16 10:22:56 -0300

    refactor: streamline ListView and FormFieldsCustomPages components, extract status code options to a new config file

--- src/views/CustomPages/FormFields/FormFieldsCustomPages.vue
+++ src/views/CustomPages/FormFields/FormFieldsCustomPages.vue
@@ -9,107 +9,15 @@
   import DrawerEdgeConnector from '@/views/EdgeConnectors/Drawer'
   import { useField, useFieldArray } from 'vee-validate'
   import { onMounted, ref } from 'vue'
-  import { edgeConnectorsService } from '@/services/v2'
-
+  // import { edgeConnectorsService } from '@/services/v2'
   import FieldSwitchBlock from '@/templates/form-fields-inputs/fieldSwitchBlock'
-
-  const getEdgeConnectors = async (query) => {
-    return await edgeConnectorsService.listEdgeConnectorsService({
-      fields: 'id,name',
-      ...query
-    })
-  }
-
-  const STATUS_CODE_OPTIONS = [
-    {
-      code: '400',
-      name: '400: Bad Request'
-    },
-    {
-      code: '401',
-      name: '401: Unauthorized'
-    },
-    {
-      code: '403',
-      name: '403: Forbidden'
-    },
-    {
-      code: '404',
-      name: '404: Not Found'
-    },
-    {
-      code: '405',
-      name: '405: Method Not Allowed'
-    },
-    {
-      code: '406',
-      name: '406: Not Acceptable'
-    },
-    {
-      code: '408',
-      name: '408: Request Timeout'
-    },
-    {
-      code: '409',
-      name: '409: Conflict'
-    },
-    {
-      code: '410',
-      name: '410: Gone'
-    },
-    {
-      code: '411',
-      name: '411: Length Required'
-    },
-    {
-      code: '414',
-      name: '414: URI Too Long'
-    },
-    {
-      code: '415',
-      name: '415: Unsupported Media Type'
-    },
-    {
-      code: '416',
-      name: '416: Range Not Satisfiable'
-    },
-    {
-      code: '426',
-      name: '426: Upgrade Required'
-    },
-    {
-      code: '429',
-      name: '429: Too Many Requests'
-    },
-    {
-      code: '431',
-      name: '431: Request Header Fields Too Large'
-    },
-    {
-      code: '500',
-      name: '500: Internal Server Error'
-    },
-    {
-      code: '501',
-      name: '501: Not Implemented'
-    },
-    {
-      code: '502',
-      name: '502: Bad Gateway'
-    },
-    {
-      code: '503',
-      name: '503: Service Unavailable'
-    },
-    {
-      code: '504',
-      name: '504: Gateway Timeout'
-    },
-    {
-      code: '505',
-      name: '505: HTTP Version Not Supported'
-    }
-  ]
+  import { STATUS_CODE_OPTIONS } from '../config/statusCode'
+  // const getEdgeConnectors = async (query) => {
+  //   return await edgeConnectorsService.listEdgeConnectorsService({
+  //     fields: 'id,name',
+  //     ...query
+  //   })
+  // }
 
   const { value: name } = useField('name')
   const { value: edgeConnectorId } = useField('edgeConnectorId')
@@ -123,6 +31,7 @@
     uri: '',
     customStatusCode: 0
   }
+
   const drawerEdgeConnectorRef = ref()
 
   const addNewPage = () => {
---
+++ src/views/CustomPages/config/statusCode.js
@@ -0,0 +1,90 @@
+export const STATUS_CODE_OPTIONS = [
+  {
+    code: '400',
+    name: '400: Bad Request'
+  },
+  {
+    code: '401',
+    name: '401: Unauthorized'
+  },
+  {
+    code: '403',
+    name: '403: Forbidden'
+  },
+  {
+    code: '404',
+    name: '404: Not Found'
+  },
+  {
+    code: '405',
+    name: '405: Method Not Allowed'
+  },
+  {
+    code: '406',
+    name: '406: Not Acceptable'
+  },
+  {
+    code: '408',
+    name: '408: Request Timeout'
+  },
+  {
+    code: '409',
+    name: '409: Conflict'
+  },
+  {
+    code: '410',
+    name: '410: Gone'
+  },
+  {
+    code: '411',
+    name: '411: Length Required'
+  },
+  {
+    code: '414',
+    name: '414: URI Too Long'
+  },
+  {
+    code: '415',
+    name: '415: Unsupported Media Type'
+  },
+  {
+    code: '416',
+    name: '416: Range Not Satisfiable'
+  },
+  {
+    code: '426',
+    name: '426: Upgrade Required'
+  },
+  {
+    code: '429',
+    name: '429: Too Many Requests'
+  },
+  {
+    code: '431',
+    name: '431: Request Header Fields Too Large'
+  },
+  {
+    code: '500',
+    name: '500: Internal Server Error'
+  },
+  {
+    code: '501',
+    name: '501: Not Implemented'
+  },
+  {
+    code: '502',
+    name: '502: Bad Gateway'
+  },
+  {
+    code: '503',
+    name: '503: Service Unavailable'
+  },
+  {
+    code: '504',
+    name: '504: Gateway Timeout'
+  },
+  {
+    code: '505',
+    name: '505: HTTP Version Not Supported'
+  }
+]
