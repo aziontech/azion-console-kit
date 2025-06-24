b8481225df429e14ad5d89dbe89af3ea05c7d069
HerbertJulio <herbert.julio@azion.com>
2025-06-24 16:37:30 -0300

    refactor: update import paths for CustomPageBlock and StatusCodeBlock for consistency

--- src/views/CustomPages/FormFields/CustomPages.vue
+++ src/views/CustomPages/FormFields/CustomPages.vue
@@ -17,7 +17,7 @@
 
 <script setup>
   import FormHorizontal from '@/templates/create-form-block/form-horizontal.vue'
-  import blockCustomPageBlock from '@/views/CustomPages/Blocks/CustomPageBlock'
-  import blockStatusCodeBlock from '@/views/CustomPages/Blocks/StatusCodeBlock'
+  import blockCustomPageBlock from '@/views/CustomPages/Blocks/customPageBlock'
+  import blockStatusCodeBlock from '@/views/CustomPages/Blocks/statusCodeBlock'
   import FieldSwitchBlock from '@/templates/form-fields-inputs/fieldSwitchBlock'
 </script>
