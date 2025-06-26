93f74d2990a393bad48f30b7ed8ecd964d6ae680
peterpaulo-azion <peterson.silva@azion.com>
2025-06-26 17:07:49 -0300

    feat: update digital certificates test selectors

--- cypress/support/selectors/product-selectors/digital-certificates.js
+++ cypress/support/selectors/product-selectors/digital-certificates.js
@@ -1,12 +1,14 @@
 export default {
-  createDigitalCertificateButton: '[data-testid="create_Digital Certificate_button"] > .p-button-icon',
+  createDigitalCertificateButton:
+    '[data-testid="create_Digital Certificate_button"] > .p-button-icon',
   digitalCertificateName: '[data-testid="digital-certificate__name-field__input"]',
   breadcrumbReturnToList: '[data-testid="page-heading-block__breadcrumb__Digital Certificates"]',
   importTrustedCARadioOption:
     '[data-testid="digital-certificate-create-form__certificate-type__radio__certificateType-radio-2"]',
   editPageTitle: '[data-testid="page_title_Edit Digital Certificate"]',
   trustedCATextArea: '[data-testid="trusted-certificates-form__certificate-field__textarea"]',
-  serverCertificateTextArea: '[data-testid="import-server-certificate-form__certificate-field__textarea"]',
+  serverCertificateTextArea:
+    '[data-testid="import-server-certificate-form__certificate-field__textarea"]',
   privateKeyTextArea: '[data-testid="import-server-certificate-form__private-key-field__textarea"]',
   generateCSRRadioOption:
     '[data-testid="digital-certificate-create-form__certificate-type__radio__certificateType-radio-1"]',
@@ -23,10 +25,10 @@ export default {
   copyCsrButton: '[data-testid="digital-certificate__copy-csr__button"]',
   copyCsrMessage: '[data-testid="digital-certificate__copy-csr__message"]',
 
-  clickImportServerCertificate: '#overlay_menu_0_0 > .p-menuitem-content > .p-menuitem-link',
+  clickImportServerCertificate: '#overlay_menu_0 > .p-menuitem-content > .p-menuitem-link',
   clickGenerateCSR: '#overlay_menu_0_1 > .p-menuitem-content > .p-menuitem-link',
-  clickImportTrustedCertificate: '#overlay_menu_1_0 > .p-menuitem-content > .p-menuitem-link',
-  clickImportCRL: '#overlay_menu_1_1 > .p-menuitem-content > .p-menuitem-link',
+  clickImportTrustedCertificate: '#overlay_menu_1 > .p-menuitem-content > .p-menuitem-link',
+  clickImportCRL: '#overlay_menu_2 > .p-menuitem-content > .p-menuitem-link',
   clickDropdown: '#pv_id_61 > .p-dropdown-label',
-  clickDropdownOption: '#pv_id_61_0',
+  clickDropdownOption: '#pv_id_61_0'
 }
