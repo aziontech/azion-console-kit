b56b61e90658c4523b9108760c457ff5891b46d8
Lucas Mendes <92529166+lucasmendes21@users.noreply.github.com>
2025-07-10 15:28:39 -0300

    [UXE-7521] feat: edge connectors UI Review and API improvements (#2498)

--- src/views/DigitalCertificates/Drawer/index.vue
+++ src/views/DigitalCertificates/Drawer/index.vue
@@ -1,7 +1,7 @@
 <script setup>
   import CreateDrawerBlock from '@templates/create-drawer-block'
   import { refDebounced } from '@vueuse/core'
-  import { ref, inject, defineExpose } from 'vue'
+  import { ref, inject, defineExpose, watch } from 'vue'
   import FormFieldsCreateDigitalCertificates from '../FormFields/FormFieldsCreateDigitalCertificates.vue'
   import { handleTrackerError } from '@/utils/errorHandlingTracker'
   import { validationSchema } from '../FormFields/composables/validation'
@@ -10,14 +10,25 @@
   defineOptions({
     name: 'digital-certificates-drawer'
   })
+
+  const props = defineProps({
+    certificate: {
+      type: String,
+      default: 'edge_certificate'
+    }
+  })
+
   const emit = defineEmits(['onSuccess', 'onEdgeApplicationCreated'])
+
   /**@type {import('@/plugins/adapters/AnalyticsTrackerAdapter').AnalyticsTrackerAdapter} */
   const tracker = inject('tracker')
   const showCreateDigitalCertificateDrawer = ref(false)
   const DEBOUNCE_TIME_IN_MS = 300
   const showCreateDrawer = refDebounced(showCreateDigitalCertificateDrawer, DEBOUNCE_TIME_IN_MS)
 
-  const { createService, PRIVATE_KEY_TYPES, CERTIFICATE_TYPES } = useDigitalCertificate()
+  const { createService, PRIVATE_KEY_TYPES, certificateType } = useDigitalCertificate(
+    props.certificate
+  )
 
   const initialValues = ref({
     digitalCertificateName: '',
@@ -32,7 +43,7 @@
     email: '',
     privateKeyType: PRIVATE_KEY_TYPES.RSA_2048,
     subjectAlternativeNames: '',
-    certificateType: CERTIFICATE_TYPES.EDGE_CERTIFICATE
+    certificateType: props.certificate
   })
 
   const handleTrackSuccessCreated = () => {
@@ -72,6 +83,15 @@
     }
     response.showToastWithActions(toast)
   }
+
+  watch(
+    () => props.certificate,
+    (newVal) => {
+      initialValues.value.certificateType = newVal
+      certificateType.value = newVal
+    },
+    { immediate: true }
+  )
 
   defineExpose({
     showCreateDrawer,
