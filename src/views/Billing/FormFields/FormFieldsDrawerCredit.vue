c52765c6b08d99eb922e15d050b77a16b692d54e
lucasmendes21 <lucas.mendes@azion.com>
2025-07-21 15:26:50 -0300

    fix: remove address block

--- src/views/Billing/FormFields/FormFieldsDrawerCredit.vue
+++ src/views/Billing/FormFields/FormFieldsDrawerCredit.vue
@@ -8,8 +8,6 @@
   import { ref } from 'vue'
   import cardFlagBlock from '@templates/card-flag-block'
   import { useRoute, useRouter } from 'vue-router'
-  import FieldText from '@/templates/form-fields-inputs/fieldText.vue'
-  import FieldDropdown from '@/templates/form-fields-inputs/fieldDropdown.vue'
 
   const { value: amount, errorMessage } = useField('amount')
 
@@ -132,105 +130,6 @@
           payment method.
         </span>
       </InlineMessage>
-    </template>
-  </FormHorizontal>
-
-  <FormHorizontal
-    :isDrawer="true"
-    title="Address Information"
-    description="Insert address information for the account."
-  >
-    <template #inputs>
-      jfhbhfjbfhjbfjhfbjbfjhbfjhb
-      <div class="flex flex-col gap-6 md:gap-8 md:flex-row">
-        <div class="flex flex-col w-full gap-2 sm:max-w-lg">
-          <FieldText
-            data-testid="account-settings__postal-code"
-            label="Postal Code"
-            required
-            name="postalCode"
-            placeholder="00.000.000-00"
-            :value="postalCode"
-            description="Postal code of the account owner."
-          />
-        </div>
-        <div class="flex flex-col w-full gap-2 sm:max-w-lg">
-          <FieldDropdown
-            label="Country"
-            required
-            name="country"
-            :options="countriesOptions.options"
-            :loading="!countriesOptions.done"
-            @change="resetRegionAndCity"
-            optionValue="geonameId"
-            optionLabel="name"
-            :value="country"
-            filter
-            appendTo="self"
-            description="Account owner's country."
-            data-testid="account-settings__country"
-          />
-        </div>
-      </div>
-      <div class="flex flex-col gap-6 md:gap-8 md:flex-row">
-        <div class="flex flex-col w-full gap-2 sm:max-w-lg">
-          <FieldDropdown
-            label="State/Region"
-            required
-            name="region"
-            :options="regionsOptions.options"
-            :loading="!regionsOptions.done"
-            optionValue="geonameId"
-            optionLabel="name"
-            :value="region"
-            filter
-            appendTo="self"
-            :disabled="hasNoCountryListOrNotSelected"
-            description="Account owner's state or region."
-            data-testid="account-settings__region"
-          />
-        </div>
-        <div class="flex flex-col w-full gap-2 sm:max-w-lg">
-          <FieldDropdown
-            label="City"
-            required
-            name="city"
-            :options="citiesOptions.options"
-            :loading="!citiesOptions.done"
-            optionValue="geonameId"
-            optionLabel="name"
-            :value="city"
-            filter
-            appendTo="self"
-            :disabled="hasNoRegionListOrNotSelected"
-            description="Account owner's city."
-            data-testid="account-settings__city"
-          />
-        </div>
-      </div>
-      <div class="flex flex-col gap-6 md:gap-8 md:flex-row">
-        <div class="flex flex-col w-full gap-2 sm:max-w-lg">
-          <FieldText
-            label="Address"
-            required
-            name="address"
-            placeholder="123 Example Ave."
-            :value="address"
-            description="Account owner's street address."
-            data-testid="account-settings__address"
-          />
-        </div>
-        <div class="flex flex-col w-full gap-2 sm:max-w-lg">
-          <FieldText
-            label="Apartment, floor, etc."
-            name="complement"
-            placeholder="1st floor"
-            :value="complement"
-            description="Additional information for the address."
-            data-testid="account-settings__complement"
-          />
-        </div>
-      </div>
     </template>
   </FormHorizontal>
 </template>
