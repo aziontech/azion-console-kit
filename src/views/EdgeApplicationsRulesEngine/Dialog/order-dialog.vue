fbdd4791e3354da9d8c17be9cf2393b337675d87
lucasmendes21 <lucas.mendes@azion.com>
2025-07-22 10:29:37 -0300

    fix: remove discard changes modal and add in action bar

--- src/views/EdgeApplicationsRulesEngine/Dialog/order-dialog.vue
+++ src/views/EdgeApplicationsRulesEngine/Dialog/order-dialog.vue
@@ -40,28 +40,20 @@
     </template>
 
     <template #footer>
-      <div class="flex flex-row justify-between w-full flex-wrap md:flex-nowrap gap-3">
+      <div class="flex flex-row w-full justify-end md:flex-nowrap gap-3">
         <PrimeButton
           outlined
-          label="Discard Changes"
-          @click="resetRules"
+          label="Cancel"
+          @click="cancelDialog()"
           data-testid="review-changes-dialog-footer-cancel-button"
         />
-        <div class="flex gap-3 flex-wrap w-full md:w-auto">
-          <PrimeButton
-            outlined
-            label="Cancel"
-            @click="cancelDialog()"
-            data-testid="review-changes-dialog-footer-cancel-button"
-          />
-          <PrimeButton
-            severity="secondary"
-            label="Save"
-            icon-pos="right"
-            @click="saveRules()"
-            data-testid="review-changes-dialog-footer-delete-button"
-          />
-        </div>
+        <PrimeButton
+          severity="secondary"
+          label="Save"
+          icon-pos="right"
+          @click="saveRules()"
+          data-testid="review-changes-dialog-footer-delete-button"
+        />
       </div>
     </template>
   </PrimeDialog>
@@ -80,10 +72,6 @@
 
   const saveRules = () => {
     dialogRef.value.close({ save: true })
-  }
-
-  const resetRules = () => {
-    dialogRef.value.close({ reset: true })
   }
 
   const cancelDialog = () => {
