4d12d1feaf1e9a58a238436bab2bd6fc5fb171f3
paulosf0 <paulo.ferreira@azion.com>
2025-07-24 15:16:27 -0300

    fix: fix let encrypt on domain page

--- src/services/v2/adapters/digital-certificates-adapter.js
+++ src/services/v2/adapters/digital-certificates-adapter.js
@@ -1,5 +1,6 @@
 import { getCurrentTimezone, checkIfFieldExist, getCurrentDateTimeIntl } from '@/helpers'
 import { parseStatusData } from '@/services/v2/utils/adapter/parse-status-utils'
+import { hasFlagBlockApiV4 } from '@/composables/user-flag'
 
 const EDGE_CERTIFICATE = 'TLS Certificate'
 const TRUSTED_CA_CERTIFICATE = 'Trusted CA Certificate'
@@ -90,7 +91,12 @@ export const DigitalCertificatesAdapter = {
     if (type === 'edge_certificate') {
       const DEFAULT_CERTIFICATES = [
         { id: 0, name: 'Azion (SAN)', status: 'active', group: 'Certificates presets' },
-        { id: 1, name: "Let's Encrypt", status: 'active', group: 'Certificates presets' }
+        {
+          id: !hasFlagBlockApiV4() ? 1 : 'lets_encrypt',
+          name: "Let's Encrypt",
+          status: 'active',
+          group: 'Certificates presets'
+        }
       ]
       const searchLowercase = search?.toLowerCase()
       const matchesSearch = (cert) => cert.name.toLowerCase().includes(searchLowercase)
