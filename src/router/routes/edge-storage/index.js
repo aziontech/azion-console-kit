645a1e36c01db7c6b34e791acb9bd369230f7f33
Guilherme Santana <guilherme30santana@gmail.com>
2025-07-31 10:22:24 -0300

    refactor: remove EdgeStorageService from routes

--- src/router/routes/edge-storage/index.js
+++ src/router/routes/edge-storage/index.js
@@ -1,5 +1,3 @@
-import * as EdgeStorageServices from '@/services/v2/edge-storage-service'
-
 /** @type {import('vue-router').RouteRecordRaw} */
 export const edgeStorageRoutes = {
   path: '/edge-storage',
@@ -9,9 +7,6 @@ export const edgeStorageRoutes = {
       path: '',
       name: 'edge-storage-list',
       component: () => import('@views/EdgeStorage/ListView.vue'),
-      props: {
-        listEdgeStorageBucketsService: EdgeStorageServices.listEdgeStorageBuckets
-      },
       meta: {
         breadCrumbs: [
           {
@@ -25,9 +20,6 @@ export const edgeStorageRoutes = {
       path: 'create',
       name: 'edge-storage-create',
       component: () => import('@views/EdgeStorage/CreateView.vue'),
-      props: {
-        createEdgeStorageBucketService: EdgeStorageServices.createEdgeStorageBucket
-      },
       meta: {
         breadCrumbs: [
           {
