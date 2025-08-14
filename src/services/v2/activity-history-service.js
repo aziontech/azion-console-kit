08d72f8a6fff6567d890680de2d3e5c5bcc44558
Paulo Sobrinho Ferreira <110847590+pauloSF0@users.noreply.github.com>
2025-08-14 15:53:39 -0300

    [UXE-7100] feat: Improvements in Activity History search (pagination + search on backend) (#2650)
    * Revert "Revert "[UXE-7100] feat: adds pagination support to activity history (#2434)" (#2629)"
    
    This reverts commit 9340c361037c4c24744bb0dcf8826201414606fc.
    
    * feat: add search behavior
    
    * feat: search history
    
    * refactor: add ilike instead like
    
    * feat: change activity history to v2 services
    
    * refactor: add a method to get offset values
    
    * refactor: add variables
    
    * refactor: query format
    
    * refactor: code refactor

---
+++ src/services/v2/activity-history-service.js
@@ -0,0 +1,118 @@
+export class ActivityHistoryService {
+  constructor(http, adapter, apiClient) {
+    this.http = http
+    this.adapter = adapter
+    this.baseURL = 'v4/events/graphql'
+    this.apiClient = apiClient
+  }
+
+  #getOffsetDate = () => {
+    const offSetEnd = new Date()
+    const offSetStart = new Date(
+      Date.UTC(offSetEnd.getFullYear(), offSetEnd.getMonth(), offSetEnd.getDate() - 30)
+    )
+    return { offSetEnd, offSetStart }
+  }
+
+  listActivityHistoryEvents = async ({ offset = 0, limit = 1000, search = '' }) => {
+    const { offSetEnd, offSetStart } = this.#getOffsetDate()
+    const baseQuery = `
+      query ActivityHistory(
+        $offset: Int, 
+        $limit: Int, 
+        $begin: DateTime!, 
+        $end: DateTime!
+        ${search ? ', $search: String' : ''}
+      ) {
+        activityHistoryEvents(
+          offset: $offset, 
+          limit: $limit, 
+          filter: { 
+            tsRange: { begin: $begin, end: $end }
+            ${search ? 'or: [{ titleIlike: $search }, { commentIlike: $search }],' : ''}
+          }, 
+          orderBy: [ts_DESC]
+        ) { 
+          ts
+          title
+          comment
+          type
+          authorName
+          authorEmail
+          accountId
+        }
+      }`
+
+    const payload = {
+      operationName: 'ActivityHistory',
+      query: baseQuery,
+      variables: {
+        offset,
+        limit,
+        begin: offSetStart.toISOString(),
+        end: offSetEnd.toISOString(),
+        ...(search ? { search: `%${search}%` } : {})
+      }
+    }
+
+    let httpResponse = await this.http.request(
+      {
+        url: `${this.baseURL}`,
+        method: 'POST',
+        body: payload
+      },
+      this.apiClient
+    )
+
+    const parsedEvents = this.adapter.transformListActivityHistoryEvents(httpResponse.data)
+
+    return {
+      body: parsedEvents,
+      statusCode: httpResponse.statusCode
+    }
+  }
+
+  getTotalRecords = async ({ search = '' }) => {
+    const { offSetEnd, offSetStart } = this.#getOffsetDate()
+
+    const query = `
+      query ActivityHistory(
+        $begin: DateTime!, 
+        $end: DateTime!
+        ${search ? ', $search: String' : ''}
+      ) {
+        activityHistoryEvents(
+          aggregate: { count: rows },
+          filter: { 
+            tsRange: { begin: $begin, end: $end }
+            ${search ? 'or: [{ titleIlike: $search }, { commentIlike: $search }],' : ''}
+          }
+        ) { 
+          count 
+        }
+      }`
+
+    const payload = {
+      operationName: 'ActivityHistory',
+      query,
+      variables: {
+        begin: offSetStart.toISOString(),
+        end: offSetEnd.toISOString(),
+        ...(search ? { search: `%${search}%` } : {})
+      }
+    }
+
+    let { data: httpResponse } = await this.http.request(
+      {
+        url: `${this.baseURL}`,
+        method: 'POST',
+        body: payload
+      },
+      this.apiClient
+    )
+
+    const totalRecords = httpResponse.data.activityHistoryEvents[0].count
+
+    return totalRecords
+  }
+}
