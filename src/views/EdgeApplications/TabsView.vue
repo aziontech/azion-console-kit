df43061c95eaa92a5499109e0ebc101bf0bd5e98
Guilherme Santana <44853071+gdsantana@users.noreply.github.com>
2025-09-02 14:15:03 -0300

    [UXE-7498] feat: conditionally show Origins and Error Responses tabs (#2710)

--- src/views/EdgeApplications/TabsView.vue
+++ src/views/EdgeApplications/TabsView.vue
@@ -240,7 +240,7 @@
     {
       header: 'Origins',
       component: EdgeApplicationsOriginsListView,
-      condition: true,
+      condition: hasFlagBlockApiV4(),
       show: showTabs.origins,
       props: () => ({
         ...props.originsServices,
@@ -262,7 +262,7 @@
     {
       header: 'Error Responses',
       component: EdgeApplicationsErrorResponseEditView,
-      condition: true,
+      condition: hasFlagBlockApiV4(),
       show: showTabs.errorResponses,
       props: () => ({
         edgeApplicationId: edgeApplicationId.value,
