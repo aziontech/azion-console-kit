d33a184a49f780efd483de7c53a95efb130c8f62
HerbertJulio <hvcotta@gmail.com>
2025-09-09 09:14:06 -0300

    refactor: implement caching for social identity providers in IAMService to optimize API calls

--- src/services/v2/iam-service.js
+++ src/services/v2/iam-service.js
@@ -3,15 +3,22 @@ export class IAMService {
     this.http = http
     this.adapter = adapter
     this.baseURL = 'v4/iam/identity_providers/social'
+    this._socialIdpsCache = null
   }
 
   listSocialIdps = async () => {
+    if (this._socialIdpsCache) {
+      return this._socialIdpsCache
+    }
+
     const { data } = await this.http.request({
       method: 'GET',
       url: this.baseURL,
       body: {}
     })
 
-    return this.adapter?.transformListSocialIdps?.(data)
+    this._socialIdpsCache = this.adapter?.transformListSocialIdps?.(data)
+
+    return this._socialIdpsCache
   }
 }
