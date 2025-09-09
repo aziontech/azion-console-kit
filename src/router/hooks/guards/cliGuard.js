8f9a60dd1320bab96e38c670243528d7831e0616
HerbertJulio <herbert.julio@azion.com>
2025-09-09 10:26:09 -0300

    fix: update CLI configuration setup to accept callback port parameter

--- src/router/hooks/guards/cliGuard.js
+++ src/router/hooks/guards/cliGuard.js
@@ -3,7 +3,7 @@ import { setupCLIConfig } from '@/helpers/redirect-cli'
 /** @type {import('vue-router').NavigationGuardWithThis} */
 export async function cliGuard({ to }) {
   if (to.name === 'login' && to.query?.next === 'cli') {
-    setupCLIConfig()
+    setupCLIConfig(2, to.query?.callback_port)
     return { name: 'login' }
   }
 }
