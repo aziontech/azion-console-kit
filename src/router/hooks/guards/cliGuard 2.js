68b00a24e310a9a6dcf04629097a7ac31b3a82e6
lucasmendes21 <lucas.mendes@azion.com>
2025-09-11 16:03:09 -0300

    fix: remove file

--- src/router/hooks/guards/cliGuard 2.js
+++
@@ -1,9 +0,0 @@
-import { setupCLIConfig } from '@/helpers/redirect-cli'
-
-/** @type {import('vue-router').NavigationGuardWithThis} */
-export async function cliGuard({ to }) {
-  if (to.name === 'login' && to.query?.next === 'cli') {
-    setupCLIConfig(2, to.query?.callback_port)
-    return { name: 'login' }
-  }
-}
