0ad7af983689fb11be406d5a16d6966b6b80eb82
HerbertJulio <herbert.julio@azion.com>
2025-09-12 10:29:40 -0300

    fix: adjusted cliGuard to route guards for enhanced navigation control

--- src/router/hooks/beforeEachRoute.js
+++ src/router/hooks/beforeEachRoute.js
@@ -4,20 +4,24 @@ import {
   themeGuard,
   billingGuard,
   redirectGuard,
-  flagGuard
+  flagGuard,
+  cliGuard
 } from '@/router/hooks/guards'
 import { useRouter } from 'vue-router'
-import { useLoadingStore } from '@/stores/loading'
 
 export default async function beforeEachRoute(guardDependency) {
   const router = useRouter()
   const { next } = guardDependency
-  const loadingStore = useLoadingStore()
 
-  // Inicia o loading quando a navegação começa
-  loadingStore.startLoading()
-
-  const guards = [logoutGuard, themeGuard, accountGuard, billingGuard, redirectGuard, flagGuard]
+  const guards = [
+    logoutGuard,
+    themeGuard,
+    accountGuard,
+    cliGuard,
+    billingGuard,
+    redirectGuard,
+    flagGuard
+  ]
 
   for (const executeGuard of guards) {
     const result = await executeGuard({ ...guardDependency, router })
--- src/router/hooks/guards/index.js
+++ src/router/hooks/guards/index.js
@@ -4,5 +4,6 @@ import { themeGuard } from './themeGuard'
 import { billingGuard } from './billingGuard'
 import { redirectGuard } from './redirectGuard'
 import { flagGuard } from './flagGuard'
+import { cliGuard } from './cliGuard'
 
-export { logoutGuard, accountGuard, themeGuard, billingGuard, redirectGuard, flagGuard }
+export { logoutGuard, accountGuard, themeGuard, billingGuard, redirectGuard, flagGuard, cliGuard }
