4fb8fa1398cb3ef4525c792c386faf4ace666558
Guilherme Santana <guilherme30santana@gmail.com>
2025-09-12 08:57:58 -0300

    refactor: css refactor to use pt property

--- src/templates/list-table-block/folder-list.vue
+++ src/templates/list-table-block/folder-list.vue
@@ -566,7 +566,7 @@
 
   const stateClassRules = (row) => {
     if (selectedItems.value.find((item) => item.id === row.id)) {
-      return 'bg-altered'
+      return 'bg-[var(--table-body-row-hover-bg)] bg-altered'
     }
     return ''
   }
@@ -603,29 +603,3 @@
     emit('on-load-data', !!hasData)
   })
 </script>
-
-<style lang="scss">
-  .p-datatable {
-    .p-datatable-tbody {
-      > tr {
-        &.p-datatable-dragpoint-top > td {
-          box-shadow: inset 0 2px 0 0 var(--text-color);
-        }
-
-        &.p-datatable-dragpoint-bottom > td {
-          box-shadow: inset 0 -2px 0 0 var(--text-color);
-        }
-
-        &.bg-altered,
-        &.p-frozen.bg-altered {
-          @media (prefers-color-scheme: dark) {
-            background-color: var(--surface-500);
-          }
-          @media (prefers-color-scheme: light) {
-            background-color: var(--surface-50);
-          }
-        }
-      }
-    }
-  }
-</style>
