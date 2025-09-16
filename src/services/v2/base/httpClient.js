ce31c7c0622740f69b151f7b9f3b7ad002418898
HerbertJulio <hvcotta@gmail.com>
2025-09-16 02:37:51 -0300

    refactor: remove token handling from HttpClient constructor and simplify header setup

--- src/services/v2/base/httpClient.js
+++ src/services/v2/base/httpClient.js
@@ -1,26 +1,15 @@
 export class HttpClient {
-  constructor(api, token = null) {
+  constructor(api) {
     this.api = api
-    this.token = token
-  }
-
-  setToken(token) {
-    this.token = token
   }
 
   async send(config) {
     const headers = config.headers || {}
     const baseURL = config.baseURL ?? '/'
 
-    // Set default headers
     headers['Accept'] = config.accept ?? 'application/json; version=3'
     headers['Content-Type'] = config.contentType ?? 'application/json; version=3'
     headers['withCredentials'] = config.withCredentials ?? true
-
-    // Add authentication if token is available
-    if (this.token) {
-      headers['Authorization'] = `Bearer ${this.token}`
-    }
 
     const requestConfig = {
       ...config,
