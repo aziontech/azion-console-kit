b9cdafb3e9e889cfa0c21d367e1b1b84ffdb2706
robsongajunior <robson.ga.junior@gmail.com>
2025-09-18 16:35:12 -0300

    chore: copy treatment to show tooltip in the click

--- src/layout/components/navbar/share-url.vue
+++ src/layout/components/navbar/share-url.vue
@@ -3,7 +3,6 @@
     type="button"
     icon="pi pi-share-alt"
     size="small"
-    label=""
     @click="toggle"
     outlined
   />
@@ -26,8 +25,10 @@
         </div>
         <div>
           <Button
+            ref="copyButton"
+            v-tooltip="tooltipConfig"
             outlined
-            icon="pi pi-copy"
+            icon="pi pi-copy" 
             size="small"
             label="Copy link"
             @click="copyToClipboard"
@@ -39,19 +40,69 @@
 </template>
 
 <script setup>
-  import { ref, computed } from 'vue'
+  import { ref, computed, nextTick } from 'vue'
   import Button from 'primevue/button'
   import InputText from 'primevue/inputtext'
   import OverlayPanel from 'primevue/overlaypanel'
 
   const overlayPanel = ref()
+  const copyButton = ref()
+  const tooltipMessage = ref('')
+  const showTooltip = ref(false)
   const url = computed(() => window.location.href)
+
+  const tooltipConfig = computed(() => {
+    if (!showTooltip.value) {
+      return { disabled: true }
+    }
+    return {
+      value: tooltipMessage.value,
+      showDelay: 0,
+      hideDelay: 2000,
+      disabled: false
+    }
+  })
 
   const toggle = (event) => {
     overlayPanel.value.toggle(event)
   }
 
-  const copyToClipboard = () => {
-    navigator.clipboard.writeText(url.value)
+  const copyToClipboard = async () => {
+    try {
+      await navigator.clipboard.writeText(url.value)
+      tooltipMessage.value = 'Copied'
+      showTooltip.value = true
+      
+      await nextTick()
+      
+      const buttonEl = copyButton.value?.$el || copyButton.value
+      if (buttonEl) {
+        const mouseEnterEvent = new MouseEvent('mouseenter', { bubbles: true })
+        buttonEl.dispatchEvent(mouseEnterEvent)
+        
+        setTimeout(() => {
+          const mouseLeaveEvent = new MouseEvent('mouseleave', { bubbles: true })
+          buttonEl.dispatchEvent(mouseLeaveEvent)
+          showTooltip.value = false
+        }, 2000)
+      }
+    } catch (err) {
+      tooltipMessage.value = 'Failed to copy'
+      showTooltip.value = true
+      
+      await nextTick()
+      
+      const buttonEl = copyButton.value?.$el || copyButton.value
+      if (buttonEl) {
+        const mouseEnterEvent = new MouseEvent('mouseenter', { bubbles: true })
+        buttonEl.dispatchEvent(mouseEnterEvent)
+        
+        setTimeout(() => {
+          const mouseLeaveEvent = new MouseEvent('mouseleave', { bubbles: true })
+          buttonEl.dispatchEvent(mouseLeaveEvent)
+          showTooltip.value = false
+        }, 2000)
+      }
+    }
   }
 </script>
