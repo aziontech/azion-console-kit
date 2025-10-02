38bd60d890fc9c089946a9546c15e0055e991b7e
Eduardo de Cesaro <44036260+cesaroeduardo@users.noreply.github.com>
2025-10-02 15:56:49 -0300

    fix: dialogs buttons size

--- src/views/PersonalTokens/Dialog/CopyTokenDialog.vue
+++ src/views/PersonalTokens/Dialog/CopyTokenDialog.vue
@@ -8,7 +8,7 @@
     header="Personal token has been created"
     data-testid="copy-token-dialog__header"
   >
-    <div class="flex flex-col gap-3.5">
+    <div class="flex flex-col gap-4">
       <InlineMessage
         severity="warn"
         data-testid="copy-token-dialog__warning__inline-message"
@@ -54,6 +54,7 @@
           icon="pi pi-clone"
           outlined
           type="button"
+          size="small"
           aria-label="Copy Personal Token"
           label="Copy"
           :disabled="!personalTokenValue"
@@ -66,6 +67,7 @@
     <template #footer>
       <PrimeButton
         label="Confirm"
+        size="small"
         severity="secondary"
         @click="closeDialog"
         data-testid="copy-token-dialog__dialog-footer__confirm-button"
