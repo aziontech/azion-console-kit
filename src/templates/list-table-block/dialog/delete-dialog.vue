ae499c1ce1061076a4087e82f8e9d37d934880bc
Eduardo de Cesaro <44036260+cesaroeduardo@users.noreply.github.com>
2025-10-03 15:41:48 -0300

    chore: delete dialog buttons

--- src/templates/list-table-block/dialog/delete-dialog.vue
+++ src/templates/list-table-block/dialog/delete-dialog.vue
@@ -77,6 +77,7 @@
       <PrimeButton
         outlined
         label="Cancel"
+        size="small"
         @click="cancelDialog()"
         data-testid="delete-dialog-footer-cancel-button"
       ></PrimeButton>
@@ -84,6 +85,7 @@
         severity="danger"
         label="Delete"
         icon-pos="right"
+        size="small"
         @click="removeItem()"
         :icon="getLoadingIcon"
         :disabled="isDisabled"
