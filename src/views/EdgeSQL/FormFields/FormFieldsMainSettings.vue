23e4d1a0dc283d807259a10520fb1d51fccfc1ec
paulosf0 <paulo.ferreira@azion.com>
2025-10-17 09:14:54 -0300

    refactor: code refactor

--- src/views/EdgeSQL/FormFields/FormFieldsMainSettings.vue
+++ src/views/EdgeSQL/FormFields/FormFieldsMainSettings.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col gap-6">
+  <div class="flex flex-col gap-6 mt-4">
     <FormHorizontal
       title="General"
       description="Set up your new SQL database with basic configuration."
