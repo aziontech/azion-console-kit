24a90e59d62930f3434818b7dcf6585195e3f6ac
paulosf0 <paulo.ferreira@azion.com>
2025-10-20 10:47:45 -0300

    feat: add query history

--- src/views/EdgeSQL/CodeEditor.vue
+++ src/views/EdgeSQL/CodeEditor.vue
@@ -1,7 +1,68 @@
 <template>
-  <div class="flex mt-4">
-    <div class="flex flex-col w-80">
-      <span class="text-sm">List tables</span>
+  <div class="flex flex-col sm:flex-row mt-4 gap-8 w-full">
+    <div class="flex flex-col !w-64">
+      <div class="flex flex-col w-full gap-4">
+        <div class="flex justify-between items-center">
+          <h3 class="text-lg font-medium text-color-primary">Query History</h3>
+          <Button
+            icon="pi pi-plus"
+            size="small"
+            outlined
+            @click="emit('create-query')"
+            data-testid="create-table-button"
+            class="w-8 h-8 p-0 flex items-center justify-center"
+          />
+        </div>
+
+        <div class="p-input-icon-left">
+          <i class="pi pi-search" />
+          <InputText
+            v-model="searchTerm"
+            placeholder="Search queries"
+            class="w-full"
+          />
+        </div>
+
+        <div class="flex-1 overflow-y-auto max-h-[calc(100svh-40%)]">
+          <div
+            v-if="isLoading"
+            class="flex flex-col gap-3"
+          >
+            <div
+              v-for="index in 3"
+              :key="index"
+              class="py-2 rounded"
+            >
+              <div class="flex items-center justify-between">
+                <Skeleton class="h-8 w-full" />
+              </div>
+            </div>
+          </div>
+          <div
+            v-else-if="!filteredHistory.length"
+            class="text-left py-2"
+          >
+            <div class="text-color-secondary text-sm">
+              {{ searchTerm ? 'No queries found.' : 'No queries created yet.' }}
+            </div>
+          </div>
+          <div v-else>
+            <div
+              v-for="query in filteredHistory"
+              :key="query.id"
+              class="group p-2 rounded cursor-pointer hover:bg-[--table-bg-color] transition-colors"
+              :class="{ 'bg-[--table-bg-color]': selectedQueryId === query.id }"
+              @click="selectQuery(query)"
+            >
+              <div class="flex items-center justify-between">
+                <span class="text-sm font-medium text-color-primary truncate">{{
+                  query.label
+                }}</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
     <div class="w-full">
       <Splitter
@@ -17,31 +78,47 @@
           :pt="{ root: { 'data-testid': 'code-editor__panel__editor' } }"
         >
           <div class="flex flex-col h-full">
-            <div class="flex justify-between border-1 border surface-border h-12 rounded-t-md p-2">
+            <div class="flex justify-between border-1 border surface-border rounded-t-md p-3">
               <Button
                 label="Run Query"
                 icon="pi pi-play"
+                size="small"
                 severity="primary"
+                @click="emit('run-query', sqlQueryCommand.value)"
+                v-tooltip="{
+                  value: 'Run Query (⌘ ↵)',
+                  pt: {
+                    arrow: {
+                      class: 'bg-primary text-white'
+                    }
+                  }
+                }"
               />
 
               <div class="flex gap-2">
                 <Button
                   label="Table Reference"
                   icon="pi pi-table"
+                  size="small"
                   severity="secondary"
                   outlined
+                  @click="emit('open-table-reference')"
                 />
                 <Button
                   label="Prettify"
                   icon="pi pi-align-left"
+                  size="small"
                   outlined
                   severity="secondary"
+                  @click="emit('prettify')"
                 />
                 <Button
                   label="Templates"
                   icon="pi pi-bolt"
+                  size="small"
                   severity="secondary"
                   outlined
+                  @click="emit('open-templates')"
                 />
               </div>
             </div>
@@ -70,14 +147,26 @@
 </template>
 
 <script setup>
+  import { ref, computed } from 'vue'
   import Splitter from 'primevue/splitter'
   import SplitterPanel from 'primevue/splitterpanel'
   import Button from 'primevue/button'
-  import { ref } from 'vue'
+  import InputText from 'primevue/inputtext'
+  import { useEdgeSQL } from './composable/useEdgeSQL'
 
-  defineOptions({ name: 'code-editor' })
+  defineOptions({ name: 'CodeEditor' })
+  const emit = defineEmits([
+    'create-query',
+    'run-query',
+    'open-table-reference',
+    'prettify',
+    'open-templates'
+  ])
 
   const sqlQueryCommand = ref('')
+  const searchTerm = ref('')
+
+  const { queryResults, isLoading } = useEdgeSQL()
 
   const monacoTheme = 'vs-dark'
 
@@ -86,6 +175,23 @@
   const isExecutingQuery = ref(false)
 
   const editorPanelSize = ref(70) // percentage height of editor panel
+
+  const filteredHistory = computed(() => {
+    const term = searchTerm.value.trim().toLowerCase()
+    if (!term) return queryResults.value
+    return queryResults.value.filter((queryItem) => {
+      const label = queryItem.label?.toString().toLowerCase() || ''
+      const original = queryItem.originalQuery?.toString().toLowerCase() || ''
+      return label.includes(term) || original.includes(term)
+    })
+  })
+
+  const selectedQueryId = ref(null)
+
+  const selectQuery = (query) => {
+    selectedQueryId.value = query.id
+    sqlQueryCommand.value = query.originalQuery
+  }
 
   const onResizeEnd = (event) => {
     // event.sizes is an array of panel sizes in percentages
