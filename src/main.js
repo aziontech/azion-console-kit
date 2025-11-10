b4bc64b8ef27e6ea77f66e94334a880a2498eda0
HerbertJulio <hvcotta@gmail.com>
2025-11-10 03:56:52 -0300

    docs: convert all comments to English in generated files
    - Convert all Portuguese comments to English in prefetchController.js
    - Update main.js comment to English
    - Fix linter errors by removing console.warn statements

--- src/main.js
+++ src/main.js
@@ -22,7 +22,7 @@ import TrackerPlugin from '@/plugins/AnalyticsTrackerAdapterPlugin'
 import SentryPlugin from '@/plugins/sentry'
 import { queryPlugin } from '@/services/v2/base/query/queryPlugin'
 import { initOAuthSecurity } from '@/helpers/oauth-security'
-import '@/services/v2/prefetch' // Inicializa o sistema de prefetch (registra módulos)
+import '@/services/v2/prefetch' // Initializes prefetch system (registers modules)
 
 import App from './App.vue'
 import router from './router'
