dbd893a0659db3a7b4f11a2b0fbb02b1f7a3aac9
ROBSON.JUNIOR <robsongajunior@users.noreply.github.com>
2025-12-11 16:37:13 -0300

    chore: Apply suggestion from @HerbertJulio - disabledList
    Co-authored-by: Herbert Vicente Cotta Julio <hvcotta@gmail.com>

--- src/views/RealTimePurge/ListView.vue
+++ src/views/RealTimePurge/ListView.vue
@@ -17,7 +17,7 @@
         v-if="hasContentToList"
         :listService="props.listRealTimePurgeService"
         :columns="getColumns"
-        :disabledList="true"
+        disabledList
         addButtonLabel="Purge"
         createPagePath="real-time-purge/create"
         @on-load-data="handleLoadData"
