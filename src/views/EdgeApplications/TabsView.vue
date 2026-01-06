67c8234048fae3940187ff403af9309de943aef9
paulosf0 <paulo.ferreira@azion.com>
2026-01-06 10:13:06 -0300

    fix: return to list after editing Function Instance

--- src/views/EdgeApplications/TabsView.vue
+++ src/views/EdgeApplications/TabsView.vue
@@ -185,7 +185,7 @@
   }
   const changeRouteByTab = (tab) => {
     const params = { id: edgeApplicationId.value, tab }
-    router.push({ name: 'edit-application', params, query: route.query })
+    router.push({ name: 'edit-application', params, query: {} })
   }
   const changeTab = (index) => {
     verifyTab(edgeApplication.value)
