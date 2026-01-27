c02315182a233c22a4b5bd6f3318cbbeb3f2304a
Robson Junior <robson.ga.junior@gmail.com>
2026-01-27 12:20:39 -0300

    chore: code sort

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,18 +1,3 @@
-/**
- * Pattern:
- * [ENG-*] -> Scope is optional
- * type -> Is required and needs to be one of [build, chore, ci, docs, feat, fix, perf, refactor, revert, style, test]
- * subject -> Is required
- *
- * See some valid examples:
- * [ENG-123] fix: typos in file Something.vue
- * [ENG-321] styles: new theme.css file with nice changes
- * feat: add a awesome create service
- * chore: some changes in router file
- *
- *
- */
-
 /* eslint-disable no-undef */
 /** @type {import('@commitlint/types').UserConfig} */
 module.exports = {
@@ -28,4 +13,17 @@ module.exports = {
   }
 }
 
-
+/**
+ * Pattern:
+ * [ENG-*] -> Scope is optional
+ * type -> Is required and needs to be one of [build, chore, ci, docs, feat, fix, perf, refactor, revert, style, test]
+ * subject -> Is required
+ *
+ * See some valid examples:
+ * [ENG-123] fix: typos in file Something.vue
+ * [ENG-321] styles: new theme.css file with nice changes
+ * feat: add a awesome create service
+ * chore: some changes in router file
+ *
+ *
+ */
