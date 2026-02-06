96cb05a8446af2624e189a52ac503dab9d48e677
Isaque Böck <isaquebockdos@gmail.com>
2026-02-06 17:20:26 -0300

    chore(home): reorder cards and hide changelog pending review

--- src/views/Home/HomeView.vue
+++ src/views/Home/HomeView.vue
@@ -11,7 +11,6 @@
   import MonthlyUsageCard from '@/templates/home-cards-block/monthly-usage-card.vue'
   import MarketplaceTrendsCard from '@/templates/home-cards-block/marketplace-trends-card.vue'
   import CommunicationsCard from '@/templates/home-cards-block/communications-card.vue'
-  import AzionChangelogCard from '@/templates/home-cards-block/azion-changelog-card.vue'
   import ResourcesBlock from '@/templates/home-cards-block/resources-block.vue'
   import LastActivitiesBlock from '@/templates/home-cards-block/last-activities-block.vue'
   import MetricsBlock from '@/templates/home-cards-block/metrics-block.vue'
@@ -107,10 +106,12 @@
           class="flex flex-col w-full gap-8"
           :class="homeStyle.secondColumn"
         >
-          <CommunicationsCard />
           <MonthlyUsageCard @viewAll="navigateToUsage" />
           <MarketplaceTrendsCard />
-          <AzionChangelogCard />
+          <CommunicationsCard />
+          <!-- 
+          Ocultado até ser revisado o processo de changelog
+          <AzionChangelogCard /> -->
         </div>
       </section>
     </template>
