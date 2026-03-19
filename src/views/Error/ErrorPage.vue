88a7d671e03982a4dde1ec94e305acafbf1c040a
robsongajunior <robson.ga.junior@gmail.com>
2026-03-19 15:03:12 -0300

    fix: refactoring error page to reusable ErrorPage.vue

---
+++ src/views/Error/ErrorPage.vue
@@ -0,0 +1,62 @@
+<template>
+  <ErrorPageBlock
+    :title="props.title"
+    :description="props.description"
+  >
+    <template #illustration>
+      <IllustrationComponent />
+    </template>
+
+    <template #actions>
+      <PrimeButton
+        :label="props.buttons.backToHome"
+        @click="handleGoBackHome"
+      />
+      <PrimeButton
+        outlined
+        :label="props.buttons.documentation"
+        @click="props.documentationService"
+      />
+    </template>
+  </ErrorPageBlock>
+</template>
+
+<script setup>
+  import { useRouter } from 'vue-router'
+  import Illustration403 from '@aziontech/webkit/svg/error-403'
+  import Illustration404 from '@aziontech/webkit/svg/error-404'
+  import PrimeButton from 'primevue/button'
+  import ErrorPageBlock from '@/templates/error-page-block'
+
+  const props = defineProps({
+    documentationService: {
+      type: Function,
+      required: true
+    },
+    statusCode: {
+      type: Number,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    }
+  })
+
+  const illustration = {
+    403: Illustration403,
+    404: Illustration404
+  }
+
+  const IllustrationComponent = illustration[props.statusCode]
+
+  const router = useRouter()
+
+  function handleGoBackHome() {
+    router.push('/')
+  }
+</script>
