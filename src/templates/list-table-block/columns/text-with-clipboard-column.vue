3667a919a0fdc1ffa3bbddc84d4fd52041f33894
HerbertJulio <herbert.julio@azion.com>
2026-03-23 21:12:10 -0300

    refactor: migrate copy-block to @aziontech/webkit
    - Replace local copy-block component with @aziontech/webkit/copy-block
    - Update imports in 12 files across views and table columns
    - Remove src/templates/copy-block/ directory

---
+++ src/templates/list-table-block/columns/text-with-clipboard-column.vue
@@ -0,0 +1,37 @@
+<template>
+  <div
+    class="gap-2 flex items-center w-full justify-between pr-1"
+    @mouseenter="isHovered = true"
+    @mouseleave="isHovered = false"
+  >
+    <expand-text-column :value="content"></expand-text-column>
+    <CopyBlock
+      :value="content"
+      :isCopyVisible="isHovered"
+      v-tooltip.top="{ value: 'Copy to clipboard', showDelay: 200 }"
+    />
+  </div>
+</template>
+
+<script>
+  import ExpandTextColumn from './expand-text-column.vue'
+  import CopyBlock from '@aziontech/webkit/copy-block'
+  export default {
+    name: 'text-with-clipboard-column',
+    components: {
+      ExpandTextColumn,
+      CopyBlock
+    },
+    data() {
+      return {
+        isHovered: false
+      }
+    },
+    props: {
+      content: {
+        type: String,
+        required: true
+      }
+    }
+  }
+</script>
