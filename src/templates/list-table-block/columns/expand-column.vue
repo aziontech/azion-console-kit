8cfe278b9c43b117d20677e76e5d300bfe55e8a5
HerbertJulio <herbert.julio@azion.com>
2026-03-23 21:12:10 -0300

    refactor: migrate copy-block to @aziontech/webkit
    - Replace local copy-block component with @aziontech/webkit/copy-block
    - Update imports in 12 files across views and table columns
    - Remove src/templates/copy-block/ directory

---
+++ src/templates/list-table-block/columns/expand-column.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="flex gap-2 align-center">
+    <ul class="flex flex-col gap-1">
+      <li
+        v-for="(item, index) in splitValue"
+        :key="index"
+      >
+        {{ item }}
+      </li>
+      <li
+        v-if="displayShowMore"
+        @click.stop="toggleShowAll"
+        class="underline cursor-pointer"
+        data-testid="table-column-expand-column__show-more__toggle"
+      >
+        {{ displayRemainingItems }}
+      </li>
+    </ul>
+    <copyBlock
+      v-if="shouldShowCopy"
+      :value="value"
+      data-testid="table-column-expand-column__copy"
+      v-tooltip.top="{ value: 'Copy to clipboard', showDelay: 200 }"
+    />
+  </div>
+</template>
+
+<script setup>
+  import copyBlock from '@aziontech/webkit/copy-block'
+  defineOptions({ name: 'expand-column' })
+  import { ref, computed } from 'vue'
+
+  const props = defineProps({
+    value: {
+      type: Array,
+      required: true
+    },
+    showCopy: {
+      type: Boolean
+    }
+  })
+
+  const showAllItems = ref(false)
+
+  const SLICE_VALUE = 2
+  const formatValue = props.value?.slice(0, SLICE_VALUE) ?? []
+  const totalItems = props.value?.length - SLICE_VALUE
+  const displayShowMore = props.value?.length > SLICE_VALUE
+
+  const splitValue = computed(() => {
+    return showAllItems.value ? props.value : formatValue
+  })
+
+  const shouldShowCopy = computed(() => {
+    return props.showCopy && props.value?.length
+  })
+
+  const displayRemainingItems = computed(() => {
+    return showAllItems.value ? 'Show less' : `Show more (${totalItems})`
+  })
+
+  const toggleShowAll = () => {
+    showAllItems.value = !showAllItems.value
+  }
+</script>
