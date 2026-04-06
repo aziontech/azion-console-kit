88aae92da594d5bcbc42fbdeb53015409e48d0b4
Robson Junior <robson.ga.junior@gmail.com>
2026-04-05 21:16:30 -0300

    feat: enabling storybook router mock

--- storybook/.storybook/preview.js
+++ storybook/.storybook/preview.js
@@ -3,6 +3,7 @@ import PrimeVue from 'primevue/config';
 import Tooltip from 'primevue/tooltip';
 import { withThemeByClassName } from '@storybook/addon-themes'
 import { injectCssVars } from '@aziontech/theme/tokens'
+import { createRouter, createWebHistory } from 'vue-router'
 
 import 'primeflex/primeflex.css'
 import '../src/styles/preview.css';
@@ -13,11 +14,30 @@ import '@aziontech/webkit/styles/country-flags'
 // Inject semantic token CSS variables for light/dark mode
 injectCssVars()
 
+// Create a mock router for Storybook
+const mockRouter = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/step-1', component: { template: '<div>Step 1</div>' } },
+    { path: '/step-2', component: { template: '<div>Step 2</div>' } },
+    { path: '/step-3', component: { template: '<div>Step 3</div>' } },
+    { path: '/personal-info', component: { template: '<div>Personal Info</div>' } },
+    { path: '/account-setup', component: { template: '<div>Account Setup</div>' } },
+    { path: '/preferences', component: { template: '<div>Preferences</div>' } },
+    { path: '/review', component: { template: '<div>Review</div>' } },
+    { path: '/complete', component: { template: '<div>Complete</div>' } },
+    { path: '/start', component: { template: '<div>Start</div>' } },
+    { path: '/finish', component: { template: '<div>Finish</div>' } },
+    { path: '/:pathMatch(.*)*', component: { template: '<div>Not Found</div>' } }
+  ]
+})
+
 setup((app) => {
   app.use(PrimeVue, {
     ripple: false
-  });
+  })
 
+  app.use(mockRouter);
   app.directive('tooltip', Tooltip);
 });
 
