a5e735989d3b32cfa4b8b951d4871994aabed800
Marcus Grando <eu@marcusgrando.com>
2026-06-01 19:13:15 -0300

    fix: restore signup checkout external scroll

--- src/templates/signup-block/choosing-plan-container.vue
+++ src/templates/signup-block/choosing-plan-container.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="w-full max-w-5xl mx-auto bg-surface rounded border surface-border lg:h-[800px]">
-    <div class="flex lg:h-full lg:min-h-0 lg:overflow-hidden flex-col-reverse lg:flex-row">
+  <div class="w-full max-w-5xl mx-auto bg-surface rounded border surface-border">
+    <div class="flex flex-col-reverse lg:flex-row">
       <CheckoutPlanBlock
         ref="planBlockRef"
         :plan="plan"
