377f911b19455871d481010376aab23eab768743
Robson Júnior <robson.ga.junior@gmail.com>
2026-06-17 10:44:17 -0300

    Revert "[ENG-37160] feat: new plans view with onboarding, billing flows and S…" (#3525)
    This reverts commit 960ea51f01ab89e5dfc3fb2c09b75f4640b3db28.

--- src/services/v2/account/account-settings-service.js
+++ src/services/v2/account/account-settings-service.js
@@ -1,22 +1,47 @@
 import { BaseService } from '@/services/v2/base/query/baseService'
 import { queryKeys } from '@/services/v2/base/query/queryKeys'
-import { AccountSettingsAdapter } from './account-settings-adapter'
 
 export class AccountSettingsService extends BaseService {
   baseUrl = 'v4/iam/account'
 
-  async fetchAccountSettingsInfo() {
+  async fetchAccountJobRole() {
     const response = await this.http.request({
       method: 'GET',
       url: this.baseUrl,
       config: { baseURL: '/api' }
     })
-    return AccountSettingsAdapter.transformAccountSettings(response.data)
+    return this._adaptJobRole(response.data)
   }
 
-  async getAccountSettingsInfo() {
-    const queryKey = queryKeys.accountSettings.info()
-    return await this.useEnsureQueryData(queryKey, async () => this.fetchAccountSettingsInfo())
+  async getAccountJobRole() {
+    const queryKey = queryKeys.accountSettings.jobRole()
+    return await this.useEnsureQueryData(queryKey, async () => this.fetchAccountJobRole())
+  }
+
+  _adaptJobRole(response) {
+    const payload = response?.data
+    if (!payload) return { jobRole: 'other' }
+
+    return { jobRole: this._replaceLegacyJobRoles(payload.job_function) }
+  }
+
+  _replaceLegacyJobRoles(currentAccountJobRoleName) {
+    const defaultJobRole = 'other'
+    const validJobRoles = [
+      'software-developer',
+      'devops-engineer',
+      'infrastructure-analyst',
+      'network-engineer',
+      'security-specialist',
+      'data-engineer',
+      'ai-ml-engineer',
+      'iot-engineer',
+      'team-lead',
+      defaultJobRole
+    ]
+
+    const isJobRoleValid = validJobRoles.some((jobName) => jobName === currentAccountJobRoleName)
+    return isJobRoleValid ? currentAccountJobRoleName : defaultJobRole
   }
 }
 
