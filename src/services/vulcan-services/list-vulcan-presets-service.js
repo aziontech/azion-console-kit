bd91686e853c1f672ea0f51789fbcfe624271ca9
paulosf0 <paulo.ferreira@azion.com>
2026-06-18 17:15:26 -0300

    refactor: use default api client for vulcan presets service

--- src/services/vulcan-services/list-vulcan-presets-service.js
+++ src/services/vulcan-services/list-vulcan-presets-service.js
@@ -1,13 +1,9 @@
 import { AxiosHttpClientAdapter, parseHttpResponse } from '../axios/AxiosHttpClientAdapter'
-import vulcanPresetsApi from './make-vulcan-presets-base-url'
 
 export const listVulcanPresetsService = async () => {
-  let httpResponse = await AxiosHttpClientAdapter.request(
-    {
-      url: '/utils/presets',
-      method: 'GET'
-    },
-    vulcanPresetsApi
-  )
+  let httpResponse = await AxiosHttpClientAdapter.request({
+    url: 'v4/utils/presets',
+    method: 'GET'
+  })
   return parseHttpResponse(httpResponse)
 }
